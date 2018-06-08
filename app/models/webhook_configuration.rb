9c96fe29c13f280d74ee4b7d34776f7ad0425c34
Thomas Cullen <thomascullen92@gmail.com>
2018-06-08 16:40:19 +0100

    Add unit tests for webhook_configuration

--- app/models/webhook_configuration.rb
+++ app/models/webhook_configuration.rb
@@ -6,36 +6,26 @@ class WebhookConfiguration < ApplicationRecord
   serialize :criteria, Array
 
   OPERATORS = [
-    :equals,
-    :dose_not_equal
+    "changes_to"
   ]
 
+  # This is a hack to prevent rails path generators from loading for single
+  # table inheritance. i.e allways use webhook_configuration_path
   def self.model_name
     return super if self == WebhookConfiguration
     WebhookConfiguration.model_name
   end
 
+  # process a given entity and emit a webhook if it mactches the criteria.
   def process(entity)
     matched_criteria = criteria.select do |c|
-      send(c["operator"], entity, c["attribute"], c["value"])
+      config.symbolize_keys
+      send(c[:operator], entity, c[:attribute], c[:value])
     end
     return unless matched_criteria.length == criteria.length
     webhook = Webhook.create(url: url, data: data(entity))
     WebhookJob.perform_later(webhook.id)
     webhook
-  end
-
-  def changes_to(entity, attribute, value)
-    return false unless entity.send("saved_change_to_#{attribute}?")
-    entity[attribute] =~ Regexp.new(value)
-  end
-
-  def equals(entity, attribute, value)
-    entity[attribute] =~ Regexp.new(value)
-  end
-
-  def does_not_equal(entity, attribute, value)
-    !(entity[attribute] =~ Regexp.new(value))
   end
 
   def data
@@ -44,14 +34,23 @@ class WebhookConfiguration < ApplicationRecord
 
   private
 
+  # operator method to check if an entities attribute has changed to a given
+  # value.
+  def changes_to(entity, attribute, value)
+    return false unless entity.send("saved_change_to_#{attribute}?")
+    entity[attribute] =~ Regexp.new(value)
+  end
+
+  private
+
   def valid_criteria
-    errors.add(:criteria, "must be an array") unless criteria.is_a? Array
-    errors.add(:criteria, "must have at least one criteria block") if criteria.length === 0
+    errors.add(:criteria, "must have at least one criteria") if criteria.length === 0
     criteria.each do |config|
-      break errors.add(:criteria, "block must have an attribute") unless config["attribute"]
-      break errors.add(:criteria, "block must have an operator") unless config["operator"]
-      break errors.add(:criteria, "#{config["operator"]} is not a supported operator") unless config["operator"]
-      break errors.add(:criteria, "block must have a value") unless config["value"]
+      config.symbolize_keys
+      break errors.add(:criteria, "block must have an attribute") unless config[:attribute]
+      break errors.add(:criteria, "block must have an operator") unless config[:operator]
+      break errors.add(:criteria, "#{config[:operator]} is not a supported operator") unless OPERATORS.include?(config[:operator])
+      break errors.add(:criteria, "block must have a value") unless config[:value]
     end
   end
 end
