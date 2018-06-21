a39b111a248cc8c33b609cdab2c29f1d23ea99e0
Thomas Cullen <thomascullen92@gmail.com>
2018-06-21 22:37:12 +0100

    Fix side scrolling issue on mobile
    fixes #8

--- app/javascript/src/components/ApplicationContainer/styles.js
+++ app/javascript/src/components/ApplicationContainer/styles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 100vw;
   display: flex;
+  overflow-x: hidden;
 
   @media screen and (max-width: 700px) {
     display: block;
