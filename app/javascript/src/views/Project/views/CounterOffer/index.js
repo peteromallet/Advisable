046c289fb11162a24c657e382db472123d45b3c5
Thomas Cullen <thomascullen92@gmail.com>
2018-07-01 20:33:09 +0100

    Add ability to make counter offer

--- app/javascript/src/views/Project/views/CounterOffer/index.js
+++ app/javascript/src/views/Project/views/CounterOffer/index.js
@@ -9,7 +9,7 @@ import Divider from "src/components/Divider";
 import Spacing from "src/components/Spacing";
 import Heading from "src/components/Heading";
 import OfferForm from "../../components/OfferForm";
-import { currencySymbol } from 'src/utilities/currency';
+import currency, { currencySymbol } from "src/utilities/currency";
 import LoadingCandidates from "../../components/LoadingCandidates";
 import FETCH_DATA from "./graphql/fetchData.graphql";
 import CREATE_OFFER from "../../graphql/createOffer.graphql";
@@ -28,17 +28,17 @@ const Offer = ({ match, history, loading, data }) => {
       </Spacing>
       <Spacing bottom="xs">
         <Heading size="l">
-          Offer for {data.project.application.specialist.name}
+          Counter-offer for {data.booking.application.specialist.name}
         </Heading>
       </Spacing>
       <Spacing bottom="xl">
-        <Text size="l">{data.project.name}</Text>
+        <Text size="l">{data.booking.application.project.name}</Text>
       </Spacing>
       <Mutation mutation={CREATE_OFFER}>
         {createOffer => (
           <OfferForm
             onCancel={goBack}
-            currency={currencySymbol(data.project.currency)}
+            currency={currencySymbol(data.booking.application.project.currency)}
             onSubmit={values => {
               createOffer({
                 variables: {
@@ -48,15 +48,18 @@ const Offer = ({ match, history, loading, data }) => {
                     rateLimit: values.rateLimit
                       ? Number(values.rateLimit.replace(/[^0-9\.-]+/g, ""))
                       : null,
-                    applicationId: data.project.application.id
+                    applicationId: data.booking.application.id
                   }
                 }
               }).then(goBack);
             }}
             initialValues={{
-              type: "Fixed",
-              rateType: "Fixed",
-              deliverables: [""]
+              type: data.booking.type,
+              rate: data.booking.rate,
+              rateType: data.booking.rateType,
+              rateLimit: data.booking.rateLimit,
+              duration: data.booking.duration,
+              deliverables: data.booking.deliverables
             }}
           />
         )}
@@ -68,8 +71,7 @@ const Offer = ({ match, history, loading, data }) => {
 export default graphql(FETCH_DATA, {
   options: ({ match }) => ({
     variables: {
-      projectID: match.params.projectID,
-      applicationID: match.params.applicationID
+      bookingID: match.params.bookingID
     }
   })
 })(Offer);
