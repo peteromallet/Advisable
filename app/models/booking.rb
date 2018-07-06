5c97ad6a30ecd3c31398c5f095164e9403893574
Thomas Cullen <thomascullen92@gmail.com>
2018-07-06 14:17:35 +0100

    Make booking statuses the same as airtable statuses

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   validates :airtable_id, presence: true
-  validates :status, inclusion: { in: %w(offered accepted declined complete) }
+  validates :status, inclusion: { in: %w(Offered Accepted Declined Complete) }
 
   # disable STI
   self.inheritance_column = :_type_disabled
@@ -9,11 +9,6 @@ class Booking < ApplicationRecord
   after_initialize :set_status, if: :new_record?
 
   serialize :deliverables, Array
-
-  # When setting the booking status. Convert it to lowercase underscore
-  def status=(status)
-    self[:status] = status.parameterize.underscore
-  end
 
   private
 
