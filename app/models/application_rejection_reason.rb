52c1e19ace2ef9446f31fa93ca51730310e20047
Thomas Cullen <thomascullen92@gmail.com>
2018-07-12 11:16:39 +0100

    Fix more failing specs

--- app/models/application_rejection_reason.rb
+++ app/models/application_rejection_reason.rb
@@ -1,5 +1,5 @@
 class ApplicationRejectionReason < ApplicationRecord
-  has_many :applications
+  has_many :applications, foreign_key: 'rejection_reason_id'
   validates :reason, presence: true
   validates :airtable_id, presence: true
 end
--- app/models/booking_rejection_reason.rb
+++ app/models/booking_rejection_reason.rb
@@ -1,5 +1,5 @@
 class BookingRejectionReason < ApplicationRecord
-  has_many :bookings
+  has_many :bookings, foreign_key: 'rejection_reason_id'
   validates :name, presence: true
   validates :airtable_id, presence: true
 end
