c0a2ed36d9bc55f7b69f57985af271b4d027eca8
Thomas Cullen <thomascullen92@gmail.com>
2018-07-27 14:25:05 +0100

    Update circle config

--- spec/support/system_tests.rb
+++ spec/support/system_tests.rb
@@ -1,13 +1,5 @@
 RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
-    if ENV["SELENIUM_DRIVER_URL"].present?
-      driven_by :selenium, using: :chrome,
-                           options: {
-                               browser: :remote,
-                               url: ENV.fetch("SELENIUM_DRIVER_URL"),
-                               desired_capabilities: :chrome}
-    else
-      driven_by :selenium, using: :chrome
-    end
+    driven_by :selenium_chrome
   end
 end
