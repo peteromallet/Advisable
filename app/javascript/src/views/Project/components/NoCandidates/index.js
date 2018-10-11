aa2c11c96d472afb41e59ac42233705eaf5b5d5f
Thomas Cullen <thomascullen92@gmail.com>
2018-10-11 14:28:42 +0100

    Update empty state sub text

--- app/javascript/src/views/Project/components/NoCandidates/index.js
+++ app/javascript/src/views/Project/components/NoCandidates/index.js
@@ -4,11 +4,12 @@ import Text from "src/components/Text";
 import Spacing from "src/components/Spacing";
 import { Card } from "./styles";
 
-export default ({ text }) => (
+export default ({ text, subText }) => (
   <Card>
     <Spacing bottom="xl">
       <img src={illustration} width={300} alt='' />
     </Spacing>
-    <Text weight="semibold" colour='dark'>{text}</Text>
+    <Text marginBottom="m" weight="semibold" colour='dark'>{text}</Text>
+    {subText && <Text>{subText}</Text>}
   </Card>
 );
