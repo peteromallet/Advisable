f73e5350ca77bb791074576e09791f970f058875
Thomas Cullen <thomascullen92@gmail.com>
2018-10-30 15:04:41 +0000

    Add tests

---
+++ spec/system/request_introduction_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Request Introduction' do
+  let(:application) { create(:application, status: "Applied") }
+
+  it 'Creates an interview request' do
+    project = application.project
+
+    airtable_interview_record = double('Airtable::Interview', id: 'interview_1')
+    expect(Airtable::Interview).to receive(:new).and_return(airtable_interview_record)
+    expect(airtable_interview_record).to receive(:create)
+
+    airtable_application_reocrd = double('Airtable::Application')
+    expect(airtable_application_reocrd).to receive(:[]=).with('Application Status', 'Application Accepted').at_least(:once)
+    expect(airtable_application_reocrd).to receive(:save)
+    expect(Airtable::Application).to receive(:find).and_return(airtable_application_reocrd)
+
+    visit "/projects/#{project.airtable_id}/applied"
+    click_on 'Request Introduction'
+    page.all("div[class^=styles__TimeCell]")[10].click
+    page.all("div[class^=styles__TimeCell]")[34].click
+    click_on "Request Call"
+
+    expect(page).to have_content("An interview request has been sent")
+  end
+end
