33fac5996e8a8ca2d1fbb7f1a17cd61bd7b67f23
Thomas Cullen <thomascullen92@gmail.com>
2018-12-10 14:17:58 +0000

    Add lazy loading for password reset routes

--- app/javascript/src/Root.js
+++ app/javascript/src/Root.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { hot, setConfig } from "react-hot-loader";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
+import Loading from 'src/components/Loading';
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Project from "./views/Project";
 import Login from "./views/Login";
@@ -17,7 +18,6 @@ import ViewProposal from "./views/ViewProposal";
 import ProjectSetup from "./views/ProjectSetup";
 import Availability from "./views/Availability";
 import EditProposal from "./views/EditProposal";
-import ResetPassword from "./views/ResetPassword";
 import NotFoundError from "./views/NotFound/error";
 import CreateProposal from "./views/CreateProposal";
 import ConfirmAccount from "./views/ConfirmAccount";
@@ -25,60 +25,64 @@ import InterviewRequest from "./views/InterviewRequest";
 import NotFoundBoundary from "./views/NotFound/NotFoundBoundary";
 import InterviewAvailability from "./views/InterviewAvailability";
 
+const ResetPassword = lazy(() => import("./views/ResetPassword"));
+
 setConfig({ pureSFC: true });
 
 const Root = () => (
   <NotFoundBoundary>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/reset_password" component={ResetPassword} />
-      <AuthenticatedRoute path="/setup" component={Setup} />
-      <AuthenticatedRoute exact path="/" component={RootPath} />
-      <Route path="/confirm_account" component={ConfirmAccount} />
-      <Route path="/project_setup/:projectID" component={ProjectSetup} />
-      <Route
-        path="/projects/:projectID/applications/:applicationID/offer"
-        component={CreateOffer}
-      />
-      <Route
-        path="/projects/:projectID/applications/:applicationID"
-        component={Applicant}
-      />
-      <Route
-        path="/projects/:projectID/proposals/:bookingID"
-        component={ViewProposal}
-      />
-      <Route
-        path="/projects/:projectID/offers/:bookingID"
-        component={CounterOffer}
-      />
-      <Route
-        path="/projects/:projectID/interviews/:interviewID/availability"
-        component={InterviewAvailability}
-      />
-      <AuthenticatedRoute path="/projects/:projectID" component={Project} />
-      <AuthenticatedRoute path="/projects" component={Projects} />
-      <Route path="/offers/:bookingID" component={ViewOffer} />
-      <Route path="/clients/:userID/availability" component={Availability} />
-      <Route
-        path="/interview_request/:interviewID"
-        component={InterviewRequest}
-      />
-      <Route
-        path="/applications/:applicationID/proposal"
-        component={CreateProposal}
-      />
-      <Route
-        path="/applications/:applicationID/proposals/:proposalID"
-        component={EditProposal}
-      />
-      <Route
-        render={() => {
-          throw new NotFoundError();
-        }}
-      />
-    </Switch>
+    <Suspense fallback={Loading}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/reset_password" component={ResetPassword} />
+        <AuthenticatedRoute path="/setup" component={Setup} />
+        <AuthenticatedRoute exact path="/" component={RootPath} />
+        <Route path="/confirm_account" component={ConfirmAccount} />
+        <Route path="/project_setup/:projectID" component={ProjectSetup} />
+        <Route
+          path="/projects/:projectID/applications/:applicationID/offer"
+          component={CreateOffer}
+        />
+        <Route
+          path="/projects/:projectID/applications/:applicationID"
+          component={Applicant}
+        />
+        <Route
+          path="/projects/:projectID/proposals/:bookingID"
+          component={ViewProposal}
+        />
+        <Route
+          path="/projects/:projectID/offers/:bookingID"
+          component={CounterOffer}
+        />
+        <Route
+          path="/projects/:projectID/interviews/:interviewID/availability"
+          component={InterviewAvailability}
+        />
+        <AuthenticatedRoute path="/projects/:projectID" component={Project} />
+        <AuthenticatedRoute path="/projects" component={Projects} />
+        <Route path="/offers/:bookingID" component={ViewOffer} />
+        <Route path="/clients/:userID/availability" component={Availability} />
+        <Route
+          path="/interview_request/:interviewID"
+          component={InterviewRequest}
+        />
+        <Route
+          path="/applications/:applicationID/proposal"
+          component={CreateProposal}
+        />
+        <Route
+          path="/applications/:applicationID/proposals/:proposalID"
+          component={EditProposal}
+        />
+        <Route
+          render={() => {
+            throw new NotFoundError();
+          }}
+        />
+      </Switch>
+    </Suspense>
   </NotFoundBoundary>
 );
 
