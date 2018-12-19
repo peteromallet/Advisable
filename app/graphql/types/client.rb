46304a0a51a09d142eddb2499d4d90e4db8bccde
Thomas Cullen <thomascullen92@gmail.com>
2018-12-19 16:05:54 +0000

    Remove client#primary_user

--- app/graphql/types/client.rb
+++ app/graphql/types/client.rb
@@ -5,12 +5,7 @@ class Types::Client < Types::BaseType
   field :projects, [Types::ProjectType], null: true
   field :availability, [GraphQL::Types::ISO8601DateTime], null: false
 
-  # As we dont have client login's yet we still need to query availability on
-  # the client level. We can forward this to the primary_user. For now it works
-  # well that we are not excluding times from scheduled interviews like we do
-  # on the user type, however, eventually we will prop want to move this
-  # functionality into an argument. e.g user { availability(excludeScheduledInterviews: true) }
   def availability
-    object.primary_user.availability || []
+    []
   end
 end
