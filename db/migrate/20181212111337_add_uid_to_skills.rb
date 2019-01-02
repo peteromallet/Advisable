0b546a37179779e587f18f31bb3cc10211fd34e9
Thomas Cullen <thomascullen92@gmail.com>
2019-01-02 11:28:51 +0000

    Remove generate uid script from migrations

--- db/migrate/20181212111337_add_uid_to_skills.rb
+++ db/migrate/20181212111337_add_uid_to_skills.rb
@@ -1,11 +1,6 @@
 class AddUidToSkills < ActiveRecord::Migration[5.2]
   def up
     add_column :skills, :uid, :string, index: true
-
-    Skill.all.each do |s|
-      s.send(:generate_uid)
-      s.save
-    end
   end
 
   def down
