06e096dbb90ce7f671d4b3e13b204db9e3e30324
Thomas Cullen <thomascullen92@gmail.com>
2019-01-18 10:08:22 +0000

    Add chrome logging to tests

--- spec/support/system_tests.rb
+++ spec/support/system_tests.rb
@@ -1,6 +1,16 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
+    Capybara.register_driver :chrome_with_logs do |app|
+      Capybara::Selenium::Driver.new(app,
+        browser: :chrome,
+        driver_opts: {
+          log_path: './tmp/chrome.log',
+          verbose:  true
+        }
+      )
+    end
     # driven_by :selenium_chrome_headless
-    driven_by :selenium_chrome
+    # driven_by :selenium_chrome
+    driven_by :chrome_with_logs
   end
 end
