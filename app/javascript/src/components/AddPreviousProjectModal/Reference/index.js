1e45b25415ef67c1c2e4eb100d09e4328a276f73
Thomas Cullen <thomascullen92@gmail.com>
2019-01-30 10:41:05 +0000

    Fix modal layout for reference forms

--- app/javascript/src/components/AddPreviousProjectModal/Reference/index.js
+++ app/javascript/src/components/AddPreviousProjectModal/Reference/index.js
@@ -10,82 +10,109 @@ import StepDots from "src/components/StepDots";
 import TextField from "src/components/TextField";
 import validationSchema from "./validationSchema";
 
-const ProjectReference = ({ setValues, values, gotoNextStep, gotoPreviousStep }) => {
+const ProjectReference = ({
+  setValues,
+  values,
+  gotoNextStep,
+  gotoPreviousStep
+}) => {
   const handleSubmit = values => {
     setValues(values);
     gotoNextStep();
-  }
+  };
 
   return (
-    <Formik onSubmit={handleSubmit} initialValues={values} validationSchema={validationSchema}>
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={values}
+      validationSchema={validationSchema}
+    >
       {formik => (
-        <form onSubmit={formik.handleSubmit}>
+        <React.Fragment>
           <Modal.Header>
             <Heading size="s">Project Reference</Heading>
           </Modal.Header>
           <Modal.Body>
-            <FieldRow>
-              <TextField
-                autoFocus
-                name="contactName"
-                placeholder="e.g Jane Doe"
-                label="What was the name of your contact with this client?"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                value={formik.values.contactName}
-                error={formik.touched.contactName && formik.errors.contactName}
-              />
-            </FieldRow>
-            <FieldRow>
-              <React.Fragment>
+            <form onSubmit={formik.handleSubmit}>
+              <FieldRow>
                 <TextField
-                  name="contactEmail"
-                  placeholder="e.g jane@company.com"
-                  marginBottom="m"
-                  label="What’s their email address?"
+                  autoFocus
+                  name="contactName"
+                  placeholder="e.g Jane Doe"
+                  label="What was the name of your contact with this client?"
                   onBlur={formik.handleBlur}
                   onChange={formik.handleChange}
-                  value={formik.values.contactEmail}
-                  error={formik.touched.contactEmail && formik.errors.contactEmail}
-                  description="If possible, please share their corporate email address. If not, if possible, please share the email address associated with their LinkedIn account."
+                  value={formik.values.contactName}
+                  error={
+                    formik.touched.contactName && formik.errors.contactName
+                  }
                 />
-                <Checkbox
-                  name="canContactClient"
-                  label="It’s ok for Advisable to contact this client to validate the project"
+              </FieldRow>
+              <FieldRow>
+                <React.Fragment>
+                  <TextField
+                    name="contactEmail"
+                    placeholder="e.g jane@company.com"
+                    marginBottom="m"
+                    label="What’s their email address?"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.contactEmail}
+                    error={
+                      formik.touched.contactEmail && formik.errors.contactEmail
+                    }
+                    description="If possible, please share their corporate email address. If not, if possible, please share the email address associated with their LinkedIn account."
+                  />
+                  <Checkbox
+                    name="canContactClient"
+                    label="It’s ok for Advisable to contact this client to validate the project"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.canContact}
+                    error={
+                      formik.touched.canContact && formik.errors.canContact
+                    }
+                  />
+                </React.Fragment>
+              </FieldRow>
+              <FieldRow>
+                <TextField
+                  name="contactRole"
+                  placeholder="e.g They supervised the project"
+                  label="What was their role for this project?"
                   onBlur={formik.handleBlur}
                   onChange={formik.handleChange}
-                  value={formik.values.canContact}
-                  error={formik.touched.canContact && formik.errors.canContact}
+                  value={formik.values.contactRole}
+                  error={
+                    formik.touched.contactRole && formik.errors.contactRole
+                  }
                 />
-              </React.Fragment>
-            </FieldRow>
-            <FieldRow>
-              <TextField
-                name="contactRole"
-                placeholder="e.g They supervised the project"
-                label="What was their role for this project?"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                value={formik.values.contactRole}
-                error={formik.touched.contactRole && formik.errors.contactRole}
-              />
-            </FieldRow>
-            <FieldRow>
-              <TextField
-                name="contactJobTitle"
-                placeholder="e.g Head of marketing"
-                label="What was their job title?"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                value={formik.values.contactJobTitle}
-                error={formik.touched.contactJobTitle && formik.errors.contactJobTitle}
-              />
-            </FieldRow>
+              </FieldRow>
+              <FieldRow>
+                <TextField
+                  name="contactJobTitle"
+                  placeholder="e.g Head of marketing"
+                  label="What was their job title?"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.contactJobTitle}
+                  error={
+                    formik.touched.contactJobTitle &&
+                    formik.errors.contactJobTitle
+                  }
+                />
+              </FieldRow>
+            </form>
           </Modal.Body>
           <Modal.Footer>
             <Flex align="center">
               <Flex.Item style={{ width: "120px" }}>
-                <Button block onClick={gotoPreviousStep} styling="outlined" size="l">
+                <Button
+                  block
+                  onClick={gotoPreviousStep}
+                  styling="outlined"
+                  size="l"
+                >
                   Back
                 </Button>
               </Flex.Item>
@@ -93,16 +120,16 @@ const ProjectReference = ({ setValues, values, gotoNextStep, gotoPreviousStep })
                 <StepDots current={4} total={4} />
               </Flex.Item>
               <Flex.Item style={{ width: "120px" }}>
-                <Button block type="submit" size="l" styling="green">
+                <Button block onClick={formik.submitForm} size="l" styling="green">
                   Next
                 </Button>
               </Flex.Item>
             </Flex>
           </Modal.Footer>
-        </form>
+        </React.Fragment>
       )}
     </Formik>
-  )
-}
+  );
+};
 
-export default ProjectReference
+export default ProjectReference;
--- app/javascript/src/components/AddPreviousProjectModal/Results/index.js
+++ app/javascript/src/components/AddPreviousProjectModal/Results/index.js
@@ -9,39 +9,55 @@ import StepDots from "src/components/StepDots";
 import TextField from "src/components/TextField";
 import validationSchema from "./validationSchema";
 
-const ProjectDetails = ({ setValues, values, gotoNextStep, gotoPreviousStep }) => {
+const ProjectDetails = ({
+  setValues,
+  values,
+  gotoNextStep,
+  gotoPreviousStep
+}) => {
   const handleSubmit = values => {
     setValues(values);
     gotoNextStep();
-  }
+  };
 
   return (
-    <Formik onSubmit={handleSubmit} initialValues={values} validationSchema={validationSchema}>
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={values}
+      validationSchema={validationSchema}
+    >
       {formik => (
-        <form onSubmit={formik.handleSubmit}>
+        <React.Fragment>
           <Modal.Header>
             <Heading size="s">Project Details</Heading>
           </Modal.Header>
           <Modal.Body>
-            <FieldRow>
-              <TextField
-                autoFocus
-                multiline
-                minRows={10}
-                name="results"
-                placeholder="What were the results of your work during this project... "
-                label="What were the results of your work during this project"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                value={formik.values.results}
-                error={formik.touched.results && formik.errors.results}
-              />
-            </FieldRow>
+            <form onSubmit={formik.handleSubmit}>
+              <FieldRow>
+                <TextField
+                  autoFocus
+                  multiline
+                  minRows={10}
+                  name="results"
+                  placeholder="What were the results of your work during this project... "
+                  label="What were the results of your work during this project"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.results}
+                  error={formik.touched.results && formik.errors.results}
+                />
+              </FieldRow>
+            </form>
           </Modal.Body>
           <Modal.Footer>
             <Flex align="center">
               <Flex.Item style={{ width: "120px" }}>
-                <Button block onClick={gotoPreviousStep} styling="outlined" size="l">
+                <Button
+                  block
+                  onClick={gotoPreviousStep}
+                  styling="outlined"
+                  size="l"
+                >
                   Back
                 </Button>
               </Flex.Item>
@@ -49,16 +65,16 @@ const ProjectDetails = ({ setValues, values, gotoNextStep, gotoPreviousStep }) =
                 <StepDots current={3} total={4} />
               </Flex.Item>
               <Flex.Item style={{ width: "120px" }}>
-                <Button block type="submit" size="l" styling="green">
+                <Button block onClick={formik.submitForm} size="l" styling="green">
                   Next
                 </Button>
               </Flex.Item>
             </Flex>
           </Modal.Footer>
-        </form>
+        </React.Fragment>
       )}
     </Formik>
-  )
-}
+  );
+};
 
-export default ProjectDetails
+export default ProjectDetails;
