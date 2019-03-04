32414c8b2e82231f8b2f65ce536edf0b5c838f7c
Thomas Cullen <thomascullen92@gmail.com>
2019-03-04 11:15:26 +0000

    Sync interview user

--- app/models/concerns/airtable/interview.rb
+++ app/models/concerns/airtable/interview.rb
@@ -5,4 +5,8 @@ class Airtable::Interview < Airtable::Base
   sync_column "Interview Time", to: :starts_at
   sync_column "Call Status", to: :status
   sync_association "Application", to: :application
+
+  sync_data do |interview|
+    interview.user = interview.application.project.user
+  end
 end
