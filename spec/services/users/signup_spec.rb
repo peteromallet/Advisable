cbb5493ec6e3d9dc3c6f9f10a11af733d02e5b91
Thomas Cullen <thomascullen92@gmail.com>
2019-03-04 12:04:22 +0000

    Move Accounts::Signup to Users::Signup

--- spec/services/users/signup_spec.rb
+++ spec/services/users/signup_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe Accounts::Signup do
+describe Users::Signup do
   context "when there is no existing user" do
     it "creates a new user account" do
       expect {
-        Accounts::Signup.call(email:"testing1234@test.com", password: "testing123", password_confirmation: "testing123")
+        Users::Signup.call(email:"testing1234@test.com", password: "testing123", password_confirmation: "testing123")
       }.to change {User.count}.by(1)
     end
   end
@@ -14,7 +14,7 @@ describe Accounts::Signup do
       it "sets their password" do
         user = create(:user, confirmed_at: nil, password: nil)
         expect(user.reload.password_digest).to be_nil
-        Accounts::Signup.call(email: user.email, password: "testing123", password_confirmation: "testing123")
+        Users::Signup.call(email: user.email, password: "testing123", password_confirmation: "testing123")
         expect(user.reload.password_digest).to_not be_nil
       end
     end
@@ -23,17 +23,9 @@ describe Accounts::Signup do
       it "has an email taken error" do
         user = create(:user, confirmed_at: DateTime.now)
         expect {
-          Accounts::Signup.call(email: user.email, password: "testing123", password_confirmation: "testing123")
+          Users::Signup.call(email: user.email, password: "testing123", password_confirmation: "testing123")
         }.to raise_error(Service::Error, 'users.email_taken')
       end
-    end
-  end
-
-  context "when the type is Specialist" do
-    it "creates a new specialist account" do
-      specialist = create(:specialist, email: "testing1234@test.com", password: nil)
-      Accounts::Signup.call(type: "Specialist", email:"testing1234@test.com", password: "testing123", password_confirmation: "testing123")
-      expect(specialist.reload.password_digest).to_not be_nil
     end
   end
 end
