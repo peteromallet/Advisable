e55f6312267a0aaf74e445a5756628dc62ade180
Thomas Cullen <thomascullen92@gmail.com>
2019-03-06 10:07:12 +0000

    Add CTA to apply to be a featured freelancer

--- app/javascript/src/views/Applications/OpenApplications/Empty.tsx
+++ app/javascript/src/views/Applications/OpenApplications/Empty.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Text from "../../../components/Text";
 import Card from "../../../components/Card";
+import Link from "../../../components/Link";
 import Styles from "../../../components/Styles";
 import Padding from "../../../components/Spacing/Padding";
 import illustration from "./illustration.png";
@@ -17,10 +18,25 @@ const Empty = () => {
             </Text>
           </Padding>
           <Styles maxWidth="450px" display="inline-block">
+            <Padding bottom="xl">
+              <Text size="xs">
+                Whenever relevant opportunities come up that match your
+                skillset, we’ll reach out and give you the opportunity to fill
+                out a tailored application that we present directly to clients.
+              </Text>
+            </Padding>
+            <Text weight="semibold" colour="dark" size="xs">
+              What more projects from Advisable?
+            </Text>
+            <Padding bottom="s">
+              <Text size="xs">
+                Request an invitation to be a featured freelancer.
+              </Text>
+            </Padding>
             <Text size="xs">
-              Whenever relevant opportunities come up that match your skillset,
-              we’ll reach out and give you the opportunity to fill out a
-              tailored application that we present directly to clients.
+              <Link href="mailto:hello@advisable.com?subject=Featured Freelancer Request">
+                Become a featured freelancer
+              </Link>
             </Text>
           </Styles>
         </Padding>
