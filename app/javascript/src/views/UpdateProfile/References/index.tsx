9a396c0d1ebcd033bc56535c700bfefe4fce7be5
Thomas Cullen <thomascullen92@gmail.com>
2019-03-07 09:04:35 +0000

    Add references to freelancer profile

---
+++ app/javascript/src/views/UpdateProfile/References/index.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { get } from "lodash";
+import { graphql } from "react-apollo";
+import Text from "../../../components/Text";
+import Button from "../../../components/Button";
+import Heading from "../../../components/Heading";
+import Padding from "../../../components/Spacing/Padding";
+import PreviousProjects from "../../../components/PreviousProjects";
+import AddPreviousProjectModal from "../../../components/AddPreviousProjectModal";
+import PREVIOUS_PROJECTS from "./previousProjects.graphql";
+
+const References = ({ data }) => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+
+  if (data.loading) return <div>loading...</div>;
+
+  const handleAddProject = () => {
+    setModalOpen(true);
+  };
+
+  return (
+    <React.Fragment>
+      <Padding bottom="s">
+        <Heading level={2}>Previous Projects</Heading>
+      </Padding>
+      <Padding bottom="m">
+        <Text size="xs">
+          Previous projects are one of the most effective ways to validate your
+          skills and experience. Advisable uses them to decide who to invite to
+          our platform, which projects to invite them to and gives you the
+          opportunity to share them with clients when you’re applying for
+          projects
+        </Text>
+      </Padding>
+
+      <Padding bottom="xl">
+        <Button styling="primary" onClick={handleAddProject}>
+          Add a previous project
+        </Button>
+      </Padding>
+      <AddPreviousProjectModal
+        isOpen={modalOpen}
+        specialistId={data.viewer.airtableId}
+        onClose={() => setModalOpen(false)}
+        mutationUpdate={(proxy, response) => {
+          const data = proxy.readQuery({ query: PREVIOUS_PROJECTS });
+          const project =
+            response.data.createOffPlatformProject.previousProject;
+          data.viewer.previousProjects.unshift(project);
+          proxy.writeQuery({ query: PREVIOUS_PROJECTS, data });
+        }}
+      />
+
+      <PreviousProjects
+        loading={data.loading}
+        specialistId={data.viewer.airtableId}
+        previousProjects={get(data, "viewer.previousProjects")}
+      />
+    </React.Fragment>
+  );
+};
+
+export default graphql(PREVIOUS_PROJECTS)(References);
--- app/javascript/src/views/UpdateProfile/index.tsx
+++ app/javascript/src/views/UpdateProfile/index.tsx
@@ -7,6 +7,7 @@ import Navigation from "./Navigation";
 import Introduction from "./Introduction";
 import Skills from "./Skills";
 import Location from "./Location";
+import References from "./References";
 
 // The navigation for a freelancers profile works differently on mobile compared
 // to desktop. On desktop the navigation is always present in the sidebar,
@@ -28,6 +29,7 @@ const UpdateProfile = ({ match }) => {
             <Route path="/profile/introduction" component={Introduction} />
             <Route path="/profile/skills" component={Skills} />
             <Route path="/profile/location" component={Location} />
+            <Route path="/profile/references" component={References} />
             {/* If we are on desktop then redirect user to /profile/introduction
             if their URL is exactly /profile */}
             {!isMobile && (
