3791dc095520765d6074fae7d830d930a2ed781b
Thomas Cullen <thomascullen92@gmail.com>
2019-03-27 08:51:26 +0000

    Add VerticalLayout component

---
+++ app/javascript/src/components/VerticalLayout/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const VerticalLayout = styled.div`
+  flex: 1 1 0%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+VerticalLayout.Header = styled.div`
+  flex-shrink: 0;
+`
+
+VerticalLayout.Content = styled.div`
+  flex-grow: 1;
+  display: flex;
+`
+
+export default VerticalLayout
