b15d8b8f996baa718ea263d6d562f4382a9fe68c
Thomas Cullen <thomascullen92@gmail.com>
2019-04-01 14:59:48 +0100

    Update syncing of client records and remove setup step

--- spec/models/concerns/airtable/client_contact_spec.rb
+++ spec/models/concerns/airtable/client_contact_spec.rb
@@ -10,30 +10,6 @@ describe Airtable::ClientContact do
     with: "test@test.com"
   }
 
-  describe "when 'Company Name' is a lookup column type" do
-    it "syncs the first item in the array" do
-      record = create(:user, company_name: nil)
-      airtable = Airtable::ClientContact.new({
-        "Company Name" => ["Testing", "Another"]
-      }, id: record.airtable_id)
-      expect { airtable.sync }.to change {
-        record.reload.company_name
-      }.from(nil).to("Testing")
-    end
-  end
-
-  describe "when 'Company Name' is not an array" do
-    it "syncs the data as is" do
-      record = create(:user, company_name: nil)
-      airtable = Airtable::ClientContact.new({
-        "Company Name" => "Testing"
-      }, id: record.airtable_id)
-      expect { airtable.sync }.to change {
-        record.reload.company_name
-      }.from(nil).to("Testing")
-    end
-  end
-
   describe "push_data" do
     let(:user) { create(:user) }
     let(:airtable) { Airtable::ClientContact.new({}, id: user.airtable_id) }
@@ -64,12 +40,6 @@ describe Airtable::ClientContact do
       expect { airtable.push(user) }.to change {
         airtable.fields['Country']
       }.from(nil).to([user.country.airtable_id])
-    end
-
-    it "syncs the company_name" do
-      expect { airtable.push(user) }.to change {
-        airtable.fields['Company Name']
-      }.from(nil).to(user.company_name)
     end
   end
 end
