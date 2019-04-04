7b6c13e4209c8040e74f9c8d806bfa76b156fce1
Thomas Cullen <thomascullen92@gmail.com>
2019-04-04 09:28:49 +0100

    Add person tracking to client side rollbar errors

--- app/javascript/src/components/Header/CurrentUser.js
+++ app/javascript/src/components/Header/CurrentUser.js
@@ -6,19 +6,38 @@ import {
 } from "./styles";
 
 const CurrentUser = ({ user, onLogout }) => {
-  if (!user) return null;
-
   const [open, setOpen] = useState(false);
   const handleBlur = () => setOpen(false);
   const handleFocus = () => setOpen(true);
+
+  React.useEffect(() => {
+    if (!window.Rollbar) return;
+    if (user) {
+      Rollbar.configure({
+        payload: {
+          environment: process.env.ROLLBAR_ENV,
+          person: {
+            id: user.airtableId,
+            email: user.email,
+          },
+        },
+      });
+    } else {
+      Rollbar.configure({
+        payload: {
+          environment: process.env.ROLLBAR_ENV
+        }
+      })
+    }
+  });
+
+  if (!user) return null;
 
   return (
     <CurrentUserWrapper tabIndex="0" onFocus={handleFocus} onBlur={handleBlur}>
       <CurrentUserToggle>
         <strong>{user.firstName}</strong>
-        {user.companyName && (
-          <span>{user.companyName}</span>
-        )}
+        {user.companyName && <span>{user.companyName}</span>}
       </CurrentUserToggle>
       <CurrentUserDropdown open={open}>
         <a href="#" onClick={onLogout}>
