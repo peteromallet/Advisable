151678f44c94dba0d7baaa500e1add03ce031699
Thomas Cullen <thomascullen92@gmail.com>
2019-04-09 13:18:03 +0100

    Update styles for proposal sent view

--- app/javascript/src/views/Proposals/Sent.tsx
+++ app/javascript/src/views/Proposals/Sent.tsx
@@ -2,26 +2,34 @@ import * as React from "react";
 import { compose, graphql } from "react-apollo";
 import Text from "../../components/Text";
 import Card from "../../components/Card";
-import Button from "../../components/Button";
+import Link from "../../components/Link";
 import Heading from "../../components/Heading";
-import TextField from "../../components/TextField";
 import { Padding } from "../../components/Spacing";
 import FETCH_BOOKING from "./fetchBooking.graphql";
+import illustration from "./illustration.png";
 
 const Send = ({ application, data }) => {
-  if (data.loading) return <>loading</>
+  if (data.loading) return <>loading</>;
+  const proposalUrl = `/applications/${application.airtableId}/proposals/${
+    data.booking.airtableId
+  }`;
 
   return (
     <Card>
-      <Padding size="l">
+      <Padding size="l" style={{ textAlign: "center" }}>
+        <img src={illustration} width={280} alt="" />
         <Padding bottom="s">
           <Heading level={3}>Your proposal has been sent!</Heading>
         </Padding>
         <Padding bottom="l">
-          <Text size="s">
-            Your proposal has been sent to {application.project.user.companyName}.
-            Some text about what the freelancer should expect to happen next.
+          <Text size="s" style={{ maxWidth: 380, margin: "0 auto" }}>
+            Your proposal has been sent to{" "}
+            {application.project.user.companyName}. Some text about what the
+            freelancer should expect to happen next.
           </Text>
+        </Padding>
+        <Padding bottom="m">
+          <Link to={proposalUrl}>Update proposal</Link>
         </Padding>
       </Padding>
     </Card>
