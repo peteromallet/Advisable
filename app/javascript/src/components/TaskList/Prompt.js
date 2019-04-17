82f606fb496bbe24df7432746462135c72c7c1e1
Thomas Cullen <thomascullen92@gmail.com>
2019-04-17 15:21:51 +0100

    Ability for a freelancer to submit a task for approval

--- app/javascript/src/components/TaskList/Prompt.js
+++ app/javascript/src/components/TaskList/Prompt.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Prompt } from "./styles";
 
 export default ({ isClient, task }) => {
-  if (task.stage === "Not Assigned") return null;
-  if (task.stage === "Quote Requested" && isClient) return null;
-  if (task.stage === "Quote Provided" && !isClient) return null;
-  if (task.stage === "Assigned" && isClient) return null;
-  if (task.stage === "Working") return null;
+  if (task.stage === "Quote Requested" && !isClient) return <Prompt />;
+  if (task.stage === "Quote Provided" && isClient) return <Prompt />;
+  if (task.stage === "Assigned" && !isClient) return <Prompt />;
+  if (task.stage === "Submitted" && isClient) return <Prompt />;
 
-  return <Prompt />
+  return null
 }
