4b9b5fe430bf6501dcef636598541dafd289b538
Thomas Cullen <thomascullen92@gmail.com>
2019-04-29 11:23:12 +0100

    Pass data with new proposal webhook events

--- app/services/proposals/accept.rb
+++ app/services/proposals/accept.rb
@@ -9,7 +9,10 @@ class Proposals::Accept < ApplicationService
     application.status = "Working"
     if application.save
       application.sync_to_airtable
-      WebhookEvent.trigger("applications.proposal_accepted")
+      WebhookEvent.trigger(
+        "applications.proposal_accepted",
+        WebhookEvent::Application.data(application)
+      )
       return application
     else
       raise Service::Error.new(application.errors.full_messages.first)
