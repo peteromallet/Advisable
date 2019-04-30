175dc52fab03adb221f220b9b4d7eed5a118fe02
Thomas Cullen <thomascullen92@gmail.com>
2019-04-30 14:11:09 +0100

    Fix airtable project syncing

--- app/models/concerns/airtable/project.rb
+++ app/models/concerns/airtable/project.rb
@@ -27,7 +27,7 @@ class Airtable::Project < Airtable::Base
 
     skills = project.skills.map(&:airtable_id)
     required_skills = fields['Skills Required'] || []
-    fields['Skills Required'].each do |skill_id|
+    required_skills.each do |skill_id|
       unless skills.include?(skill_id)
         skill = ::Skill.find_by_airtable_id(skill_id)
         skill = Airtable::Skill.find(skill_id).sync unless skill.present?
