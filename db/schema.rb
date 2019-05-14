0c80d3d7c861521e9ef6b7f6078ef6cdc9dc1c3a
Thomas Cullen <thomascullen92@gmail.com>
2019-05-14 12:23:24 +0100

    Change application rate column from int to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_080302) do
+ActiveRecord::Schema.define(version: 2019_05_14_112004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_080302) do
   end
 
   create_table "applications", force: :cascade do |t|
-    t.integer "rate"
+    t.decimal "rate"
     t.string "availability"
     t.string "status"
     t.text "introduction"
