0dbda6b849dc82db159cc7f422d0672ad166b475
Thomas Cullen <thomascullen92@gmail.com>
2019-05-16 16:55:52 +0100

    Fix failing JS tests

--- app/javascript/src/__tests__/applications-view.js
+++ app/javascript/src/__tests__/applications-view.js
@@ -1,52 +1,66 @@
-import { cleanup, fireEvent } from "react-testing-library";
+import { cleanup } from "react-testing-library";
 import renderApp from "../testHelpers/renderApp";
 import viewer from "../graphql/queries/viewer";
 import generateTypes from "../__mocks__/graphqlFields";
-import getApplications from "../views/Candidates/graphql/fetchProject";
+import getApplications from "../views/Project/fetchProject";
 
 afterEach(cleanup);
 
-test("Renders the candidates", async () => {
-  const { findByText } = renderApp({
-    route: "/projects/rec1234/applied",
-    graphQLMocks: [
-      {
-        request: {
-          query: viewer,
-        },
-        result: {
-          data: {
-            viewer: generateTypes.user(),
+describe("Viewing 'Applied'", () => {
+  const render = () =>
+    renderApp({
+      route: "/projects/rec1234/applied",
+      graphQLMocks: [
+        {
+          request: {
+            query: viewer,
+          },
+          result: {
+            data: {
+              viewer: generateTypes.user(),
+            },
           },
         },
-      },
-      {
-        request: {
-          query: getApplications,
-          variables: {
-            id: "rec1234",
+        {
+          request: {
+            query: getApplications,
+            variables: {
+              id: "rec1234",
+            },
           },
-        },
-        result: {
-          data: {
-            project: generateTypes.project({
-              id: "rec1233",
-              applications: [
-                generateTypes.application({
-                  specialist: generateTypes.specialist({
-                    name: "Test Account",
+          result: {
+            data: {
+              project: generateTypes.project({
+                id: "rec1233",
+                applications: [
+                  generateTypes.application({
+                    status: "Applied",
+                    specialist: generateTypes.specialist({
+                      name: "Test Account",
+                    }),
                   }),
-                }),
-                generateTypes.application({
-                  specialist: generateTypes.specialist(),
-                }),
-              ],
-            }),
+                  generateTypes.application({
+                    status: "Application Rejected",
+                    specialist: generateTypes.specialist({
+                      name: "Mr Rejected",
+                    }),
+                  }),
+                ],
+              }),
+            },
           },
         },
-      },
-    ],
+      ],
+    });
+
+  test("includes applications with a status of 'Applied'", async () => {
+    const { findByText } = render();
+    expect(await findByText("Test Account")).toBeInTheDocument();
   });
 
-  expect(await findByText("Test Account")).toBeInTheDocument();
+  test("excludes applications without a status of 'Applied'", async () => {
+    const { findByText, queryByText } = render();
+    await findByText("Test Account");
+    expect(queryByText("Mr Rejected")).not.toBeInTheDocument();
+  });
 });
