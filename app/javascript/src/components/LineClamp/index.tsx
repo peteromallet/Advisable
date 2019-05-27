4cda080dec0b8902c9216517ea7afadf3a5926aa
Thomas Cullen <thomascullen92@gmail.com>
2019-05-27 08:48:06 +0100

    Fix LineClamp component when child is null

--- app/javascript/src/components/LineClamp/index.tsx
+++ app/javascript/src/components/LineClamp/index.tsx
@@ -3,20 +3,23 @@ import * as React from "react";
 const LineClamp = ({ children, maxHeight, character = "…" }) => {
   const ref = React.useRef(null);
   const [min, setMin] = React.useState(0);
-  const [max, setMax] = React.useState(children.length);
+  const [max, setMax] = React.useState(Boolean(children) ? children.length : 0);
+
+  if (!Boolean(children)) return null;
+
   const midPoint = Math.floor((min + max) / 2);
 
   const handleResize = () => {
-    setMin(0)
+    setMin(0);
     setMax(children.length);
-  }
+  };
 
   React.useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   React.useLayoutEffect(() => {
     if (min >= max) return;
@@ -26,8 +29,8 @@ const LineClamp = ({ children, maxHeight, character = "…" }) => {
     } else {
       setMax(midPoint);
     }
-  }, [midPoint])
-  
+  }, [midPoint]);
+
   const text = `${children.slice(0, midPoint)}${character}`;
 
   return <span ref={ref}>{text}</span>;
