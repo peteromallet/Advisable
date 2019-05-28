e15120b932e8fa56e68ecbeb782b03e9ea00b344
Thomas Cullen <thomascullen92@gmail.com>
2019-05-28 09:50:26 +0100

    Allow client to edit the monthly limit for a project

--- app/javascript/src/views/Booking/Sidebar.js
+++ app/javascript/src/views/Booking/Sidebar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { get } from "lodash";
 import Sticky from "react-stickynode";
+import Button from "../../components/Button";
 import Text from "../../components/Text";
+import Modal from "../../components/Modal";
 import Avatar from "../../components/Avatar";
 import Layout from "../../components/Layout";
 import Heading from "../../components/Heading";
@@ -10,12 +12,17 @@ import { Padding } from "../../components/Spacing";
 import { FadeIn } from "../../components/Animation";
 import AttributeList from "../../components/AttributeList";
 import { useMobile } from "../../components/Breakpoint";
+import ProjectMonthlyLimit from "../../components/ProjectMonthlyLimit";
 
 export default ({ data }) => {
   const isMobile = useMobile();
   const application = data.application;
-  const project = application.project;
   const specialist = application.specialist;
+  const [monthlyLimitModal, setMonthlyLimitModal] = React.useState(false);
+
+  const handleEditMonthlyLimit = () => {
+    setMonthlyLimitModal(true);
+  };
 
   return (
     <Layout.Sidebar size="m">
@@ -40,6 +47,40 @@ export default ({ data }) => {
                   label="Hourly Rate"
                   value={currency(parseFloat(application.rate) * 100.0)}
                 />
+              )}
+
+              {application.projectType === "Flexible" && (
+                <>
+                  <Modal
+                    isOpen={monthlyLimitModal}
+                    onClose={() => setMonthlyLimitModal(false)}
+                  >
+                    <Padding size="xl">
+                      <ProjectMonthlyLimit
+                        applicationId={application.airtableId}
+                        onUpdate={() => setMonthlyLimitModal(false)}
+                        initialValues={{
+                          monthlyLimit: application.monthlyLimit,
+                        }}
+                      />
+                    </Padding>
+                  </Modal>
+                  <AttributeList.Item
+                    label="Monthly Limit"
+                    value={
+                      <>
+                        {application.monthlyLimit} hours
+                        <br />
+                        <Button
+                          onClick={handleEditMonthlyLimit}
+                          styling="plain"
+                        >
+                          Edit
+                        </Button>
+                      </>
+                    }
+                  />
+                </>
               )}
             </AttributeList>
           </Padding>
