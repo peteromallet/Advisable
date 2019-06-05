1a66d61bccd9a56e459f7ede3f57ad86210e5550
Thomas Cullen <thomascullen92@gmail.com>
2019-06-05 11:10:10 +0100

    Simplify previous project modal to

--- app/javascript/src/components/PreviousProjectModal/ProjectDetails.js
+++ app/javascript/src/components/PreviousProjectModal/ProjectDetails.js
@@ -15,50 +15,36 @@ const title = project => {
   return `${project.primarySkill} at ${companyName(project)}`;
 };
 
-const clientDescription = project => {
-  if (project.__typename === "Project") return project.companyDescription;
-  return project.clientDescription;
-};
-
-const requirements = project => {
-  if (project.__typename === "Project") return project.specialistDescription;
-  return project.requirements;
-};
-
 export default ({ previousProject }) => {
   const { project, reviews } = previousProject;
 
   return (
     <React.Fragment>
       <Modal.Header>
-        <Heading paddingBottom="xs" level={2}>{title(project)}</Heading>
+        <Heading paddingBottom="xs" level={2}>
+          {title(project)}
+        </Heading>
         <ProjectValidationStatus status={project.validationStatus} />
       </Modal.Header>
       <Modal.Body>
         <Spacing paddingBottom="s">
-          <Heading level={6}>Client Description</Heading>
-          <Text size="s" marginBottom="l">
-            {clientDescription(project)}
-          </Text>
+          {project.__typename === "Project" && (
+            <>
+              <Heading level={6}>Client Description</Heading>
+              <Text size="s" marginBottom="l">
+                {project.companyDescription}
+              </Text>
 
-          <Heading level={6}>Requirements</Heading>
-          <Text size="s" marginBottom="l">
-            {requirements(project)}
-          </Text>
-
+              <Heading level={6}>Requirements</Heading>
+              <Text size="s" marginBottom="l">
+                {project.specialistDescription}
+              </Text>
+            </>
+          )}
           <Heading level={6}>Project Description</Heading>
           <Text size="s" marginBottom="l">
             {project.description}
           </Text>
-
-          {project.results && (
-            <Fragment>
-              <Heading level={6}>Results</Heading>
-              <Text size="s" marginBottom="l">
-                {project.results}
-              </Text>
-            </Fragment>
-          )}
         </Spacing>
 
         {reviews.map(review => (
