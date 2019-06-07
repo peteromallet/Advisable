9acf9832ffecd75a3743cc1afce441e5ce594741
Thomas Cullen <thomascullen92@gmail.com>
2019-06-07 08:47:52 +0100

    Dont lock scroll with modals any more

--- app/javascript/src/components/Modal/index.js
+++ app/javascript/src/components/Modal/index.js
@@ -45,27 +45,25 @@ const Modal = ({
         ({ item, key, props }) =>
           item && (
             <WindowContainer key={key} size={size} style={props}>
-              <RemoveScroll>
-                <Window
-                  className="ModalWindow"
-                  expandOnMobile={expandOnMobile}
-                  {...extractSpacingProps(componentProps)}
-                >
-                  {onClose && (
-                    <CloseModal aria-label="Close Modal" onClick={onClose}>
-                      <svg width={13} height={12} fill="none">
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M11.705.295a1.007 1.007 0 0 0-1.42-.006L6 4.573 1.716.29a1.007 1.007 0 0 0-1.42.006c-.39.39-.396 1.03-.006 1.42L4.574 6 .29 10.284c-.39.39-.385 1.03.006 1.42.39.39 1.03.397 1.42.006l4.285-4.284 4.284 4.284c.39.39 1.03.385 1.42-.006.39-.39.397-1.03.006-1.42L7.427 6l4.284-4.285c.39-.39.384-1.03-.006-1.42z"
-                          fill="#969CB9"
-                        />
-                      </svg>
-                    </CloseModal>
-                  )}
-                  {children}
-                </Window>
-              </RemoveScroll>
+              <Window
+                className="ModalWindow"
+                expandOnMobile={expandOnMobile}
+                {...extractSpacingProps(componentProps)}
+              >
+                {onClose && (
+                  <CloseModal aria-label="Close Modal" onClick={onClose}>
+                    <svg width={13} height={12} fill="none">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M11.705.295a1.007 1.007 0 0 0-1.42-.006L6 4.573 1.716.29a1.007 1.007 0 0 0-1.42.006c-.39.39-.396 1.03-.006 1.42L4.574 6 .29 10.284c-.39.39-.385 1.03.006 1.42.39.39 1.03.397 1.42.006l4.285-4.284 4.284 4.284c.39.39 1.03.385 1.42-.006.39-.39.397-1.03.006-1.42L7.427 6l4.284-4.285c.39-.39.384-1.03-.006-1.42z"
+                        fill="#969CB9"
+                      />
+                    </svg>
+                  </CloseModal>
+                )}
+                {children}
+              </Window>
             </WindowContainer>
           )
       )}
