9a1740ce1bd80dfb197db6c976408a7e46e3942d
Thomas Cullen <thomascullen92@gmail.com>
2019-07-02 08:35:53 +0100

    Improve scheduling of webhooks (#174)

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -2,14 +2,27 @@
 class Webhook < ApplicationRecord
   serialize :response, Hash
   after_initialize :set_pending, if: :new_record?
+  after_commit :perform, on: :create
 
+  # Takes a given record and searches for webhook configurations for that record
+  # type. It then executes each of these webhook configurations and fire a
+  # webhook if any of the criteria are met.
+  # @deprecated Use the WebhookEvent model instead to trigger specific webhooks.
   def self.process(entity)
     configs = WebhookConfiguration.where(type: "WebhookConfiguration::#{entity.class.to_s}")
     configs.map { |c| c.process(entity) }.compact
   end
 
+  # Creates a sidekiq WebhookJob to process the webhook. This is automatically
+  # called after the record is inserted into the database using an after_commit
+  # callback.
+  def perform
+    WebhookJob.perform_later(id)
+  end
+
   private
 
+  # Sets the default status for the webhook.
   def set_pending
     self.status = "pending"
   end
