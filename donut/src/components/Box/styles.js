197d546815fdfd7e844394c6cbf2d2967bbdbbb4
Thomas Cullen <thomascullen92@gmail.com>
2019-07-16 18:29:36 +0100

    Show payment method in booking page

--- donut/src/components/Box/styles.js
+++ donut/src/components/Box/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import { space, layout, color, flex, position } from "styled-system";
+import { space, border, layout, color, flex, position } from "styled-system";
 
 const Box = styled.div`
   ${flex}
   ${color}
   ${space}
   ${layout}
+  ${border}
   ${position}
 `;
 
