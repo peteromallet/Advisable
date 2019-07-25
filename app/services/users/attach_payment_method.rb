e9b3804f716dc1cb9f1147c56aa59bdc4dd0d4e6
Thomas Cullen <thomascullen92@gmail.com>
2019-07-25 08:38:33 +0100

    Fix 500 error for some payment intent webhooks (#193)
    When payment intents are created off platform, e.g through the
    stripe dashboard, they don't have any metadata. This was causing
    an error when the webhook tried to access the metadata.
    
    This also fixes an issue when attempting to attach a card that
    has already been attached to a customer.

--- app/services/users/attach_payment_method.rb
+++ app/services/users/attach_payment_method.rb
@@ -7,10 +7,18 @@ class Users::AttachPaymentMethod < ApplicationService
   end
 
   def call
-    # Attach the payment method to the customer
-    Stripe::PaymentMethod.attach(payment_method_id, {
-      customer: user.stripe_customer_id
-    })
+    existing = Stripe::PaymentMethod.list(
+      customer: user.stripe_customer_id,
+      type: "card"
+    )
+
+    ids = existing.data.map(&:id)
+    unless ids.include?(payment_method_id)
+      # Attach the payment method to the customer
+      Stripe::PaymentMethod.attach(payment_method_id, {
+        customer: user.stripe_customer_id
+      })
+    end
 
     # Set the payment method as the customer's default payment method
     Stripe::Customer.update(user.stripe_customer_id, {
