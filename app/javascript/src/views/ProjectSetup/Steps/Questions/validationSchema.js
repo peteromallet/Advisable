7e199fc04c93819e0640c4e3a4fed0d6310ae23f
Thomas Cullen <thomascullen92@gmail.com>
2019-09-19 13:27:58 +0100

    Make qualification questions optional during project setup (#211)

--- app/javascript/src/views/ProjectSetup/Steps/Questions/validationSchema.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-const validationSchema = Yup.object().shape({
-  questions: Yup.array().of(Yup.string()).compact().min(1, "Please add at least one question")
-});
-
-export default validationSchema;
