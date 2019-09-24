5fa0768615f26c902a9ae429cad48b1a456d2a9f
Thomas Cullen <thomascullen92@gmail.com>
2019-09-24 08:26:04 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_135721) do
+ActiveRecord::Schema.define(version: 2019_09_23_160928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
