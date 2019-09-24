617f1bbb05eb927e2184c61751c9c501b187f7e9
Thomas Cullen <thomascullen92@gmail.com>
2019-09-24 14:17:25 +0100

    Prevent invite page crashing when application record not found

--- app/javascript/src/views/JobListing/index.js
+++ app/javascript/src/views/JobListing/index.js
@@ -8,19 +8,25 @@ import ApplicationsClosed from "../ApplicationsClosed";
 import GET_APPLICATION from "./fetchProject";
 
 let JobListingContainer = ({ history, match }) => {
-  const query = useQuery(GET_APPLICATION, {
+  const { loading, data, error } = useQuery(GET_APPLICATION, {
     variables: {
       id: match.params.applicationId,
     },
   });
 
-  if (query.loading) return <Loading />;
-  if (!query.data.application) return <NotFound />;
+  if (loading) return <Loading />;
 
-  const open = get(query.data, "application.project.applicationsOpen");
+  if (error) {
+    const code = error.graphQLErrors[0].extensions.code;
+    if (code === "recordNotFound") {
+      return <NotFound />;
+    }
+  }
+
+  const open = get(data, "application.project.applicationsOpen");
   if (!open) return <ApplicationsClosed />;
 
-  return <JobListing history={history} application={query.data.application} />;
+  return <JobListing history={history} application={data.application} />;
 };
 
 export default JobListingContainer;
