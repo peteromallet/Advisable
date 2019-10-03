9223d5c1dd0516ecd1a53a9756e71ab0dc31a30b
Thomas Cullen <thomascullen92@gmail.com>
2019-10-03 09:14:55 +0100

    Show off platform project validation URL in app (#225)
    * Show validation URL in app
    
    * Fix useViewer hook when there is no user logged in

--- app/javascript/src/components/CopyURL/styles.js
+++ app/javascript/src/components/CopyURL/styles.js
@@ -1,3 +1,4 @@
+import { rgba } from "polished";
 import styled, { keyframes } from "styled-components";
 
 const copiedAnimation = keyframes`
@@ -13,14 +14,14 @@ const copiedAnimation = keyframes`
     opacity: 0;
     transform: translateY(-15px);
   }
-`
+`;
 
 export const CopyURL = styled.div`
   height: 40px;
   margin: 0 auto;
-  max-width: 500px;
+  max-width: 100%;
   position: relative;
-  background: #f5f7ff;
+  background: ${props => props.bg || "#f5f7ff"};
   border-radius: 30px;
 
   &::after {
@@ -35,8 +36,8 @@ export const CopyURL = styled.div`
     border-bottom-right-radius: 50px;
     background: linear-gradient(
       270deg,
-      #f5f7ff 70%,
-      rgba(245, 247, 255, 0) 100%
+      ${props => props.bg || "#f5f7ff"} 70%,
+      ${props => rgba(props.bg || "#f5f7ff", 0)} 100%
     );
   }
 
@@ -80,7 +81,7 @@ export const CopyURL = styled.div`
     right: 0;
     top: -20px;
     color: #173fcd;
-    font-size: 14px;
+    font-size: 12px;
     position: absolute;
     animation: ${copiedAnimation} 1.5s cubic-bezier(0.3, 0, 0, 1) forwards;
   }
---
+++ app/javascript/src/components/ProjectValidationPrompt/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Text, Icon } from "@advisable/donut";
+import CopyURL from "../CopyURL";
+
+const ProjectValidationPrompt = ({ project }) => {
+  let name = project.contactFirstName;
+  if (project.contactLastName) name += ` ${project.contactLastName}`;
+
+  return (
+    <Box borderRadius={12} bg="yellow.0" padding="s">
+      <Box display="flex">
+        <Icon
+          width={24}
+          color="yellow.5"
+          strokeWidth={1.6}
+          icon="alert-circle"
+        />
+        <Box ml="xs">
+          <Text fontSize="s" fontWeight="medium" color="neutral.9" mb="xxs">
+            Verification required
+          </Text>
+          <Text fontSize="xs" lineHeight="s" color="neutral.8" mb="s">
+            Please send the following verification URL to {name} from{" "}
+            {project.clientName} so they can verify the project.
+          </Text>
+        </Box>
+      </Box>
+      <CopyURL bg="white">{`${location.origin}/verify_project/${project.uid}`}</CopyURL>
+    </Box>
+  );
+};
+
+export default ProjectValidationPrompt;
