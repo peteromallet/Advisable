b4a89bad34a636db3e6b0b9ff735caeb76d3d608
Thomas Cullen <thomascullen92@gmail.com>
2019-11-27 22:34:28 +0000

    Remove topic radio options

--- app/javascript/src/views/RequestConsultation/Topic.js
+++ app/javascript/src/views/RequestConsultation/Topic.js
@@ -3,31 +3,13 @@ import React from "react";
 import { useMutation } from "react-apollo";
 import { useParams, useLocation } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
-import {
-  Icon,
-  Box,
-  Text,
-  RoundedButton,
-  Radio,
-  RadioGroup,
-} from "@advisable/donut";
+import { Icon, Box, Text, RoundedButton } from "@advisable/donut";
 import TextField from "../../components/TextField";
 import REQUEST_CONSULTATION from "./requestConsultation";
 
 const validationSchema = Yup.object({
   topic: Yup.string().required(),
-  customTopic: Yup.string().when("topic", {
-    is: "OTHER",
-    then: Yup.string().required(),
-  }),
 });
-
-const commaSeparated = skills => {
-  if (skills.length === 1) return skills;
-  if (skills.length === 2) return `${skills[0]} and ${skills[1]}`;
-  const length = skills.length;
-  return `${skills.slice(0, length - 1).join(", ")} and ${skills[length - 1]}`;
-};
 
 const Topic = ({ data, nextStep }) => {
   const { specialistId } = useParams();
@@ -35,8 +17,7 @@ const Topic = ({ data, nextStep }) => {
   const [requestConsultation] = useMutation(REQUEST_CONSULTATION);
 
   const initialValues = {
-    topic: undefined,
-    customTopic: "",
+    topic: "",
   };
 
   const handleSubmit = async values => {
@@ -45,7 +26,7 @@ const Topic = ({ data, nextStep }) => {
         input: {
           ...location.state,
           specialist: specialistId,
-          topic: values.topic === "OTHER" ? values.customTopic : values.topic,
+          topic: values.topic,
         },
       },
     });
@@ -82,92 +63,16 @@ const Topic = ({ data, nextStep }) => {
       >
         {formik => (
           <Form>
-            <RadioGroup mb={formik.values.topic === "OTHER" ? "m" : "xl"}>
+            <Box mb="xl">
               <Field
-                type="radio"
-                as={Radio}
+                autoFocus
+                multiline
+                minRows={6}
                 name="topic"
-                label={`We're interested in learning more about ${commaSeparated(
-                  location.state.skills
-                )}`}
-                value={`We're interested in learning more about ${commaSeparated(
-                  location.state.skills
-                )}`}
+                as={TextField}
+                placeholder="What would you like to talk about..."
               />
-              <Field
-                type="radio"
-                as={Radio}
-                name="topic"
-                label={`We're interested in talking to a ${commaSeparated(
-                  location.state.skills
-                )} expert`}
-                value={`We're interested in talking to a ${commaSeparated(
-                  location.state.skills
-                )} expert`}
-              />
-              <Field
-                type="radio"
-                as={Radio}
-                name="topic"
-                label={`We need help figuring out our ${commaSeparated(
-                  location.state.skills
-                )} strategy`}
-                value={`We need help figuring out our ${commaSeparated(
-                  location.state.skills
-                )} strategy`}
-              />
-              <Field
-                type="radio"
-                as={Radio}
-                name="topic"
-                label={`We need help fixing issues with our ${commaSeparated(
-                  location.state.skills
-                )} strategy`}
-                value={`We need help fixing issues with our ${commaSeparated(
-                  location.state.skills
-                )} strategy`}
-              />
-              <Field
-                type="radio"
-                as={Radio}
-                name="topic"
-                label={`We're looking for advice on ${commaSeparated(
-                  location.state.skills
-                )} implementation`}
-                value={`We're looking for advice on ${commaSeparated(
-                  location.state.skills
-                )} implementation`}
-              />
-              <Field
-                type="radio"
-                as={Radio}
-                name="topic"
-                label={`We're considering adding ${commaSeparated(
-                  location.state.skills
-                )} skills to our team`}
-                value={`We're considering adding ${commaSeparated(
-                  location.state.skills
-                )} skills to our team`}
-              />
-              <Field
-                type="radio"
-                as={Radio}
-                name="topic"
-                label="Other"
-                value="OTHER"
-              />
-            </RadioGroup>
-            {formik.values.topic === "OTHER" && (
-              <Box mb="l">
-                <Field
-                  autoFocus
-                  multiline
-                  as={TextField}
-                  name="customTopic"
-                  placeholder="What would you like to talk about..."
-                />
-              </Box>
-            )}
+            </Box>
             <RoundedButton
               type="submit"
               width={["100%", "auto"]}
