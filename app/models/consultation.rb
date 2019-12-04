cc445b07cab5ae6f0d8864ba358ba4b5f7a94797
Thomas Cullen <thomascullen92@gmail.com>
2019-12-04 08:05:23 +0000

    Return nil from #sync if the record fails to sync

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -5,8 +5,4 @@ class Consultation < ApplicationRecord
   belongs_to :user
   belongs_to :skill
   belongs_to :interview, required: false
-
-  validates :skill_id, presence: true
-  validates :user_id, presence: true
-  validates :specialist_id, presence: true
 end
