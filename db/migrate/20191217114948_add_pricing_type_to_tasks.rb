d558f4519dc0b0ccbd4fccae65eb5b4268b715c3
Thomas Cullen <thomascullen92@gmail.com>
2019-12-20 09:40:49 +0000

    Remove pricing_type migration

--- db/migrate/20191217114948_add_pricing_type_to_tasks.rb
+++ db/migrate/20191217114948_add_pricing_type_to_tasks.rb
@@ -1,10 +1,6 @@
 class AddPricingTypeToTasks < ActiveRecord::Migration[6.0]
   def up
     add_column :tasks, :pricing_type, :string
-
-    Task.where(pricing_type: nil).find_each do |t|
-      t.update(pricing_type: 'Hourly')
-    end
   end
 
   def down
