c85d47cca32e796817b56db0c3ce11b3c6bd0187
Thomas Cullen <thomascullen92@gmail.com>
2020-01-16 08:58:25 +0000

    Remove original base styles

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import Routes from "./Routes";
-import BaseStyling from "./BaseStyling";
 import ApplicationProvider from "./components/ApplicationProvider";
 import RootErrorBoundary from "./views/RootErrorBoundary";
 import { NotificationsProvider } from "./components/Notifications";
 
 const App = () => {
   return (
-    <>
-      <BaseStyling />
-      <RootErrorBoundary>
-        <ApplicationProvider>
-          <NotificationsProvider>
-            <Routes />
-          </NotificationsProvider>
-        </ApplicationProvider>
-      </RootErrorBoundary>
-    </>
+    <RootErrorBoundary>
+      <ApplicationProvider>
+        <NotificationsProvider>
+          <Routes />
+        </NotificationsProvider>
+      </ApplicationProvider>
+    </RootErrorBoundary>
   );
 };
 
