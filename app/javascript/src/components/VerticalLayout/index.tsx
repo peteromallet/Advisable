a2f7dd88ccbece1436c21926eb3117f0dfbdacdc
Thomas Cullen <thomascullen92@gmail.com>
2020-01-17 12:47:05 +0000

    Fix task drawer sizing (#288)

--- app/javascript/src/components/VerticalLayout/index.tsx
+++ app/javascript/src/components/VerticalLayout/index.tsx
@@ -5,20 +5,21 @@ const VerticalLayout = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-`
+`;
 
 VerticalLayout.Header = styled.div`
   flex-shrink: 0;
-`
+`;
 
 VerticalLayout.Content = styled.div`
   height: 100%;
   flex-grow: 1;
   display: flex;
-`
+  min-height: 1px;
+`;
 
 VerticalLayout.Footer = styled.div`
   flex-shrink: 0;
-`
+`;
 
-export default VerticalLayout
+export default VerticalLayout;
