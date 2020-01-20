772aefa945f8299a17052d871ad7cf9fabfe54c5
Thomas Cullen <thomascullen92@gmail.com>
2020-01-20 12:48:56 +0000

    Fix cases of displaying floats as currency (#291)

--- app/javascript/src/utilities/currency.js
+++ app/javascript/src/utilities/currency.js
@@ -4,8 +4,15 @@ const defaultOptions = {
   format: "$0,0",
 };
 
+const isFloat = n => n % 1 !== 0;
+
 export default (amount = 0, options = {}) => {
-  const dinero = new Dinero({ currency: "USD", amount: Number(amount || 0) });
+  // Dinero expects an int not a float. There are rare cases when a float may
+  // be passed in. (e.g an old application rate from airtable). In these cases
+  // we want to round up the decimal.
+  let asNumber = Math.ceil(Number(amount || 0));
+
+  const dinero = new Dinero({ currency: "USD", amount: asNumber });
   const opts = { ...defaultOptions, ...options };
   return dinero.toFormat(opts.format);
 };
