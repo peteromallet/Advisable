edf5ed11f237b67cb4e276eea9c1710bc13f6551
Thomas Cullen <thomascullen92@gmail.com>
2020-01-23 23:44:52 +0000

    Update react-remove-scroll

---
+++ donut/src/components/Tabs/Tabs.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Tabs from "./";
+import Card from "../Card";
+
+export default {
+  title: "Tabs",
+};
+
+export const tabs = () => {
+  return (
+    <Card mx="auto" p="xl" maxWidth={500}>
+      <Tabs>
+        <Tabs.Tab icon="list" title="Active Tasks">
+          This is the active tasks section
+        </Tabs.Tab>
+        <Tabs.Tab icon="check-square" title="Completed Tasks">
+          This is the completed tasks section
+        </Tabs.Tab>
+      </Tabs>
+    </Card>
+  );
+};
