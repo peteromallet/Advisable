37dee8b653fefb5ca867e0f06476ec3f38a2f3a6
Thomas Cullen <thomascullen92@gmail.com>
2020-01-27 22:28:33 +0000

    Trigger build

--- app/javascript/src/views/Applications/index.js
+++ app/javascript/src/views/Applications/index.js
@@ -34,7 +34,7 @@ const Applications = ({ history }) => {
             </Alert>
           </Box>
         )}
-        <button onClick={() => asdf()}>Break</button>
+        <button onClick={() => asdf()}>Test Error</button>
         <ApplicationInvitations
           onHold={onHold}
           loading={loading}
