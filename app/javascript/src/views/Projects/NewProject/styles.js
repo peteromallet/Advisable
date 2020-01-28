760521c12f3b06471915525f138cf35acf84279a
Thomas Cullen <thomascullen92@gmail.com>
2020-01-28 08:26:59 +0000

    Improve design of 'find a new freelancer' CTA (#298)

--- app/javascript/src/views/Projects/NewProject/styles.js
+++ app/javascript/src/views/Projects/NewProject/styles.js
@@ -1,10 +1,11 @@
 import { rgba } from "polished";
 import styled from "styled-components";
-import { theme } from "@advisable/donut";
+import { theme, StyledCard } from "@advisable/donut";
 
 export const StyledNewProjectIcon = styled.div`
   width: 60px;
   height: 60px;
+  color: white;
   display: flex;
   position: relative;
   margin-bottom: 24px;
@@ -14,23 +15,27 @@ export const StyledNewProjectIcon = styled.div`
   &::before {
     content: "";
     width: 100%;
-    z-index: -1;
+    z-index: 0;
     height: 100%;
-    background: white;
     border-radius: 50%;
     position: absolute;
+    background: ${theme.colors.blue[5]};
     transition: background 700ms, transform 700ms;
     transition-timing-function: cubic-bezier(0.2, 0, 0, 1.6);
     box-shadow: 0 2px 8px ${rgba(theme.colors.neutral[9], 0.1)};
   }
+
+  svg {
+    z-index: 1;
+  }
 `;
 
-export const StyledNewProject = styled.div`
+export const StyledNewProject = styled(StyledCard)`
   width: 100%;
   height: 280px;
-  font-size: 15px;
+  font-size: 16px;
   cursor: pointer;
-  font-weight: 500;
+  font-weight: 400;
   border-radius: 2px;
   text-align: center;
   position: relative;
@@ -40,16 +45,13 @@ export const StyledNewProject = styled.div`
   flex-direction: column;
   justify-content: center;
   letter-spacing: -0.01em;
-  color: ${theme.colors.neutral[7]};
-  border: 2px dashed ${theme.colors.neutral[3]};
+  color: ${theme.colors.neutral[8]};
 
   &:hover {
     color: ${theme.colors.blue[8]};
     border-color: ${theme.colors.neutral[4]};
 
     ${StyledNewProjectIcon} {
-      color: white;
-
       &::before {
         transform: scale(1.1);
         background: ${theme.colors.blue[8]};
