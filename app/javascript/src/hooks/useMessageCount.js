1e281e3a308ffde19182ff1b2b48123c1c1a261b
Thomas Cullen <thomascullen92@gmail.com>
2020-01-30 18:44:48 +0000

    Fix bugs with TalkJS (#304)

--- app/javascript/src/hooks/useMessageCount.js
+++ app/javascript/src/hooks/useMessageCount.js
@@ -1,18 +1,23 @@
-import { useEffect, useState } from "react";
-import useTalkSession from "./useTalkSession";
+import Talk from "talkjs";
+import { useEffect, useRef, useState } from "react";
+import useViewer from "./useViewer";
+import createTalkSession from "../utilities/createTalkSession";
 
 const useMessageCount = () => {
+  const viewer = useViewer();
   const [messageCount, setMessageCount] = useState(0);
-  const talkSession = useTalkSession();
 
   const handleUnread = conversations => {
     setMessageCount(conversations.length);
   };
 
   useEffect(() => {
-    if (!talkSession) return;
-    talkSession.unreads.on("change", handleUnread);
-  }, [talkSession]);
+    if (!viewer) return;
+    Talk.ready.then(() => {
+      const session = createTalkSession(viewer);
+      session.unreads.on("change", handleUnread);
+    });
+  }, [viewer]);
 
   return messageCount;
 };
