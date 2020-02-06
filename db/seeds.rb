c998b39f16af80d10b512a203b4cb0c6346e403d
Thomas Cullen <thomascullen92@gmail.com>
2020-02-06 14:56:26 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-Airtable::Skill.sync_from_airtable
-Airtable::Industry.sync_from_airtable
-Airatble::ClientContact.sync_from_airtable
-Airtable::Specialist.sync_from_airtable
-Airtable::Project.sync_from_airtable
-Airtable::Application.sync_from_airtable
-Airtable::Task.sync_from_airtable
+Airtable::Skill.sync(filter: nil)
+Airtable::Industry.sync(filter: nil)
+Airtable::ClientContact.sync(filter: nil)
+Airtable::Specialist.sync(filter: nil)
+Airtable::Project.sync(filter: nil)
+Airtable::Application.sync(filter: nil)
+Airtable::Task.sync(filter: nil)
 
 User.first.update(password: 'testing123', confirmed_at: 2.hours.ago)
 Specialist.first.update(password: 'testing123', confirmed_at: 2.hours.ago)
