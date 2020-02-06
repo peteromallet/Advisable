a85843c0d8fd116b0b7d5e8a77dbc949676fa83e
Thomas Cullen <thomascullen92@gmail.com>
2020-02-06 15:08:12 +0000

    Add app.json for review apps (#317)
    * Add app.json for review apps
    
    * Update app.json
    
    * Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
-`rails data:industries`
+Airtable::Skill.sync(filter: nil)
+Airtable::Industry.sync(filter: nil)
+Airtable::ClientContact.sync(filter: nil)
+Airtable::Specialist.sync(filter: nil)
+Airtable::Project.sync(filter: nil)
+Airtable::Application.sync(filter: nil)
+Airtable::Task.sync(filter: nil)
+
+User.first.update(password: 'testing123', confirmed_at: 2.hours.ago)
+Specialist.first.update(password: 'testing123', confirmed_at: 2.hours.ago)
