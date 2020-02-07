f8ca2955930294de909b81470be6ec33a0f87755
Thomas Cullen <thomascullen92@gmail.com>
2020-02-07 09:40:10 +0000

    Render CLIENT_CONFIG as AdvisableConfig global

--- config/initializers/client_config.rb
+++ config/initializers/client_config.rb
@@ -1,3 +1,9 @@
+# The client config is rendered inside of application.html.erb inside a script tag as a way
+# to pass environment variables to the react app under the AdvisableConfig window object.
+# We do this instead of using the webpack environment variables plugin to keep the app
+# stateless and allow us to promote the app through the deploy pipeline without needing
+# to recompile the assets.
+
 CLIENT_CONFIG = {
   'ROLLBAR_ENV' => ENV['ROLLBAR_ENV'],
   'ROLLBAR_CLIENT_TOKEN' => ENV['ROLLBAR_CLIENT_TOKEN'],
