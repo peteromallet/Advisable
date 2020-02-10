5dd857627a0b9d61e7641c26cbc08e4a958544db
Thomas Cullen <thomascullen92@gmail.com>
2020-02-10 14:06:38 +0000

    Change consultation project owner to Sanah (#322)

--- app/graphql/mutations/accept_consultation.rb
+++ app/graphql/mutations/accept_consultation.rb
@@ -5,12 +5,15 @@ class Mutations::AcceptConsultation < Mutations::BaseMutation
 
   def resolve(**args)
     ActiveRecord::Base.transaction do
-      consultation = Consultation.find_by_uid_or_airtable_id!(args[:consultation])
+      consultation =
+        Consultation.find_by_uid_or_airtable_id!(args[:consultation])
 
       project = create_project(consultation)
       application = create_application(project, consultation.specialist)
       interview = create_interview(application)
-      consultation.update(interview: interview, status: "Accepted By Specialist")
+      consultation.update(
+        interview: interview, status: 'Accepted By Specialist'
+      )
       consultation.sync_to_airtable
       { interview: interview }
     end
@@ -20,35 +23,37 @@ class Mutations::AcceptConsultation < Mutations::BaseMutation
 
   def create_project(consultation)
     user = consultation.user
-    project = Project.create({
-      user: user,
-      skills: [consultation.skill],
-      sales_status: "Open",
-      status: "Project Created",
-      service_type: "Consultation",
-      primary_skill: consultation.skill.name,
-      owner: "peteromalley",
-      name: "#{user.company_name} - #{consultation.skill.name}"
-    })
+    project =
+      Project.create(
+        {
+          user: user,
+          skills: [consultation.skill],
+          sales_status: 'Open',
+          status: 'Project Created',
+          service_type: 'Consultation',
+          primary_skill: consultation.skill.name,
+          owner: ENV['CONSULTATION_PROJECT_OWNER'],
+          name: "#{user.company_name} - #{consultation.skill.name}"
+        }
+      )
     project.sync_to_airtable
     project
   end
 
   def create_application(project, specialist)
-    application = Application.create({
-      project: project,
-      status: "Applied",
-      specialist: specialist
-    })
-    application.sync_to_airtable({ "Source" => "consultation-request" })
+    application =
+      Application.create(
+        { project: project, status: 'Applied', specialist: specialist }
+      )
+    application.sync_to_airtable({ 'Source' => 'consultation-request' })
     application
   end
 
   def create_interview(application)
-    interview = application.interviews.create({
-      status: "Call Requested",
-      user: application.project.user,
-    })
+    interview =
+      application.interviews.create(
+        { status: 'Call Requested', user: application.project.user }
+      )
     interview.sync_to_airtable
     interview
   end
