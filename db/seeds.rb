56ab63813ebb76729d6ba63a8c56c61ca2b5081d
Thomas Cullen <thomascullen92@gmail.com>
2020-02-28 09:09:11 +0000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@ Airtable::Project.sync(filter: nil)
 Airtable::Application.sync(filter: nil)
 Airtable::Task.sync(filter: nil)
 Airtable::SalesPerson.sync(filter: nil)
+Airtable::PreviousProject.sync(filter: nil)
+Airtable::SpecialistReview.sync(filter: nil)
 
 User.first.update(password: 'testing123', confirmed_at: 2.hours.ago)
 Specialist.first.update(password: 'testing123', confirmed_at: 2.hours.ago)
