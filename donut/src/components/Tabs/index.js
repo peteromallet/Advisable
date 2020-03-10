00761d1faffec42b7e5eac7a69ba3f770f4ccbd9
Thomas Cullen <thomascullen92@gmail.com>
2020-03-10 11:09:37 +0000

    Remove @reach/tabs

--- donut/src/components/Tabs/index.js
+++ donut/src/components/Tabs/index.js
@@ -1,27 +1,37 @@
 import React from "react";
 import Icon from "../Icon";
-import { Tabs as TabStyles, TabList, Tab, TabPanels, TabPanel } from "./styles";
+import { useTabState } from "reakit/Tab";
+import { StyledTab, StyledTabList, StyledTabPanel } from "./styles";
 
-const Tabs = ({ tabListProps, children }) => {
+const Tabs = ({ label, initialState, tabListProps, children }) => {
+  const tab = useTabState({
+    selectedId: "tab-0",
+    ...initialState,
+  });
+
   return (
-    <TabStyles>
-      <TabList {...tabListProps}>
-        {React.Children.map(children, child => {
-          return (
-            <Tab>
-              {child.props.icon && <Icon icon={child.props.icon} height={16} />}
-              {child.props.title}
-            </Tab>
-          );
-        })}
-      </TabList>
-
-      <TabPanels>
-        {React.Children.map(children, child => {
-          return <TabPanel>{child.props.children}</TabPanel>;
-        })}
-      </TabPanels>
-    </TabStyles>
+    <>
+      <StyledTabList {...tabListProps} {...tab} aria-label={label}>
+        {React.Children.map(children, (child, i) => (
+          <StyledTab {...tab} stopId={`tab-${i}`}>
+            {child.props.icon && (
+              <Icon
+                mr="xs"
+                width={18}
+                strokeWidth={1.5}
+                icon={child.props.icon}
+              />
+            )}
+            {child.props.title}
+          </StyledTab>
+        ))}
+      </StyledTabList>
+      {React.Children.map(children, (child, i) => (
+        <StyledTabPanel {...tab} stopId={`tab-${i}`}>
+          {React.cloneElement(child)}
+        </StyledTabPanel>
+      ))}
+    </>
   );
 };
 
