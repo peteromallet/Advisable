27d08f0d5afcbcf8f61d982a7eba13ba8e0398df
Thomas Cullen <thomascullen92@gmail.com>
2020-03-24 09:08:52 +0000

    Update opp primary_skill structure

--- app/models/off_platform_project.rb
+++ app/models/off_platform_project.rb
@@ -1,13 +1,25 @@
+# An OffPlatformProject represents a previous project that a freelancer completed off of Advisable.
+# Currently we have the PreviousProject model to represent a previous project wether its on or
+# off platform. Eventually we will rename OffPlatformProject to 'PreviousProject' and create
+# records for on platform projects.
 class OffPlatformProject < ApplicationRecord
   include Uid
   include Airtable::Syncable
+
+  uid_prefix 'opp'
   belongs_to :specialist
   has_many :reviews, as: :project
+
+  # Project skills
   has_many :project_skills, as: :project
   has_many :skills, through: :project_skills
+  has_one :primary_project_skill,
+          -> { where primary: true },
+          as: :project, class_name: 'ProjectSkill'
+  has_one :primary_skill, through: :primary_project_skill, source: :skill
+
   has_many :project_industries, as: :project
   has_many :industries, through: :project_industries
-  uid_prefix 'opp'
 
   scope :validated, -> { where(validation_status: 'Validated') }
 
