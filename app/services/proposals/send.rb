686313f5725cb0d9ca4542a02ac7c0682a3db945
Thomas Cullen <thomascullen92@gmail.com>
2020-03-31 08:54:50 +0100

    Set project status to 'Proposal Recieved' when proposal is sent

--- app/services/proposals/send.rb
+++ app/services/proposals/send.rb
@@ -10,16 +10,24 @@ class Proposals::Send < ApplicationService
 
   def call
     application.proposal_comment = comment
-    application.status = "Proposed"
+    application.status = 'Proposed'
     if application.save
       application.sync_to_airtable
+      update_project(application.project)
       WebhookEvent.trigger(
-        "applications.proposal_sent",
+        'applications.proposal_sent',
         WebhookEvent::Application.data(application)
       )
       return application
     else
       raise Service::Error.new(application.errors.full_messages.first)
     end
+  end
+
+  private
+
+  def update_project(project)
+    project.update(status: 'Proposal Received')
+    project.sync_to_airtable
   end
 end
