2724102d2cdbfd6e4741d5b3232e666890a52b8f
Thomas Cullen <thomascullen92@gmail.com>
2020-05-05 07:33:37 +0100

    Use headless chrome

--- spec/support/system_tests.rb
+++ spec/support/system_tests.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    # driven_by :selenium, using: :headless_chrome
-    driven_by :selenium, using: :chrome
+    driven_by :selenium, using: :headless_chrome
+    # driven_by :selenium, using: :chrome
   end
 end
