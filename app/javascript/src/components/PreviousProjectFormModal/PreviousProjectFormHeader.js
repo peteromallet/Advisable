c17b589edfb5d1f53b777a4cdf1b68b18d79ad98
Thomas Cullen <thomascullen92@gmail.com>
2020-05-19 06:37:56 +0100

    Prompt project validation with modal once added (#400)

--- app/javascript/src/components/PreviousProjectFormModal/PreviousProjectFormHeader.js
+++ app/javascript/src/components/PreviousProjectFormModal/PreviousProjectFormHeader.js
@@ -1,25 +1,19 @@
 import React from "react";
-import { ChevronDown, X } from "@styled-icons/feather";
-import { Box, useBreakpoint } from "@advisable/donut";
+import { X } from "@styled-icons/feather";
+import { Box } from "@advisable/donut";
 import {
   StyledPreviousProjectFormHeader,
   StyledClosePreviousProjectFormButton,
 } from "./styles";
 
-export default function PreviousProjectFormHeader({ modal, data, children }) {
-  const mUp = useBreakpoint("mUp");
-
+export default function PreviousProjectFormHeader({ modal, children }) {
   return (
     <StyledPreviousProjectFormHeader>
       {children}
       <StyledClosePreviousProjectFormButton onClick={modal.hide}>
-        {mUp ? "Save and close" : "Close"}
+        Close
         <Box ml="xxs">
-          {mUp ? (
-            <ChevronDown size={24} strokeWidth={2} />
-          ) : (
-            <X size={24} strokeWidth={2} />
-          )}
+          <X size={24} strokeWidth={2} />
         </Box>
       </StyledClosePreviousProjectFormButton>
     </StyledPreviousProjectFormHeader>
