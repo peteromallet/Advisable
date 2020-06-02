0fef683fee3cf74e75d09627d13f2a178f111da3
Thomas Cullen <thomascullen92@gmail.com>
2020-06-02 07:50:56 +0100

    Fix messages view

--- app/javascript/src/components/DeclineApplicationButton/index.js
+++ app/javascript/src/components/DeclineApplicationButton/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@advisable/donut";
+import { Button, useModal } from "@advisable/donut";
 import RejectModal from "../RejectModal";
 import RequestIntroduction from "../RequestIntroduction";
 import { Trash } from "@styled-icons/feather";
@@ -7,19 +7,16 @@ import { Trash } from "@styled-icons/feather";
 // Reners a button which when clicked will open the modal to decline an
 // application.
 const DeclineApplicationButton = ({ application }) => {
+  const modal = useModal();
   const [isOpen, setOpen] = React.useState(false);
   const [requestIntro, setRequestIntro] = React.useState(false);
 
   return (
     <>
-      <RequestIntroduction
-        isOpen={requestIntro}
-        application={application}
-        onClose={() => setRequestIntro(false)}
-      />
+      <RequestIntroduction modal={modal} application={application} />
       <RejectModal
         isOpen={isOpen}
-        onRequestCall={() => setRequestIntro(true)}
+        onRequestCall={modal.toggle}
         application={application}
         onClose={() => setOpen(false)}
       />
