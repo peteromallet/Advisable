ff2d0979db8ec5d7ba4661e7a9e350f94c1c882c
Stas SUȘCOV <stas@nerd.ro>
2020-06-16 18:03:16 +0100

    DRY a bit mailer templates. Updated previews.

--- test/mailers/previews/specialist_mailer_preview.rb
+++ test/mailers/previews/specialist_mailer_preview.rb
@@ -1,6 +1,13 @@
 class SpecialistMailerPreview < ActionMailer::Preview
+  def confirm
+    SpecialistMailer.confirm(
+      uid: FactoryBot.create(:specialist).uid,
+      token: Faker::Internet.password
+    )
+  end
+
   def verify_project
-    project = PreviousProject.last
+    project = FactoryBot.create(:previous_project)
     SpecialistMailer.verify_project(project.uid)
   end
 end
