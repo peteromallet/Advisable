5e441f0881f3f518e7296150506aee3c3d6790c3
Thomas Cullen <thomascullen92@gmail.com>
2020-06-24 23:34:26 +0100

    Dont run airtable seed data when in test mode

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,21 @@
-Airtable::Skill.sync(filter: nil)
-Airtable::Industry.sync(filter: nil)
-Airtable::ClientContact.sync(filter: nil)
-Airtable::Specialist.sync(filter: nil)
-Airtable::Project.sync(filter: nil)
-Airtable::Application.sync(filter: nil)
-Airtable::Task.sync(filter: nil)
-Airtable::SalesPerson.sync(filter: nil)
-Airtable::SpecialistReview.sync(filter: nil)
+unless Rails.env.test?
+  Airtable::Skill.sync(filter: nil)
+  Airtable::Industry.sync(filter: nil)
+  Airtable::ClientContact.sync(filter: nil)
+  Airtable::Specialist.sync(filter: nil)
+  Airtable::Project.sync(filter: nil)
+  Airtable::Application.sync(filter: nil)
+  Airtable::Task.sync(filter: nil)
+  Airtable::SalesPerson.sync(filter: nil)
+  Airtable::SpecialistReview.sync(filter: nil)
 
-User.all.each do |user|
-  user.update(password: 'testing123', confirmed_at: 2.hours.ago)
-end
+  User.all.each do |user|
+    user.update(password: 'testing123', confirmed_at: 2.hours.ago)
+  end
 
-Specialist.all.each do |specialist|
-  specialist.update(
-    password: 'testing123', confirmed_at: 2.hours.ago, average_score: 85
-  )
+  Specialist.all.each do |specialist|
+    specialist.update(
+      password: 'testing123', confirmed_at: 2.hours.ago, average_score: 85
+    )
+  end
 end
