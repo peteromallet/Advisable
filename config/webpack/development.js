d3c23091f458fbd7bce978600169f2f839738110
Thomas Cullen <thomascullen92@gmail.com>
2020-07-07 20:57:34 +0100

    💨 Enable react fast refresh

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const environment = require("./environment");
 const WebpackBar = require("webpackbar");
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
@@ -15,6 +16,32 @@ environment.config.set("devServer", {
 });
 
 environment.plugins.append("webpackbar", new WebpackBar());
+
+if (process.env.REACT_REFRESH === "true") {
+  environment.plugins.append(
+    "Refresh",
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockPort: 3035,
+      },
+    }),
+  );
+
+  environment.loaders.append("fastrefresh", {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: require.resolve("babel-loader"),
+        options: {
+          // ... other options
+          // DO NOT apply the Babel plugin in production mode!
+          plugins: [require.resolve("react-refresh/babel")].filter(Boolean),
+        },
+      },
+    ],
+  });
+}
 
 module.exports = environment.toWebpackConfig();
 // module.exports = smp.wrap(environment.toWebpackConfig());
