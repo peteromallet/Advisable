c78becbf1c1562a604e0c2ea793e7b5677e1585c
Thomas Cullen <thomascullen92@gmail.com>
2020-07-15 08:41:05 +0100

    Update gems (#424)
    * Update rails
    
    * Update aws sdk
    
    * Update dev and test gems
    
    * Update pg gem
    
    * Upgrade sidekiq
    
    * Update listen
    
    * Update administrate
    
    * Update money gem

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,24 +6,25 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    http_basic_authenticate_with name: ENV.fetch("ADMIN_USERNAME"), password: ENV.fetch("ADMIN_PASSWORD")
+    http_basic_authenticate_with name: ENV.fetch('ADMIN_USERNAME'),
+                                 password: ENV.fetch('ADMIN_PASSWORD')
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
     #   params[:per_page] || 20
     # end
-    before_action :default_params
+    # before_action :default_params
 
-    def default_params
-      params[:order] ||= "created_at"
-      params[:direction] ||= "desc"
-    end
+    # def default_params
+    #   params[:order] ||= "created_at"
+    #   params[:direction] ||= "desc"
+    # end
 
     def resync
-      return unless ENV["STAGING"]
+      return unless ENV['STAGING']
       Airtable.sync
-      redirect_to "/admin", notice: "Airtable has been synced"
+      redirect_to '/admin', notice: 'Airtable has been synced'
     end
   end
 end
