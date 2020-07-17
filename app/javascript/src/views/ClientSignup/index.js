7541ce6b99f78a72474ae061948ba9877adc96e3
Yurko Turskiy <guandjoy@gmail.com>
2020-07-17 21:38:43 +0300

    Implement first version

--- app/javascript/src/views/ClientSignup/index.js
+++ app/javascript/src/views/ClientSignup/index.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import Criteria from "./Criteria";
-import SaveSearch from "./SaveSearch";
-import Specialists from "./Specialists";
+import { Switch, Redirect } from "react-router-dom";
 import useViewer from "../../hooks/useViewer";
+import useSteps from "./useSteps";
+import steps from "./Steps";
+import Testimonials from "./Testimonials";
+import { Box } from "@advisable/donut";
+import { Step } from "./styles";
+import Progress from "./Progress";
 
 function ClientSignup() {
+  const { routes, currentStepIndex } = useSteps(steps);
   const viewer = useViewer();
 
   if (viewer) {
     return <Redirect to="/" />;
   }
 
+  const currentStepNumber = currentStepIndex();
+
   return (
-    <Switch>
-      <Route path="/clients/signup/specialists" component={Specialists} />
-      <Route path="/clients/signup/save" component={SaveSearch} />
-      <Route path="/clients/signup" component={Criteria} />
-    </Switch>
+    <>
+      <Testimonials />
+      <Box paddingRight={{ _: null, l: 550 }}>
+        <Box py="xxl" maxWidth={600} margin="0 auto" px="m">
+          <React.Fragment>
+            <Step>
+              Step {currentStepNumber} of {steps.length - 1}
+            </Step>
+            <Progress amount={(currentStepNumber / steps.length) * 100} />
+          </React.Fragment>
+          <Switch>{routes}</Switch>
+        </Box>
+      </Box>
+    </>
   );
 }
 
