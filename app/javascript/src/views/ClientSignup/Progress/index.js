c3e2b832d79d7c19dba9bf75525653e0664e5186
Yurko Turskiy <guandjoy@gmail.com>
2020-07-17 21:34:31 +0300

    Paste Progress component

---
+++ app/javascript/src/views/ClientSignup/Progress/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const Bar = styled.div`
+  height: 3px;
+  margin: 20px 0;
+  border-radius: 2px;
+  background: rgba(183, 189, 213, 0.34);
+`;
+
+const Inner = styled(motion.div)`
+  height: 100%;
+  background: #17cda1;
+`;
+
+export default ({ amount }) => {
+  return (
+    <Bar>
+      <Inner
+        initial={{ width: `${amount}%` }}
+        animate={{ width: `${amount}%` }}
+      />
+    </Bar>
+  );
+};
