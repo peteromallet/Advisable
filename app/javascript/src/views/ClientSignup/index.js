e85c6460f559718578cc044b39d38b9eeea280f1
Yurko Turskiy <guandjoy@gmail.com>
2020-07-17 21:38:43 +0300

    Implement first version

--- app/javascript/src/views/ClientSignup/index.js
+++ app/javascript/src/views/ClientSignup/index.js
@@ -29,6 +29,8 @@ function ClientSignup() {
   const numberOfSteps = numberOfActiveSteps - 1;
   const progressLength = (currentStepNumber / numberOfSteps) * 100;
 
+  const currentStepNumber = currentStepIndex();
+
   return (
     <Box display="flex" flexDirection="row">
       {isDesktop && <Testimonials />}
