f6815c9dbc54807826f14b22cd64187d838596a3
Yurko Turskiy <guandjoy@gmail.com>
2020-07-17 13:24:32 +0300

    Copy Testimonials folder to root

---
+++ app/javascript/src/views/ClientSignup/Testimonials/data.js
@@ -0,0 +1,50 @@
+import prioWealth from "./prioWealth.png";
+import travelPerk from "./travelperk.png";
+import practisewise from "./practisewise.png";
+import protectLine from "./protectline.png";
+import babbel from "./babbel.png";
+
+const TESTIMONIALS = [
+  {
+    name: "Gideon Pridor",
+    role: "VP of Marketing",
+    company: "TravelPerk",
+    content:
+      "Advisable found us the precise freelancer we needed, almost over night.",
+    logo: travelPerk,
+  },
+  {
+    name: "Bryan J. Stewart",
+    role: "CEO",
+    company: "PracticeWise",
+    content:
+      "A great partner and source for professional, highly qualified freelance talent.",
+    logo: practisewise,
+  },
+  {
+    name: "Serena Li",
+    role: "Senior Research Manager",
+    company: "Babbel",
+    content:
+      "Advisable helped us identify great freelancers multiple times. Would definitely recommend!",
+    logo: babbel,
+  },
+  {
+    name: "Yolanda Taylor",
+    role: "Managing Director",
+    company: "Prio Wealth",
+    content:
+      "We worked with an Advisable freelancer for social media help and he was excellent.",
+    logo: prioWealth,
+  },
+  {
+    name: "Chris Reed",
+    role: "Head Of Business Development",
+    company: "Protect Line",
+    content:
+      "After a simple selection process we have found a perfect contractor.",
+    logo: protectLine,
+  },
+];
+
+export default TESTIMONIALS;
