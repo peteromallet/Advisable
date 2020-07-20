3ef2cea8abdf0a237d0eb827f4387becd3ac8e87
Thomas Cullen <thomascullen92@gmail.com>
2020-07-20 16:04:50 +0100

    Add new client application slack notification

---
+++ app/jobs/client_application_submitted_notification_job.rb
@@ -0,0 +1,79 @@
+class ClientApplicationSubmittedNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(id)
+    user = User.find(id)
+    return unless user.present?
+
+    Slack.message(
+      channel: 'new_client_application',
+      text: 'New ASAP call request',
+      blocks: [
+        {
+          "type": 'section',
+          "text": {
+            "type": 'mrkdwn',
+            "text":
+              "#{user.first_name} from #{
+                user.company_name
+              } has submitted their application and has been *#{
+                signup_result(user)
+              }*."
+          }
+        },
+        { "type": 'divider' },
+        {
+          "type": 'section',
+          "fields": [
+            {
+              "type": 'mrkdwn',
+              "text":
+                "*Company Details*\nName: `#{user.company_name}`\nIndustry: `#{
+                  user.industry.try(:name)
+                }`\nCompany Type: `#{user.company_type}`"
+            },
+            {
+              "type": 'mrkdwn',
+              "text":
+                "*Contact Details*\nName: `#{user.name}`\nEmail: `#{
+                  user.email
+                }`"
+            }
+          ]
+        },
+        {
+          "type": 'section',
+          "text": {
+            "type": 'mrkdwn',
+            "text":
+              "*Requirements*\nHow many freelancers over next six months: *#{
+                user.number_of_freelancers
+              }*\nSkills interested in: *#{
+                user.skills.map(&:name).join(',')
+              }*\nSpend on freelancers per annum:*#{
+                user.budget / 100.0
+              }*\nLevel of talent they're looking for: *#{user.talent_quality}*"
+          }
+        },
+        {
+          "type": 'section',
+          "text": {
+            "type": 'mrkdwn',
+            "text":
+              '*<https://airtable.com/tblKMhI3m3iBlA8Dg/viw5RTNS9NixGW3x8?blocks=hide|View Application Details>*'
+          }
+        }
+      ]
+    )
+  end
+
+  private
+
+  def signup_result(user)
+    if user.rejection_reason == 'not_hiring'
+      return 'offered to setup a 6 month reminder'
+    end
+    return 'sent to upwork' if user.rejection_reason == 'cheap_talent'
+    'invited to schedule a call'
+  end
+end
