1d4a94b939c9b0a7ecde5de253acda91042f5385
Yurko Turskiy <guandjoy@gmail.com>
2020-07-20 07:02:00 +0300

    Refactor fetching and usage of industries

--- app/javascript/src/views/ClientSignup/queries.js
+++ app/javascript/src/views/ClientSignup/queries.js
@@ -71,9 +71,8 @@ export const ABOUT_COMPANY_QUERY = gql`
   ${clientApplicationFragment}
   query ClientApplication($id: ID!) {
     industries {
-      id
-      name
-      color
+      label: name
+      value: name
     }
     clientApplication(id: $id) {
       ...Application
