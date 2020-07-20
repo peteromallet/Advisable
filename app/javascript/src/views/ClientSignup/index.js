958b870e68c4614c7a2eb30c7a04de70a643246a
Yurko Turskiy <guandjoy@gmail.com>
2020-07-20 06:33:57 +0300

    Fix Progress line and refactor root component

--- app/javascript/src/views/ClientSignup/index.js
+++ app/javascript/src/views/ClientSignup/index.js
@@ -12,9 +12,7 @@ function ClientSignup() {
   const { routes, currentStepIndex } = useSteps(steps);
   const viewer = useViewer();
 
-  if (viewer) {
-    return <Redirect to="/" />;
-  }
+  if (viewer) return <Redirect to="/" />;
 
   const currentStepNumber = currentStepIndex();
 
@@ -23,12 +21,10 @@ function ClientSignup() {
       <Testimonials />
       <Box paddingRight={{ _: null, l: 550 }}>
         <Box py="xxl" maxWidth={600} margin="0 auto" px="m">
-          <React.Fragment>
-            <Step>
-              Step {currentStepNumber} of {steps.length - 1}
-            </Step>
-            <Progress amount={(currentStepNumber / steps.length) * 100} />
-          </React.Fragment>
+          <Step>
+            Step {currentStepNumber} of {steps.length - 1}
+          </Step>
+          <Progress amount={(currentStepNumber / steps.length - 1) * 100} />
           <Switch>{routes}</Switch>
         </Box>
       </Box>
