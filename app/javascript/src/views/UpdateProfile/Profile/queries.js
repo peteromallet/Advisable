b3d74c129a9cbcf67f6b3072656b07062aa11c7b
Thomas Cullen <thomascullen92@gmail.com>
2020-07-20 15:09:20 +0100

    Update profile queries to update cached data

---
+++ app/javascript/src/views/UpdateProfile/Profile/queries.js
@@ -0,0 +1,42 @@
+import gql from "graphql-tag";
+
+const fields = gql`
+  fragment SpecialistFields on Specialist {
+    id
+    bio
+    name
+    avatar
+    skills {
+      name
+    }
+    hourlyRate
+    publicUse
+  }
+`;
+
+export const GET_DATA = gql`
+  ${fields}
+
+  query getData {
+    skills(local: true) {
+      value: name
+      label: name
+    }
+    viewer {
+      ... on Specialist {
+        ...SpecialistFields
+      }
+    }
+  }
+`;
+
+export const UPDATE_PROFILE = gql`
+  ${fields}
+  mutation UpdateProfile($input: UpdateProfileInput!) {
+    updateProfile(input: $input) {
+      specialist {
+        ...SpecialistFields
+      }
+    }
+  }
+`;
