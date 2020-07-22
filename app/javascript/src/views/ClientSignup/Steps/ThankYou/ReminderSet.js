ec90b78e9a353c567a8e2ec10d7346385e33bd38
Yurko Turskiy <guandjoy@gmail.com>
2020-07-22 12:59:58 +0300

    Init ThankYou components

---
+++ app/javascript/src/views/ClientSignup/Steps/ThankYou/ReminderSet.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "@advisable/donut";
+
+function ThankYouReminderSet() {
+  return (
+    <>
+      <Text>Reminder Set</Text>
+      <Text>We&apos;ll follow up in six months</Text>
+    </>
+  );
+}
+
+export default ThankYouReminderSet;
