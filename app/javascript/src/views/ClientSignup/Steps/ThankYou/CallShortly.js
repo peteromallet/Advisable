f7e36da30d1bd9f1a52422d09c82026da08f757d
Yurko Turskiy <guandjoy@gmail.com>
2020-07-22 12:59:58 +0300

    Init ThankYou components

---
+++ app/javascript/src/views/ClientSignup/Steps/ThankYou/CallShortly.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "@advisable/donut";
+
+function ThankYouCallShortly() {
+  return (
+    <>
+      <Text>Your call is booked</Text>
+      <Text>We look forward to speaking!</Text>
+    </>
+  );
+}
+
+export default ThankYouCallShortly;
