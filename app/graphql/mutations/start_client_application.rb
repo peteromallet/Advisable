f9af889eb07dd9c01cbadb4c5149f6f8caabef28
Thomas Cullen <thomascullen92@gmail.com>
2020-07-23 13:43:40 +0100

    Prevent using taken emails

--- app/graphql/mutations/start_client_application.rb
+++ app/graphql/mutations/start_client_application.rb
@@ -6,10 +6,19 @@ class Mutations::StartClientApplication < Mutations::BaseMutation
   field :clientApplication, Types::ClientApplicationType, null: true
 
   def resolve(**args)
+    email_blacklisted?(args[:email])
+    check_existing_specialist_account(args[:email])
     user =
       User.find_or_initialize_by(email: args[:email]) do |u|
         u.application_status = :started
       end
+
+    if user.has_account?
+      ApiError.invalidRequest(
+        'existingAccount',
+        'An account already exists with this email'
+      )
+    end
 
     if user.application_status == :started
       user.first_name = args[:first_name]
@@ -19,5 +28,20 @@ class Mutations::StartClientApplication < Mutations::BaseMutation
     end
 
     { clientApplication: user }
+  end
+
+  private
+
+  def email_blacklisted?(email)
+    return if BlacklistedDomain.email_allowed?(email)
+    ApiError.invalidRequest('emailNotAllowed', 'This email is not allowed')
+  end
+
+  def check_existing_specialist_account(email)
+    return unless Specialist.exists?(email: email)
+    ApiError.invalidRequest(
+      'existingAccount',
+      'This email belongs to a specialist account'
+    )
   end
 end
