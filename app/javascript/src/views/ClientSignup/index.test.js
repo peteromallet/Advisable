6a923c6b69f0a109aa70f776e94c5647ae13e747
Yurko Turskiy <guandjoy@gmail.com>
2020-07-28 15:01:39 +0300

    Implement test case 'Successful client application flow...'

--- app/javascript/src/views/ClientSignup/index.test.js
+++ app/javascript/src/views/ClientSignup/index.test.js
@@ -8,132 +8,244 @@ import {
   mockQuery,
   mockMutation,
   waitFor,
+  waitForElementToBeRemoved,
 } from "../../testHelpers/test-utils";
-import renderApp from "../../testHelpers/renderApp";
-import { fireEvient } from "@testing-library/react";
-
-import generateTypes from "../../__mocks__/graphqlFields";
 
 import {
   ABOUT_COMPANY_QUERY,
   GET_CLIENT_APPLICATION,
   START_CLIENT_APPLICATION,
+  ABOUT_COMPANY_UPDATE,
+  ABOUT_REQUIREMENTS_QUERY,
+  ABOUT_REQUIREMENTS_UPDATE,
+  SUBMIT_CLIENT_APPLICATION,
+  REQUEST_APPLICATION_CALLBACK,
 } from "./queries";
-import updateApplication from "../ApplicationFlow/updateApplication";
-import getApplication from "../ApplicationFlow/fetchApplication";
 
-test("Successful application flow", async () => {
-  const skill = generateTypes.skill();
-  const industry = generateTypes.industry();
+test("Successful client application flow and ASAP call", async () => {
+  // Mock data
+  const skill = mockData.skill();
+  const industry = mockData.industry();
+  const clientApplication = mockData.clientApplication();
+  const email = "test@test.com";
+  const companyName = "Test Corp";
+  const companyType = "Startup";
+  const numberOfFreelancers = "1-3";
+  const budget = "10000";
+  const localityImportance = 3;
+  const acceptedGuaranteeTerms = false;
+  const talentQuality = "GOOD";
+  const phoneNumber = "+380(93)555-66-99";
+
   const graphQLMocks = [
     mockViewer(null),
     mockQuery(
       ABOUT_COMPANY_QUERY,
-      { id: "app_id_001" },
+      { id: clientApplication.id },
       {
         industries: [
           {
-            ...industry,
+            __typename: "Industry",
             label: industry.name,
             value: industry.name,
           },
         ],
         skills: [
           {
-            ...skill,
+            __typename: "Skill",
+            label: skill.name,
+            value: skill.name,
+          },
+        ],
+        clientApplication,
+      },
+    ),
+    mockQuery(
+      GET_CLIENT_APPLICATION,
+      { id: clientApplication.id },
+      { clientApplication },
+    ),
+    mockQuery(
+      ABOUT_REQUIREMENTS_QUERY,
+      { id: clientApplication.id },
+      {
+        skills: [
+          {
+            __typename: "Skill",
             label: skill.name,
             value: skill.name,
           },
         ],
         clientApplication: {
-          id: "app_id_001",
-          firstName: "Yurko",
-          lastName: "Turskiy",
-          companyName: null,
-          industry: null,
-          companyType: null,
-          skills: [],
-          numberOfFreelancers: null,
-          budget: null,
-          localityImportance: null,
-          acceptedGuaranteeTerms: false,
-          talentQuality: null,
-          status: "STARTED",
-          rejectionReason: null,
-        },
-      },
-    ),
-    mockQuery(
-      GET_CLIENT_APPLICATION,
-      { id: "app_id_001" },
-      {
-        clientApplication: {
-          id: "app_id_001",
-          firstName: "Yurko",
-          lastName: "Turskiy",
-          companyName: null,
-          industry: null,
-          companyType: null,
-          skills: [],
-          numberOfFreelancers: null,
-          budget: null,
-          localityImportance: null,
-          acceptedGuaranteeTerms: false,
-          talentQuality: null,
-          status: "STARTED",
-          rejectionReason: null,
+          ...clientApplication,
+          skills: [{ __typename: "Skill", name: skill.name }],
+          numberOfFreelancers,
+          budget: Number(budget) * 100,
         },
       },
     ),
     mockMutation(
       START_CLIENT_APPLICATION,
       {
-        firstName: "Yurko",
-        lastName: "Turskiy",
-        email: "yurko.turskiy@advisable.com",
+        firstName: clientApplication.firstName,
+        lastName: clientApplication.lastName,
+        email,
       },
       {
         startClientApplication: {
           __typename: "StartClientApplicationPayload",
+          clientApplication,
+        },
+      },
+    ),
+    mockMutation(
+      ABOUT_COMPANY_UPDATE,
+      {
+        id: clientApplication.id,
+        companyName,
+        industry: industry.name,
+        companyType,
+      },
+      {
+        updateClientApplication: {
+          __typename: "UpdateClientApplicationPayload",
           clientApplication: {
             __typename: "ClientApplication",
-            id: "app_id_001",
-            firstName: "Yurko",
-            lastName: "Turskiy",
-            companyName: null,
-            industry: null,
-            companyType: null,
-            skills: [],
-            numberOfFreelancers: null,
-            budget: null,
-            localityImportance: null,
-            acceptedGuaranteeTerms: false,
-            talentQuality: null,
-            status: "STARTED",
+            id: clientApplication.id,
+            companyName,
+            industry: {
+              __typename: "Industry",
+              name: industry.name,
+            },
+            companyType,
+          },
+        },
+      },
+    ),
+    mockMutation(
+      ABOUT_REQUIREMENTS_UPDATE,
+      {
+        id: clientApplication.id,
+        skills: [skill.name],
+        numberOfFreelancers,
+        budget: Number(budget) * 100,
+      },
+      {
+        updateClientApplication: {
+          __typename: "UpdateClientApplicationPayload",
+          clientApplication: {
+            __typename: "ClientApplication",
+            id: clientApplication.id,
+            skills: [{ __typename: "Skill", name: skill.name }],
+            numberOfFreelancers,
+            budget,
+          },
+        },
+      },
+    ),
+    mockMutation(
+      SUBMIT_CLIENT_APPLICATION,
+      {
+        id: clientApplication.id,
+        localityImportance,
+        acceptedGuaranteeTerms,
+        talentQuality,
+      },
+      {
+        submitClientApplication: {
+          __typename: "SubmitClientApplicationPayload",
+          clientApplication: {
+            __typename: "ClientApplication",
+            id: clientApplication.id,
+            localityImportance,
+            acceptedGuaranteeTerms,
+            talentQuality,
+            status: "ACCEPTED",
             rejectionReason: null,
+          },
+        },
+      },
+    ),
+    mockMutation(
+      REQUEST_APPLICATION_CALLBACK,
+      {
+        id: clientApplication.id,
+        phoneNumber,
+      },
+      {
+        requestApplicationCallback: {
+          __typename: "RequestApplicationCallbackPayload",
+          clientApplication: {
+            __typename: "ClientApplication",
+            id: clientApplication.id,
           },
         },
       },
     ),
   ];
 
-  const app = renderApp({
+  renderRoute({
     route: "/clients/signup",
     graphQLMocks,
   });
 
-  const firstNameInput = await screen.findByPlaceholderText("First Name");
-  fireEvent.change(firstNameInput, { target: { value: "Yurko" } });
-  const lastNameInput = await screen.findByPlaceholderText("Last Name");
-  fireEvent.change(lastNameInput, { target: { value: "Turskiy" } });
+  // 0 Step. Start application
+  fireEvent.change(await screen.findByPlaceholderText(/First Name/i), {
+    target: { value: clientApplication.firstName },
+  });
+  fireEvent.change(await screen.findByPlaceholderText(/Last Name/i), {
+    target: { value: clientApplication.lastName },
+  });
   const emailInput = await screen.findByPlaceholderText(
     "ospencer@umbrellacorp.com",
   );
-  fireEvent.change(emailInput, {
-    target: { value: "yurko.turskiy@advisable.com" },
+  fireEvent.change(emailInput, { target: { value: email } });
+  fireEvent.click(screen.getByLabelText("Continue"));
+
+  // 1 Step. About Your Company
+  await screen.findByText("About Your Company");
+  fireEvent.change(screen.getByPlaceholderText("Umbrella Corporation"), {
+    target: { value: companyName },
   });
-  // screen.debug();
-  const btn = await screen.findByLabelText("Continue");
-  fireEvent.click(btn);
-  await app.findByText("About Your Company");
+  const industryInput = await screen.findByPlaceholderText("Biotechnology");
+  fireEvent.click(industryInput);
+  fireEvent.keyDown(industryInput, { key: "ArrowDown" });
+  fireEvent.keyDown(industryInput, { key: "Enter" });
+  fireEvent.change(screen.getByTestId("companyType"), {
+    target: { value: companyType },
+  });
+  fireEvent.click(screen.getByLabelText("Continue"));
+
+  // 2 Step. About Your Requirements
+  await screen.findByText("About Your Requirements");
+  const skillsInput = await screen.findByPlaceholderText(
+    /Facebook Ads/i,
+    {},
+    { timeout: 5000 },
+  );
+  fireEvent.click(skillsInput);
+  fireEvent.keyDown(skillsInput, { key: "ArrowDown" });
+  fireEvent.keyDown(skillsInput, { key: "Enter" });
+  fireEvent.change(screen.getByTestId("numberOfFreelancers"), {
+    target: { value: numberOfFreelancers },
+  });
+  fireEvent.change(screen.getByTestId("budget"), { target: { value: budget } });
+  fireEvent.click(screen.getByLabelText("Continue"));
+
+  // 3 Step. About Your Preferences
+  await screen.findByText("About Your Preferences");
+  fireEvent.click(screen.getByLabelText("3"));
+  fireEvent.click(screen.getByLabelText(/No/i));
+  fireEvent.click(screen.getByLabelText(/good/i));
+  fireEvent.click(screen.getByLabelText("Continue"));
+
+  // 4 Step. Let's get started
+  await screen.findByText(/Let’s get started/i);
+  fireEvent.click(screen.getByLabelText(/call me/i));
+  fireEvent.change(await screen.getByPlaceholderText(/contact number/i), {
+    target: { value: phoneNumber },
+  });
+  const submitBtn = screen.getByLabelText(/submit/i);
+  fireEvent.click(submitBtn);
+  await screen.findByText("Your call is booked");
 });
