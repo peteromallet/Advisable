c9842207adcaa23a2638cf9897cb3c5f722b7deb
Yurko Turskiy <guandjoy@gmail.com>
2020-07-29 23:47:47 +0300

    Refactor ThankYou component

--- app/javascript/src/views/ClientSignup/Steps/ThankYou/ReminderSet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ThankYou from ".";
-
-function ThankYouReminderSet() {
-  return <ThankYou title="Reminder Set" text="We'll follow up in six months" />;
-}
-
-export default ThankYouReminderSet;
