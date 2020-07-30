77abf1ac83817c4efe8b28116b7a0d4f3d8dce59
Thomas Cullen <thomascullen92@gmail.com>
2020-07-30 09:52:44 +0100

    Improve documentation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,12 +2,12 @@ class Address
   attr_accessor :line1, :line2, :city, :state, :country, :postcode
 
   def initialize(address)
-    @line1 = address["line1"]
-    @line2 = address["line2"]
-    @city = address["city"]
-    @state = address["state"]
-    @country = address["country"]
-    @postcode = address["postcode"]
+    @line1 = address['line1']
+    @line2 = address['line2']
+    @city = address['city']
+    @state = address['state']
+    @country = address['country']
+    @postcode = address['postcode']
   end
 
   def to_s
@@ -22,26 +22,28 @@ class Address
 
   def to_h
     {
-      "line1" => line1,
-      "line2" => line2,
-      "city" => city,
-      "state" => state,
-      "country" => country,
-      "postcode" => postcode
+      'line1' => line1,
+      'line2' => line2,
+      'city' => city,
+      'state' => state,
+      'country' => country,
+      'postcode' => postcode
     }
   end
 
   def self.parse(address)
-    parts = address.split(',').map { |s| s.squish }
+    parts = address.split(',').map(&:squish)
     has_line_2 = parts.length == 6
 
-    new({
-      "line1" => parts[0],
-      "line2" => has_line_2 ? parts[1] : nil,
-      "city" => parts[has_line_2 ? 2 : 1],
-      "state" => parts[has_line_2 ? 3 : 2],
-      "country" => parts[has_line_2 ? 4 : 3],
-      "postcode" => parts[has_line_2 ? 5 : 4],
-    })
+    new(
+      {
+        'line1' => parts[0],
+        'line2' => has_line_2 ? parts[1] : nil,
+        'city' => parts[has_line_2 ? 2 : 1],
+        'state' => parts[has_line_2 ? 3 : 2],
+        'country' => parts[has_line_2 ? 4 : 3],
+        'postcode' => parts[has_line_2 ? 5 : 4]
+      }
+    )
   end
 end
