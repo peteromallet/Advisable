77c70b7dcbc5f8e3a0e680da600501f731cce733
Yurko Turskiy <guandjoy@gmail.com>
2020-07-31 12:47:16 +0300

    Implement 5 second processing delay

---
+++ app/javascript/src/views/ClientSignup/Steps/AboutPreferences/ProcessingApplication/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Text } from "@advisable/donut";
+import ProcessingApplicationWrapper from "./styles";
+import Loading from "../../../../../components/Loading";
+import { motion } from "framer-motion";
+
+function ProcessingApplication() {
+  return (
+    <ProcessingApplicationWrapper
+      as={motion.div}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      position="absolute"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+      height="100%"
+      zIndex="2"
+    >
+      <Box mt="-35%">
+        <Loading />
+        <Text mt="-30%" fontSize="xs" color="neutral400">
+          Processing your application
+        </Text>
+      </Box>
+    </ProcessingApplicationWrapper>
+  );
+}
+
+export default ProcessingApplication;
