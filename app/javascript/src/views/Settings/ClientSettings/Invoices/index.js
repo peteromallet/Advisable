04f82cf98fd711401414adf1456b6df25985ac04
Yurko Turskiy <guandjoy@gmail.com>
2020-08-13 18:38:52 +0300

    Update Invoices component

--- app/javascript/src/views/Settings/ClientSettings/Invoices/index.js
+++ app/javascript/src/views/Settings/ClientSettings/Invoices/index.js
@@ -1,35 +1,13 @@
 import React from "react";
-import Loading from "../../../../components/Loading";
-import PropTypes from "prop-types";
-import { Box, Text, Card, Stack, Link as DonutLink } from "@advisable/donut";
-import { useInvoices } from "./queries";
-import styled from "styled-components";
-import { variant } from "styled-system";
-import { Link } from "react-router-dom";
 import { DateTime } from "luxon";
-
-export const Status = styled.div`
-  display: inline;
-  padding: 6px 11px 4px;
-  border-radius: 18px;
-  font-size: 14px;
-  ${variant({
-    variants: {
-      open: { bg: "yellow100", color: "yellow800" },
-      due: { bg: "neutral50", color: "neutral500" },
-      paid: { bg: "cyan100", color: "cyan800" },
-    },
-  })};
-`;
-
-Status.defaultProps = {
-  variant: "open",
-};
-
-const capitalize = (s) => {
-  if (typeof s !== "string") return "";
-  return s.charAt(0).toUpperCase() + s.slice(1);
-};
+import { Link } from "react-router-dom";
+// Components
+import { Box, Text, Card, Stack } from "@advisable/donut";
+import NotFound from "../../../NotFound";
+import { Badge } from "./styles";
+import Loading from "./Loading";
+// Queries
+import { useInvoices, GET_INVOICE } from "./queries";
 
 const createTimeFormatter = (timezone) => (iso) =>
   DateTime.fromISO(iso).setZone(timezone).toFormat("d MMMM yyyy");
@@ -37,14 +15,24 @@ const createTimeFormatter = (timezone) => (iso) =>
 function Invoices() {
   const timezone = Intl.DateTimeFormat()?.resolvedOptions()?.timeZone;
   const formatTime = createTimeFormatter(timezone);
-  const { loading, error, data } = useInvoices();
+  const { loading, error, data, client } = useInvoices();
 
   if (loading) return <Loading />;
+  if (error) return <NotFound />;
 
   const invoices = data?.viewer?.invoices.map((i) => {
     return (
       <Link key={i.id} to={`/settings/invoices/${i.id}`}>
-        <Card p="24px" pr="26px">
+        <Card
+          p="24px"
+          pr="26px"
+          onMouseOver={() =>
+            client.query({
+              query: GET_INVOICE,
+              variables: { id: i.id },
+            })
+          }
+        >
           <Box
             display="flex"
             flexDirection="row"
@@ -52,13 +40,25 @@ function Invoices() {
             alignItems="center"
           >
             <Box mr="auto">
-              <Text color="blue900" fontSize="17px" mb="xxs">
+              <Text
+                color="blue900"
+                fontSize="17px"
+                mb="xxs"
+                fontWeight="medium"
+              >
                 {formatTime(i.createdAt)}
               </Text>
-              <Text color="neutral500">#{i.number}</Text>
+              <Text color="neutral500" fontWeight="medium" fontSize="xs">
+                #{i.number}
+              </Text>
             </Box>
-            <Status variant={i.status}>{capitalize(i.status)}</Status>
-            <Text ml="xs" fontSize="17px" color="neutral900">
+            <Badge variant={i.status}>{i.status}</Badge>
+            <Text
+              ml={["xs", "s"]}
+              fontSize="17px"
+              color="neutral900"
+              fontWeight="medium"
+            >
               ${i.amount / 100}
             </Text>
           </Box>
@@ -69,7 +69,7 @@ function Invoices() {
 
   return (
     <Box>
-      <Text as="h2" fontSize="xl" color="blue900" mb="m">
+      <Text as="h2" fontSize="xl" color="blue900" mb="m" fontWeight="medium">
         Invoices
       </Text>
       <Stack spacing="xs">{invoices}</Stack>
