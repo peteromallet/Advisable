16b9920576bffcd7d0d4c8db1e989eeed9c97545
Thomas Cullen <thomascullen92@gmail.com>
2020-08-14 10:11:56 +0100

    Set project sales state to "Open"

--- app/graphql/mutations/publish_project.rb
+++ app/graphql/mutations/publish_project.rb
@@ -24,6 +24,7 @@ class Mutations::PublishProject < Mutations::BaseMutation
   def resolve(id:)
     project = Project.find_by_uid_or_airtable_id!(id)
     project.status = :pending_review
+    project.sales_status = 'Open'
     project.sync_to_airtable if project.save
     { project: project }
   end
