d2b4935ad840f974a9bdc769d974c24fd821d642
Yurko Turskiy <guandjoy@gmail.com>
2020-08-14 22:06:43 +0300

    Add hover state for invoice list

--- app/javascript/src/views/Settings/ClientSettings/Invoices/index.js
+++ app/javascript/src/views/Settings/ClientSettings/Invoices/index.js
@@ -2,15 +2,28 @@ import React from "react";
 import { DateTime } from "luxon";
 import { Link } from "react-router-dom";
 // Components
-import { Box, Text, Card, Stack } from "@advisable/donut";
+import { Box, Text, Stack, theme, StyledCard } from "@advisable/donut";
 import NotFound from "../../../NotFound";
 import { Badge } from "./styles";
 import Loading from "./Loading";
 // Queries
 import { useInvoices, GET_INVOICE } from "./queries";
+import styled from "styled-components";
+import { rgba } from "polished";
 
 const createTimeFormatter = (timezone) => (iso) =>
   DateTime.fromISO(iso).setZone(timezone).toFormat("d MMMM yyyy");
+
+const StyledInvoiceCard = styled(StyledCard)`
+  transition: box-shadow 0.3s, transform 0.2s;
+  box-shadow: 0px 2px 10px -4px ${rgba(theme.colors.blue900, 0.12)};
+  padding: 24px 26px 24px 24px;
+
+  &:hover {
+    transform: translateY(-1px);
+    box-shadow: 0px 2px 13px -3px ${rgba(theme.colors.blue900, 0.24)};
+  }
+`;
 
 function Invoices() {
   const timezone = Intl.DateTimeFormat()?.resolvedOptions()?.timeZone;
@@ -22,48 +35,42 @@ function Invoices() {
 
   const invoices = data?.viewer?.invoices.map((i) => {
     return (
-      <Link key={i.id} to={`/settings/invoices/${i.id}`}>
-        <Card
-          p="24px"
-          pr="26px"
-          onMouseOver={() =>
-            client.query({
-              query: GET_INVOICE,
-              variables: { id: i.id },
-            })
-          }
+      <StyledInvoiceCard
+        as={Link}
+        key={i.id}
+        to={`/settings/invoices/${i.id}`}
+        onMouseOver={() =>
+          client.query({
+            query: GET_INVOICE,
+            variables: { id: i.id },
+          })
+        }
+      >
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="flex-end"
+          alignItems="center"
         >
-          <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="flex-end"
-            alignItems="center"
-          >
-            <Box mr="auto">
-              <Text
-                color="blue900"
-                fontSize="17px"
-                mb="xxs"
-                fontWeight="medium"
-              >
-                {formatTime(i.createdAt)}
-              </Text>
-              <Text color="neutral500" fontWeight="medium" fontSize="xs">
-                #{i.number}
-              </Text>
-            </Box>
-            <Badge variant={i.status}>{i.status}</Badge>
-            <Text
-              ml={["xs", "s"]}
-              fontSize="17px"
-              color="neutral900"
-              fontWeight="medium"
-            >
-              ${i.amount / 100}
+          <Box mr="auto">
+            <Text color="blue900" fontSize="17px" mb="xxs" fontWeight="medium">
+              {formatTime(i.createdAt)}
+            </Text>
+            <Text color="neutral500" fontWeight="medium" fontSize="xs">
+              #{i.number}
             </Text>
           </Box>
-        </Card>
-      </Link>
+          <Badge variant={i.status}>{i.status}</Badge>
+          <Text
+            ml={["xs", "s"]}
+            fontSize="17px"
+            color="neutral900"
+            fontWeight="medium"
+          >
+            ${i.amount / 100}
+          </Text>
+        </Box>
+      </StyledInvoiceCard>
     );
   });
 
