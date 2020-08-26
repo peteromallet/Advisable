56c5b01a77c155f61074860cec521872013d7462
Yurii Turskyi <yurko@Yuriis-MacBook-Pro.local>
2020-08-27 01:59:54 +0300

    Make white background on mobile for ApplicationSent

--- app/javascript/src/views/ApplicationFlow/ApplicationSent/index.js
+++ app/javascript/src/views/ApplicationFlow/ApplicationSent/index.js
@@ -1,14 +1,31 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { Box, Card, Text, Button } from "@advisable/donut";
+import {
+  Box,
+  Card,
+  Text,
+  Button,
+  useTheme,
+  useBreakpoint,
+} from "@advisable/donut";
 import illustration from "./illustration.png";
 
 const ApplicationSent = ({ match }) => {
+  const theme = useTheme();
+  const isWidescreen = useBreakpoint("sUp");
   let { applicationId } = match.params;
+
+  React.useEffect(() => {
+    theme.updateTheme({
+      background: isWidescreen ? "default" : "white",
+    });
+    return () => theme.updateTheme({ background: "default" });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isWidescreen]);
 
   return (
     <Box maxWidth={450} mx="auto" py="l" textAlign="center">
-      <Card padding={{ _: "xl", m: "xxl" }}>
+      <Card padding={{ _: "xl", m: "xxl" }} elevation={["none", "m"]}>
         <Box mb="l">
           <img
             alt=""
