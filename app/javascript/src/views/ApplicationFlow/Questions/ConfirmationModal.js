f0cc8f87fabb22636c34c8a25c2d3080e14e4854
guandjoy <guandjoy@gmail.com>
2020-08-28 11:33:35 +0300

    Fix num of words

--- app/javascript/src/views/ApplicationFlow/Questions/ConfirmationModal.js
+++ app/javascript/src/views/ApplicationFlow/Questions/ConfirmationModal.js
@@ -19,9 +19,9 @@ const ConfirmationModal = ({ modal, formik, loading, numOfWords }) => {
 
         <Text color="neutral800" fontSize="s" lineHeight="m" mb="l">
           The average successful response to a qualifying question is 100 words.
-          Yours is {numOfWords} {pluralize(numOfWords, "word", "words")}. Try to
-          add more content to describing your experience in order to make the
-          best possible impression on the client!
+          Yours is {pluralize(numOfWords, "word", "words")}. Try to add more
+          content to describing your experience in order to make the best
+          possible impression on the client!
         </Text>
 
         <Box display="flex" flexDirection={["column", "row"]}>
