8d977195558c634e19ac5aa9d669c6ead8a4a020
Thomas Cullen <thomascullen92@gmail.com>
2020-09-03 14:31:06 +0100

    Adds session config

--- config/initializers/session_config.rb
+++ config/initializers/session_config.rb
@@ -1,2 +1,4 @@
-# Rails.application.config.session_store :cookie_store,
-#                                   key: '_advisable_session', domain: :all
+unless ENV['STAGING'] == 'true'
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_advisable_session', domain: :all
+end
