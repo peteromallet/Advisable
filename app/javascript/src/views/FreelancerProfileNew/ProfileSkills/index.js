2f13aaa3be2d6c71cb230ee20ed5bc010acc07d7
guandjoy <guandjoy@gmail.com>
2020-09-08 15:32:40 +0300

    Duplicate FreelancerProfile as FreelancerProfileNew

---
+++ app/javascript/src/views/FreelancerProfileNew/ProfileSkills/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Text, Tag } from "@advisable/donut";
+
+function ProfileSkills({ data }) {
+  const skills = data.specialist.projectSkills.nodes.slice(0, 12);
+
+  return (
+    <Box pt="xl">
+      <Text fontSize="l" mb="xs" fontWeight="medium">
+        Skills
+      </Text>
+      {skills.map(skill => (
+        <Tag mr="xxs" mb="xxs" key={skill.id}>
+          {skill.name}
+        </Tag>
+      ))}
+    </Box>
+  );
+}
+
+export default ProfileSkills;
