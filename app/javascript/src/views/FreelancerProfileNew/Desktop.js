0d8b85389d2e564f28c025dad65231b7d7fa585b
guandjoy <guandjoy@gmail.com>
2020-09-11 16:08:10 +0300

    Arrange initial content

--- app/javascript/src/views/FreelancerProfileNew/Desktop.js
+++ app/javascript/src/views/FreelancerProfileNew/Desktop.js
@@ -79,6 +79,80 @@ const reducer = (state, action) => {
 const createDispatcher = (dispatch) => (type) => (payload) =>
   dispatch({ type, payload });
 
+const getProjectValues = (projects) =>
+  projects.reduce(
+    (acc, project) => {
+      const skills = project.skills.map((skill) => skill.name);
+      const industries = project.industries.map((industry) => industry.name);
+      return {
+        clientNames: { [project.clientName]: "https://test.com/" },
+        industries: [...acc.industries, ...industries],
+        skills: [...acc.skills, ...skills],
+        reviews: [...acc.reviews, ...project.reviews],
+      };
+    },
+    { clientNames: [], industries: [], skills: [], reviews: [] },
+  );
+
+const setFilterParams = (list, filter) =>
+  list.reduce(
+    (acc, item) => ({
+      ...acc,
+      [item]: {
+        number: acc[item]?.number + 1 || 1,
+        selected: filter.includes(item),
+      },
+    }),
+    {},
+  );
+
+const init = (data) => {
+  const projects = data.specialist.profileProjects;
+  const {
+    clientNames,
+    industries: industriesList,
+    skills: skillsList,
+    reviews,
+  } = getProjectValues(projects);
+  const skillsFilter = [];
+  const skillsParams = setFilterParams(skillsList, skillsFilter);
+  const industriesFilter = [];
+  const industriesParams = setFilterParams(industriesList, industriesFilter);
+  const hasReviews = reviews.length > 0;
+  return {
+    id: data.specialist.id,
+    projects,
+    skillsList,
+    skillsFilter,
+    skillsParams,
+    industriesList,
+    industriesFilter,
+    industriesParams,
+    clientNames,
+    testimonials: {},
+    hasReviews,
+    reviews,
+  };
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_SKILLS_STATE":
+      return;
+    case "UPDATE_INDUSTRIES_STATE":
+      return;
+    case "SWITCH_SKILL_FILTER":
+      return;
+    case "SWITCH_INDUSTRY_FILTER":
+      return;
+    default:
+      return state;
+  }
+};
+
+const createDispatcher = (dispatch) => (type) => (payload) =>
+  dispatch({ type, payload });
+
 function FreelancerProfileDesktop({ data }) {
   console.log("data", data);
   const viewer = useViewer();
