122e05fcbbf1acd8766102856aa493201c7e6a37
Miha Rekar <miha.rekar@advisable.com>
2020-09-15 08:32:16 +0200

    Spec AnswerQuestion mutation

--- app/graphql/mutations/answer_question.rb
+++ app/graphql/mutations/answer_question.rb
@@ -1,11 +1,11 @@
 class Mutations::AnswerQuestion < Mutations::BaseMutation
   argument :question, ID, required: true
-  argument :answer, String, required: true
+  argument :content, String, required: true
 
   field :answer, Types::AnswerType, null: false
 
   def authorized?(**args)
-    requires_current_user
+    requires_current_user!
 
     unless current_user.respond_to?(:answers)
       ApiError.invalid_request(
@@ -19,8 +19,8 @@ class Mutations::AnswerQuestion < Mutations::BaseMutation
 
   def resolve(**args)
     question = Question.find_by_uid!(args[:question])
-    answer = current_user.answers.find_or_initialize_by!(question: question)
-    answer.content = args[:answer]
+    answer = current_user.answers.find_or_initialize_by(question: question)
+    answer.content = args[:content]
     answer.save!
     return {answer: answer}
   rescue Service::Error => e
---
+++ spec/graphql/mutations/answer_question_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+describe Mutations::AnswerQuestion do
+  let(:specialist) { create(:specialist) }
+  let(:question) { create(:question) }
+  let(:context) { {current_user: specialist} }
+
+  let(:query) do
+    <<-GRAPHQL
+    mutation {
+      answerQuestion(input: {
+        question: "#{question.uid}",
+        content: "It depends."
+      }) {
+        answer {
+          id
+          content
+          question {
+            id
+            content
+          }
+        }
+      }
+    }
+    GRAPHQL
+  end
+
+  context "no answer exists" do
+    it "creates a new answer" do
+      expect(Answer.find_by(question: question)).to be_nil
+      response = AdvisableSchema.execute(query, context: context)
+      expect(Answer.find_by(question: question).content).to eq("It depends.")
+    end
+  end
+
+  context "answer exists already" do
+    let!(:answer) { create(:answer, question: question, specialist: specialist, content: "Black bear.") }
+    it "overwrites answer" do
+      expect(Answer.find_by(question: question).content).to eq("Black bear.")
+      response = AdvisableSchema.execute(query, context: context)
+      expect(Answer.find_by(question: question).content).to eq("It depends.")
+    end
+  end
+
+  describe 'errors' do
+    context "specialist not logged in" do
+      let(:context) { {current_user: nil} }
+
+      it "raises an error" do
+        response = AdvisableSchema.execute(query, context: context)
+        error = response["errors"].first["extensions"]["type"]
+        expect(error).to eq("NOT_AUTHENTICATED")
+      end
+    end
+
+    context "logged in user does not have answers" do
+      let(:context) { {current_user: create(:user)} }
+
+      it "raises an error" do
+        response = AdvisableSchema.execute(query, context: context)
+        error = response["errors"].first["extensions"]["type"]
+        expect(error).to eq("INVALID_REQUEST")
+      end
+    end
+  end
+end
