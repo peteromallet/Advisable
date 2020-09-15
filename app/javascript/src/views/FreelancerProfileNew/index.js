278e333420b1e3793a82725e5407a9342dd3541b
guandjoy <guandjoy@gmail.com>
2020-09-15 11:24:24 +0300

    Remove redundant components

--- app/javascript/src/views/FreelancerProfileNew/index.js
+++ app/javascript/src/views/FreelancerProfileNew/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { useBreakpoint } from "@advisable/donut";
 import NotFound, { isNotFound } from "../NotFound";
 import useLogoURL from "../../components/ApplicationProvider/useLogoURL";
+import Loading from "../../components/Loading";
 import { GET_PROFILE } from "./queries";
-import Mobile from "./Mobile";
 import Desktop from "./Desktop";
-import Loading from "./Loading";
 import useViewer from "../../hooks/useViewer";
 
 function FreelancerProfileNew() {
@@ -15,7 +13,6 @@ function FreelancerProfileNew() {
   const params = useParams();
   const viewer = useViewer();
   console.log("viewer", viewer);
-  const isDesktop = useBreakpoint("mUp");
   const { loading, data, error } = useQuery(GET_PROFILE, {
     variables: {
       id: params.id,
