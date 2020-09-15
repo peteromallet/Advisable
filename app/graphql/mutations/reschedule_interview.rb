c17de1fc059e71846d7920a8557f5a697b9b6586
Miha Rekar <miha.rekar@advisable.com>
2020-09-15 15:05:07 +0200

    Add basic framework for interview rescheduling

---
+++ app/graphql/mutations/reschedule_interview.rb
@@ -0,0 +1,29 @@
+class Mutations::RescheduleInterview < Mutations::BaseMutation
+  argument :interview, ID, required: true
+  argument :starts_at, GraphQL::Types::ISO8601DateTime, required: true
+
+  def resolve(**args)
+    interview = Interview.find_by_uid_or_airtable_id!(args[:interview])
+
+    unless ["Specialist Requested Reschedule", "Client Requested Reschedule"].include?(interview.status)
+      ApiError.invalid_request(
+        code: "INTERVIEW_IS_NOT_RESCHEDULABLE",
+        message: "Interview was not requested to reschedule."
+      )
+    end
+
+    if interview.user.availability.include?(args[:starts_at])
+      interview.starts_at = args[:starts_at]
+    else
+      ApiError.invalid_request(
+        code: "STARTS_AT_OUT_OF_BOUNDS",
+        message: "Argument `starts_at` is not inside of the client's availability."
+      )
+    end
+    interview.save!
+
+    # send emails
+
+    {interview: interview}
+  end
+end
