7cae01fb17a697164175367fa1d6f56afd38ef82
Miha Rekar <miha.rekar@advisable.com>
2020-09-18 11:24:07 +0200

    Extract requires_specialist!

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -6,6 +6,19 @@ class Mutations::BaseMutation < GraphQL::Schema::RelayClassicMutation
   private
 
   def requires_current_user!
-    ApiError.not_authenticated if current_user.nil?
+    return true if current_user.present?
+    
+    ApiError.not_authenticated
+  end
+
+  def requires_specialist!
+    requires_current_user!
+
+    return true if current_user.is_a?(Specialist)
+
+    ApiError.invalid_request(
+      code: "MUST_BE_SPECIALIST",
+      message: "Current user must be a Specialist."
+    )
   end
 end
