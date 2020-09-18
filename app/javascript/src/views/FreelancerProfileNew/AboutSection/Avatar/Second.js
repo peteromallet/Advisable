c36a50d93a23996dbf5eede0c1d997e8326ac8ae
guandjoy <guandjoy@gmail.com>
2020-09-18 16:59:38 +0300

    Update AboutSection

---
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/Avatar/Second.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { theme, Box, StyledCard } from "@advisable/donut";
+import { rgba } from "polished";
+import styled from "styled-components";
+
+const StyledAvatarCard = styled(StyledCard)`
+  transition: box-shadow 300ms;
+  cursor: pointer;
+  box-shadow: 0px 8px 12px -4px ${rgba(theme.colors.neutral900, 0.04)},
+    0px 4px 20px -4px ${rgba(theme.colors.neutral900, 0.22)};
+
+  &:hover {
+    box-shadow: 0px 12px 24px -12px ${rgba(theme.colors.neutral900, 0.08)},
+      0px 24px 40px -24px ${rgba(theme.colors.neutral900, 0.3)};
+  }
+`;
+
+function Avatar({ avatar }) {
+  return (
+    <StyledAvatarCard
+      width={190}
+      height={234}
+      elevation="m"
+      mt="-66px"
+      ml="32px"
+      mr="30px"
+      borderRadius={16}
+    >
+      <Box
+        as="img"
+        src={avatar}
+        width="190px"
+        height="234px"
+        borderRadius={16}
+        css="object-fit: cover;"
+      />
+    </StyledAvatarCard>
+  );
+}
+
+export default Avatar;
--- app/javascript/src/views/FreelancerProfileNew/Desktop.js
+++ app/javascript/src/views/FreelancerProfileNew/Desktop.js
@@ -1,47 +1,19 @@
-// Renders the desktop version of the freelancer profile
 import React from "react";
-import { Box, Text, Card } from "@advisable/donut";
+import { Box } from "@advisable/donut";
 import PreviousProjects from "./PreviousProjects";
 import Testimonials from "./Testimonials";
+import AboutSection from "./AboutSection";
+import useViewer from "src/hooks/useViewer";
 
 function FreelancerProfileDesktop({ data }) {
   console.log("data", data);
+  const { id: viewerId } = useViewer();
+  const isOwner = viewerId === data.specialist.id;
 
   return (
     <Box maxWidth="960px" mx="auto">
-      <Card minHeight="514px" bg="#fff" mt="m" p="12px" borderRadius={8} mb="l">
-        <Box
-          as="img"
-          src="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/92b19080680791.5ce7e63751fcd.jpg"
-          css="object-fit: cover;"
-          width="100%"
-          bg="red500"
-          height="300px"
-          mx="auto"
-          borderRadius={8}
-        />
-        <Box display="flex">
-          <Box
-            as="img"
-            src={data.specialist.avatar}
-            width="148px"
-            height="148px"
-            borderRadius="50%"
-            css="object-fit: cover;"
-            mt="-32px"
-            ml="l"
-            mr="m"
-          />
-          <Box>
-            <Text fontSize="xxxl" fontWeight="semibold" color="neutral900">
-              {data.specialist.name}
-            </Text>
-            <Text>{data.specialist.location}</Text>
-            <Text>{data.specialist.bio}</Text>
-          </Box>
-        </Box>
-      </Card>
-      <PreviousProjects data={data} />
+      <AboutSection specialist={data.specialist} isOwner={isOwner} />
+      <PreviousProjects data={data} isOwner={isOwner} />
       <Testimonials reviews={data.specialist.reviews} />
     </Box>
   );
