43f64e6cd423417077b396666abfae553dedc26d
Geoff Ereth <geoffereth@gmail.com>
2020-09-18 11:04:41 -0700

    Adds redux like devtools for zustand

--- app/javascript/guild/stores/Feed/index.js
+++ app/javascript/guild/stores/Feed/index.js
@@ -1,21 +1,26 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
 const initialState = {
   selectedFilter: "For You",
   selectedTopics: [],
 };
 
-export const feedStore = create((set, get) => ({
-  ...initialState,
-  selectedTopicsIds: () => get().selectedTopics.map(({ id }) => id),
-  updateSelectedTopics: (topic) =>
-    set(({ selectedTopics: prev }) =>
-      prev.includes(topic)
-        ? prev.splice(prev.indexOf(topic), 1) // zustand requires a destructive mutation
-        : prev.push(topic),
-    ),
-  resetTopics: () => set(() => ({ selectedTopics: [] })),
-  resetFilters: () =>
-    set(() => ({ selectedTopics: [], selectedFilter: "For You" })),
-  clear: () => set({}, true),
-}));
+/* 
+  Light state management store for posts feed filters.
+*/
+export const feedStore = create(
+  devtools((set, get) => ({
+    ...initialState,
+    selectedTopicsIds: () => get().selectedTopics.map(({ id }) => id),
+    updateSelectedTopics: (topic) =>
+      set(({ selectedTopics: prev }) =>
+        prev.includes(topic)
+          ? prev.splice(prev.indexOf(topic), 1)
+          : prev.push(topic),
+      ),
+    resetTopics: () => set(() => ({ selectedTopics: [] })),
+    resetFilters: () => set(() => ({ ...initialState, selectedTopics: [] })),
+    clear: () => set({}, true),
+  })),
+);
