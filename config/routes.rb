d19a6f5a9be68b38ec38fec9f320871bab593267
Miha Rekar <miha.rekar@advisable.com>
2020-09-18 09:47:43 +0200

    Add endpoint for scheduling interviews

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   # Routes for internal tooling
   get '/internal', to: 'application#internal', as: :internal_root
   get '/internal/*guild_path', to: 'application#internal'
+  post '/projects/send_invites'
 
   # match every other route to the frontend codebase
   root 'application#frontend'
