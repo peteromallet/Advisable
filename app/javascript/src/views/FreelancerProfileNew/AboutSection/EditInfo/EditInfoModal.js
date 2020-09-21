c154ba1f70995bf508cfa07c6135b2c8f0af9e6b
guandjoy <guandjoy@gmail.com>
2020-09-21 17:02:20 +0300

    Init Edit Info dialog

---
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/EditInfo/EditInfoModal.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Modal, Box, Text, Textarea, Select } from "@advisable/donut";
+import SubmitButton from "src/components/SubmitButton";
+import FormField from "src/components/FormField";
+import { Form, Formik } from "formik";
+import { useMutation, useQuery } from "@apollo/client";
+import FETCH_COUNTRIES from "./fetchCountries.graphql";
+import { UPDATE_PROFILE } from "../../queries";
+import { get } from "lodash-es";
+
+function EditInfoModal({ modal, specialist }) {
+  const [mutate] = useMutation(UPDATE_PROFILE);
+  const initialValues = {
+    city: specialist.city || "",
+    country: get(specialist, "country.id"),
+    bio: specialist.bio || "",
+    linkedin: specialist.linkedin || "",
+    website: specialist.website || "",
+  };
+  const handleSubmit = async (values) => {
+    await mutate({ variables: { input: { ...values } } });
+    modal.hide();
+  };
+  const countriesQuery = useQuery(FETCH_COUNTRIES);
+  console.log("countries query", countriesQuery);
+  return (
+    <Modal modal={modal} p="xxl" label="Edit profile info" width={640}>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {() => (
+          <Form>
+            <Text
+              as="h2"
+              fontSize="xxxl"
+              fontWeight="medium"
+              color="neutral900"
+              mb="l"
+            >
+              Edit profile info
+            </Text>
+            <Box display="flex" mb="l">
+              <Box mr="s" width="100%">
+                <FormField
+                  name="city"
+                  label="City"
+                  width="100%"
+                  placeholder="e.g Berlin"
+                />
+              </Box>
+              <Box width="100%">
+                <FormField as={Select} name="country" label="Country">
+                  {countriesQuery.data.countries.map((c) => (
+                    <option key={c.value} value={c.value}>
+                      {c.label}
+                    </option>
+                  ))}
+                </FormField>
+              </Box>
+            </Box>
+            <Box mb="l">
+              <FormField
+                as={Textarea}
+                name="bio"
+                minRows={5}
+                label="About me"
+                description="Add a short 2 - 3 sectence bio to describe who you are."
+                placeholder="Add a short 2 - 3 sectence bio to describe who you are."
+                caption="A well structured bio demonstrates your experience and expertise by referencing past projects and achievements, including notable clients or numeric results. You will have a chance to customize this each time you apply for a project."
+              />
+            </Box>
+            <Box mb="l">
+              <FormField
+                name="linkedin"
+                label="LinkedIn"
+                placeholder="https://linkedin.com/in/your-name"
+                error={null}
+              />
+            </Box>
+            <Box mb="xl">
+              <FormField
+                name="website"
+                label="Website"
+                placeholder="https://your-website.com"
+                error={null}
+              />
+            </Box>
+            <SubmitButton>Update</SubmitButton>
+          </Form>
+        )}
+      </Formik>
+    </Modal>
+  );
+}
+
+export default EditInfoModal;
