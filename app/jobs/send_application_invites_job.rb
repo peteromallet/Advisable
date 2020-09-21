e9ef5d2645d90ee257809e6c8f334407a9a614d8
Miha Rekar <miha.rekar@advisable.com>
2020-09-21 12:02:15 +0200

    Add project information specialist filter and mailer

--- app/jobs/send_application_invites_job.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-
-class SendApplicationInvitesJob < ApplicationJob
-  queue_as :default
-
-  def perform(project)
-    # Find compatible people and send them an email
-  end
-end
