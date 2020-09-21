d33eb36eb5c81bf4680b6274823b3dd14ae2014e
guandjoy <guandjoy@gmail.com>
2020-09-21 11:30:25 +0300

     Fix viewer and isOwner bug

--- app/javascript/src/views/FreelancerProfileNew/AboutSection/index.js
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/index.js
@@ -48,6 +48,7 @@ function AboutSection({ specialist, isOwner, viewer }) {
                   Get in touch
                 </GetInTouchButton>
               )}
+              {!isOwner && viewer && <Button>Get in touch</Button>}
             </Box>
           </Box>
           <Text color="neutral800" lineHeight="22px" pr="58px">
