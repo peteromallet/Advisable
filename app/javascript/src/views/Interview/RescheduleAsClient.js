341a75fc931697226d2941a2fa8996665268db75
Thomas Cullen <thomascullen92@gmail.com>
2020-09-23 11:55:02 +0100

    Add flow for client to reschedule interview

--- app/javascript/src/views/Interview/RescheduleAsClient.js
+++ app/javascript/src/views/Interview/RescheduleAsClient.js
@@ -4,25 +4,37 @@ import UpdateAvailablityForm from "./UpdateAvailabilityForm";
 import { useRequestInterviewReschedule } from "./queries";
 
 export default function RequestRescheduleAsClient({ interview }) {
-  const [requestRequest] = useRequestInterviewReschedule();
+  const [requestReschedule] = useRequestInterviewReschedule();
+
+  const handleRequest = React.useCallback(async () => {
+    await requestReschedule({
+      variables: {
+        input: {
+          interview: interview.id,
+        },
+      },
+    });
+  }, [requestReschedule, interview]);
 
   return (
     <Box padding="xl">
-      <Box maxWidth="350px">
-        <Text
-          fontSize="3xl"
-          marginBottom="xs"
-          fontWeight="medium"
-          letterSpacing="-0.02em"
-        >
-          Reschedule your interview with {interview.specialist.firstName}
-        </Text>
-      </Box>
+      <Text
+        fontSize="3xl"
+        marginBottom="xs"
+        fontWeight="medium"
+        letterSpacing="-0.02em"
+      >
+        Reschedule your interview with {interview.specialist.firstName}
+      </Text>
       <Paragraph marginBottom="xl">
         Update your availability below and we will notify{" "}
         {interview.specialist.firstName} to reschdule the interview.
       </Paragraph>
-      <UpdateAvailablityForm buttonLabel="Request To Reschedule" />
+      <UpdateAvailablityForm
+        interview={interview}
+        onUpdate={handleRequest}
+        buttonLabel="Request To Reschedule"
+      />
     </Box>
   );
 }
