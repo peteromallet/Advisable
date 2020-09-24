0f7de843ca43fa801ee302a03c0e540be6d7cef9
Miha Rekar <miha.rekar@advisable.com>
2020-09-24 16:27:34 +0200

    Get the token in plain text via admin

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin,
-           ENV['LINKEDIN_KEY'],
-           ENV['LINKEDIN_SECRET'],
-           scope: 'r_basicprofile rw_ads',
-           fields: ['id', 'first-name', 'last-name', 'picture-url']
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], scope: 'r_basicprofile', fields: ['id', 'first-name', 'last-name', 'picture-url']
+
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], scope: 'r_liteprofile rw_ads', name: 'linkedin_ads', fields: ['id', 'first-name', 'last-name', 'picture-url']
 end
