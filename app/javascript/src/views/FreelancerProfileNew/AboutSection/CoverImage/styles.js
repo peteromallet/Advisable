be1db2793bdc4f1c9370a1f83526d2997e12cbcd
guandjoy <guandjoy@gmail.com>
2020-09-24 18:43:46 +0300

    Implement CoverPhoto upload

---
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/CoverImage/styles.js
@@ -0,0 +1,46 @@
+import { rgba } from "polished";
+import styled, { css } from "styled-components";
+import { theme } from "@advisable/donut";
+
+export const CoverImageWrapper = styled.div`
+  position: relative;
+`;
+
+export const FileUploader = styled.div`
+  width: 42px;
+  height: 42px;
+  display: flex;
+  overflow: hidden;
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  color: ${theme.colors.neutral700};
+  background: ${rgba(theme.colors.neutral100, 0.5)};
+  transition: background 0.2s, color 0.2s, opacity 0.2s;
+  opacity: 0;
+
+  &:hover {
+    color: ${theme.colors.neutral800};
+    background: ${rgba(theme.colors.neutral100, 0.7)};
+  }
+  ${CoverImageWrapper}:hover & {
+    opacity: 1;
+  }
+
+  input {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0;
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    cursor: pointer;
+    position: absolute;
+  }
+`;
