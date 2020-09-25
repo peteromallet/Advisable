6ee6ffecc9a7418cd0d21f1bbcc44c687dab2d17
guandjoy <guandjoy@gmail.com>
2020-09-25 12:33:17 +0300

    Implement redirection if not signed in

--- app/javascript/src/views/JobOpportunity/index.js
+++ app/javascript/src/views/JobOpportunity/index.js
@@ -6,6 +6,8 @@ import JobOpportunity from "./JobOpportunity";
 import NotFound from "../NotFound";
 import ApplicationsClosed from "../ApplicationsClosed";
 import { GET_APPLICATION } from "./queries";
+import useViewer from "../../hooks/useViewer";
+import { useNotifications } from "../../components/Notifications";
 
 let JobOpportunityContainer = ({ history, match }) => {
   const { loading, data, error } = useQuery(GET_APPLICATION, {
@@ -13,6 +15,12 @@ let JobOpportunityContainer = ({ history, match }) => {
       id: match.params.projectId,
     },
   });
+  const viewer = useViewer();
+  const notifications = useNotifications();
+  if (!viewer) {
+    history.push("/login");
+    notifications.notify("Login first please");
+  }
 
   if (loading) return <Loading />;
 
