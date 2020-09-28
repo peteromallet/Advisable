106d4f2c0f0ee4ab2a2a1a524368841dbe2a7e26
guandjoy <guandjoy@gmail.com>
2020-09-28 16:23:08 +0300

    Make SkeletonHeading use Box instead of div

--- app/javascript/src/components/SkeletonHeading/index.js
+++ app/javascript/src/components/SkeletonHeading/index.js
@@ -1,6 +1,7 @@
 // This component is used to render a loading indicator that represents a
 // heading
 import styled, { keyframes } from "styled-components";
+import { Box } from "@advisable/donut";
 
 const animation = keyframes`
   from {
@@ -10,14 +11,14 @@ const animation = keyframes`
   to {
     opacity: 0.1;
   }
-`
+`;
 
-const SkeletonHeading = styled.div`
+const SkeletonHeading = styled(Box)`
   height: 22px;
   max-width: 120px;
   border-radius: 5px;
-  background: #3C466A;
+  background: #3c466a;
   animation: ${animation} 0.6s linear infinite alternate;
-`
+`;
 
 export default SkeletonHeading;
