1906ffb926ec6de9fd9204af99b21f8218d712d0
guandjoy <guandjoy@gmail.com>
2020-09-30 16:54:33 +0300

    Init SpecialistSettings

---
+++ app/javascript/src/views/Settings/SpecialistSettings/General/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@advisable/donut";
+
+function General() {
+  return <Box>General settings</Box>;
+}
+
+export default General;
---
+++ app/javascript/src/views/Settings/SpecialistSettings/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Layout from "components/Layout";
+import General from "./General";
+import PreviousProjects from "./PreviousProjects";
+import PaymentSettings from "./PaymentSettings";
+import { useBreakpoint } from "@advisable/donut";
+
+function SpecialistSettings({ match }) {
+  let breakpointS = useBreakpoint("sUp");
+
+  return (
+    <Layout>
+      {/* On mobile we only show the navigation menu if the URL is exactly
+      /settings. On desktop we want to display it as a sidebar on all settings
+      pages. We use a Route with exact prop to achieve this. */}
+      <Route path={match.path} component={Sidebar} exact={!breakpointS} />
+      <Layout.Main>
+        <Switch>
+          <Route path="/settings/general" component={General} />
+          <Route
+            path="/settings/previous-projects"
+            exact
+            component={PreviousProjects}
+          />
+          <Route
+            path="/settings/payment-settings"
+            component={PaymentSettings}
+          />
+          {/* If the user is not on a small screen, then redirect them to the
+          first settings page when they are on exactly /settings */}
+          {breakpointS && (
+            <Route
+              exact
+              path={match.path}
+              render={() => <Redirect to="/settings/general" />}
+            />
+          )}
+        </Switch>
+      </Layout.Main>
+    </Layout>
+  );
+}
+
+export default SpecialistSettings;
