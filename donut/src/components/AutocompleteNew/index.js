aab6292703143a2328644d4f02ca55034c2abbb8
Thomas Cullen <thomascullen92@gmail.com>
2020-09-30 10:02:46 +0100

    Add basic skeleton for new autocomplete component

--- donut/src/components/AutocompleteNew/index.js
+++ donut/src/components/AutocompleteNew/index.js
@@ -1,114 +1,182 @@
-import React, { useState, useRef } from "react";
-import { createPopper } from "@popperjs/core";
-import { useCombobox } from "downshift";
+import React from "react";
+import Fuse from "fuse.js";
+import styled from "styled-components";
 import Input from "../Input";
-import {
-  StyledAutocomplete,
-  StyledAutoCompleteMenu,
-  StyledAutocompleteMenuItem,
-} from "./styles";
 
-function stateReducer(state, actionAndChanges) {
-  const { type, changes } = actionAndChanges;
-  // returning an uppercased version of the item string.
-  switch (type) {
-    case useCombobox.stateChangeTypes.ItemClick:
-    case useCombobox.stateChangeTypes.InputKeyDownEnter:
-    case useCombobox.stateChangeTypes.InputBlur:
-      return {
-        ...changes,
-        // if we had an item selected.
-        ...(changes.selectedItem && {
-          // we will show it uppercased.
-          inputValue: changes.selectedItem.label,
-        }),
-      };
-    default:
-      return changes; // otherwise business as usual.
+const fuseOptions = {
+  shouldSort: true,
+  threshold: 0.6,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: ["label"],
+};
+
+const ENTER = 13;
+const ESCAPE = 27;
+const ARROW_UP = 38;
+const ARROW_DOWN = 40;
+
+const StyledAutocompleteOptions = styled.div`
+  background: ${(p) => (p.$isSeleted ? "#DDD" : "transparent")};
+`;
+
+export default function Autocomplete({ options, value, onChange, ...props }) {
+  const inputRef = React.useRef(null);
+  const listboxRef = React.useRef(null);
+  const [isOpen, setOpen] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [selectionIndex, setSelectionIndex] = React.useState(-1);
+
+  function handleOpen() {
+    if (!props.disabled && !isOpen) {
+      setOpen(true);
+    }
   }
-}
 
-export default function Autocomplete({ prefix, suffix, options }) {
-  const popperInstance = useRef(null);
-  const [inputItems, setInputItems] = useState(options);
-  const [referenceElement, setReferenceElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
+  function handleFocus(e) {
+    if (!isOpen) setOpen(true);
+    if (props.onFocus) props.onFocus(e);
+  }
 
-  const combobox = useCombobox({
-    stateReducer,
-    items: inputItems,
-    onInputValueChange: ({ inputValue }) => {
-      setInputItems(
-        options.filter((item) =>
-          item.label.toLowerCase().startsWith(inputValue.toLowerCase()),
-        ),
-      );
-    },
-  });
-
-  React.useEffect(() => {
-    if (referenceElement == null || popperElement == null) {
+  function handleBlur(e) {
+    const listboxEl = listboxRef.current;
+    console.log(e.relatedTarget);
+    if (listboxEl && e.relatedTarget && listboxEl.contains(e.relatedTarget)) {
       return;
     }
 
-    popperInstance.current = createPopper(referenceElement, popperElement, {
-      placement: "bottom",
-      modifiers: [
-        {
-          name: "offset",
-          options: {
-            offset: [0, 8],
-          },
-        },
-      ],
-    });
+    setOpen(false);
+    setSearchValue("");
+    setSelectionIndex(-1);
+    if (props.onBlur) props.onBlur(e);
+  }
 
-    return () => popperInstance.current.destroy();
-  }, [popperElement, referenceElement]);
+  function handleInputChange(e) {
+    handleOpen();
+    setSelectionIndex(-1);
+    setSearchValue(e.target.value);
+  }
 
-  React.useEffect(() => {
-    popperInstance.current?.update();
-  }, [combobox.inputValue]);
+  function handleClick(e) {
+    handleOpen();
+  }
+
+  function handleKeyDown(e) {
+    if (e.keyCode === ARROW_DOWN) {
+      e.preventDefault();
+      handleOpen();
+      setSelectionIndex((prev) => {
+        let next = prev + 1;
+        if (next > filteredOptions.length - 1) {
+          next = 0;
+        }
+        return next;
+      });
+    }
+
+    if (e.keyCode === ARROW_UP) {
+      e.preventDefault();
+      setSelectionIndex((prev) => {
+        let next = prev - 1;
+        if (next < -1) {
+          next = filteredOptions.length - 1;
+        }
+        return next;
+      });
+    }
+
+    if (e.keyCode === ENTER) {
+      const clickedOption = filteredOptions[selectionIndex];
+      if (clickedOption) {
+        e.preventDefault();
+        setOpen(false);
+        setSearchValue("");
+        setSelectionIndex(-1);
+        onChange(clickedOption.value);
+      }
+    }
+
+    if (e.keyCode === ESCAPE) {
+      setOpen(false);
+      setSelectionIndex(-1);
+      setSearchValue("");
+    }
+  }
+
+  function handleOptionClick(index) {
+    const clickedOption = filteredOptions[index];
+    if (clickedOption) {
+      setOpen(false);
+      setSearchValue("");
+      setSelectionIndex(-1);
+      onChange(clickedOption.value);
+    }
+  }
+
+  function handleOptionHover(index) {
+    const option = filteredOptions[index];
+    if (option) {
+      setSelectionIndex(index);
+    }
+  }
+
+  const fuse = React.useMemo(() => {
+    return new Fuse(options, fuseOptions);
+  }, [options]);
+
+  const filteredOptions = React.useMemo(() => {
+    if (searchValue.length > 0) {
+      return fuse.search(searchValue).map((obj) => obj.item);
+    }
+
+    return options;
+  }, [fuse, searchValue, options]);
+
+  const selectedLabel = React.useMemo(() => {
+    return options.find((o) => o.value === value)?.label || "";
+  }, [options, value]);
 
   return (
-    <StyledAutocomplete>
-      <div
-        {...combobox.getComboboxProps({
-          ref: setReferenceElement,
-        })}
-      >
-        <Input
-          prefix={prefix}
-          suffix={suffix}
-          {...combobox.getInputProps({
-            onFocus: () => {
-              if (!combobox.isOpen) {
-                combobox.openMenu();
-              }
-            },
-          })}
-        />
-      </div>
-      <StyledAutoCompleteMenu
-        {...combobox.getMenuProps({
-          ref: setPopperElement,
-        })}
-      >
-        {combobox.isOpen &&
-          inputItems.map((item, index) => (
-            <StyledAutocompleteMenuItem
-              style={
-                combobox.highlightedIndex === index
-                  ? { backgroundColor: "#bde4ff" }
-                  : {}
-              }
-              key={`${item}${index}`}
-              {...combobox.getItemProps({ item, index })}
+    <div>
+      <Input
+        {...props}
+        ref={inputRef}
+        value={isOpen ? searchValue : selectedLabel}
+        onBlur={handleBlur}
+        onClick={handleClick}
+        onFocus={handleFocus}
+        onKeyDown={handleKeyDown}
+        onChange={handleInputChange}
+      />
+      {isOpen && (
+        <div role="listbox" ref={listboxRef} tabIndex="-1">
+          {filteredOptions.map((option, index) => (
+            <AutocompleteOption
+              key={option.value}
+              selected={selectionIndex === index}
+              onClick={() => handleOptionClick(index)}
+              onMouseOver={() => handleOptionHover(index)}
             >
-              {item.label}
-            </StyledAutocompleteMenuItem>
+              {option.label}
+            </AutocompleteOption>
           ))}
-      </StyledAutoCompleteMenu>
-    </StyledAutocomplete>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function AutocompleteOption({ children, selected, ...props }) {
+  return (
+    <StyledAutocompleteOptions
+      role="option"
+      aria-selected={selected}
+      $isSeleted={selected}
+      {...props}
+    >
+      {children}
+    </StyledAutocompleteOptions>
   );
 }
