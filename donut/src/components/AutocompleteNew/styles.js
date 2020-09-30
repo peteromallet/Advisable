a13da8948e6989b52c383bcc575c287d1406cb3d
Thomas Cullen <thomascullen92@gmail.com>
2020-09-30 17:01:20 +0100

    Add basic styles

--- donut/src/components/AutocompleteNew/styles.js
+++ donut/src/components/AutocompleteNew/styles.js
@@ -1,33 +1,40 @@
 import styled, { css } from "styled-components";
-import { variant, space } from "styled-system";
-import {
-  BORDER_RADIUS,
-  StyledInput,
-  StyledInputControl,
-  StyledInputDecoration,
-} from "../Input/styles";
 import theme from "../../theme";
 
 export const StyledAutocomplete = styled.div`
   position: relative;
 `;
 
-export const StyledAutocompleteInput = styled(StyledInput)``;
+const StyledAutocompleteMenu_Closed = css`
+  visibility: hidden;
+  pointer-events: none;
+`;
 
-export const StyledAutocompleteDecoration = styled(StyledInputDecoration)``;
-
-export const StyledAutocompleteInputControl = styled(StyledInputControl)``;
-
-export const StyledAutoCompleteMenu = styled.div`
+export const StyledAutocompleteMenu = styled.ul`
   width: 100%;
+  outline: none;
+  padding: 4px 0;
   background: white;
   max-height: 300px;
   overflow-y: scroll;
   border-radius: 12px;
   box-shadow: 0 24px 64px rgba(0, 0, 0, 0.2);
+  ${(p) => !p.$isOpen && StyledAutocompleteMenu_Closed};
 `;
 
-export const StyledAutocompleteMenuItem = styled.div`
-  padding: 8px 12px;
+export const StyledAutocompleteMenuItem = styled.li`
+  padding: 0 4px;
   font-size: 15px;
+  user-select: none;
+
+  span {
+    display: block;
+    font-size: 15px;
+    border-radius: 10px;
+    padding: 10px 12px;
+    color: ${(p) =>
+      p.$isSelected ? theme.colors.blue900 : theme.colors.neutral900};
+    background: ${(p) =>
+      p.$isSelected ? theme.colors.blue100 : "transparent"};
+  }
 `;
