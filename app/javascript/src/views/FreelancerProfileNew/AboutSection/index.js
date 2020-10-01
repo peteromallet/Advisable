0e658d11fda8a6fca80702168a3edacd74e06562
guandjoy <guandjoy@gmail.com>
2020-10-01 14:32:25 +0300

    Increase bottom margin of AboutSection

--- app/javascript/src/views/FreelancerProfileNew/AboutSection/index.js
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/index.js
@@ -10,7 +10,7 @@ import EditInfo from "./EditInfo";
 
 function AboutSection({ specialist, isOwner, viewer }) {
   return (
-    <Card minHeight="514px" bg="#fff" mt="m" p="12px" borderRadius={12} mb="l">
+    <Card minHeight="514px" bg="#fff" mt="m" p="12px" borderRadius={12} mb="xl">
       <CoverImage coverPhoto={specialist.coverPhoto} isOwner={isOwner} />
       <Box display="flex">
         <Avatar avatar={specialist.avatar} />
