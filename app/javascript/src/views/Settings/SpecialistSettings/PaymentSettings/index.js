a53305c086807629e57b66cef2a452cdc14a3b21
guandjoy <guandjoy@gmail.com>
2020-10-01 13:25:20 +0300

    Move PaymentSettings to Settings

--- app/javascript/src/views/Settings/SpecialistSettings/PaymentSettings/index.js
+++ app/javascript/src/views/Settings/SpecialistSettings/PaymentSettings/index.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Box } from "@advisable/donut";
+import { Card, Text } from "@advisable/donut";
+import { useNotifications } from "components/Notifications";
+import UpdatePaymentSettingsForm from "components/UpdatePaymentSettingsForm";
 
-function PaymentSettings() {
-  return <Box>Payment Settings</Box>;
-}
+const PaymentSettings = () => {
+  let notifications = useNotifications();
+
+  const handleSuccess = () => {
+    notifications.notify("Your payment settings have been updated.");
+  };
+
+  return (
+    <Card p="l">
+      <Text size="xl" weight="medium" color="neutral800" mb="xxs">
+        Payment Settings
+      </Text>
+      <Text size="s" mb="s" color="neutral500">
+        Advisable aims to process your payment the Friday following client
+        approval of a task.
+      </Text>
+      <UpdatePaymentSettingsForm onSuccess={handleSuccess} />
+    </Card>
+  );
+};
 
 export default PaymentSettings;
