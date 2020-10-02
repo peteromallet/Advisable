27f417495c1050777745bf1378bfe63fd2880527
Thomas Cullen <thomascullen92@gmail.com>
2020-10-02 12:02:03 +0100

    Add join and leave sound effects

--- app/javascript/src/views/VideoCall/Participants.js
+++ app/javascript/src/views/VideoCall/Participants.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useSound from "use-sound";
 import { Box } from "@advisable/donut";
 import Participant from "./Participant";
 import useParticipants from "./useParticipants";
 import WaitingForOthers from "./WaitingForOthers";
+import usePrevious from "../../utilities/usePrevious";
+import joinSound from "./sounds/join.mp3";
+import leaveSound from "./sounds/leave.mp3";
+
+const SOUND_EFFECT_VOLUME = 0.5;
 
 function gridTemplate(numberOfParticipants) {
   if (numberOfParticipants >= 7) {
@@ -16,6 +22,23 @@ function gridTemplate(numberOfParticipants) {
 
 export default function Participants() {
   const participants = useParticipants();
+  const [playJoinSound] = useSound(joinSound, {
+    volume: SOUND_EFFECT_VOLUME,
+  });
+  const [playLeaveSound] = useSound(leaveSound, {
+    volume: SOUND_EFFECT_VOLUME,
+  });
+  const participantCount = usePrevious(participants.length);
+
+  useEffect(() => {
+    if (participants.length > participantCount) {
+      playJoinSound();
+    }
+
+    if (participants.length < participantCount) {
+      playLeaveSound();
+    }
+  }, [participants, participantCount, playJoinSound, playLeaveSound]);
 
   if (participants.length === 0) {
     return <WaitingForOthers />;
