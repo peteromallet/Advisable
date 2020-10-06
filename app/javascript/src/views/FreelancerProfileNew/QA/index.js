9065a53a5a1a7d49507f476b6340b710b8703f7a
guandjoy <guandjoy@gmail.com>
2020-10-06 15:14:56 +0300

    Create section header component

--- app/javascript/src/views/FreelancerProfileNew/QA/index.js
+++ app/javascript/src/views/FreelancerProfileNew/QA/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, Box, Card } from "@advisable/donut";
 import Answer from "./Answer";
 import AnswerQuestions from "./AnswerQuestions";
+import SectionHeader from "../components/SectionHeader";
 
 function QA({ questions, answers }) {
   const questionsHash = questions.reduce((acc, question) => {
@@ -28,12 +29,9 @@ function QA({ questions, answers }) {
 
   return (
     <Box mb="xxxl">
-      <Box display="flex" alignItems="center" mb="xxs" px="xs">
-        <Text as="h2" fontSize="xl" fontWeight="medium" color="neutral600">
-          Questions & Answers
-        </Text>
-        <AnswerQuestions qaHash={qaHash} />
-      </Box>
+      <SectionHeader actionButton={<AnswerQuestions qaHash={qaHash} />}>
+        Questions & Answers
+      </SectionHeader>
       <Card borderRadius={8} p="xl">
         <Box>{answersList}</Box>
       </Card>
