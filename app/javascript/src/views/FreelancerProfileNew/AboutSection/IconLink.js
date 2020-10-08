ed4f4554d2e18496a62dfc2d00a186464035cf41
guandjoy <guandjoy@gmail.com>
2020-10-08 18:27:15 +0300

    Implement hover state for IconLink component

--- app/javascript/src/views/FreelancerProfileNew/AboutSection/IconLink.js
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/IconLink.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Link, Circle } from "@advisable/donut";
+import { Link, Circle, theme } from "@advisable/donut";
+import { rgba } from "polished";
+import styled from "styled-components";
+
+const StyledCircle = styled(Circle)`
+  transition: color 0.2s, background 0.2s;
+  &:hover {
+    background: ${rgba(theme.colors.neutral100, 0.95)};
+    color: ${rgba(theme.colors.neutral600, 0.8)};
+  }
+`;
 
 function IconLink({ Icon, url, strokeWidth }) {
   return (
     <Link.External href={url} mx="xxs" target="_blank">
-      <Circle size={42} bg="neutral100" color="neutral600">
+      <StyledCircle size={42} bg="neutral100" color="neutral600">
         <Icon size={24} strokeWidth={strokeWidth} />
-      </Circle>
+      </StyledCircle>
     </Link.External>
   );
 }
