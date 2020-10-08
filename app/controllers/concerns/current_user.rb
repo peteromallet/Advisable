462a88fb3ffced2514c1bd95e05063a551155e84
Thomas Cullen <thomascullen92@gmail.com>
2020-10-08 08:34:43 +0100

    Code cleannup and refactor

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -13,8 +13,7 @@ module CurrentUser
     end
 
     def authenticate_with_magic_link
-      return if user_logged_in?
-      return unless magic_link.present?
+      return if user_logged_in? || magic_link.blank?
       magic_link.use
       # TODO: Eventually start_session should accept the account record
       user_or_specialist = magic_link.account.user || magic_link.account.specialist
@@ -28,9 +27,9 @@ module CurrentUser
       @magic_link ||= begin
         token = params.fetch("mlt", nil)
         uid = params.fetch("mluid", nil)
-        return unless token.present? && uid.present?
+        return if token.blank? || uid.blank?
         account = Account.find_by_uid(uid)
-        return unless account.present?
+        return if account.blank?
         MagicLink.for_path(account: account, token: token, path: request.path)
       end
     end
