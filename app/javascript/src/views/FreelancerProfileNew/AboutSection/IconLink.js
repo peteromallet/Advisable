707a68d2b5fa1eeebfdd17693c213f72c1c9e94a
guandjoy <guandjoy@gmail.com>
2020-10-08 19:27:36 +0300

    Update colors of IconLink

--- app/javascript/src/views/FreelancerProfileNew/AboutSection/IconLink.js
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/IconLink.js
@@ -5,16 +5,18 @@ import styled from "styled-components";
 
 const StyledCircle = styled(Circle)`
   transition: color 0.2s, background 0.2s;
+  color: ${rgba(theme.colors.neutral700, 0.9)};
+  background: ${rgba(theme.colors.neutral200, 0.9)};
   &:hover {
-    background: ${rgba(theme.colors.neutral100, 0.95)};
-    color: ${rgba(theme.colors.neutral600, 0.8)};
+    background: ${rgba(theme.colors.neutral200, 0.8)};
+    color: ${rgba(theme.colors.neutral700, 0.7)};
   }
 `;
 
 function IconLink({ Icon, url, strokeWidth }) {
   return (
     <Link.External href={url} mx="xxs" target="_blank">
-      <StyledCircle size={42} bg="neutral100" color="neutral600">
+      <StyledCircle size={42}>
         <Icon size={24} strokeWidth={strokeWidth} />
       </StyledCircle>
     </Link.External>
