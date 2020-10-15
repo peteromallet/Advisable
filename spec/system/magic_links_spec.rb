43c704e2bf4b6d5f37e782d0247671fdc115bae7
Miha Rekar <miha.rekar@advisable.com>
2020-10-15 08:48:13 +0200

    Make Account#email case insensitive

--- spec/system/magic_links_spec.rb
+++ spec/system/magic_links_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Magic links" do
-  it "authenticates the user and uses the magic link" do
-    user = create(:user)
-    account = create(:account, user: user)
-    magic_link = create(:magic_link, account: account, path: "/projects")
+  let(:user) { create(:user) }
+  let(:account) { user.account }
+  let(:magic_link) { create(:magic_link, account: account, path: "/projects") }
 
+  it "authenticates the user and uses the magic link" do
     visit "/projects?mlt=#{magic_link.token}&mluid=#{account.uid}&another=param"
     expect(page).to have_content("Find new talent")
     expect(page).to have_current_path("/projects?another=param")
@@ -13,21 +13,14 @@ RSpec.describe "Magic links" do
 
   context "when trying to access a diferent path" do
     it "does not authenticate the user" do
-      user = create(:user)
-      account = create(:account, user: user)
-      magic_link = create(:magic_link, account: account, path: "/projects")
-
       visit "/settings?mlt=#{magic_link.token}&mluid=#{account.uid}"
       expect(current_path).to eq("/login")
     end
   end
 
   context "When the magic link has expired" do
+    let(:magic_link) { create(:magic_link, expires_at: 1.hour.ago, account: account, path: "/projects") }
     it "does not authenticate the user" do
-      user = create(:user)
-      account = create(:account, user: user)
-      magic_link = create(:magic_link, expires_at: 1.hour.ago, account: account, path: "/projects")
-
       visit "/projects?mlt=#{magic_link.token}&mluid=#{account.uid}"
       expect(current_path).to eq("/login")
     end
@@ -35,9 +28,6 @@ RSpec.describe "Magic links" do
 
   context "When given an invalid token" do
     it "it ignores it" do
-      user = create(:user)
-      account = create(:account, user: user)
-      magic_link = create(:magic_link, account: account, path: "/projects")
       visit "/projects?mlt=invalidToken&mluid=#{account.uid}"
       expect(current_path).to eq("/login")
     end
@@ -45,9 +35,6 @@ RSpec.describe "Magic links" do
 
   context "When given a valid token but invalid uid" do
     it "it ignores it" do
-      user = create(:user)
-      account = create(:account, user: user)
-      magic_link = create(:magic_link, account: account, path: "/projects")
       visit "/projects?mlt=#{magic_link.token}&mluid=invalidUID"
       expect(current_path).to eq("/login")
     end
