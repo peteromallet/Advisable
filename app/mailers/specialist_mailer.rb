ded31bba3eed9e6cefe822d8a0e8e47d77f1a926
Miha Rekar <miha.rekar@advisable.com>
2020-10-19 14:32:58 +0200

    Address PR review missing items

--- app/mailers/specialist_mailer.rb
+++ app/mailers/specialist_mailer.rb
@@ -4,9 +4,9 @@ class SpecialistMailer < ApplicationMailer
   layout 'styled_mailer'
 
   def confirm(uid:, token:)
-    @account = Specialist.find_by_uid(uid)
+    @specialist = Specialist.find_by_uid(uid)
     @token = token
-    mail(to: @account.email, subject: 'Account Confirmation')
+    mail(to: @specialist.account.email, subject: 'Account Confirmation')
   end
 
   def verify_project(uid)
