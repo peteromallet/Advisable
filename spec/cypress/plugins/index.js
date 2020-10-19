243a75e0780a6908d4113b35aca905e5879a20f6
Thomas Cullen <thomascullen92@gmail.com>
2020-10-19 14:25:52 +0100

    Increase cypress timeouts

--- spec/cypress/plugins/index.js
+++ spec/cypress/plugins/index.js
@@ -51,10 +51,12 @@ module.exports = (on, config) => {
         await page.type("input[name=email]", email);
         await page.type("input[type=password]", "testing123");
         await page.click("[data-testid=loginButton]");
-        await delay(2000);
-        await page.waitForSelector("[data-testid=joinCall]:not([disabled])");
+        await delay(5000);
+        await page.waitForSelector("[data-testid=joinCall]:not([disabled])", {
+          timeout: 60000,
+        });
         await page.click("[data-testid=joinCall]:not([disabled])");
-        await delay(2000);
+        await delay(5000);
         await page.waitForSelector("[data-testid=leaveCall]", {
           timeout: 60000,
         });
