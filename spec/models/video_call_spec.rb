16e3eb637e5da708b43718d4a67cb6d5bd985284
Thomas Cullen <thomascullen92@gmail.com>
2020-10-19 13:57:28 +0100

    Add factory valiation spec for VideoCall

--- spec/models/video_call_spec.rb
+++ spec/models/video_call_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe VideoCall, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    video_call = build(:video_call)
+    expect(video_call).to be_valid
+  end
 end
