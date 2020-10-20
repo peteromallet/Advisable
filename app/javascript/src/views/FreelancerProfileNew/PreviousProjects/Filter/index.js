3b2d653ec24a35f74310a541d4de9fc205bb4369
guandjoy <guandjoy@gmail.com>
2020-10-20 23:07:35 +0300

    Refactor Filter and fix bugs

--- app/javascript/src/views/FreelancerProfileNew/PreviousProjects/Filter/index.js
+++ app/javascript/src/views/FreelancerProfileNew/PreviousProjects/Filter/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useReducer } from "react";
-import queryString from "query-string";
 import { Box, Text, StyledCard, Button, theme } from "@advisable/donut";
 import { rgba } from "polished";
 import { Info } from "@styled-icons/feather";
@@ -14,7 +13,6 @@ import {
 } from "./reducerHandlers";
 import styled from "styled-components";
 import { AnimatePresence, motion } from "framer-motion";
-import { useLocation } from "react-router";
 
 function arrayToSentence(array) {
   return (
