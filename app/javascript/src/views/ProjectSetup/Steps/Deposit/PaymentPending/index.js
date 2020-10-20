47e5e4dff920cb6ab048e8149aa0e4eb92701876
Thomas Cullen <thomascullen92@gmail.com>
2020-10-20 11:33:04 +0100

    Stop polling deposit after completion

--- app/javascript/src/views/ProjectSetup/Steps/Deposit/PaymentPending/index.js
+++ app/javascript/src/views/ProjectSetup/Steps/Deposit/PaymentPending/index.js
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
-import { useApolloClient } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Text } from "@advisable/donut";
+import { Redirect } from "react-router-dom";
 import React, { Fragment } from "react";
 import Loading from "src/components/Loading";
 
@@ -8,42 +9,25 @@ export const GET_DEPOSIT = gql`
   query getProject($id: ID!) {
     project(id: $id) {
       id
+      airtableId
       depositOwed
     }
   }
 `;
 
-const PaymentPending = ({ id, onSuccess }) => {
-  const client = useApolloClient();
-  const [seconds, setSeconds] = React.useState(0);
-  const [timer, setTimer] = React.useState(null);
+const PaymentPending = ({ id }) => {
+  const { data } = useQuery(GET_DEPOSIT, {
+    variables: { id },
+    fetchPolicy: "no-cache",
+    pollInterval: 2000,
+  });
 
-  const poll = () => {
-    setTimer(
-      setInterval(async () => {
-        let response = await client.query({
-          fetchPolicy: "network-only",
-          query: GET_DEPOSIT,
-          variables: {
-            id,
-          },
-        });
+  const depositOwed = data?.project?.depositOwed;
 
-        const { project } = response.data;
-        if (project.depositOwed === 0) {
-          onSuccess();
-          return;
-        }
-
-        setSeconds(seconds + 2);
-      }, 2000),
-    );
-  };
-
-  React.useEffect(() => {
-    poll();
-    return clearInterval(timer);
-  }, []);
+  if (depositOwed === 0) {
+    const id = data.project.airtableId;
+    return <Redirect to={`/project_setup/${id}/confirm`} />;
+  }
 
   return (
     <Fragment>
