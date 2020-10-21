e5e1ea17636001b161a7cb49c7984a5a22ae3612
guandjoy <guandjoy@gmail.com>
2020-10-21 23:27:45 +0300

    Merge avatar files into a single Avatar.js file

--- app/javascript/src/views/FreelancerProfileNew/AboutSection/Info/Avatar.js
+++ app/javascript/src/views/FreelancerProfileNew/AboutSection/Info/Avatar.js
@@ -1,12 +1,39 @@
 import React from "react";
-import FileUpload from "../../FileUpload";
-import { StyledAvatarCard, StyledAvatarImage } from "./styles";
-import { User } from "@styled-icons/feather";
-import { Box } from "@advisable/donut";
+import { rgba } from "polished";
+import styled from "styled-components";
+// Hooks
 import { useMutation } from "@apollo/client";
 import { useNotifications } from "src/components/Notifications";
-import { UPDATE_PROFILE } from "../../../queries";
 import useImageOnLoad from "src/hooks/useImageOnLoad";
+// Components
+import { Box, theme, StyledCard } from "@advisable/donut";
+import { User } from "@styled-icons/feather";
+import FileUpload from "../FileUpload";
+// Queries
+import { UPDATE_PROFILE } from "../../queries";
+
+export const StyledAvatarCard = styled(StyledCard)`
+  z-index: 4;
+  position: relative;
+  transition: box-shadow 300ms;
+  background-color: ${theme.colors.neutral200};
+  overflow: hidden;
+  cursor: pointer;
+  box-shadow: 0px 8px 12px -4px ${rgba(theme.colors.neutral900, 0.04)},
+    0px 4px 20px -4px ${rgba(theme.colors.neutral900, 0.22)};
+
+  &:hover {
+    box-shadow: 0px 12px 24px -12px ${rgba(theme.colors.neutral900, 0.08)},
+      0px 24px 40px -24px ${rgba(theme.colors.neutral900, 0.3)};
+  }
+`;
+
+export const StyledAvatarImage = styled(Box)`
+  border-radius: 16px;
+  object-fit: cover;
+  opacity: ${(props) => (props.loaded ? 1 : 0)};
+  transition: opacity 400ms;
+`;
 
 function DefaultAvatar() {
   return (
