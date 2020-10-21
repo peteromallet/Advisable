b87b942847c2eed32c98b30877c4de852606918a
guandjoy <guandjoy@gmail.com>
2020-10-21 14:52:43 +0300

    Merge Desktop component into index root file

--- app/javascript/src/views/FreelancerProfileNew/index.js
+++ app/javascript/src/views/FreelancerProfileNew/index.js
@@ -1,23 +1,57 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
+// Hooks
+import useLogoURL from "src/components/ApplicationProvider/useLogoURL";
 import { useParams } from "react-router-dom";
+import useViewer from "src/hooks/useViewer";
+import { useQuery } from "@apollo/client";
+// Components
 import NotFound, { isNotFound } from "../NotFound";
-import useLogoURL from "../../components/ApplicationProvider/useLogoURL";
-import Loading from "../../components/Loading";
+import PreviousProjects from "./PreviousProjects";
+import Loading from "src/components/Loading";
+import Testimonials from "./Testimonials";
+import AboutSection from "./AboutSection";
+import { Box } from "@advisable/donut";
+import NoProjects from "./NoProjects";
+// Queries
 import { GET_PROFILE } from "./queries";
-import Desktop from "./Desktop";
 
 function FreelancerProfileNew() {
   useLogoURL("https://advisable.com");
   const params = useParams();
+  const viewer = useViewer();
+
   const { loading, data, error } = useQuery(GET_PROFILE, {
     variables: {
       id: params.id,
     },
   });
+
   if (loading) return <Loading />;
   if (isNotFound(error)) return <NotFound />;
-  return <Desktop data={data} />;
+
+  const isOwner = viewer?.id === data.specialist.id;
+  const hasReviews = data.specialist.reviews.length > 0;
+
+  return (
+    <Box
+      maxWidth={["100%", "100%", "100%", "960px"]}
+      mx={["12px", "32px", "32px", "auto"]}
+      mb="64px"
+    >
+      <AboutSection
+        specialist={data.specialist}
+        isOwner={isOwner}
+        viewer={viewer}
+      />
+      {data.specialist.profileProjects.length > 0 && (
+        <PreviousProjects data={data} isOwner={isOwner} />
+      )}
+      {data.specialist.profileProjects.length === 0 && (
+        <NoProjects data={data} isOwner={isOwner} />
+      )}
+      {hasReviews && <Testimonials reviews={data.specialist.reviews} />}
+    </Box>
+  );
 }
 
 export default FreelancerProfileNew;
