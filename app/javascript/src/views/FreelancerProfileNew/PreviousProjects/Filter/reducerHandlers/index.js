d2a09b9f3b489df57ff2e7d5b2893fb3c06462bf
guandjoy <guandjoy@gmail.com>
2020-10-22 21:19:22 +0300

    Move setSectionsRatio func under setLayout action

--- app/javascript/src/views/FreelancerProfileNew/PreviousProjects/Filter/reducerHandlers/index.js
+++ app/javascript/src/views/FreelancerProfileNew/PreviousProjects/Filter/reducerHandlers/index.js
@@ -1,3 +1,2 @@
-export { default as setSectionsRatio } from "./setSectionsRatio";
 export { default as handleSectionParams } from "./handleSectionParams";
 export { default as setLayout } from "./setLayout";
--- app/javascript/src/views/FreelancerProfileNew/PreviousProjects/Filter/reducerHandlers/setLayout/setSectionsRatio.js
+++ app/javascript/src/views/FreelancerProfileNew/PreviousProjects/Filter/reducerHandlers/setLayout/setSectionsRatio.js
@@ -3,15 +3,15 @@ import { sumBy } from "lodash-es";
 const setSectionsRatio = (state) => {
   const sectionKeys = Object.keys(state.sections);
   const ratioExist = sectionKeys.every((key) => state.sections[key].ratio);
-  if (ratioExist) return state;
+  if (ratioExist) return { ...state };
   const sectionsArea = sumBy(sectionKeys, (key) => state.sections[key].area);
   const percent = sectionsArea / 100;
-  const sectionsWithRatio = sectionKeys.reduce((acc, key) => {
+  const sections = sectionKeys.reduce((acc, key) => {
     // Calc and set ratio to each section
     const ratio = acc[key].area / percent;
     return { ...acc, [key]: { ...acc[key], ratio } };
   }, state.sections);
-  return { ...state, sections: sectionsWithRatio };
+  return { ...state, sections };
 };
 
 export default setSectionsRatio;
