00f2c160bb128f5676158dedcc54c561c1239857
Miha Rekar <miha.rekar@advisable.com>
2020-10-27 09:30:45 +0100

    Fix User and Specialist API creation

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,6 @@ class AccountsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   before_action :verify_key!
-  before_action :verify_email!, only: [:user, :specialist]
   before_action :create_account, only: [:user, :specialist]
 
   def me
@@ -12,45 +11,43 @@ class AccountsController < ApplicationController
   end
 
   def user
-    user = User.find_or_create_by!(account: account)
-
+    user = User.find_or_create_by(account: account)
     [:first_name, :last_name, :email, :airtable_id, :company_name].each do |key|
-      user.public_send("#{key}=", params[key]) if params[key].present?
+      user.public_send("#{key}=", params[key].strip) if params[key].present?
     end
     user.save!
 
     render json: {user_uid: user.uid, account_uid: account.uid}
   rescue ActiveRecord::RecordInvalid => e
     Raven.capture_exception(e, extra: {params: params})
+    render json: {error: "Something went wrong."}, status: :unprocessable_entity
   end
 
   def specialist
-    specialist = Specialist.find_or_create_by!(account: account)
-
+    specialist = Specialist.find_or_create_by(account: account)
     [:first_name, :last_name, :email, :airtable_id, :application_stage].each do |key|
-      specialist.public_send("#{key}=", params[key]) if params[key].present?
+      specialist.public_send("#{key}=", params[key].strip) if params[key].present?
     end
     specialist.save!
 
     render json: {specialist_uid: specialist.uid, account_uid: account.uid}
   rescue ActiveRecord::RecordInvalid => e
     Raven.capture_exception(e, extra: {params: params})
+    render json: {error: "Something went wrong."}, status: :unprocessable_entity
   end
 
   private
 
   def create_account
-    @account = Account.find_or_create_by!(email: account_params[:email])
-    account.update!(account_params)
+    @account = Account.find_or_create_by!(email: account_params[:email].strip)
+    [:first_name, :last_name].each do |key|
+      account.public_send("#{key}=", params[key].strip) if params[key].present?
+    end
+    account.save!
   end
 
   def account_params
-    params.require(:account).permit(:first_name, :last_name, :email)
-  end
-
-  def verify_email!
-    return if params[:email].present?
-    render json: {error: "Can not create without email."}, status: :unprocessable_entity
+    @account_params ||= params.require(:account).permit(:first_name, :last_name, :email)
   end
 
   def verify_key!
