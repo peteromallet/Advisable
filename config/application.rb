ca2e3773bab7525488dc6f01e23f93e94bdf4ff4
Miha Rekar <miha.rekar@advisable.com>
2020-10-29 16:29:43 +0100

    Send POST data to Sentry

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ if ENV['SENTRY_API_DSN']
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_API_DSN']
     config.current_environment = ENV['SENTRY_ENVIRONMENT']
+    config.processors -= [Raven::Processor::PostData]
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
