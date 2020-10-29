f963797c80062540f0360bba71c5839a2990b788
guandjoy <guandjoy@gmail.com>
2020-10-29 20:32:58 +0200

    Reprioritise variants of testimonials

--- app/javascript/src/views/FreelancerProfile/Testimonials/Review/Wide/Fourth.js
+++ app/javascript/src/views/FreelancerProfile/Testimonials/Review/Wide/Fourth.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text, Card, Avatar, theme } from "@advisable/donut";
+import { Box, Text, Avatar, theme } from "@advisable/donut";
 import { QuoteAltLeft } from "@styled-icons/boxicons-solid";
 
 function Fourth({ review }) {
@@ -7,77 +7,42 @@ function Fourth({ review }) {
   const atCompany = review.companyName && `at ${review.companyName}`;
   return (
     <Box my="l">
-      <Box display="flex" position="relative" alignItems="flex-start">
-        <Box
-          display="flex"
-          position="absolute"
-          top="m"
-          border={`4px solid ${theme.colors.neutral50}`}
-          borderRadius="50%"
-          zIndex="2"
-        >
+      <Box display="flex" alignItems="flex-start">
+        <Box display="flex" mr="l" position="relative">
           <Box width={100} height={100}>
             <Avatar size="xl" name={review.name} url={review.avatar} />
           </Box>
+          <Box zIndex="2" position="absolute" bottom="2px" right="-12px">
+            <QuoteAltLeft
+              size={38}
+              fill="rgba(255,255,255,0.6)"
+              stroke={theme.colors.blue500}
+              strokeWidth={1}
+            />
+          </Box>
         </Box>
-        <Box pl="4xl">
-          <Box bg="neutral100" p="2xl" pl="5xl" borderRadius="12px" mb="xs">
-            <Box position="relative">
-              <Text
-                fontStyle="italic"
-                fontSize="xl"
-                lineHeight="130%"
-                color="neutral800"
-              >
-                <Text
-                  as="span"
-                  fontSize="5xl"
-                  lineHeight="14px"
-                  color="yellow500"
-                  css={`
-                    font-size: 48px;
-                    color: ${theme.colors.blue500};
-                    position: absolute;
-                    top: 6px;
-                    left: -22px;
-                    opacity: 0.6;
-                    vertical-align: bottom;
-                  `}
-                >
-                  &quot;
-                </Text>
-                {review.comment}
-                <Text
-                  as="span"
-                  fontSize="36px"
-                  lineHeight="12px"
-                  color="yellow500"
-                  css={`
-                    font-size: 48px;
-                    color: ${theme.colors.blue500};
-                    position: relative;
-                    top: 6px;
-                    right: 4px;
-                    opacity: 0.6;
-                    vertical-align: bottom;
-                  `}
-                >
-                  &quot;
-                </Text>
+        <Box>
+          <Box bg="neutral100" p="l" borderRadius="12px" mb="xs">
+            <Text
+              fontStyle="italic"
+              fontSize="xl"
+              lineHeight="120%"
+              color="neutral800"
+            >
+              &quot;{review.comment}&quot;
+            </Text>
+            <Text
+              fontWeight="medium"
+              fontSize="l"
+              color="neutral600"
+              pt="m"
+              //   pl="l"
+            >
+              — {review.name}
+              <Text as="span" color="neutral600" fontSize="l" mb="s" pl="xs">
+                {role} {atCompany}
               </Text>
-              <Text
-                fontWeight="medium"
-                fontSize="l"
-                color="neutral600"
-                pt="m"
-                //   pl="l"
-              >
-                — {review.name}
-                <Text as="span" color="neutral600" fontSize="l" mb="s" pl="xs">
-                  {role} {atCompany}
-                </Text>
-              </Text>
-            </Box>
+            </Text>
           </Box>
         </Box>
       </Box>
