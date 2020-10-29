3e817bac0469d44e0d6ec8544d88e6dcd7ad3ac7
Miha Rekar <miha.rekar@advisable.com>
2020-10-29 16:29:22 +0100

    RuboCop stuff

--- config/application.rb
+++ config/application.rb
@@ -8,25 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Advisable
   class Application < Rails::Application
+    ORIGIN_HOST = ENV['ORIGIN'] || "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
     config.active_job.queue_adapter = :sidekiq
-
-    config.action_mailer.default_url_options = {
-      host: ENV['ORIGIN'] || "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
-    }
-    config.action_mailer.asset_host =
-      ENV['ORIGIN'] || "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
-    config.action_mailer.preview_path = "#{Rails.root}/test/mailers/previews"
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-
+    config.action_mailer.default_url_options = {host: ORIGIN_HOST}
+    config.action_mailer.asset_host = ORIGIN_HOST
+    config.action_mailer.preview_path = Rails.root.join("test/mailers/previews")
     config.action_mailbox.ingress = :sendgrid
+    config.assets.paths << Rails.root.join("app/assets/fonts")
   end
 end
 
@@ -34,7 +25,6 @@ if ENV['SENTRY_API_DSN']
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_API_DSN']
     config.current_environment = ENV['SENTRY_ENVIRONMENT']
-    config.sanitize_fields =
-      Rails.application.config.filter_parameters.map(&:to_s)
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
