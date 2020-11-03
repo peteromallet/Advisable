a363c9614749d5f2030af536b6a5400bb9436296
Thomas Cullen <thomascullen92@gmail.com>
2020-11-03 10:04:14 +0000

    Assert password change worked

--- spec/system/specialiset_settings_spec.rb
+++ spec/system/specialiset_settings_spec.rb
@@ -63,5 +63,6 @@ RSpec.describe 'Specialist settings' do
     fill_in "passwordConfirmation", with: "changed123"
     click_on "Update password"
     expect(page).to have_content("password has been updated")
+    expect(account.authenticate("changed123")).to be_truthy
   end
 end
