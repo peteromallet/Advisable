392f4b5f5f59f5dedfdb08ff25848a97c7db1b4a
Miha Rekar <miha.rekar@advisable.com>
2020-11-09 18:31:32 +0100

    Better geocoder guard

--- app/jobs/geocode_user_job.rb
+++ app/jobs/geocode_user_job.rb
@@ -6,14 +6,12 @@ class GeocodeUserJob < ApplicationJob
   def perform(id, ip)
     user = User.find(id)
     return unless user
+
     results = geocode_ip(ip)
-    return unless results
-    country =
-      Country.where.not(alpha2: nil).find_by_alpha2(results.first.country)
-    user.update(
-      country: country,
-      address: { city: results.first.city, country: results.first.country }
-    )
+    return if results.blank? || !results.first.respond_to?(:country)
+
+    country = Country.where.not(alpha2: nil).find_by(alpha2: results.first.country)
+    user.update(country: country, address: {city: results.first.city, country: results.first.country})
     user.sync_to_airtable
   end
 
@@ -21,9 +19,9 @@ class GeocodeUserJob < ApplicationJob
 
   def geocode_ip(ip)
     if Rails.env.development?
-      return [OpenStruct.new(country: 'IE', city: 'Dublin')]
+      [OpenStruct.new(country: 'IE', city: 'Dublin')]
+    else
+      Geocoder.search(ip)
     end
-
-    Geocoder.search(ip)
   end
 end
