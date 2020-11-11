7a9390c071dcf4b74000ed9479cc53c1dcd3e5e7
Thomas Cullen <thomascullen92@gmail.com>
2020-11-11 16:32:22 +0000

    Improve validation

--- app/javascript/guild/components/ComposerModal/validationSchemas.js
+++ app/javascript/guild/components/ComposerModal/validationSchemas.js
@@ -1,5 +1,6 @@
 import { object, string } from "yup";
 
 export const yourPostValidationSchema = object({
-  title: string().required("Please add a title for this post").min(8),
+  title: string().required("Please add a title for this post"),
+  body: string().required("Please add some content for this post"),
 });
