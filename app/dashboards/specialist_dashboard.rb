4b9f1ea35d47f7dc59537961a72e97e94fb61f3b
Thomas Cullen <thomascullen92@gmail.com>
2020-11-12 10:04:30 +0000

    Remove account fields in admin UI causing n+1's

--- app/dashboards/specialist_dashboard.rb
+++ app/dashboards/specialist_dashboard.rb
@@ -55,7 +55,6 @@ class SpecialistDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    account
     airtable_id
     skills
     linkedin
