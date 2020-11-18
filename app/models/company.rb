9b5d2284fc684525798443b455e7e2c59dacb8f6
Miha Rekar <miha.rekar@advisable.com>
2020-11-18 11:36:12 +0100

    Create Company model

---
+++ app/models/company.rb
@@ -0,0 +1,13 @@
+class Company < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_075912) do
+ActiveRecord::Schema.define(version: 2020_11_18_103535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -236,6 +236,13 @@ ActiveRecord::Schema.define(version: 2020_11_17_075912) do
     t.datetime "updated_at", null: false
     t.string "domain"
     t.index ["airtable_id"], name: "index_clients_on_airtable_id"
+  end
+
+  create_table "companies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "consultations", force: :cascade do |t|
