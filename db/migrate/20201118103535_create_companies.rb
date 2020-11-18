73b17458f99ef2e7d5bf47673c6e13eaaa712ece
Miha Rekar <miha.rekar@advisable.com>
2020-11-18 15:00:08 +0100

    Type is no good since it clashes with Ruby's #type

--- db/migrate/20201118103535_create_companies.rb
+++ db/migrate/20201118103535_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies, id: :uuid do |t|
       t.string :name
-      t.string :type
+      t.string :kind
 
       t.timestamps
     end
