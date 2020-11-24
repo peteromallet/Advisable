04e077dfc6b6ed304adbd409985cce8046736155
Miha Rekar <miha.rekar@advisable.com>
2020-11-24 10:34:48 +0100

    Address PR review points

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -5,10 +5,6 @@ class Mutations::BaseMutation < GraphQL::Schema::RelayClassicMutation
 
   private
 
-  # TODO: Thomas: do you have a better name suggestion?
-  # just `responsible_for`?
-  # or `with_current_account`?
-  # or something else?
   def current_account_responsible_for(&block)
     Logidze.with_responsible(current_account_id, &block)
   end
