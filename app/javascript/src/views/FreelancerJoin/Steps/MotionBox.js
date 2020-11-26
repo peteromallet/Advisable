5039f7d729d8d2201b053b8b9850fbfaac909536
guandjoy <guandjoy@gmail.com>
2020-11-27 00:34:29 +0200

    Refactor cardAnimation framer-motion variant

--- app/javascript/src/views/FreelancerJoin/Steps/MotionBox.js
+++ app/javascript/src/views/FreelancerJoin/Steps/MotionBox.js
@@ -1,7 +1,31 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Box, useBreakpoint } from "@advisable/donut";
-import cardAnimations from "./framerVariant";
+
+const cardAnimations = {
+  enter: ({ largeScreen, forwards }) => {
+    return {
+      x: largeScreen ? 0 : forwards ? 80 : -80,
+      y: largeScreen ? (forwards ? 80 : -80) : 0,
+      opacity: 0,
+    };
+  },
+  center: {
+    x: 0,
+    y: 0,
+    zIndex: 1,
+    opacity: 1,
+  },
+  exit: ({ largeScreen, forwards }) => {
+    return {
+      y: largeScreen ? (forwards ? -80 : 80) : 0,
+      x: largeScreen ? 0 : forwards ? -80 : 80,
+      opacity: 0,
+      zIndex: 1,
+      transition: { duration: 0.3 },
+    };
+  },
+};
 
 export default function MotionBox({ children, forwards, ...props }) {
   const largeScreen = useBreakpoint("lUp");
