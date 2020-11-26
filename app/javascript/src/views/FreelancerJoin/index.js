e757913555e4ad53e77d62ecafae36a789a56b40
guandjoy <guandjoy@gmail.com>
2020-11-26 13:16:56 +0200

    Implement redirection if client/specialist logged in

--- app/javascript/src/views/FreelancerJoin/index.js
+++ app/javascript/src/views/FreelancerJoin/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import queryString from "query-string";
 import { AnimatePresence } from "framer-motion";
-import { Switch, useLocation } from "react-router";
+import { Switch, useHistory, useLocation } from "react-router";
+import useViewer from "src/hooks/useViewer";
 import useSteps from "src/hooks/useSteps";
 import steps from "./Steps";
 import { Box, SharedOrbitsProvider } from "@advisable/donut";
@@ -8,6 +10,18 @@ import { Box, SharedOrbitsProvider } from "@advisable/donut";
 function FreelancerJoin() {
   const { routes } = useSteps(steps);
   const location = useLocation();
+  const history = useHistory();
+  const viewer = useViewer();
+  const project_id = queryString.parse(location.search)?.pid;
+
+  // Redirect to opportunity if specialist logged in
+  if (viewer?.isSpecialist && project_id) {
+    history.replace(`/opportunities/${project_id}`);
+  }
+  // Redirect to root if client or specialist logged in
+  if (viewer?.isClient || (viewer?.isSpecialist && !project_id)) {
+    history.replace("/");
+  }
 
   return (
     <Box height="100vh" width="100%" position="relative">
