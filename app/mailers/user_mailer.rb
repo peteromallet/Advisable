cc60144bd3f1a8c837568e721d5618850f3fed8f
Miha Rekar <miha.rekar@advisable.com>
2020-11-27 10:49:15 +0100

    Add invited_by_manager to UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,12 @@
+module DeleteThisClassHelper
+  def password_link_goes_here_thomas(param)
+    "http://advisable.com/delete-me"
+  end
+end
+
 class UserMailer < ApplicationMailer
+  helper DeleteThisClassHelper
+
   def confirm(uid:, token:)
     @user = User.find_by(uid: uid)
     @token = token
@@ -8,5 +16,11 @@ class UserMailer < ApplicationMailer
   def interview_reschedule_request(interview)
     @interview = interview
     mail(from: interview.user.sales_person.email_with_name, to: interview.user.account.email, subject: 'Interview Reschedule Request')
+  end
+
+  def invited_by_manager(manager, user)
+    @manager = manager
+    @user = user
+    mail(to: user.account.email, subject: "#{manager.account.first_name} invited you to Advisable")
   end
 end
