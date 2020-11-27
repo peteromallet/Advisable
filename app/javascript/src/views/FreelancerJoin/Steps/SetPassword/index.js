71aa987d41c2c90e5e50bc67f788da0e01c3a187
guandjoy <guandjoy@gmail.com>
2020-11-27 16:57:46 +0200

    Create HaveAccount component

--- app/javascript/src/views/FreelancerJoin/Steps/SetPassword/index.js
+++ app/javascript/src/views/FreelancerJoin/Steps/SetPassword/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import queryString from "query-string";
-import { Box, Card, Text, Button } from "@advisable/donut";
+import { Box, Card, Text } from "@advisable/donut";
 import { Form, Formik } from "formik";
 import SubmitButton from "components/SubmitButton";
 import FormField from "components/FormField";
 import validationSchema from "./validationSchema";
 import { useHistory, useLocation } from "react-router";
 import MotionBox from "../MotionBox";
+import HaveAccount from "../HaveAccount";
 
 export default function SetPassword({ nextStep, forwards }) {
   const location = useLocation();
@@ -23,58 +24,53 @@ export default function SetPassword({ nextStep, forwards }) {
     history.push(nextPath);
   };
   return (
-    <>
-      <MotionBox forwards={forwards} py="xl" zIndex={2} position="relative">
-        <Card padding="2xl" width={650} marginX="auto">
-          <Box mb="xl">
-            <Text as="h2" fontSize="4xl" mb="xs" color="neutral900">
-              Welcome to Advisable!
-            </Text>
-            <Text as="p" color="neutral800" fontSize="m" lineHeight="m">
-              Set the password on your Advisable account to see the status of
-              your applications on Advisable and manage your work.
-            </Text>
-          </Box>
-          <Formik
-            onSubmit={handleSubmit}
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-          >
-            <Form>
-              <Box mb="m">
-                <FormField
-                  type="password"
-                  name="password"
-                  placeholder="assistanttotheregionalmanager1"
-                  label="Password"
-                />
-              </Box>
-              <Box mb="2xl">
-                <FormField
-                  type="password"
-                  name="passwordConfirmation"
-                  placeholder="assistanttotheregionalmanager1"
-                  label="Confirm password"
-                />
-              </Box>
-              <Box display="flex">
-                <SubmitButton size="l">Get Started</SubmitButton>
-                <Box ml="auto" display="flex" flexDirection="column">
-                  <Text>Already have an account?</Text>
-                  <Button
-                    onClick={() => history.push("/login")}
-                    size="m"
-                    variant="ghost"
-                    ml="auto"
-                  >
-                    Login
-                  </Button>
-                </Box>
-              </Box>
-            </Form>
-          </Formik>
-        </Card>
-      </MotionBox>
-    </>
+    <MotionBox
+      forwards={forwards}
+      py="xl"
+      zIndex={2}
+      position="relative"
+      my="auto"
+      pb={20}
+    >
+      <Card padding="2xl" width={650} marginX="auto">
+        <Box mb="xl">
+          <Text as="h2" fontSize="4xl" mb="xs" color="neutral900">
+            Welcome to Advisable!
+          </Text>
+          <Text as="p" color="neutral800" fontSize="m" lineHeight="m">
+            Set the password on your Advisable account to see the status of your
+            applications on Advisable and manage your work.
+          </Text>
+        </Box>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+        >
+          <Form>
+            <Box mb="m">
+              <FormField
+                type="password"
+                name="password"
+                placeholder="assistanttotheregionalmanager1"
+                label="Password"
+              />
+            </Box>
+            <Box mb="2xl">
+              <FormField
+                type="password"
+                name="passwordConfirmation"
+                placeholder="assistanttotheregionalmanager1"
+                label="Confirm password"
+              />
+            </Box>
+            <Box display="flex">
+              <SubmitButton size="l">Get Started</SubmitButton>
+              <HaveAccount />
+            </Box>
+          </Form>
+        </Formik>
+      </Card>
+    </MotionBox>
   );
 }
--- app/javascript/src/views/FreelancerJoin/Steps/StartApplication/index.js
+++ app/javascript/src/views/FreelancerJoin/Steps/StartApplication/index.js
@@ -5,11 +5,12 @@ import { useQuery } from "@apollo/client";
 import { useHistory, useLocation } from "react-router";
 import FormField from "src/components/FormField";
 import SubmitButton from "src/components/SubmitButton";
-import { Box, Card, Text, Input, Button } from "@advisable/donut";
+import { Box, Card, Text, Input } from "@advisable/donut";
 import validationSchema from "./validationSchema";
 import Description from "./Description";
 import { GET_PROJECT } from "../queries";
 import MotionBox from "../MotionBox";
+import HaveAccount from "../HaveAccount";
 
 export default function StartApplication({ nextStep, forwards }) {
   const history = useHistory();
@@ -38,59 +39,53 @@ export default function StartApplication({ nextStep, forwards }) {
   };
 
   return (
-    <>
-      <MotionBox forwards={forwards} py="xl" zIndex={2} position="relative">
-        <Card padding="2xl" width={650} marginX="auto">
-          <Box mb={8}>
-            <Description project={data?.project} />
-            {project_id && error && (
-              <Text color="red400" pt={2}>
-                The project you&apos;ve tried to apply is not available.
-              </Text>
-            )}
-          </Box>
-          <Formik
-            onSubmit={handleSubmit}
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-          >
-            <Form>
-              <Box mb="m">
-                <FormField
-                  as={Input}
-                  name="fullName"
-                  placeholder="Dwight Schrutt"
-                  label="Full Name"
-                />
-              </Box>
-              <Box mb="2xl">
-                <FormField
-                  as={Input}
-                  name="email"
-                  placeholder="dwight@dundermifflin.com"
-                  label="Email"
-                />
-              </Box>
-              <Box display="flex">
-                <SubmitButton size="l" variant="dark">
-                  Get Started
-                </SubmitButton>
-                <Box ml="auto" display="flex" flexDirection="column">
-                  <Text>Already have an account?</Text>
-                  <Button
-                    onClick={() => history.push("/login")}
-                    size="m"
-                    variant="ghost"
-                    ml="auto"
-                  >
-                    Login
-                  </Button>
-                </Box>
-              </Box>
-            </Form>
-          </Formik>
-        </Card>
-      </MotionBox>
-    </>
+    <MotionBox
+      forwards={forwards}
+      zIndex={2}
+      position="relative"
+      my="auto"
+      pb={20}
+    >
+      <Card padding="2xl" width={650} marginX="auto">
+        <Box mb={8}>
+          <Description project={data?.project} />
+          {project_id && error && (
+            <Text color="red400" pt={2}>
+              The project you&apos;ve tried to apply is not available.
+            </Text>
+          )}
+        </Box>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+        >
+          <Form>
+            <Box mb="m">
+              <FormField
+                as={Input}
+                name="fullName"
+                placeholder="Dwight Schrutt"
+                label="Full Name"
+              />
+            </Box>
+            <Box mb="2xl">
+              <FormField
+                as={Input}
+                name="email"
+                placeholder="dwight@dundermifflin.com"
+                label="Email"
+              />
+            </Box>
+            <Box display="flex">
+              <SubmitButton size="l" variant="dark">
+                Get Started
+              </SubmitButton>
+              <HaveAccount />
+            </Box>
+          </Form>
+        </Formik>
+      </Card>
+    </MotionBox>
   );
 }
