5501e17c9ed06eb58d0616bdb807402ad57c086c
Thomas Cullen <thomascullen92@gmail.com>
2020-11-30 15:17:13 +0000

    Update header navigation

--- app/javascript/guild/components/Header/index.js
+++ app/javascript/guild/components/Header/index.js
@@ -1,39 +1,28 @@
 import React from "react";
-import { rgba } from "polished";
-import styled from "styled-components";
+import { Home, Chat } from "@styled-icons/heroicons-solid";
 import { useQuery, useMutation } from "@apollo/client";
-import { useHistory, useLocation } from "react-router-dom";
-import { theme, Box, Link } from "@advisable/donut";
-import logo from "@advisable-main/components/Header/logo";
+import { NavLink, Link } from "react-router-dom";
+import { Box } from "@advisable/donut";
+import logo from "@advisable-main/components/Header/logo.svg";
 import { useToggle } from "@guild/hooks/useToggle";
-import { Notification, Messages, Menu } from "@guild/icons";
+import { Notification, Menu } from "@guild/icons";
 import Notifications from "@guild/components/Notifications";
 import UserMenu from "@guild/components/UserMenu";
-import { NavIcon } from "./styles";
+import {
+  StyledHeader,
+  StyledHeaderLink,
+  StyledHeaderBadge,
+  NavIcon,
+} from "./styles";
 import { GuildBox } from "@guild/styles";
 import { GUILD_LAST_READ_QUERY } from "./queries";
 import { GUILD_UPDATE_LAST_READ } from "./mutations";
 
-const StyledHeader = styled(Box)`
-  z-index: 5;
-  height: 58px;
-  width: 100%;
-  color: white;
-  display: flex;
-  position: fixed;
-  align-items: center;
-  justify-content: space-between;
-  background: ${theme.colors.blue800};
-  box-shadow: 0 1px 2px ${rgba(theme.colors.neutral900, 0.3)};
-`;
-
 const TWO_MINUTES = 120000;
 
 const Header = () => {
-  const location = useLocation();
   const [notificationsOpen, toggleNotifications] = useToggle();
   const [userMenuOpen, toggleUserMenu] = useToggle();
-  const history = useHistory();
 
   const { data: lastReadData } = useQuery(GUILD_LAST_READ_QUERY, {
     pollInterval: TWO_MINUTES,
@@ -48,10 +37,6 @@ const Header = () => {
       });
     },
   });
-
-  const handleMessages = () => {
-    history.push("/messages");
-  };
 
   const handleNotifications = () => {
     toggleNotifications();
@@ -72,27 +57,36 @@ const Header = () => {
   const handleUpdateLastRead = async (input) =>
     await guildUpdateLastRead({ variables: { input } });
 
-  const messagesOpen = location.pathname.match(/messages(?:.*)/);
+  const unreadMessages = lastReadData?.viewer?.guildUnreadMessages;
 
   return (
     <>
       <Notifications open={notificationsOpen} />
       <UserMenu open={userMenuOpen} onToggle={toggleUserMenu} />
       <StyledHeader px="lg">
-        <Box display="flex">
-          <Link to={"/"}>
-            <img src={logo} alt="" />
-          </Link>
+        <Box display="flex" alignItems="center">
+          <Box mr={8}>
+            <Link to="/">
+              <img src={logo} alt="" />
+            </Link>
+          </Box>
+
+          <nav>
+            <StyledHeaderLink as={NavLink} to="/feed">
+              <Home />
+              Feed
+            </StyledHeaderLink>
+            <StyledHeaderLink as={NavLink} to="/messages">
+              <Chat />
+              {unreadMessages ? (
+                <StyledHeaderBadge top="4px" left="32px" />
+              ) : null}
+              Messages
+            </StyledHeaderLink>
+          </nav>
         </Box>
 
         <GuildBox spaceChildrenHorizontal={24} display="flex">
-          <NavIcon
-            unread={lastReadData?.viewer?.guildUnreadMessages && !messagesOpen}
-            onClick={handleMessages}
-            open={messagesOpen}
-          >
-            <Messages />
-          </NavIcon>
           <NavIcon
             unread={lastReadData?.viewer?.guildUnreadNotifications}
             open={notificationsOpen}
