f073b122c25c532adac6ff1a36b4ea7aacc662da
Thomas Cullen <thomascullen92@gmail.com>
2020-11-30 15:17:13 +0000

    Update header navigation

--- app/javascript/guild/components/Header/styles.js
+++ app/javascript/guild/components/Header/styles.js
@@ -1,5 +1,57 @@
+import { rgba } from "polished";
+import { position } from "styled-system";
 import styled, { css } from "styled-components";
-import { theme } from "@advisable/donut";
+import { theme, Box } from "@advisable/donut";
+
+export const StyledHeader = styled(Box)`
+  z-index: 5;
+  height: 58px;
+  width: 100%;
+  color: white;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: space-between;
+  background: ${theme.colors.blue800};
+  box-shadow: 0 1px 2px ${rgba(theme.colors.neutral900, 0.3)};
+`;
+
+export const StyledHeaderLink = styled.a`
+  color: ${rgba(theme.colors.blue100, 0.6)};
+  align-items: center;
+  display: inline-flex;
+  padding: 6px 12px;
+  border-radius: 12px;
+  margin-right: 8px;
+  position: relative;
+
+  &:hover {
+    color: white;
+    opacity: 1;
+  }
+
+  &.active {
+    opacity: 1;
+    color: white;
+    background: ${rgba("#FFF", 0.1)};
+  }
+
+  svg {
+    width: 24px;
+    height: 24px;
+    margin-right: 6px;
+  }
+`;
+
+export const StyledHeaderBadge = styled.div`
+  ${position};
+
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  position: absolute;
+  background: ${theme.colors.froly100};
+`;
 
 export const NavIcon = styled.div`
   position: relative;
