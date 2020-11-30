d518873bf20230e1b705fbf077b7ccb5465eb029
guandjoy <guandjoy@gmail.com>
2020-11-30 12:43:22 +0200

    Refactor link to login path

--- app/javascript/src/views/FreelancerJoin/Steps/HaveAccount.js
+++ app/javascript/src/views/FreelancerJoin/Steps/HaveAccount.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { useHistory } from "react-router";
 import { Box, Link, Text } from "@advisable/donut";
 
 export default function HaveAccount() {
-  const history = useHistory();
   return (
     <Box ml="auto" display="flex" flexDirection="column">
       <Text color="neutral600" mb={1.5}>
         Already have an account?
       </Text>
-      <Link
-        onClick={() => history.push("/login")}
-        fontWeight="medium"
-        color="blue500"
-        ml="auto"
-      >
+      <Link to="/login" fontWeight="medium" color="blue500" ml="auto">
         Login
       </Link>
     </Box>
