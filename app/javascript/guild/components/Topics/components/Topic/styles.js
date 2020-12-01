f2b7d9cd443137001cd9b243d97f89096faebc57
Thomas Cullen <thomascullen92@gmail.com>
2020-12-01 17:45:30 +0000

    Use icon for hashtag

--- app/javascript/guild/components/Topics/components/Topic/styles.js
+++ app/javascript/guild/components/Topics/components/Topic/styles.js
@@ -3,25 +3,38 @@ import { GuildBox } from "@guild/styles";
 import styled, { css } from "styled-components";
 
 export const activeTopicStyle = css`
-  background-color: ${theme.colors.lavender};
-  color: ${theme.colors.calatlinaBlue50};
+  background-color: ${theme.colors.neutral200};
+  color: ${theme.colors.blue700};
+
+  svg {
+    color: ${theme.colors.blue700};
+  }
 `;
 
 export const StyledTopic = styled(GuildBox)`
   font-size: 1rem;
   overflow: hidden;
-  padding: 12px 12px;
+  padding: 10px 12px;
   margin-left: -12px;
-  border-radius: 20px;
+  border-radius: 25px;
   white-space: nowrap;
   text-overflow: ellipsis;
+  min-width: 0;
+  user-select: none;
   color: ${theme.colors.neutral800};
+
+  svg {
+    width: 16px;
+    height: 16px;
+    margin-right: 2px;
+    color: ${theme.colors.neutral500};
+  }
 
   ${({ selected }) => selected && activeTopicStyle}
 
   &:hover {
     cursor: pointer;
-    transition: background-color 220ms;
+    transition: background-color 100ms;
     text-decoration: none;
     ${activeTopicStyle}
   }
