0fb525365cab45bb2af1533f334d52c650e759d5
Thomas Cullen <thomascullen92@gmail.com>
2020-12-02 15:45:27 +0000

    Improve notifications designs

--- app/javascript/guild/components/Notifications/styles.js
+++ app/javascript/guild/components/Notifications/styles.js
@@ -1,45 +1,10 @@
 import styled from "styled-components";
-import { Box, theme } from "@advisable/donut";
-import { flex, breakpoint } from "@guild/styles";
-import ClampLines from "react-clamp-lines";
-
-const { space, colors } = theme;
-
-export const NotificationDropdown = styled(Box)`
-  ${breakpoint.smallAndUp} {
-    right: 76px;
-    padding: 24px;
-    width: 442px;
-  }
-  max-height: 80%;
-  right: 0;
-  width: 100%;
-  padding: 8px;
-  z-index: 3;
-  top: 68px;
-  background: white;
-  position: fixed;
-  border-radius: 4px;
-  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.5);
-  transition: opacity 250ms cubic-bezier(0, 1, 0.4, 1),
-    transform 250ms cubic-bezier(0.18, 1.25, 0.4, 1);
-
-  opacity: ${(props) => (props.open ? "1" : "0")};
-  pointer-events: ${(props) => (props.open ? "all" : "none")};
-  transform: ${(props) => (props.open ? "scale(1)" : "scale(0.7)")};
-
-  ${flex.spaceChildrenVertical(space.m)}
-  overflow: scroll;
-`;
+import { Box } from "@advisable/donut";
 
 export const NotificationItem = styled(Box)`
   display: flex;
-  flex-shrink: 0;
-  max-width: 392px;
+  min-width: 0;
+  width: 100%;
+  flex-wrap: wrap;
   align-items: center;
-`;
-
-export const CommentNotificationBody = styled(ClampLines)`
-  color: ${colors.catalinaBlue100};
-  font-size: 14px;
 `;
