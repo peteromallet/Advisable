789854d8c2cd78c302f78fd8e5e27b6862a027cc
Thomas Cullen <thomascullen92@gmail.com>
2020-12-04 18:39:33 +0000

    Dont show navigation or notifications in header if not logged in

--- app/javascript/guild/components/Header/index.js
+++ app/javascript/guild/components/Header/index.js
@@ -6,6 +6,7 @@ import { Box } from "@advisable/donut";
 import logo from "@advisable-main/components/Header/logo.svg";
 import CurrentUser from "./CurrentUser";
 import MobileNavigation from "./MobileNavigation";
+import useViewer from "src/hooks/useViewer";
 import {
   StyledHeader,
   StyledHeaderLink,
@@ -18,10 +19,12 @@ import Notifications from "./Notifications";
 const TWO_MINUTES = 120000;
 
 const Header = () => {
+  const viewer = useViewer();
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
   const { data: lastReadData } = useQuery(GUILD_LAST_READ_QUERY, {
     pollInterval: TWO_MINUTES,
+    skip: !viewer,
   });
 
   const unreadMessages = lastReadData?.viewer?.guildUnreadMessages;
@@ -50,25 +53,29 @@ const Header = () => {
             setMobileNavOpen={setMobileNavOpen}
           />
 
-          <Box as="nav" display={{ _: "none", m: "block" }}>
-            <StyledHeaderLink as={NavLink} to="/feed">
-              <Home />
-              Feed
-            </StyledHeaderLink>
-            <StyledHeaderLink as={NavLink} to="/messages">
-              <Chat />
-              {unreadMessages ? (
-                <StyledHeaderBadge top="4px" left="32px" />
-              ) : null}
-              Messages
-            </StyledHeaderLink>
-          </Box>
+          {viewer ? (
+            <Box as="nav" display={{ _: "none", m: "block" }}>
+              <StyledHeaderLink as={NavLink} to="/feed">
+                <Home />
+                Feed
+              </StyledHeaderLink>
+              <StyledHeaderLink as={NavLink} to="/messages">
+                <Chat />
+                {unreadMessages ? (
+                  <StyledHeaderBadge top="4px" left="32px" />
+                ) : null}
+                Messages
+              </StyledHeaderLink>
+            </Box>
+          ) : null}
         </Box>
 
         <Box display="flex" alignItems="center">
-          <Box mr={4}>
-            <Notifications hasUnread={hasUnreadNotifications} />
-          </Box>
+          {viewer ? (
+            <Box mr={4}>
+              <Notifications hasUnread={hasUnreadNotifications} />
+            </Box>
+          ) : null}
           <CurrentUser />
         </Box>
       </StyledHeader>
