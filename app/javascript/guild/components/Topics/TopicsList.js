035a94af88a1781ed0624d9898b5d988f121bfb9
Geoff Ereth <geoffereth@gmail.com>
2020-12-04 11:27:57 -0800

    Adds button to reset all selected guild topics

--- app/javascript/guild/components/Topics/TopicsList.js
+++ app/javascript/guild/components/Topics/TopicsList.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Stack } from "@advisable/donut";
+import { Stack, Button } from "@advisable/donut";
+import { X } from "@styled-icons/feather";
 import { useToggle } from "@guild/hooks/useToggle";
 import ShowMore from "@guild/components/ShowMore";
+import { feedStore } from "@guild/stores/Feed";
 import Topic from "./components/Topic";
 
 export default function TopicsList({ topics }) {
   const [moreTopics, toggleMoreTopics] = useToggle();
+  const [selectedTopics, resetTopics] = feedStore((store) => [
+    store.selectedTopics,
+    store.resetTopics,
+  ]);
 
   return (
     <>
+      {!!selectedTopics.length && (
+        <Button
+          size="s"
+          mb={4}
+          variant="subtle"
+          onClick={resetTopics}
+          prefix={<X />}
+        >
+          Clear All
+        </Button>
+      )}
       <Stack spacing={0.5} marginBottom="lg">
         {topics.map(
           (topic, key) =>
