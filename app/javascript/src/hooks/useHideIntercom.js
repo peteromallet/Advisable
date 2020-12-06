0b3c7f4aea3481e55a9ae1f676c6af9d66c3449c
Thomas Cullen <thomascullen92@gmail.com>
2020-12-06 15:11:15 +0000

    Hide intercom

---
+++ app/javascript/src/hooks/useHideIntercom.js
@@ -0,0 +1,17 @@
+import { useLayoutEffect } from "react";
+
+export default function useHideIntercom() {
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      window.Intercom("update", {
+        hide_default_launcher: true,
+      });
+    }, 500);
+
+    return () => {
+      window.Intercom("update", {
+        hide_default_launcher: false,
+      });
+    };
+  }, []);
+}
