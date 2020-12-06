613dcf75f4f67e525700edd75248f13359d37397
Thomas Cullen <thomascullen92@gmail.com>
2020-12-06 16:16:23 +0000

    Show notice to non managers when trying to start working with a freelancer

--- app/javascript/src/views/BookingSetup/RequiresTeamManager.js
+++ app/javascript/src/views/BookingSetup/RequiresTeamManager.js
@@ -1,20 +1,22 @@
 import React from "react";
 import CopyURL from "src/components/CopyURL";
-import { Card, Text, Paragraph } from "@advisable/donut";
+import { Box, Card, Text, Paragraph } from "@advisable/donut";
 
 export default function RequiresTeamManager({ data }) {
   const name = data.application.specialist.name;
 
   return (
-    <Card padding={12} textAlign="center">
-      <Text fontSize="xl" fontWeight="medium" mb={2} letterSpacing="-0.02rem">
-        Your company has not setup project payments yet.
-      </Text>
-      <Paragraph mb={6}>
-        Please send the following link to a team manager to setup project
-        payments and start working with {name}.
-      </Paragraph>
-      <CopyURL>{window.location.href}</CopyURL>
-    </Card>
+    <Box maxWidth="600px" mx="auto" py={12}>
+      <Card padding={12} textAlign="center">
+        <Text fontSize="xl" fontWeight="medium" mb={2} letterSpacing="-0.02rem">
+          Only managers can start working with freelancers
+        </Text>
+        <Paragraph mb={6}>
+          Please send the following link to a team manager to setup project
+          payments and start working with {name}.
+        </Paragraph>
+        <CopyURL>{window.location.href}</CopyURL>
+      </Card>
+    </Box>
   );
 }
