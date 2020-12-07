ebaed4a49af52898cd668d26aa70f16f29644bb2
Thomas Cullen <thomascullen92@gmail.com>
2020-12-07 14:35:32 +0000

    Use new message-id in each chat mailer notification

--- app/mailboxes/guild_chat_replies_mailbox.rb
+++ app/mailboxes/guild_chat_replies_mailbox.rb
@@ -3,12 +3,13 @@ class GuildChatRepliesMailbox < ApplicationMailbox
     reply = EmailReplyParser.parse_reply(mail_body(mail))
 
     encoded_regexp = /(?<encoded>.*)@#{ENV.fetch('GUILD_REPLIES_DOMAIN')}/
+    message_id_regexp = /(?<encoded>.*)\.\d*/
 
     @encoded = mail.to.first.match(encoded_regexp)
     unless @encoded
       # Fall back to the References header
       field = mail.header.fields.find { |f| f.name.include?("References") }
-      @encoded = field.unparsed_value.match(/^<#{encoded_regexp}/)
+      @encoded = field.unparsed_value.match(/^<#{message_id_regexp}/)
     end
 
     if (chat_metadata = @encoded.try(:[], :encoded))
