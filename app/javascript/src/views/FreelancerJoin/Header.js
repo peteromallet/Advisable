b2e6ebc6b82fa2cdc2fddfe3153a6d3b74f7c3b7
guandjoy <guandjoy@gmail.com>
2020-12-09 13:50:31 +0200

    Use AdvisableLogo from src/components/Logo

--- app/javascript/src/views/FreelancerJoin/Header.js
+++ app/javascript/src/views/FreelancerJoin/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link, Box } from "@advisable/donut";
-import AdvisableLogo from "./AdvisableLogo";
+import AdvisableLogo from "src/components/Logo";
 
 export default function Header() {
   return (
     <Box gridArea="header" alignSelf="end">
       <Link.External href="https://advisable.com/">
-        <AdvisableLogo alt="Advisable logo" />
+        <AdvisableLogo alt="Advisable logo" white="true" />
       </Link.External>
     </Box>
   );
