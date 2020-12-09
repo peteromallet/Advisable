58024658a9bda9d3c11af80ca2e2d73d6ca7f15e
Miha Rekar <miha.rekar@advisable.com>
2020-12-09 17:43:42 +0100

    Add puma worker killer to aid with memory issues

--- config/puma.rb
+++ config/puma.rb
@@ -17,6 +17,9 @@ on_worker_boot do
 end
 
 before_fork do
+  require 'puma_worker_killer'
+  PumaWorkerKiller.enable_rolling_restart # Default is every 6 hours
+
   # worker specific setup
   Barnes.start # Must have enabled worker mode for this to block to be called
 end
