7a17d3489c6d96c1df85e915bb7eb439f3fa2c85
Thomas Cullen <thomascullen92@gmail.com>
2020-12-15 08:27:46 +0000

    Add dev env clause

--- config/initializers/session_config.rb
+++ config/initializers/session_config.rb
@@ -1,3 +1,3 @@
-unless ENV['STAGING'] == 'true'
+unless ENV['STAGING'] == 'true' || Rails.development?
   Rails.application.config.session_store :cookie_store, key: '_advisable_session', domain: '.advisable.com'
 end
