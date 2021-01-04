d899f9b292cdb357cc6a45fa25a0072200ecb447
guandjoy <guandjoy@gmail.com>
2021-01-05 01:15:37 +0200

    Set font size of tag suffix from 14px to 12px

--- app/javascript/src/views/FreelancerProfile/PreviousProjects/ProjectCard/Tags.js
+++ app/javascript/src/views/FreelancerProfile/PreviousProjects/ProjectCard/Tags.js
@@ -4,7 +4,7 @@ import { Text, Box, Tag } from "@advisable/donut";
 function TagSuffix({ children, color }) {
   return (
     <Box bg="white" borderRadius={4} px={1} py={0.5} mr={1.5}>
-      <Text color={color} fontSize="xs">
+      <Text color={color} fontSize="2xs">
         {children}
       </Text>
     </Box>
