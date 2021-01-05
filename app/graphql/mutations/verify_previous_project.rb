469c25079f8c18c7885218cdf90005eac564bfd5
Miha Rekar <miha.rekar@advisable.com>
2021-01-05 08:44:52 +0100

    Some more responsibilities

--- app/graphql/mutations/verify_previous_project.rb
+++ app/graphql/mutations/verify_previous_project.rb
@@ -5,16 +5,17 @@ class Mutations::VerifyPreviousProject < Mutations::BaseMutation
 
   def authorized?(id:)
     return false unless context[:oauth_viewer]
+
     project = PreviousProject.find_by_uid!(id)
-    verifier = PreviousProject::Verifier.new(context[:oauth_viewer], project)
+    verifier = PreviousProject::Verifier.new(context[:oauth_viewer], project, responsible_id: current_account_id)
     verifier.can_verify?
   end
 
   def resolve(id:)
     project = PreviousProject.find_by_uid!(id)
-    verifier = PreviousProject::Verifier.new(context[:oauth_viewer], project)
+    verifier = PreviousProject::Verifier.new(context[:oauth_viewer], project, responsible_id: current_account_id)
     verifier.verify
 
-    { previous_project: project }
+    {previous_project: project}
   end
 end
