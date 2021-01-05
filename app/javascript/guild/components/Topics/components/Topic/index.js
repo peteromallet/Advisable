8b1722f88558b796404bdbecd02b0b0fafcdfac8
Geoff Ereth <geoffereth@gmail.com>
2021-01-05 15:45:07 -0800

    Changes Topic in sidebar to a NavLink

--- app/javascript/guild/components/Topics/components/Topic/index.js
+++ app/javascript/guild/components/Topics/components/Topic/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import { lowerDashed } from "@guild/utils";
 import { Hashtag } from "@styled-icons/heroicons-solid";
 import { StyledTopic } from "./styles";
-import { useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const Topic = ({ topic }) => {
-  const history = useHistory();
-  const handleSelectedTopic = () => history.push(`/topics/${topic.id}`);
-
-  return (
-    <StyledTopic onClick={handleSelectedTopic}>
-      <Hashtag />
-      {lowerDashed(topic.name)}
-    </StyledTopic>
-  );
-};
+const Topic = ({ topic }) => (
+  <StyledTopic as={NavLink} to={`/topics/${topic.id}`}>
+    <Hashtag />
+    {lowerDashed(topic.name)}
+  </StyledTopic>
+);
 
 export default Topic;
