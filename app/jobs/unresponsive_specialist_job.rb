e97066a6e8ae57c7fcb4b9f1634958e4fdca8bfa
Miha Rekar <miha.rekar@advisable.com>
2021-01-06 13:45:33 +0100

    Update job

--- app/jobs/unresponsive_specialist_job.rb
+++ app/jobs/unresponsive_specialist_job.rb
@@ -3,18 +3,18 @@
 class UnresponsiveSpecialistJob < ApplicationJob
   queue_as :default
 
-  attr_reader :application
+  attr_reader :report
 
-  def perform(application)
-    @application = application
-    unresponsive_since
+  def perform(unresponsiveness_report)
+    @report = unresponsiveness_report
+    # send email to sales person with report.message and unresponsive_since
   end
 
   private
 
   def unresponsive_since
-    messages = TalkjsApi.new.messages(application.uid)
-    last_message_from_specialist = messages.find { |m| m["senderId"] == application.specialist.uid }
+    messages = TalkjsApi.new.messages(report.application.uid)
+    last_message_from_specialist = messages.find { |m| m["senderId"] == report.application.specialist.uid }
     Time.zone.at(last_message_from_specialist["createdAt"] / 1000)
   end
 end
