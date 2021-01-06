ce686a8df8a1d1629e97fbd15759d85420392e92
Thomas Cullen <thomascullen92@gmail.com>
2021-01-06 11:13:56 +0000

    Allow team managers to start working with freelancers

--- app/javascript/src/views/BookingSetup/BookingType.js
+++ app/javascript/src/views/BookingSetup/BookingType.js
@@ -2,18 +2,20 @@ import React from "react";
 import { useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 import { Card, Text } from "@advisable/donut";
+import { useNotifications } from "src/components/Notifications";
 import BookingTypeForm from "../../components/BookingTypeForm";
 import START_WORKING from "./startWorking";
 import dataLayer from "../../utilities/dataLayer";
 
 const BookingType = ({ data }) => {
   const history = useHistory();
+  const { error } = useNotifications();
   const [startWorking] = useMutation(START_WORKING);
   const firstName = data.application.specialist.firstName;
 
   const handleSubmit = async (values) => {
     const id = data.application.id;
-    await startWorking({
+    const { errors } = await startWorking({
       variables: {
         input: {
           ...values,
@@ -21,6 +23,11 @@ const BookingType = ({ data }) => {
         },
       },
     });
+
+    if (errors) {
+      error("Something went wrong, please try again.");
+      return;
+    }
 
     dataLayer.push({
       event: "hiredFreelancer",
