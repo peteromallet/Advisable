0ff7d4046c71bf939ad9996f6c7ca8576781b64e
Miha Rekar <miha.rekar@advisable.com>
2021-01-06 13:42:25 +0100

    Add UnresponsivenessReport model

---
+++ db/migrate/20210106123252_create_unresponsiveness_reports.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateUnresponsivenessReports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :unresponsiveness_reports, id: :uuid do |t|
+      t.references :application, null: false, foreign_key: true
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
