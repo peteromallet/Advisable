acb7d09ff5973569f07ea8c026539d09252a285b
Miha Rekar <miha.rekar@advisable.com>
2021-01-07 09:17:31 +0100

    Build unresponsive report email

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class StaffMailer < ApplicationMailer
-  def unresponsive_specialist(report, unresponsive_for)
+  def unresponsive_specialist(report)
     @report = report
     @project = report.application.project
     @specialist = report.application.specialist
+    @sales_person = @project.sales_person || @project.user.sales_person
 
-    mail(to: @project.sales_person.email_with_name, subject: "#{@specialist.account.name} Unresponsive on #{@project.name}")
+    mail(to: @sales_person.email_with_name, subject: "#{@specialist.account.name} is unresponsive on #{@project.name}")
   end
 end
