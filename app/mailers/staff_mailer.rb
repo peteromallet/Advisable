04d699600aafca88dd6ebff98cdebc756068434a
Miha Rekar <miha.rekar@advisable.com>
2021-01-08 09:02:15 +0100

    Mailer improvements

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -3,10 +3,16 @@
 class StaffMailer < ApplicationMailer
   def unresponsive_specialist(report)
     @report = report
-    @project = report.application.project
-    @specialist = report.application.specialist
-    @sales_person = @project.sales_person || @project.user.sales_person
+    @project = @report.application.project
+    @specialist = @report.application.specialist
+    @sales_person = sales_person_for(project: @project)
 
     mail(to: @sales_person.email_with_name, subject: "#{@specialist.account.name} is unresponsive on #{@project.name}")
+  end
+
+  private
+
+  def sales_person_for(project:)
+    project.sales_person || project.user.sales_person
   end
 end
