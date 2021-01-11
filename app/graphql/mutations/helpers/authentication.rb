ff9e4297f3bced9d5331fb616ff73a62e8826a7b
Thomas Cullen <thomascullen92@gmail.com>
2021-01-11 08:21:22 +0000

    Also set context[:current_user] on login

--- app/graphql/mutations/helpers/authentication.rb
+++ app/graphql/mutations/helpers/authentication.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 module Mutations::Helpers::Authentication
+  delegate :logout, to: :session_manager
+
   def session_manager
     context[:session_manager]
   end
 
   def login_as(account)
+    context[:current_user] = account.specialist_or_user
     session_manager.login(account)
-  end
-
-  def logout
-    session_manager.logout
   end
 end
