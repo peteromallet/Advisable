c72001dbfad529f75c5764016b35fe82be2715be
Miha Rekar <miha.rekar@advisable.com>
2021-01-11 12:43:37 +0100

    Extract blacklisted domain detection

--- app/models/blacklisted_domain.rb
+++ app/models/blacklisted_domain.rb
@@ -1,9 +1,6 @@
+# frozen_string_literal: true
+
 class BlacklistedDomain < ApplicationRecord
-  def self.email_allowed?(email)
-    domain = email.split('@').last
-    return false if BlacklistedDomain.exists?(domain: domain)
-    true
-  end
 end
 
 # == Schema Information
