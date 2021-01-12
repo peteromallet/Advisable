307a99fe31401d5a71511a23d125b5a252036b77
Geoff Ereth <geoffereth@gmail.com>
2021-01-12 11:31:20 -0800

    Add default avatar as an image asset

--- app/mailers/guild/post_boost_mailer.rb
+++ app/mailers/guild/post_boost_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Guild::PostBoostMailer < ApplicationMailer
   layout 'styled_mailer'
 
@@ -6,9 +8,6 @@ class Guild::PostBoostMailer < ApplicationMailer
     @follower = Specialist.find(follower_id)
     @author = @guild_post.specialist
     @followed_topic_names = (@guild_post.guild_topic_list & @follower.guild_followed_topics.pluck(:name)).join(', ')
-
-    # Default if there is no avatar
-    attachments.inline["avatar.png"] = File.read(Rails.root.join('app/views/guild/post_boost_mailer/assets/avatar.png').to_s)
 
     mail(
       to: @follower.account.email,
