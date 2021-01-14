8d5c47fac772f9dc510b11f218f31c20018967e8
Thomas Cullen <thomascullen92@gmail.com>
2021-01-14 12:50:51 +0000

    Improve project status in project details modal

--- app/javascript/src/components/PreviousProjectDetails/styles.js
+++ app/javascript/src/components/PreviousProjectDetails/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { layout, space, variant, padding } from "styled-system";
-import { theme, Text } from "@advisable/donut";
+import { theme, Text, StyledCircle } from "@advisable/donut";
 
 export const StyledImageThumbnail = styled.div`
   ${layout};
@@ -17,33 +17,24 @@ export const StyledImageThumbnail = styled.div`
   }
 `;
 
-export const StyledBadgePrefix = styled.div`
-  margin-right: 8px;
-  padding-top: 2px;
-
-  svg {
-    width: 32px;
-    height: 32px;
-    stroke-width: 1.25;
-  }
-`;
-
 export const StyledTitle = styled(Text)``;
 export const StyledMessage = styled(Text)``;
 
 export const VARIANTS = variant({
   variants: {
     neutral: {
-      backgroundColor: theme.colors.neutral100,
-      svg: { color: theme.colors.neutral500 },
-      [StyledTitle]: { color: theme.colors.neutral800 },
-      [StyledMessage]: { color: theme.colors.neutral600 },
+      backgroundColor: "neutral100",
+      svg: { color: "neutral600" },
+      [StyledCircle]: { bg: "neutral200" },
+      [StyledTitle]: { color: "neutral800" },
+      [StyledMessage]: { color: "neutral600" },
     },
     red: {
-      backgroundColor: theme.colors.red100,
-      svg: { color: theme.colors.red400 },
-      [StyledTitle]: { color: theme.colors.red600 },
-      [StyledMessage]: { color: theme.colors.red500 },
+      backgroundColor: "red100",
+      svg: { color: "red600" },
+      [StyledCircle]: { bg: "red200" },
+      [StyledTitle]: { color: "red700" },
+      [StyledMessage]: { color: "red700" },
     },
   },
 });
@@ -51,6 +42,11 @@ export const VARIANTS = variant({
 export const StyledBadge = styled.div`
   ${padding}
   ${VARIANTS}
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
 
   font-weight: 400;
   border-radius: 12px;
