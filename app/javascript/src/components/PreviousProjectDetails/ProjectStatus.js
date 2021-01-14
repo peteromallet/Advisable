3e02bf84531b6e32e78c870ce60025bc38a4c5f1
Thomas Cullen <thomascullen92@gmail.com>
2021-01-14 12:50:51 +0000

    Improve project status in project details modal

--- app/javascript/src/components/PreviousProjectDetails/ProjectStatus.js
+++ app/javascript/src/components/PreviousProjectDetails/ProjectStatus.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { Box } from "@advisable/donut";
+import { Box, Circle } from "@advisable/donut";
 import Review from "src/components/Review";
-import { RefreshCcw, Edit3, XOctagon } from "@styled-icons/feather";
+import { Pencil, Exclamation } from "@styled-icons/heroicons-outline";
 import ProjectValidationPrompt from "src/components/ProjectValidationPrompt";
-import {
-  StyledBadge,
-  StyledBadgePrefix,
-  StyledMessage,
-  StyledTitle,
-} from "./styles";
+import { StyledBadge, StyledMessage, StyledTitle } from "./styles";
 
 function Validated({ project }) {
   return project.reviews.length > 0 && project.reviews[0]?.comment ? (
@@ -24,14 +19,14 @@ function Status(props) {
   return (
     <StyledBadge variant={props.variant} prefix={props.icon} p={3}>
       <Box display="flex">
-        <Box mr={1}>
-          <StyledBadgePrefix>{props.icon}</StyledBadgePrefix>
-        </Box>
+        <Circle size={40} mr={3}>
+          {props.icon}
+        </Circle>
         <Box>
-          <StyledTitle fontSize="s" fontWeight="medium" lineHeight="m">
+          <StyledTitle fontSize="md" fontWeight="medium" lineHeight="m">
             {props.label}
           </StyledTitle>
-          <StyledMessage fontSize="xs">{props.message}</StyledMessage>
+          <StyledMessage fontSize="sm">{props.message}</StyledMessage>
         </Box>
       </Box>
     </StyledBadge>
@@ -46,23 +41,20 @@ const STATUSES = {
     component: Status,
     variant: "neutral",
     label: "Draft Project",
-    message: "This project is not visible to others",
-    icon: <Edit3 />,
+    message:
+      "This project will not be visible to others until it has been published",
+    icon: <Pencil />,
   },
   Pending: {
     component: Pending,
-    variant: "yellow",
-    label: "Pending Validation",
-    message:
-      "This project will not be visible to others until it has been verified",
-    icon: <RefreshCcw />,
   },
   "Validation Failed": {
     component: Status,
     variant: "red",
     label: "Validation Failed",
-    message: "Client rejected this project and it's not publicly visible",
-    icon: <XOctagon />,
+    message:
+      "Unfortunately the client was unable to verify this project. It will not be visible to others.",
+    icon: <Exclamation />,
   },
 };
 
