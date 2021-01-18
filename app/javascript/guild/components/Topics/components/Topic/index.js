d58f40b0bc5bbe9a247a813ba49257cbe096cc8a
Geoff Ereth <geoffereth@gmail.com>
2021-01-18 15:29:23 -0800

    Truncates long topic names in sidebar

--- app/javascript/guild/components/Topics/components/Topic/index.js
+++ app/javascript/guild/components/Topics/components/Topic/index.js
@@ -3,11 +3,14 @@ import { lowerDashed } from "@guild/utils";
 import { Hashtag } from "@styled-icons/heroicons-solid";
 import { StyledTopic } from "./styles";
 import { NavLink } from "react-router-dom";
+import { truncate } from "lodash-es";
 
 const Topic = ({ topic }) => (
   <StyledTopic as={NavLink} to={`/topics/${topic.id}`}>
-    <Hashtag />
-    {lowerDashed(topic.name)}
+    <span>
+      <Hashtag />
+      {truncate(lowerDashed(topic.name), { length: 29 })}
+    </span>
   </StyledTopic>
 );
 
