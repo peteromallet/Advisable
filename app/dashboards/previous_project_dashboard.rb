7a6b477df2616a9bf982b261c45be9e7c95b1d49
Thomas Cullen <thomascullen92@gmail.com>
2021-01-18 09:48:47 +0000

    Update fields used in previous projects dashboard

--- app/dashboards/previous_project_dashboard.rb
+++ app/dashboards/previous_project_dashboard.rb
@@ -10,47 +10,34 @@ class PreviousProjectDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    account: Field::HasOne.with_options(
+      searchable: true,
+      searchable_fields: %w[first_name last_name email]
+    ),
     specialist: Field::BelongsTo,
     project_skills: Field::HasMany,
-    skills: Field::HasMany,
     primary_skill: Field::HasOne.with_options(class_name: 'Skill'),
     id: Field::Number,
-    airtable_id: Field::String,
-    industry: Field::String,
     contact_first_name: Field::String,
     contact_last_name: Field::String,
     contact_job_title: Field::String,
     client_name: Field::String,
     client_description: Field::Text,
     description: Field::Text,
-    requirements: Field::Text,
-    results: Field::Text,
     confidential: Field::Boolean,
-    validated: Field::Boolean,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    can_contact: Field::Boolean,
-    validation_url: Field::String,
-    contact_email: Field::String,
-    validation_method: Field::String,
     validation_status: Field::String,
-    validated_by_client: Field::Boolean,
-    validation_explanation: Field::String,
     company_type: Field::String,
     public_use: Field::Boolean,
     uid: Field::String,
     goal: Field::String,
-    contact_relationship: Field::String,
     hide_from_profile: Field::Boolean,
-    priority: Field::Number,
-    advisable_score: Field::Number,
     draft: Field::Boolean,
-    description_requires_update: Field::Boolean,
     industry_relevance: Field::Number,
     location_relevance: Field::Number,
     cost_to_hire: Field::Number,
     execution_cost: Field::Number,
-    pending_description: Field::String,
     validation_failed_reason: Field::String,
   }.freeze
 
@@ -59,52 +46,32 @@ class PreviousProjectDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = %i[specialist client_name primary_skill].freeze
+  COLLECTION_ATTRIBUTES = %i[specialist primary_skill client_name].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    uid
     specialist
-    project_skills
-    skills
     primary_skill
-    id
-    airtable_id
-    industry
+    project_skills
+    description
+    goal
     contact_first_name
     contact_last_name
     contact_job_title
     client_name
-    client_description
-    description
-    requirements
-    results
     confidential
-    validated
     created_at
     updated_at
-    can_contact
-    validation_url
-    contact_email
-    validation_method
     validation_status
-    validated_by_client
-    validation_explanation
     company_type
     public_use
-    uid
-    goal
-    contact_relationship
-    hide_from_profile
-    priority
-    advisable_score
     draft
-    description_requires_update
     industry_relevance
     location_relevance
     cost_to_hire
     execution_cost
-    pending_description
     validation_failed_reason
   ].freeze
 
@@ -112,45 +79,22 @@ class PreviousProjectDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    specialist
-    project_skills
-    skills
-    primary_skill
-    airtable_id
-    industry
+    client_name
+    description
+    goal
     contact_first_name
     contact_last_name
     contact_job_title
-    client_name
-    client_description
-    description
-    requirements
-    results
     confidential
-    validated
-    can_contact
-    validation_url
-    contact_email
-    validation_method
     validation_status
-    validated_by_client
-    validation_explanation
     company_type
     public_use
-    uid
-    goal
-    contact_relationship
     hide_from_profile
-    priority
-    advisable_score
     draft
-    description_requires_update
     industry_relevance
     location_relevance
     cost_to_hire
     execution_cost
-    pending_description
-    validation_failed_reason
   ].freeze
 
   # COLLECTION_FILTERS
