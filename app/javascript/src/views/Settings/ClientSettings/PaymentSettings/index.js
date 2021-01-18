ea5d99807640682de07918bcdc4f081e0c83714c
Thomas Cullen <thomascullen92@gmail.com>
2021-01-18 10:18:50 +0000

    Remove bank transfer as option in client payment settings

--- app/javascript/src/views/Settings/ClientSettings/PaymentSettings/index.js
+++ app/javascript/src/views/Settings/ClientSettings/PaymentSettings/index.js
@@ -2,17 +2,8 @@ import React from "react";
 import { get } from "lodash-es";
 import { Redirect } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
-import { Formik, Form, Field } from "formik";
-import {
-  Box,
-  Card,
-  Link,
-  Text,
-  Button,
-  Skeleton,
-  Radio,
-  RadioGroup,
-} from "@advisable/donut";
+import { Formik, Form } from "formik";
+import { Box, Card, Text, Button, Skeleton } from "@advisable/donut";
 import Modal from "../../../../components/Modal";
 import useViewer from "src/hooks/useViewer";
 import { useNotifications } from "../../../../components/Notifications";
@@ -54,7 +45,6 @@ const PaymentSettings = () => {
   };
 
   let initialValues = {
-    paymentMethod: get(data, "viewer.projectPaymentMethod"),
     invoiceSettings: {
       name: get(data, "viewer.invoiceSettings.name", get(data, "viewer.name")),
       companyName: get(
@@ -91,69 +81,24 @@ const PaymentSettings = () => {
   }
 
   return (
-    <Card p="l">
+    <Card p={10} borderRadius="12px">
       <Text
         mb="l"
         as="h1"
-        fontSize="xxl"
-        color="blue900"
-        fontWeight="semibold"
-        letterSpacing="-0.015em"
+        fontSize="4xl"
+        color="neutral900"
+        fontWeight="medium"
+        letterSpacing="-0.02rem"
       >
         Payment Preferences
       </Text>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         {(formik) => (
           <Form>
-            <Text
-              mb="xxs"
-              fontSize="l"
-              color="neutral700"
-              fontWeight="semibold"
-              letterSpacing="-0.01rem"
-            >
-              Payment Method
-            </Text>
-            <Text fontSize="s" color="neutral700" mb="s">
-              This is what we will use to collect payment for the freelancers
-              you work with.
-            </Text>
-            <RadioGroup>
-              <Field
-                as={Radio}
-                type="radio"
-                value="Card"
-                name="paymentMethod"
-                label="Payments via card"
-                description="We will collect payment by charging your card"
-              />
-              <Field
-                as={Radio}
-                type="radio"
-                value="Bank Transfer"
-                name="paymentMethod"
-                label="Payments via bank transfer"
-                disabled={!data.viewer.bankTransfersEnabled}
-                description="We will collect payment by sending you an invoice"
-              />
-            </RadioGroup>
-            {!data.viewer.bankTransfersEnabled && (
-              <Text fontSize="xs" color="neutral700" mt="m">
-                Please contact{" "}
-                <Link.External href="mailto:payments@advisable.com">
-                  payments@advisable.com
-                </Link.External>{" "}
-                to enable bank transfers for larger payments.
-              </Text>
-            )}
-            <Box height={1} bg="neutral100" my="l" />
-
-            {formik.values.paymentMethod === "Card" && (
-              <CardPaymentSettings
-                paymentMethod={data.viewer.paymentMethod}
-                openCardModal={() => setPaymentMethodModal(true)}
-              />
-            )}
+            <CardPaymentSettings
+              paymentMethod={data.viewer.paymentMethod}
+              openCardModal={() => setPaymentMethodModal(true)}
+            />
 
             <Text
               fontSize="l"
