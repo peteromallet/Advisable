ce8850e6936c5fe40944b57219f8c72b76306465
Geoff Ereth <geoffereth@gmail.com>
2021-01-18 15:08:24 -0800

    Add links to guild posts

--- app/javascript/guild/components/Markdown/index.js
+++ app/javascript/guild/components/Markdown/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { StyledMarkdown } from "./styles";
+import urlRegex from "url-regex";
 
 export default function Markdown({ children }) {
+  const formatLinks = (source) => source.replace(urlRegex(), "[$&]($&)");
+
+  const renderLinks = ({ href }) => {
+    const uri = href.startsWith("http") ? href : `//${href}`;
+    return (
+      <a href={uri} rel="nofollow">
+        {href}
+      </a>
+    );
+  };
+
   return (
     <StyledMarkdown>
-      <ReactMarkdown>{children}</ReactMarkdown>
+      <ReactMarkdown
+        source={formatLinks(children)}
+        renderers={{ link: renderLinks }}
+      >
+        {children}
+      </ReactMarkdown>
     </StyledMarkdown>
   );
 }
