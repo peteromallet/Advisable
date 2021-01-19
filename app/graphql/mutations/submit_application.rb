98215cf849397c08b76c2ae777fcb5eecb6140e8
Miha Rekar <miha.rekar@advisable.com>
2021-01-19 10:16:08 +0100

    Require specialist owner of an Application on SubmitApplication

--- app/graphql/mutations/submit_application.rb
+++ app/graphql/mutations/submit_application.rb
@@ -5,6 +5,16 @@ class Mutations::SubmitApplication < Mutations::BaseMutation
   argument :id, ID, required: true
 
   field :application, Types::ApplicationType, null: true
+  field :errors, [Types::Error], null: true
+
+  def authorized?(id:, **args)
+    requires_specialist!
+
+    application = Application.find_by!(uid: id)
+    return true if current_user == application.specialist
+
+    raise ApiError::InvalidRequest.new("invalidApplication", "The application does not belong to signed in user.")
+  end
 
   def resolve(id:)
     application = Application.find_by_uid_or_airtable_id!(id)
