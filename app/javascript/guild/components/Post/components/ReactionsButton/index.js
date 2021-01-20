f8d4295adfab3998cbdfff1b9afe1f451ea7abbe
Geoff Ereth <geoffereth@gmail.com>
2021-01-19 22:44:00 -0800

    Add guild feed walkthrough

--- app/javascript/guild/components/Post/components/ReactionsButton/index.js
+++ app/javascript/guild/components/Post/components/ReactionsButton/index.js
@@ -85,6 +85,7 @@ const ReactionsButton = ({ size, post }) => {
           color={reacted ? "white" : "yellow700"}
           bg={reacted ? "yellow500" : "yellow100"}
           icon={reacted ? <BulbFilled /> : <Bulb />}
+          data-walkthrough={post.pinned ? "postReaction" : null}
         />
       </Tooltip>
     </>
