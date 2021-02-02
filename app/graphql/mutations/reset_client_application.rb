4354cdbf7ff198a00cd2984831ac25711b3dbd7d
guandjoy <guandjoy@gmail.com>
2021-02-02 23:21:48 +0200

    Update reset_client_application mutation and test

--- app/graphql/mutations/reset_client_application.rb
+++ app/graphql/mutations/reset_client_application.rb
@@ -1,28 +1,25 @@
 # frozen_string_literal: true
 
-class Mutations::ResetClientApplication < Mutations::BaseMutation
-  argument :id, ID, required: true
+module Mutations
+  class ResetClientApplication < Mutations::BaseMutation
+    argument :id, ID, required: true
 
-  field :clientApplication, Types::ClientApplicationType, null: true
+    field :clientApplication, Types::ClientApplicationType, null: true
 
-  def resolve(**args)
-    user = User.find_by_uid_or_airtable_id!(args[:id])
+    def resolve(**args)
+      user = User.find_by_uid_or_airtable_id!(args[:id])
 
-    user.application_status = "Application Started"
-    user.accepted_guarantee_terms_at = false
-    user.budget = nil
-    user.company_name = nil
-    user.company.update(kind: nil, industry: nil, name: nil)
-    user.client&.update(name: nil)
-    user.client&.sync_to_airtable
-    user.skill_ids = []
-    user.locality_importance = nil
-    user.number_of_freelancers = nil
-    user.rejection_reason = nil
-    user.talent_quality = nil
-    user.save
-    user.sync_to_airtable
+      user.application_status = "Application Started"
+      user.accepted_guarantee_terms_at = nil
+      user.budget = nil
+      user.skill_ids = []
+      user.locality_importance = nil
+      user.number_of_freelancers = nil
+      user.rejection_reason = nil
+      user.talent_quality = nil
+      user.save_and_sync!
 
-    {clientApplication: user}
+      {clientApplication: user}
+    end
   end
 end
