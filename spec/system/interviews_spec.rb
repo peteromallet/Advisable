d063ec6b2dbdc20cbbda68e0e153052d2b7c587b
Thomas Cullen <thomascullen92@gmail.com>
2021-02-03 16:28:51 +0000

    Add system tests for inviting team members to an interview

--- spec/system/interviews_spec.rb
+++ spec/system/interviews_spec.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'Interview flow' do
-  let(:user) {
-    create(:user, {
-      availability: [
-        2.days.from_now.change({ hour: 10, min: 0, secs: 0 }),
-        2.days.from_now.change({ hour: 10, min: 30, secs: 0 }),
-        2.days.from_now.change({ hour: 11, min: 0, secs: 0 }),
-        2.days.from_now.change({ hour: 11, min: 30, secs: 0 }),
-      ]
-    })
-  }
+RSpec.describe 'Interviews', type: :system do
+  let(:user) do
+    create(:user, availability:
+      [
+        2.days.from_now.change({hour: 10, min: 0, secs: 0}),
+        2.days.from_now.change({hour: 10, min: 30, secs: 0}),
+        2.days.from_now.change({hour: 11, min: 0, secs: 0}),
+        2.days.from_now.change({hour: 11, min: 30, secs: 0})
+      ])
+  end
 
-  before :each do
+  before do
     allow_any_instance_of(Interview).to receive(:sync_to_airtable)
     allow_any_instance_of(Specialist).to receive(:sync_to_airtable)
   end
@@ -84,5 +85,30 @@ RSpec.describe 'Interview flow' do
       click_on "Confirm Call"
       expect(page).to have_content("has been scheduled!")
     end
+  end
+
+  it 'allows the user to invite a member of their team' do
+    application = create(:application, status: "Application Accepted")
+    account = create(:account, first_name: "Thomas")
+    create(:user, account: account, company: application.project.user.company)
+    create(:interview, application: application, status: "Call Scheduled", starts_at: 2.days.from_now)
+    authenticate_as(application.project.user)
+    visit "/projects/#{application.project.uid}/candidates/#{application.uid}"
+    click_on("Invite Others")
+    click_on("Share with Thomas")
+    expect(page).to have_content("Invite sent")
+  end
+
+  it 'allows the user to invite a new member of their team' do
+    allow_any_instance_of(User).to receive(:sync_to_airtable)
+    application = create(:application, status: "Application Accepted")
+    create(:interview, application: application, status: "Call Scheduled", starts_at: 2.days.from_now)
+    authenticate_as(application.project.user)
+    visit "/projects/#{application.project.uid}/candidates/#{application.uid}"
+    click_on("Invite Others")
+    fill_in("name", with: "Jim Halpert")
+    fill_in("email", with: "jim@dundermifflin.com")
+    click_on("Invite")
+    expect(page).to have_content("Invite sent")
   end
 end
