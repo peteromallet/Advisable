0b21861f10c8fb2e61054f17aa78c1844efd20e3
Thomas Cullen <thomascullen92@gmail.com>
2021-02-05 14:10:01 +0000

    Add orbits

--- app/javascript/src/views/Login/index.js
+++ app/javascript/src/views/Login/index.js
@@ -2,21 +2,28 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import useScrollRestore from "../../utilities/useScrollRestore";
+import { Box } from "@advisable/donut";
 import LoginForm from "./LoginForm";
 import Signup from "./Signup";
+import Orbits from "./Orbits";
 
 const Login = () => {
   useScrollRestore();
 
   return (
-    <Switch>
-      <Route path="/login/signup">
-        <Signup />
-      </Route>
-      <Route>
-        <LoginForm />
-      </Route>
-    </Switch>
+    <>
+      <Orbits />
+      <Box position="relative">
+        <Switch>
+          <Route path="/login/signup">
+            <Signup />
+          </Route>
+          <Route>
+            <LoginForm />
+          </Route>
+        </Switch>
+      </Box>
+    </>
   );
 };
 
