0f6cfa3c84667bbe280818150c0ce12c2538dc57
Miha Rekar <miha.rekar@advisable.com>
2021-02-05 12:24:31 +0100

    Warn sentry about usage of deprecated methods

--- app/graphql/base_field.rb
+++ app/graphql/base_field.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # The base field class that should be used for all grapql fields.
 # This includes some additional functionality arround authorization for fields.
 class BaseField < GraphQL::Schema::Field
@@ -14,12 +16,28 @@ class BaseField < GraphQL::Schema::Field
 
   def authorized?(object, args, context)
     super
+    check_for_deprecation(context)
     return true unless authorize_methods
+
     policy = policy(context[:current_user], object)
-    authorize_methods.any? { |auth_method| policy.public_send("#{auth_method}") }
+    authorize_methods.any? { |auth_method| policy.public_send(auth_method.to_s) }
   end
 
   def policy(user, record)
     Pundit.policy!(user, record)
+  end
+
+  private
+
+  def check_for_deprecation(context)
+    return unless deprecation_reason
+
+    Raven.capture_message(
+      "Deprecated #{name} (#{original_name}) requested on #{owner_type}",
+      backtrace: caller,
+      level: 'debug',
+      query: context&.query&.query_string,
+      deprecation_reason: deprecation_reason
+    )
   end
 end
