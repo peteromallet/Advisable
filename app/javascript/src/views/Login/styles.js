86aad83b02198d3d33f86a942ec15940bbc95c63
Thomas Cullen <thomascullen92@gmail.com>
2021-02-05 13:46:34 +0000

    Add signup page to login page

--- app/javascript/src/views/Login/styles.js
+++ app/javascript/src/views/Login/styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { theme, StyledButton } from "@advisable/donut";
 
 export const Container = styled.div`
@@ -32,29 +31,6 @@ export const Error = styled.div`
   text-align: center;
   display: inline-block;
   border: 1px solid #ff0073;
-`;
-
-export const SignupLinks = styled.div`
-  margin-bottom: 50px;
-`;
-
-export const SignupLink = styled(Link)`
-  flex-grow: 1;
-  display: block;
-  font-size: 14px;
-  font-weight: 400;
-  padding: 16px 20px;
-  border-radius: 8px;
-  text-align: center;
-  text-decoration: none;
-  color: ${theme.colors.neutral700};
-  border: 1px solid ${theme.colors.neutral200};
-  margin-bottom: 10px;
-
-  &:hover {
-    color: ${theme.colors.neutral800};
-    border-color: ${theme.colors.neutral300};
-  }
 `;
 
 export const StyledLoginWithGoogle = styled(StyledButton)`
