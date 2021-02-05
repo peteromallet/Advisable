1aa8111c67455ae146f3ef88cd0461db9e056441
Miha Rekar <miha.rekar@advisable.com>
2021-02-05 09:40:42 +0100

    🤷‍♂️

--- app/graphql/types/skill.rb
+++ app/graphql/types/skill.rb
@@ -3,7 +3,7 @@
 module Types
   class Skill < Types::BaseType
     field :id, ID, null: false, deprecation_reason: "We're moving away from Airtable. Please stop using Airtable IDs."
-    field :uid, ID
+    field :uid, ID, null: true
     field :name, String, null: false
     field :goal_placeholder, String, null: true
     field :characteristic_placeholder, String, null: true
