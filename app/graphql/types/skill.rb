5c3a0d3cad3e9767a1e2cd83a697a79a41b13221
Miha Rekar <miha.rekar@advisable.com>
2021-02-05 10:23:48 +0100

    Return uid instead of airtable_id in Types::Skill

--- app/graphql/types/skill.rb
+++ app/graphql/types/skill.rb
@@ -2,15 +2,14 @@
 
 module Types
   class Skill < Types::BaseType
-    field :id, ID, null: false, deprecation_reason: "We're moving away from Airtable. Please stop using Airtable IDs."
-    field :uid, ID, null: true
+    field :id, ID, null: false
     field :name, String, null: false
     field :goal_placeholder, String, null: true
     field :characteristic_placeholder, String, null: true
 
     # TODO: Can we just return uid here, Thomas?
     def id
-      object.airtable_id
+      object.uid
     end
   end
 end
