c259134e408fd37e09ef182eff740e777fdf13a0
Thomas Cullen <thomascullen92@gmail.com>
2021-02-06 00:38:51 +0000

    Remove dayjs library in favour of luxon

--- app/javascript/guild/views/Messages/components/Message.js
+++ app/javascript/guild/views/Messages/components/Message.js
@@ -120,7 +120,7 @@ export default function Message({ message, author, isAuthor }) {
             mr={isAuthor ? 1 : null}
             textAlign={isAuthor ? "right" : "left"}
           >
-            {relativeDate(message.dateCreated)} ago
+            {relativeDate(message.dateCreated)}
           </Text>
         </Box>
         {isAuthor ? (
