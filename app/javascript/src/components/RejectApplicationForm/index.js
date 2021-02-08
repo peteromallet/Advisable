b471ab6e490dfca7300e9b6ad81e0d807c3bf4c5
Thomas Cullen <thomascullen92@gmail.com>
2021-02-08 14:35:40 +0000

    Specific type's on submit buttons

--- app/javascript/src/components/RejectApplicationForm/index.js
+++ app/javascript/src/components/RejectApplicationForm/index.js
@@ -98,6 +98,7 @@ function Reason({ data, formik, onNext, onCancel }) {
       )}
       <Box marginTop={8}>
         <Button
+          type="button"
           variant="dark"
           marginRight={3}
           onClick={onNext}
