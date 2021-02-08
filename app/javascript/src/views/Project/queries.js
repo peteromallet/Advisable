9f29166125cbc53465aa713e5828e5286188031b
Thomas Cullen <thomascullen92@gmail.com>
2021-02-08 11:08:01 +0000

    Update apollo client

--- app/javascript/src/views/Project/queries.js
+++ app/javascript/src/views/Project/queries.js
@@ -292,69 +292,57 @@ export function useRejectCacheUpdate(application) {
   const projectId = params.id;
 
   return function rejectApplicationCacheUpdate() {
-    // Apollo annoyingly throws an error when readQuery is called with a query
-    // that is not in the cache.
-    // The user may be rejected a candidate from the view accepted candidate
-    // page in which case the matches query wont be in the apollo cache and so
-    // we need to wrap in a try catch.
-    try {
-      const data = client.readQuery({
-        query: GET_MATCHES,
-        variables: {
-          id: projectId,
-        },
-      });
+    const matchData = client.readQuery({
+      query: GET_MATCHES,
+      variables: {
+        id: projectId,
+      },
+    });
 
-      const isLastApplication = data.project.matches.length === 1;
-      const hasRequestedIntroductions = data.project.accepted.length > 0;
+    if (matchData) {
+      const isLastApplication = matchData.project.matches.length === 1;
+      const hasRequestedIntroductions = matchData.project.accepted.length > 0;
       client.writeQuery({
         query: GET_MATCHES,
         variables: {
           id: projectId,
         },
         data: {
-          ...data,
+          ...matchData,
           project: {
-            ...data.project,
+            ...matchData.project,
             sourcing: !(isLastApplication && hasRequestedIntroductions),
-            matches: data.project.matches.filter((app) => {
+            matches: matchData.project.matches.filter((app) => {
               return app.id !== application.id;
             }),
           },
         },
       });
-    } catch (_) {
-      // Cache isnt populated
     }
 
-    // The user may be rejecting the candidate by going straight to the detail
-    // view in which case the candidates query wont be in the cache and so we
-    // need to use try catch to rescue from apollo throwing error on readQuery
-    try {
-      const data = client.readQuery({
-        query: GET_CANDIDATES,
-        variables: {
-          id: projectId,
-        },
-      });
+    const candidatesData = client.readQuery({
+      query: GET_CANDIDATES,
+      variables: {
+        id: projectId,
+      },
+    });
 
+    if (candidatesData) {
       client.writeQuery({
         query: GET_CANDIDATES,
         variables: {
           id: projectId,
         },
         data: {
-          ...data,
+          ...candidatesData,
           project: {
-            ...data.project,
-            candidates: data.project.candidates.filter((app) => {
+            ...candidatesData.project,
+            candidates: candidatesData.project.candidates.filter((app) => {
               return app.id !== application.id;
             }),
           },
         },
       });
-    } catch (_) {
-      // cache isnt populated
     }
   };
 }
