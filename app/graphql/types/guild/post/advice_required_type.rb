d2c64c8d18e312021394171a42487ae87fb98e75
Thomas Cullen <thomascullen92@gmail.com>
2021-02-08 10:21:40 +0000

    Deprecate needHelp field

--- app/graphql/types/guild/post/advice_required_type.rb
+++ app/graphql/types/guild/post/advice_required_type.rb
@@ -7,6 +7,12 @@ module Types
         implements Types::Guild::PostInterface
 
         graphql_name "GuildPostAdviceRequired"
+
+        field :need_help, Boolean, null: true, deprecation_reason: "needHelp is no longer needed"
+
+        def need_help
+          object.data["need_help"]
+        end
       end
     end
   end
