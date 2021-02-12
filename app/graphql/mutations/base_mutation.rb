93a97f5c3b55a2365a82cb10e92bb1f1e6285563
Thomas Cullen <thomascullen92@gmail.com>
2021-02-12 08:38:54 +0000

    Fix viewing an invoice

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,55 +1,57 @@
 # frozen_string_literal: true
 
-class Mutations::BaseMutation < GraphQL::Schema::RelayClassicMutation
-  def current_user
-    context[:current_user]
-  end
+module Mutations
+  class BaseMutation < GraphQL::Schema::RelayClassicMutation
+    def current_user
+      context[:current_user]
+    end
 
-  def current_company
-    current_user&.company
-  end
+    def current_company
+      current_user&.company
+    end
 
-  private
+    private
 
-  def current_account_responsible_for(&block)
-    Logidze.with_responsible(current_account_id, &block)
-  end
+    def current_account_responsible_for(&block)
+      Logidze.with_responsible(current_account_id, &block)
+    end
 
-  def current_account_id
-    context[:current_account]&.id
-  end
+    def current_account_id
+      context[:current_account]&.id
+    end
 
-  def requires_current_user!
-    return true if current_user.present?
+    def requires_current_user!
+      return true if current_user.present?
 
-    ApiError.not_authenticated
-  end
+      ApiError.not_authenticated
+    end
 
-  def requires_client!
-    requires_current_user!
-    return true if current_user.is_a?(User)
+    def requires_client!
+      requires_current_user!
+      return true if current_user.is_a?(::User)
 
-    ApiError.invalid_request("MUST_BE_USER", "Current user must be a User.")
-  end
+      ApiError.invalid_request("MUST_BE_USER", "Current user must be a User.")
+    end
 
-  def requires_specialist!
-    requires_current_user!
-    return true if current_user.is_a?(Specialist)
+    def requires_specialist!
+      requires_current_user!
+      return true if current_user.is_a?(Specialist)
 
-    ApiError.invalid_request("MUST_BE_SPECIALIST", "Current user must be a Specialist.")
-  end
+      ApiError.invalid_request("MUST_BE_SPECIALIST", "Current user must be a Specialist.")
+    end
 
-  def requires_guild_user!
-    requires_current_user!
-    return true if current_user.guild
+    def requires_guild_user!
+      requires_current_user!
+      return true if current_user.guild
 
-    ApiError.invalid_request("invalidPermissions", "Not a guild user")
-  end
+      ApiError.invalid_request("invalidPermissions", "Not a guild user")
+    end
 
-  def requires_team_manager!
-    requires_client!
-    return true if current_user.account.team_manager?
+    def requires_team_manager!
+      requires_client!
+      return true if current_user.account.team_manager?
 
-    ApiError.invalid_request("MUST_BE_TEAM_MANAGER", "Current user must have team management permission.")
+      ApiError.invalid_request("MUST_BE_TEAM_MANAGER", "Current user must have team management permission.")
+    end
   end
 end
--- app/graphql/mutations/complete_setup.rb
+++ app/graphql/mutations/complete_setup.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
 # Updates the specialist application_stage from 'Started' to 'On Hold'
-class Mutations::CompleteSetup < Mutations::BaseMutation
-  field :specialist, Types::SpecialistType, null: true
+module Mutations
+  class CompleteSetup < Mutations::BaseMutation
+    field :specialist, Types::SpecialistType, null: true
 
-  # The completeSetup mutation requires a specalist to be logged in.
-  def authorized?(**args)
-    specialist = context[:current_user]
+    # The completeSetup mutation requires a specalist to be logged in.
+    def authorized?(**_args)
+      specialist = context[:current_user]
 
-    ApiError.not_authenticated unless specialist
-    ApiError.not_authenticated("You are logged in as a client") if specialist.is_a?(User)
+      ApiError.not_authenticated unless specialist
+      ApiError.not_authenticated("You are logged in as a client") if specialist.is_a?(::User)
 
-    if specialist.application_stage != "Started"
-      ApiError.invalid_request("invalidApplicationStage", "The account status must be 'Started' but it is #{specialist.application_stage}")
+      ApiError.invalid_request("invalidApplicationStage", "The account status must be 'Started' but it is #{specialist.application_stage}") if specialist.application_stage != "Started"
+
+      true
     end
 
-    true
-  end
+    def resolve(**_args)
+      specialist = context[:current_user]
+      specialist.application_stage = "On Hold"
+      Logidze.with_responsible(specialist.account_id) do
+        specialist.save
+      end
+      specialist.sync_to_airtable
 
-  def resolve(**args)
-    specialist = context[:current_user]
-    specialist.application_stage = "On Hold"
-    Logidze.with_responsible(specialist.account_id) do
-      specialist.save
+      {specialist: specialist}
     end
-    specialist.sync_to_airtable
-
-    {specialist: specialist}
   end
 end
