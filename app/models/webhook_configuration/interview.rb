79ac7ca523ab945eecf18e0a10d95bfca176609d
Miha Rekar <miha.rekar@advisable.com>
2021-02-15 14:24:29 +0100

    Use uid instead of airtable id in webhooks

--- app/models/webhook_configuration/interview.rb
+++ app/models/webhook_configuration/interview.rb
@@ -4,14 +4,12 @@
 class WebhookConfiguration::Interview < WebhookConfiguration
   def data(interview)
     {
-      id: interview.id,
-      airtable_id: interview.airtable_id, # TODO: What about this?
+      id: interview.uid,
       starts_at: interview.starts_at,
       status: interview.status,
       time_zone: interview.time_zone,
       application: {
-        id: interview.application.id,
-        airtable_id: interview.application.airtable_id
+        id: interview.application.uid
       }
     }
   end
