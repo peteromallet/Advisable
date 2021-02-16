c9dbfff986eee7da584f8f96298a817d947f437f
guandjoy <guandjoy@gmail.com>
2021-02-16 17:08:52 +0200

    Init content for Welcome step

--- app/javascript/src/views/FreelancerApplication/Welcome.js
+++ app/javascript/src/views/FreelancerApplication/Welcome.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Box, Text, Card, Link, Button } from "@advisable/donut";
+import image from "./images/welcome-illustration.png";
 
 export default function Welcome() {
-  return <div>Welcome Step</div>;
+  return (
+    <Card px={10} pt={12} pb={16} borderRadius="12px">
+      <Box textAlign="center">
+        <Box as="img" src={image} width="352px" marginBottom={6} />
+        <Text
+          color="#00404E"
+          fontSize="5xl"
+          fontWeight="medium"
+          marginBottom={3}
+        >
+          Welcome to Advisable
+        </Text>
+        <Text fontSize="lg" color="neutral800" marginBottom={10}>
+          We need to know a little more about you before we can find you some
+          projects. Once you are accepted into the Advisable network we will
+          begin matching you with projects where you can choose to apply or not.
+        </Text>
+        <Button as={Link} to="/freelancers/apply/introduction">
+          Get Started
+        </Button>
+      </Box>
+    </Card>
+  );
 }
