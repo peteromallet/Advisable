64e357db23a9f2f826b6239d77b5d90d5aecf4fe
guandjoy <guandjoy@gmail.com>
2021-02-16 22:22:20 +0200

    Init content and fields of Previous Work step

--- app/javascript/src/views/FreelancerApplication/PreviousWork.js
+++ app/javascript/src/views/FreelancerApplication/PreviousWork.js
@@ -1,5 +1,68 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import { useHistory } from "react-router-dom";
+import { Card, Box, Textarea } from "@advisable/donut";
+import FormField from "src/components/FormField";
+import SubmitButton from "src/components/SubmitButton";
+import StepNumber from "./StepNumber";
+import { Description, Header } from "./styles";
+import { object, string } from "yup";
+import { ArrowRight } from "@styled-icons/feather";
+
+const validationSchema = object().shape({
+  projectDescription: string().required(),
+  proud: string().required(),
+});
 
 export default function PreviousWork() {
-  return <div>Previous Work Step</div>;
+  const history = useHistory();
+
+  const initialValues = {
+    projectDescription: "",
+    proud: "",
+  };
+
+  const handleSubmit = () => {
+    history.push("/freelancers/apply/preferences");
+  };
+
+  return (
+    <Card padding={10} borderRadius="12px">
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <StepNumber>Step 3 of 5</StepNumber>
+          <Header>Previous work</Header>
+          <Description>
+            Every freelancer has that one project that stands out in there mind.
+            The one that you were so excited to complete and add to your
+            portfolio. Tell us about one of your previous projects that you are
+            most proud of and why.
+          </Description>
+          <Box mb="l">
+            <FormField
+              as={Textarea}
+              name="projectDescription"
+              minRows={5}
+              label="Please provide a brief description of a project you’re proud of"
+              placeholder="What were the goals of this project? What type of work was involved?"
+            />
+          </Box>
+          <Box mb="l">
+            <FormField
+              as={Textarea}
+              name="proud"
+              minRows={5}
+              label="Why are you proud of this project?"
+              placeholder="What were the results of this project? Why does it stand out to you?"
+            />
+          </Box>
+          <SubmitButton suffix={<ArrowRight />}>Continue</SubmitButton>
+        </Form>
+      </Formik>
+    </Card>
+  );
 }
