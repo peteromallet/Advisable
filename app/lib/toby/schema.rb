54ee521f91f1b37e94378c8ad312d41d3e24b19c
Miha Rekar <miha.rekar@advisable.com>
2021-02-17 09:46:14 +0100

    n+1 wip

--- app/lib/toby/schema.rb
+++ app/lib/toby/schema.rb
@@ -1,4 +1,11 @@
-class Toby::Schema < GraphQL::Schema
-  query Toby::Types::QueryType
-  # mutation Admin::Types::MutationType
+# frozen_string_literal: true
+
+module Toby
+  class Schema < GraphQL::Schema
+    query Toby::Types::QueryType
+
+    lazy_resolve(Toby::LazyAccount, :account)
+
+    # mutation Admin::Types::MutationType
+  end
 end
