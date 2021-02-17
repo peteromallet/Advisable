d104e714e08e61b61d02cfea7c0b68f717d348fe
Miha Rekar <miha.rekar@advisable.com>
2021-02-17 12:22:29 +0100

    Revert app/advisatable/* to master

---
+++ app/advisatable/advisatable/columns/string.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Advisatable
+  module Columns
+    class String < Base
+      def type
+        GraphQL::Types::String
+      end
+
+      def input_type
+        GraphQL::Types::String
+      end
+    end
+  end
+end
