b015b4bc204c3889a62eb15cc42362dc4bff696e
Thomas Cullen <thomascullen92@gmail.com>
2021-02-18 23:25:57 +0000

    Rename id attribute 'contains' filter to 'one_of'

--- app/javascript/toby/filters/OneOf.js
+++ app/javascript/toby/filters/OneOf.js
@@ -5,7 +5,11 @@ export default function OneOf({ onChange }) {
 
   const handleChange = (e) => {
     setValue(e.target.value);
-    onChange(e.target.value.split(",").map((v) => v.trim()));
+    const values = e.target.value
+      .split(",")
+      .map((v) => v.trim())
+      .filter(Boolean);
+    onChange(values);
   };
 
   return <input value={value} onChange={handleChange} type="text" />;
