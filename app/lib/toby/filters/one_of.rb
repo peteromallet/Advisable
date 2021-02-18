e3b0b23df3d1d07e09654728553390fae7ff2962
Thomas Cullen <thomascullen92@gmail.com>
2021-02-18 23:21:06 +0000

    Dont filter anything if no contents are passed

--- app/lib/toby/filters/one_of.rb
+++ app/lib/toby/filters/one_of.rb
@@ -3,7 +3,9 @@
 module Toby
   module Filters
     class OneOf
-      def self.apply(records, name, contents)
+      def self.apply(records, name, contents = [])
+        return records if contents.empty?
+
         records.where(name => contents)
       end
     end
--- app/lib/toby/filters/string_contains.rb
+++ app/lib/toby/filters/string_contains.rb
@@ -4,6 +4,8 @@ module Toby
   module Filters
     class StringContains
       def self.apply(records, name, contents = [])
+        return records if contents.empty?
+
         records.where("#{name.to_s.underscore} ilike ?", "%#{contents.first}%")
       end
     end
