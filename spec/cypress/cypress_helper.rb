d15749b6dd72985dbb4773c7889d8ed97eb36663
Miha Rekar <miha.rekar@advisable.com>
2021-02-22 15:13:49 +0100

    Database cleaner changed API

--- spec/cypress/cypress_helper.rb
+++ spec/cypress/cypress_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # This is loaded once before the first command is executed
-require 'database_cleaner'
+require 'database_cleaner/active_record'
 require 'factory_bot_rails'
 require 'cypress_on_rails/smart_factory_wrapper'
 
@@ -7,10 +9,10 @@ factory = CypressOnRails::SimpleRailsFactory
 factory = FactoryBot if defined?(FactoryBot)
 
 CypressOnRails::SmartFactoryWrapper.configure(
-    always_reload: !Rails.configuration.cache_classes,
-    factory: factory,
-    files: [
-      Rails.root.join('spec', 'factories.rb'),
-      Rails.root.join('spec', 'factories', '**', '*.rb')
-    ]
+  always_reload: !Rails.configuration.cache_classes,
+  factory: factory,
+  files: [
+    Rails.root.join('spec/factories.rb'),
+    Rails.root.join('spec/factories/**/*.rb')
+  ]
 )
