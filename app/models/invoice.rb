925ef1d57bcdd24d98157bed061c0bf5dd2699e8
Miha Rekar <miha.rekar@advisable.com>
2021-02-25 11:28:01 +0100

    Add Invoice model

---
+++ app/models/invoice.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Invoice < ApplicationRecord
+  enum status: {draft: 0, open: 1, paid: 2, paid_out: 3, void: 4}
+
+  belongs_to :company
+  belongs_to :application
+  delegate :specialist, to: :application
+end
+
+# == Schema Information
+#
+# Table name: invoices
+#
+#  id                :uuid             not null, primary key
+#  paid_at           :datetime
+#  paid_out_at       :datetime
+#  period_end        :datetime
+#  period_start      :datetime
+#  status            :integer          default("draft")
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  application_id    :bigint           not null
+#  company_id        :uuid             not null
+#  stripe_invoice_id :string
+#
+# Indexes
+#
+#  index_invoices_on_application_id  (application_id)
+#  index_invoices_on_company_id      (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (application_id => applications.id)
+#  fk_rails_...  (company_id => companies.id)
+#
---
+++ db/migrate/20210225101929_create_invoices.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class CreateInvoices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :invoices, id: :uuid do |t|
+      t.references :company, null: false, foreign_key: true, type: :uuid
+      t.references :application, null: false, foreign_key: true
+      t.integer :status, default: 0
+      t.string :stripe_invoice_id
+      t.datetime :period_start
+      t.datetime :period_end
+      t.datetime :paid_at
+      t.datetime :paid_out_at
+
+      t.timestamps
+    end
+  end
+end
