1ad1d21f5c722db1caba7fa49dc4114755b7e437
Miha Rekar <miha.rekar@advisable.com>
2021-02-26 08:10:31 +0100

    Graphql switched to symbols

--- app/lib/toby/resolvers/collection.rb
+++ app/lib/toby/resolvers/collection.rb
@@ -8,14 +8,14 @@ module Toby
 
         filters.each do |filter|
           args = filter.arguments.argument_values.transform_values(&:value)
-          name = args["attribute"].underscore.to_sym
+          name = args[:attribute].underscore.to_sym
           attribute = field.resource.attributes.find { |attr| attr.name == name }
           next if attribute.nil?
 
-          filter_class = attribute.class.filters[args["type"].to_sym]
+          filter_class = attribute.class.filters[args[:type].to_sym]
           next if filter_class.nil?
 
-          records = filter_class.apply(records, name, args["value"])
+          records = filter_class.apply(records, name, args[:value])
         end
 
         records
