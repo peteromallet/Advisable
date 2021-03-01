dc74aed60fbaec38178047804b496f8183705c05
guandjoy <guandjoy@gmail.com>
2021-03-01 23:56:00 +0200

    Move CheckIcon to components folder

--- app/javascript/src/components/MultistepMenu/MultistepMenuItem.js
+++ app/javascript/src/components/MultistepMenu/MultistepMenuItem.js
@@ -10,25 +10,7 @@ import {
   StyledNavigationMenuItemSteps,
   StyledNavigationMenuItemNumber,
 } from "./styles";
-
-function Check() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="10"
-      height="8"
-      fill="none"
-      viewBox="0 0 10 8"
-    >
-      <path
-        fill="currentColor"
-        fillRule="evenodd"
-        d="M9.799.243a.87.87 0 01-.016 1.131l-5.893 6.4a.66.66 0 01-.994 0L.217 4.865a.87.87 0 01-.015-1.131.662.662 0 011.01-.017l2.18 1.569L8.789.226a.662.662 0 011.01.017z"
-        clipRule="evenodd"
-      ></path>
-    </svg>
-  );
-}
+import CheckIcon from "src/components/CheckIcon";
 
 function ProgressCircle({ steps }) {
   const location = useLocation();
@@ -113,8 +95,8 @@ export default function MultistepMenuItem({
         {isComplete !== undefined && (
           <StyledNavigationMenuItemNumber>
             {steps.length > 0 && <ProgressCircle steps={steps} />}
-            {isComplete ? <Check /> : null}
-            {!isComplete && !isDisabled ? <Check /> : null}
+            {isComplete ? <CheckIcon /> : null}
+            {!isComplete && !isDisabled ? <CheckIcon /> : null}
             {isDisabled ? <LockClosed width={14} /> : null}
           </StyledNavigationMenuItemNumber>
         )}
