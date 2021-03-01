08e5455fc6d5b636f2648e33ad4ab6bc87d090b4
Thomas Cullen <thomascullen92@gmail.com>
2021-03-01 12:27:39 +0000

    Cleanup styling of proposal flow

--- app/javascript/src/views/Proposal/Sent.js
+++ app/javascript/src/views/Proposal/Sent.js
@@ -1,30 +1,27 @@
 import * as React from "react";
-import Text from "../../components/Text";
-import { Box, Card } from "@advisable/donut";
+import { Box, Card, Text } from "@advisable/donut";
 import Link from "../../components/Link";
-import Heading from "../../components/Heading";
 import illustration from "./illustration.png";
 
 const Send = ({ application }) => {
   const proposalUrl = `/applications/${application.id}/proposal`;
 
   return (
-    <Card>
-      <Box padding="l" style={{ textAlign: "center" }}>
-        <img src={illustration} width={280} alt="" />
-        <Box paddingBottom="s">
-          <Heading level={3}>Your proposal has been sent!</Heading>
-        </Box>
-        <Box paddingBottom="l">
-          <Text size="s" style={{ maxWidth: 380, margin: "0 auto" }}>
-            Your proposal has been sent to{" "}
-            {application.project.user.companyName}. We&apos;ll try to get a
-            response from them ASAP.
-          </Text>
-        </Box>
-        <Box paddingBottom="m">
-          <Link to={proposalUrl}>Update proposal</Link>
-        </Box>
+    <Card borderRadius="12px" padding={8} textAlign="center">
+      <img src={illustration} width={280} alt="" />
+      <Text paddingBottom={2} fontWeight="500" fontSize="3xl" as="h3">
+        Your proposal has been sent!
+      </Text>
+      <Text
+        paddingBottom={6}
+        lineHeight="1.2"
+        style={{ maxWidth: 380, margin: "0 auto" }}
+      >
+        Your proposal has been sent to {application.project.user.companyName}.
+        We&apos;ll try to get a response from them ASAP.
+      </Text>
+      <Box paddingBottom="m">
+        <Link to={proposalUrl}>Update proposal</Link>
       </Box>
     </Card>
   );
