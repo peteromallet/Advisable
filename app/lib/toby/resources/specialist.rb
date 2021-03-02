80229efb332bd9f8643bcb8899b0900e04bf5cfd
Thomas Cullen <thomascullen92@gmail.com>
2021-03-02 11:13:50 +0000

    Add CurrencyAttribute type

--- app/lib/toby/resources/specialist.rb
+++ app/lib/toby/resources/specialist.rb
@@ -5,9 +5,11 @@ module Toby
     class Specialist < BaseResource
       model_name ::Specialist
       # query_names collection: :accounts
-      attribute :uid, Attributes::String
+      attribute :uid, Attributes::String, readonly: true
+      attribute :application_stage, Attributes::Select, options: []
       attribute :account, Attributes::BelongsTo, labeled_by: :name
       attribute :bio, Attributes::String
+      attribute :hourly_rate, Attributes::Currency
       attribute :country, Attributes::BelongsTo, labeled_by: :name
       attribute :applications, Attributes::HasMany
       attribute :skills, Attributes::HasManyThrough
