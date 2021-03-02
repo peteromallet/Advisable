4d2aea1ae55dd98728b3b6ccbc528e4688a211be
Thomas Cullen <thomascullen92@gmail.com>
2021-03-02 10:45:58 +0000

    Add HasOneAttribute type

---
+++ app/lib/toby/attributes/has_one.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Toby
+  module Attributes
+    class HasOne < HasMany
+      extension_field :labeled_by, GraphQL::Types::String
+
+      def type
+        "Toby::Types::#{model}"
+      end
+
+      def input_type
+        GraphQL::Types::ID
+      end
+
+      def lazy_read_class
+        Toby::Lazy::Single
+      end
+    end
+  end
+end
--- app/lib/toby/resources/account.rb
+++ app/lib/toby/resources/account.rb
@@ -4,11 +4,15 @@ module Toby
   module Resources
     class Account < BaseResource
       model_name ::Account
-      attribute :uid, Attributes::String
+      attribute :uid, Attributes::String, readonly: true
       attribute :first_name, Attributes::String
       attribute :last_name, Attributes::String
       attribute :name, Attributes::String, readonly: true
       attribute :email, Attributes::String
+      attribute :user, Attributes::HasOne, labeled_by: :uid
+      attribute :specialist, Attributes::HasOne, labeled_by: :uid
+      attribute :permissions, Attributes::TextArray
+      attribute :unsubscribed_from, Attributes::TextArray
     end
   end
 end
