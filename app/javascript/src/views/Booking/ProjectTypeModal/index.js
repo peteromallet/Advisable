653bf82cfea149c38f0ca077e87d4f06b4de7f99
Thomas Cullen <thomascullen92@gmail.com>
2021-03-03 11:48:21 +0000

    Update project type modal to use Donut modals

--- app/javascript/src/views/Booking/ProjectTypeModal/index.js
+++ app/javascript/src/views/Booking/ProjectTypeModal/index.js
@@ -2,15 +2,12 @@
 // a new booking.
 import * as React from "react";
 import { useMutation } from "@apollo/client";
-import { Box, Text } from "@advisable/donut";
-import Modal from "../../../components/Modal";
-import BookingTypeForm from "../../../components/BookingTypeForm";
+import { Text, Modal } from "@advisable/donut";
+import BookingTypeForm from "src/components/BookingTypeForm";
 import SET_PROJECT_TYPE from "./setProjectType";
 
-const ProjectTypeModal = ({ isOpen, onClose, application }) => {
-  const [setProjectType] = useMutation(SET_PROJECT_TYPE, {
-    onCompleted: onClose,
-  });
+const ProjectTypeModal = ({ modal, application }) => {
+  const [setProjectType] = useMutation(SET_PROJECT_TYPE);
 
   const initialValues = {
     projectType: application.projectType,
@@ -31,17 +28,20 @@ const ProjectTypeModal = ({ isOpen, onClose, application }) => {
         },
       },
     });
+
+    modal.hide();
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <Box padding="l">
+    <>
+      <Modal modal={modal} padding={8}>
         <Text
+          mb={6}
           as="h4"
+          fontSize="3xl"
+          fontWeight="500"
           color="neutral900"
-          fontWeight="semibold"
-          fontSize="xl"
-          mb="m"
+          letterSpacing="-0.03rem"
         >
           How do you want to work with {application.specialist.firstName}?
         </Text>
@@ -52,8 +52,8 @@ const ProjectTypeModal = ({ isOpen, onClose, application }) => {
           buttonLabel="Update Project Type"
           onSubmit={handleSubmit}
         />
-      </Box>
-    </Modal>
+      </Modal>
+    </>
   );
 };
 
