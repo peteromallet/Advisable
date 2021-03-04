4ef7d3a8a24c5a4846c645dd1af5593f0473d34a
Thomas Cullen <thomascullen92@gmail.com>
2021-03-04 10:53:27 +0000

    Add equals filter

--- app/lib/toby/attributes/boolean.rb
+++ app/lib/toby/attributes/boolean.rb
@@ -3,6 +3,10 @@
 module Toby
   module Attributes
     class Boolean < BaseAttribute
+      filter :is, Filters::Equals
+      filter :empty, Filters::CheckNil
+      filter :not_empty, Filters::CheckNotNil
+
       def type
         GraphQL::Types::Boolean
       end
