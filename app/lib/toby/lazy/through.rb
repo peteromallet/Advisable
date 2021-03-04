7b6f5076a957c697868f742ae2960d349a213a15
Miha Rekar <miha.rekar@advisable.com>
2021-03-04 13:02:05 +0100

    Handle when same object is referenced multiple times

--- app/lib/toby/lazy/through.rb
+++ app/lib/toby/lazy/through.rb
@@ -12,16 +12,15 @@ module Toby
       end
 
       def load_records
-        # TODO: Miha clear this up!
-        # as a comment to look into attribute thingy
+        # look into attribute thingy
         # attribute.reflection
 
         mapping = through.where(column => state[:pending])
         mapping = mapping.where(constraint) if constraint
-        mapping = mapping.pluck(through_column, column).group_by(&:first)
+        mapping = mapping.pluck(through_column, column).group_by(&:shift).transform_values(&:flatten)
 
         model.where(source_id_column => mapping.keys).each do |record|
-          mapping[record.public_send(source_id_column)].map(&:second).each do |r|
+          mapping[record.public_send(source_id_column)].each do |r|
             state[:loaded][r] ||= []
             state[:loaded][r] << record
           end
