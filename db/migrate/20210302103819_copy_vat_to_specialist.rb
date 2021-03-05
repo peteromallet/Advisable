4aeec68b0c624800fc74c0f9930d27aa39b9db17
Miha Rekar <miha.rekar@advisable.com>
2021-03-05 09:00:26 +0100

    Only load Specialists with VAT number

--- db/migrate/20210302103819_copy_vat_to_specialist.rb
+++ db/migrate/20210302103819_copy_vat_to_specialist.rb
@@ -2,8 +2,8 @@
 
 class CopyVatToSpecialist < ActiveRecord::Migration[6.1]
   def up
-    vat_numbers = Account.pluck(:id, :vat_number).to_h
-    Specialist.find_each do |specialist|
+    vat_numbers = Account.where.not(vat_number: ['', nil]).pluck(:id, :vat_number).to_h
+    Specialist.where(account_id: vat_numbers.keys).find_each do |specialist|
       specialist.update_columns(vat_number: vat_numbers[specialist.account_id]) # rubocop:disable Rails/SkipsModelValidations
     end
   end
