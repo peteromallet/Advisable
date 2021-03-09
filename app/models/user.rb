d9410f649c6e2f6ab7e70dea5e498b7299eb8406
guandjoy <guandjoy@gmail.com>
2021-03-09 21:14:17 +0200

    Run db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -129,7 +129,6 @@ end
 #  application_rejected_at           :datetime
 #  application_reminder_at           :datetime
 #  availability                      :text
-#  bank_transfers_enabled            :boolean          default(FALSE)
 #  budget                            :bigint
 #  campaign_medium                   :string
 #  campaign_name                     :string
