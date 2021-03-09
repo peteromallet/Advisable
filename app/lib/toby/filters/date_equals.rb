d966ca6e2781a83a6b3f175afb813ef3adb653b5
Thomas Cullen <thomascullen92@gmail.com>
2021-03-09 15:47:44 +0000

    Handle cases when blank value is passed to filters

--- app/lib/toby/filters/date_equals.rb
+++ app/lib/toby/filters/date_equals.rb
@@ -5,6 +5,7 @@ module Toby
     class DateEquals
       def self.apply(records, attribute, value: [], **_opts)
         return records if value.empty?
+        return records if value.first.blank?
 
         records.where(attribute.name => value.first.to_date.all_day)
       end
