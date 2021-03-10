32737e6d756c3edd9ce4cd7ddc94bca440ff8feb
Thomas Cullen <thomascullen92@gmail.com>
2021-03-10 22:17:52 +0000

    Remove a ton of dead files

--- app/javascript/src/components/Icon/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Icon = styled.span`
-  display: inline-flex;
-  svg {
-    margin: 0 auto;
-    display: block;
-  }
-`;
--- app/javascript/src/utilities/validators.js
+++
@@ -1,4 +0,0 @@
-export const required = (error = 'required') => value => {
-  if (value && value.trim().length > 0) return
-  return error
-}
