ec62591d125684f4859c2628fe4a8ec0631e722b
Miha Rekar <miha.rekar@advisable.com>
2021-03-10 10:45:05 +0100

    Do not use AR classes in migrations

--- db/migrate/20210217200148_add_notifications_from_reactions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddNotificationsFromReactions < ActiveRecord::Migration[6.1]
-  def up
-    Guild::Reaction.order(:created_at).find_each { |r| r.create_notification!(read_at: Time.current) }
-  end
-end
