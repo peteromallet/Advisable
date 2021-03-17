2da6f4333c39bd550b89f9ad137cda1ad96c7e78
Miha Rekar <miha.rekar@advisable.com>
2021-03-17 08:15:25 +0100

    Fix sentry reporting

--- app/models/concerns/resized_image.rb
+++ app/models/concerns/resized_image.rb
@@ -33,7 +33,7 @@ module ResizedImage
     end
   rescue ActiveStorage::InvariableError
     image.purge
-    Raven.capture_message("Deleted image that wasn't really an image", backtrace: caller, level: "debug", extra: {object: self, image: name})
+    Raven.capture_message("Deleted image that wasn't really an image", backtrace: caller, level: "debug", extra: {object_class: self.class.name, object_id: id, image: name})
   end
 
   def get_resized_image_url(name, options)
