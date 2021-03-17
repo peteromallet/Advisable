3edea2b63ad8f0dd8b10d39d5c7e99fde7ac8d59
Miha Rekar <miha.rekar@advisable.com>
2021-03-17 13:43:11 +0100

    Add authorization to CreatePreviousProject mutation

--- app/graphql/mutations/create_previous_project.rb
+++ app/graphql/mutations/create_previous_project.rb
@@ -1,19 +1,23 @@
-class Mutations::CreatePreviousProject < Mutations::BaseMutation
-  # TODO: Eventually the specialist ID should be removed and this mutation should expect a specialist to be logged in.
-  argument :specialist, ID, required: true
-  argument :client_name, String, required: true
-  argument :confidential, Boolean, required: false
-  argument :industries, [String], required: true
-  argument :primary_industry, String, required: true
-  argument :company_type, String, required: true
+# frozen_string_literal: true
 
-  field :previous_project, Types::PreviousProject, null: true
+module Mutations
+  class CreatePreviousProject < Mutations::BaseMutation
+    # TODO: Eventually the specialist ID should be removed and this mutation should expect a specialist to be logged in.
+    argument :specialist, ID, required: false, deprecation_reason: "Do not provide this anymore"
+    argument :client_name, String, required: true
+    argument :confidential, Boolean, required: false
+    argument :industries, [String], required: true
+    argument :primary_industry, String, required: true
+    argument :company_type, String, required: true
 
-  def resolve(**args)
-    specialist = Specialist.find_by_uid_or_airtable_id!(args[:specialist])
+    field :previous_project, Types::PreviousProject, null: true
 
-    project =
-      specialist.previous_projects.new(
+    def authorized?(**_args)
+      requires_specialist!
+    end
+
+    def resolve(**args)
+      project = current_user.previous_projects.new(
         client_name: args[:client_name],
         confidential: args[:confidential],
         company_type: args[:company_type],
@@ -22,16 +26,17 @@ class Mutations::CreatePreviousProject < Mutations::BaseMutation
         draft: true
       )
 
-    args[:industries].each do |industry|
-      project.project_industries <<
-        ProjectIndustry.new(
-          industry: Industry.find_by_name!(industry),
-          primary: args[:primary_industry] == industry
-        )
+      args[:industries].each do |industry|
+        project.project_industries <<
+          ProjectIndustry.new(
+            industry: Industry.find_by_name!(industry),
+            primary: args[:primary_industry] == industry
+          )
+      end
+
+      current_account_responsible_for { project.save }
+
+      {previous_project: project}
     end
-
-    current_account_responsible_for { project.save }
-
-    {previous_project: project}
   end
 end
