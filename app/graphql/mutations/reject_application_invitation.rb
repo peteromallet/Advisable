8ac2ceba618c4aa772c339e7e0e59a52f7a6dab8
Miha Rekar <miha.rekar@advisable.com>
2021-03-18 08:41:53 +0100

    Add authorization to RejectApplicationInvitation mutation

--- app/graphql/mutations/reject_application_invitation.rb
+++ app/graphql/mutations/reject_application_invitation.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
-class Mutations::RejectApplicationInvitation < Mutations::BaseMutation
-  argument :id, ID, required: true
-  argument :reason, String, required: true
+module Mutations
+  class RejectApplicationInvitation < Mutations::BaseMutation
+    argument :id, ID, required: true
+    argument :reason, String, required: true
 
-  field :application, Types::ApplicationType, null: true
+    field :application, Types::ApplicationType, null: true
 
-  def resolve(id:, reason:)
-    application = Applications::RejectApplicationInvitation.call(
-      application_id: id,
-      reason: reason,
-      current_account_id: current_account_id
-    )
+    def authorized?(id:, **_args)
+      requires_specialist!
+      application = Application.find_by!(uid: id)
+      policy = ApplicationPolicy.new(current_user, application)
+      policy.reject_invitation?
+    end
 
-    {application: application}
+    def resolve(id:, reason:)
+      application = Applications::RejectApplicationInvitation.call(
+        application_id: id,
+        reason: reason,
+        current_account_id: current_account_id
+      )
+
+      {application: application}
+    end
   end
 end
