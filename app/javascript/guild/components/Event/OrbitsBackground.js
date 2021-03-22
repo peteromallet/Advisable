267f195f470d71ed41ec46c50d2c751d45a3a59f
Thomas Cullen <thomascullen92@gmail.com>
2021-03-22 11:22:57 +0000

    Update svg's and use as background image instead

--- app/javascript/guild/components/Event/OrbitsBackground.js
+++ app/javascript/guild/components/Event/OrbitsBackground.js
@@ -1,79 +1,11 @@
 import React from "react";
-import { Box, theme } from "@advisable/donut";
+import { Box } from "@advisable/donut";
+import blue from "./orbits/blue.svg";
+import cyan from "./orbits/cyan.svg";
+import purple from "./orbits/purple.svg";
+import orange from "./orbits/orange.svg";
 
-const Colors = {
-  blue: {
-    stroke: theme.colors.blue200,
-    fill: theme.colors.blue100,
-  },
-  orange: {
-    stroke: theme.colors.orange300,
-    fill: theme.colors.orange100,
-  },
-  cyan: {
-    stroke: theme.colors.cyan300,
-    fill: theme.colors.cyan100,
-  },
-  purple: {
-    fill: "#F2EFFC",
-    stroke: "#DEDBE8",
-  },
-};
-
-const OrbitSvg = ({ fill, stroke }) => (
-  <svg width="100%">
-    <path
-      d="M 1400 -50 m -1020, 0 a 1020,1020 0 1,0 2040,0 a 1020,1020 0 1,0 -1840,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 900 -50 m -1020, 0 a 1020,1020 0 1,0 2040,0 a 1020,1020 0 1,0 -1840,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 700 -50 m -920, 0 a 920,920 0 1,0 1840,0 a 920,920 0 1,0 -1840,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 600 -50 m -820, 0 a 820,820 0 1,0 1640,0 a 820,820 0 1,0 -1640,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 500 -50 m -720, 0 a 720,720 0 1,0 1440,0 a 720,720 0 1,0 -1440,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 400 -50 m -600, 0 a 600,600 0 1,0 1200,0 a 600,600 0 1,0 -1200,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 300 -50 m -480, 0 a 480,480 0 1,0 960,0 a 480,480 0 1,0 -960,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 200 -50 m -360, 0 a 360,360 0 1,0 720,0 a 360,360 0 1,0 -720,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 100 -50 m -240, 0 a 240,240 0 1,0 480,0 a 240,240 0 1,0 -480,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-    <path
-      d="M 0 -50 m -120, 0 a 120,120 0 1,0 240,0 a 120,120 0 1,0 -240,0"
-      fill={fill}
-      stroke={stroke}
-    ></path>
-  </svg>
-);
+const ORBITS = { purple, blue, orange, cyan };
 
 export default function OrbitsBackground({ color = "blue", ...props }) {
   return (
@@ -82,9 +14,10 @@ export default function OrbitsBackground({ color = "blue", ...props }) {
       width="100%"
       position="relative"
       overflow="hidden"
+      backgroundImage={`url(${ORBITS[color] || "blue"})`}
+      backgroundPosition="center"
+      backgroundSize="cover"
       {...props}
-    >
-      <OrbitSvg {...Colors[color]} />
-    </Box>
+    />
   );
 }
