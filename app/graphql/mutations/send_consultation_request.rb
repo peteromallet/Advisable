738d8ce8fadfe5588b8336273bc991edf3a0d9f7
Miha Rekar <miha.rekar@advisable.com>
2021-03-25 15:13:35 +0100

    Add authentication to SendConsultationRequest mutation

--- app/graphql/mutations/send_consultation_request.rb
+++ app/graphql/mutations/send_consultation_request.rb
@@ -1,16 +1,27 @@
-class Mutations::SendConsultationRequest < Mutations::BaseMutation
-  argument :consultation, ID, required: true
-  argument :likely_to_hire, Integer, required: true
+# frozen_string_literal: true
 
-  field :consultation, Types::ConsultationType, null: true
+module Mutations
+  class SendConsultationRequest < Mutations::BaseMutation
+    argument :consultation, ID, required: true
+    argument :likely_to_hire, Integer, required: true
 
-  def resolve(**args)
-    consultation = Consultation.find_by_uid_or_airtable_id!(args[:consultation])
-    consultation.update(
-      status: 'Request Completed', likely_to_hire: args[:likely_to_hire]
-    )
+    field :consultation, Types::ConsultationType, null: true
 
-    consultation.sync_to_airtable
-    { consultation: consultation }
+    def authorized?(consultation:, **_args)
+      requires_client!
+
+      consultation = Consultation.find_by_uid_or_airtable_id!(consultation)
+      ConsultationPolicy.new(current_user, consultation).send_request?
+    end
+
+    def resolve(**args)
+      consultation = Consultation.find_by_uid_or_airtable_id!(args[:consultation])
+      consultation.update(
+        status: 'Request Completed', likely_to_hire: args[:likely_to_hire]
+      )
+
+      consultation.sync_to_airtable
+      {consultation: consultation}
+    end
   end
 end
