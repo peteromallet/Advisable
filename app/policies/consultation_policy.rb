d0ab2d459c8550497d4aeb43d810c11c471120e7
Miha Rekar <miha.rekar@advisable.com>
2021-03-25 15:11:00 +0100

    Add authentication to UpdateConsultation mutation

--- app/policies/consultation_policy.rb
+++ app/policies/consultation_policy.rb
@@ -2,13 +2,21 @@
 
 class ConsultationPolicy < BasePolicy
   def accept?
-    owner? || admin?
+    specialist_owner? || admin?
   end
   alias decline? accept?
 
+  def update?
+    user_owner? || admin?
+  end
+
   private
 
-  def owner?
-    record.specialist == current_user
+  def specialist_owner?
+    record.specialist == user
+  end
+
+  def user_owner?
+    record.user == current_user
   end
 end
