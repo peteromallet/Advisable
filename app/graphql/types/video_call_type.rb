d97683c51cd0c2b1a5fb69628b0c03612696722e
Miha Rekar <miha.rekar@advisable.com>
2021-03-26 08:59:08 +0100

    Predicate method renaming

--- app/graphql/types/video_call_type.rb
+++ app/graphql/types/video_call_type.rb
@@ -1,48 +1,52 @@
-class Types::VideoCallType < Types::BaseType
-  field :id, ID, null: false
-  field :fallback, Boolean, null: true
+# frozen_string_literal: true
 
-  field :zoom_meeting_id, String, null: true do
-    authorize :is_participant
-  end
+module Types
+  class VideoCallType < Types::BaseType
+    field :id, ID, null: false
+    field :fallback, Boolean, null: true
 
-  field :zoom_passcode, String, null: true do
-    authorize :is_participant
-  end
+    field :zoom_meeting_id, String, null: true do
+      authorize :participant?
+    end
 
-  field :zoom_url, String, null: true do
-    authorize :is_participant
-  end
+    field :zoom_passcode, String, null: true do
+      authorize :participant?
+    end
 
-  field :access_token, String, null: true do
-    authorize :is_participant
-  end
+    field :zoom_url, String, null: true do
+      authorize :participant?
+    end
 
-  field :participant, Types::ViewerUnion, null: false do
-    argument :id, ID, required: true
-    authorize :is_participant
-  end
+    field :access_token, String, null: true do
+      authorize :participant?
+    end
 
-  def participant(id:)
-    SpecialistOrUser.find_by_uid!(id)
-  end
+    field :participant, Types::ViewerUnion, null: false do
+      argument :id, ID, required: true
+      authorize :participant?
+    end
 
-  def id
-    object.uid
-  end
+    def participant(id:)
+      SpecialistOrUser.find_by_uid!(id)
+    end
 
-  def access_token
-    grant = Twilio::JWT::AccessToken::VideoGrant.new
-    grant.room = object.uid
+    def id
+      object.uid
+    end
 
-    token = Twilio::JWT::AccessToken.new(
-      ENV["TWILIO_SID"],
-      ENV["TWILIO_API_KEY_SID"],
-      ENV["TWILIO_API_KEY_SECRET"],
-      [grant],
-      identity: current_user.uid
-    )
+    def access_token
+      grant = Twilio::JWT::AccessToken::VideoGrant.new
+      grant.room = object.uid
 
-    token.to_jwt
+      token = Twilio::JWT::AccessToken.new(
+        ENV["TWILIO_SID"],
+        ENV["TWILIO_API_KEY_SID"],
+        ENV["TWILIO_API_KEY_SECRET"],
+        [grant],
+        identity: current_user.uid
+      )
+
+      token.to_jwt
+    end
   end
 end
