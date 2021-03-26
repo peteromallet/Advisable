6279018783c8d33b2f5b74053960543115ae9ca6
Miha Rekar <miha.rekar@advisable.com>
2021-03-26 08:59:08 +0100

    Predicate method renaming

--- app/policies/guild/post_policy.rb
+++ app/policies/guild/post_policy.rb
@@ -1,18 +1,23 @@
-class Guild::PostPolicy < Guild::BasePolicy
-  def show
-    record.specialist == current_user || published_and_guild? || published_and_shareable?
-  end
+# frozen_string_literal: true
 
-  def create_comment
-    published_and_guild?
-  end
+module Guild
+  class PostPolicy < Guild::BasePolicy
+    def show
+      record.specialist == current_user || published_and_guild? || published_and_shareable?
+    end
 
-  private
-  def published_and_guild?
-    record&.published? && is_guild_user?
-  end
+    def create_comment
+      published_and_guild?
+    end
 
-  def published_and_shareable?
-    record&.published? && record&.shareable
+    private
+
+    def published_and_guild?
+      record&.published? && guild_user?
+    end
+
+    def published_and_shareable?
+      record&.published? && record&.shareable
+    end
   end
 end
