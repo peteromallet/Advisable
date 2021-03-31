7592b56b7cb6a83b2e4f65cf3954a54818594204
Miha Rekar <miha.rekar@advisable.com>
2021-03-31 08:30:17 +0200

    Authorize sales_person on CompanyType

--- app/policies/company_policy.rb
+++ app/policies/company_policy.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class CompanyPolicy < BasePolicy
+  def read?
+    record_belongs_to_company? || admin?
+  end
+
+  private
+
   def company_of_record
     record
   end
