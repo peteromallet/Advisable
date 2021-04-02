828cb5ca95b997765fb0d961b617817437d886f9
Thomas Cullen <thomascullen92@gmail.com>
2021-04-02 11:10:25 +0100

    Fix updates for has many and has many through attributes

--- app/lib/toby/attributes/has_many_through.rb
+++ app/lib/toby/attributes/has_many_through.rb
@@ -10,6 +10,13 @@ module Toby
       def lazy_read_class
         Toby::Lazy::Through
       end
+
+      def write(resource, value)
+        klass = reflection.source_reflection.klass
+        attribute = reflection.through_reflection.active_record_primary_key
+        records = klass.where(attribute => value)
+        resource.public_send("#{name}=", records)
+      end
     end
   end
 end
