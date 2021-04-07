bb76dffe03b6730ff58a07d27d979e774a16d778
Miha Rekar <miha.rekar@advisable.com>
2021-04-07 10:57:36 +0200

    Fix generators to use rspec and factory_bot

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.config.generators do |g|
+  g.test_framework :rspec
+  g.fixture_replacement :factory_bot
+  g.factory_bot dir: "spec/factories"
   g.orm :active_record, primary_key_type: :uuid
 end
