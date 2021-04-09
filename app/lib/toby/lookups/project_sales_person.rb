3c7c80ea75b48e2086466e5412b7eb847ef3c5db
Miha Rekar <miha.rekar@advisable.com>
2021-04-09 09:19:02 +0200

    Get lazy loading working

--- app/lib/toby/lookups/project_sales_person.rb
+++ app/lib/toby/lookups/project_sales_person.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 module Toby
+  class CustomLazy < Lazy::Single
+    def id
+      resource.user_id
+    end
+
+    private
+
+    def load_records
+      User.includes(company: :sales_person).where(id: state[:pending]).each do |record|
+        state[:loaded][record.id] ||= []
+        state[:loaded][record.id] << record&.company&.sales_person&.username
+      end
+      state[:pending].clear
+    end
+  end
+
   module Lookups
     class ProjectSalesPerson < Attributes::String
       filter :is, Filters::Equals do |records, value|
@@ -16,14 +32,9 @@ module Toby
         true
       end
 
-      def read(resource)
-        resource.user&.company&.sales_person&.username
+      def lazy_read_class
+        Toby::CustomLazy
       end
-
-      # TODO: Miha, figure it out
-      # def lazy_read_class
-      #   Toby::Lazy::Single
-      # end
     end
   end
 end
