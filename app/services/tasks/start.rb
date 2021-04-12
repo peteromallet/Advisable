7d88d6463c33359a3f708d32002696b2969b3fa0
Miha Rekar <miha.rekar@advisable.com>
2021-04-12 12:06:52 +0200

    RuboCop

--- app/services/tasks/start.rb
+++ app/services/tasks/start.rb
@@ -1,32 +1,37 @@
-class Tasks::Start < ApplicationService
-  attr_reader :task, :responsible_id
+# frozen_string_literal: true
 
-  def initialize(task:, responsible_id: nil)
-    @task = task
-    @responsible_id = responsible_id
-  end
+module Tasks
+  class Start < ApplicationService
+    attr_reader :task, :responsible_id
 
-  def call
-    raise Service::Error, 'tasks.mustBeAssigned' if task.stage != 'Assigned'
-    raise Service::Error, 'tasks.estimateRequired' if task.estimate.blank?
-    raise Service::Error, 'tasks.dueDateRequired' if task.due_date.blank?
-
-    updated = Logidze.with_responsible(responsible_id) { task.update(stage: 'Working', started_working_at: Time.zone.now) }
-    if updated
-      add_invoice_item if task.application.project_type == 'Fixed'
-      task.sync_to_airtable
-      WebhookEvent.trigger('tasks.started', WebhookEvent::Task.data(task))
+    def initialize(task:, responsible_id: nil)
+      super()
+      @task = task
+      @responsible_id = responsible_id
     end
 
-    task
-  end
+    def call
+      raise Service::Error, 'tasks.mustBeAssigned' if task.stage != 'Assigned'
+      raise Service::Error, 'tasks.estimateRequired' if task.estimate.blank?
+      raise Service::Error, 'tasks.dueDateRequired' if task.due_date.blank?
 
-  private
+      updated = Logidze.with_responsible(responsible_id) { task.update(stage: 'Working', started_working_at: Time.zone.now) }
+      if updated
+        add_invoice_item if task.application.project_type == 'Fixed'
+        task.sync_to_airtable
+        WebhookEvent.trigger('tasks.started', WebhookEvent::Task.data(task))
+      end
 
-  def add_invoice_item
-    Tasks::CreateInvoiceItem.call(task: task, responsible_id: responsible_id)
-  rescue Stripe::StripeError => e
-    # Still log the error in sentry
-    Sentry.capture_exception(e)
+      task
+    end
+
+    private
+
+    def add_invoice_item
+      Tasks::CreateInvoiceItem.call(task: task, responsible_id: responsible_id)
+    rescue Stripe::StripeError => e
+      # Still log the error in sentry
+      Sentry.capture_exception(e)
+    end
   end
 end
