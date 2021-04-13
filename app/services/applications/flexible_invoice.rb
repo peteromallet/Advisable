6247f0b335f34ca632a444a8f1f0ba1be3cb5f29
Miha Rekar <miha.rekar@advisable.com>
2021-04-13 14:57:17 +0200

    Fix task arguments

--- app/services/applications/flexible_invoice.rb
+++ app/services/applications/flexible_invoice.rb
@@ -1,18 +1,21 @@
-class Applications::FlexibleInvoice < ApplicationService
-  attr_reader :application
+# frozen_string_literal: true
 
-  def initialize(application:)
-    @application = application
-  end
+module Applications
+  class FlexibleInvoice < ApplicationService
+    attr_reader :application
 
-  def call
-    Users::AddInvoiceItem.call(
-      {
+    def initialize(application:)
+      super()
+      @application = application
+    end
+
+    def call
+      Users::AddInvoiceItem.call(
         user: application.project.user,
         unit_amount: application.invoice_rate,
         quantity: (application.monthly_limit / 2.0).ceil,
         description: "50% of monthly limit + #{application.specialist.account.name}"
-      }
-    )
+      )
+    end
   end
 end
