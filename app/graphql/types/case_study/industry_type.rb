2e41936fec59dda2d1515eb71baf045438a9974e
Miha Rekar <miha.rekar@advisable.com>
2021-04-13 13:49:50 +0200

    Add CaseStudy::IndustryType

--- app/graphql/types/case_study/industry_type.rb
+++ app/graphql/types/case_study/industry_type.rb
@@ -3,7 +3,12 @@
 module Types
   module CaseStudy
     class IndustryType < Types::BaseType
+      graphql_name "CaseStudyIndustry"
       description "Type definition for CaseStudy::Industry"
+
+      field :id, ID, null: false
+      field :article, ArticleType, null: true
+      field :industry, Types::IndustryType, null: true
     end
   end
 end
