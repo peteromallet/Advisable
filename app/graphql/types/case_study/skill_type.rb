53a847e87706583400d357a790813ca39a20a015
Miha Rekar <miha.rekar@advisable.com>
2021-04-13 13:44:58 +0200

    Add CaseStudy::SkillType

--- app/graphql/types/case_study/skill_type.rb
+++ app/graphql/types/case_study/skill_type.rb
@@ -3,7 +3,13 @@
 module Types
   module CaseStudy
     class SkillType < Types::BaseType
+      graphql_name "CaseStudySkill"
       description "Type definition for CaseStudy::Skill"
+
+      field :id, ID, null: false
+      field :primary, Boolean, null: true
+      field :article, ArticleType, null: true
+      field :skill, Skill, null: true
     end
   end
 end
