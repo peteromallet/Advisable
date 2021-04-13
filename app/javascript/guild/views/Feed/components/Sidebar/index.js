d06c95f563dd5897e840eb72277c5c88142fe69c
Geoff Ereth <geoffereth@gmail.com>
2021-04-13 10:06:54 -0700

    Revert sidebar query consolidation

--- app/javascript/guild/views/Feed/components/Sidebar/index.js
+++ app/javascript/guild/views/Feed/components/Sidebar/index.js
@@ -2,27 +2,19 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import Sticky from "react-stickynode";
 import { Box } from "@advisable/donut";
-import useFeatureFlag from "src/hooks/useFeatureFlag";
 import { SIDEBAR_QUERY } from "./queries";
 import Referral from "@guild/components/Referral";
 import FeaturedMembers from "@guild/components/FeaturedMembers";
 import SuggestedConnection from "@guild/components/SuggestedConnection";
 
 export default function Sidebar() {
-  const includeRecommendation = useFeatureFlag("guild_recommendations");
-  const { data, loading } = useQuery(SIDEBAR_QUERY, {
-    variables: { includeRecommendation },
-  });
+  const { data, loading } = useQuery(SIDEBAR_QUERY);
   const featuredMembers = data?.guildFeaturedMembers;
-  const specialistRecommendation = data?.specialistRecommendation;
 
   return (
     <Box width="260px" flexShrink="0">
       <Sticky top={98} enabled>
-        <SuggestedConnection
-          loading={loading}
-          specialistRecommendation={specialistRecommendation}
-        />
+        <SuggestedConnection />
         <FeaturedMembers loading={loading} featuredMembers={featuredMembers} />
         <Referral />
       </Sticky>
--- app/javascript/guild/views/Feed/components/Sidebar/queries.js
+++ app/javascript/guild/views/Feed/components/Sidebar/queries.js
@@ -1,27 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const SIDEBAR_QUERY = gql`
-  query sidebar($includeRecommendation: Boolean!) {
-    specialistRecommendation @include(if: $includeRecommendation) {
-      recommendation {
-        id
-        name
-        avatar
-        firstName
-      }
-      ... on SkillRecommendation {
-        skills {
-          id
-          name
-        }
-      }
-      ... on IndustryRecommendation {
-        industries {
-          id
-          name
-        }
-      }
-    }
+  query sidebar {
     guildFeaturedMembers {
       id
       avatar
