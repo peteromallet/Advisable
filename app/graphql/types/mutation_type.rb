d251bbe665c6a0ddc6fde970cd76274ebb1f27b6
Miha Rekar <miha.rekar@advisable.com>
2021-04-13 10:15:23 +0200

    Improve MutationType to support namespaces

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,23 +2,18 @@
 
 module Types
   class MutationType < GraphQL::Schema::Object
-    EXCLUDED_CLASSES = %i[BaseMutation Helpers Guild].freeze
-    (Mutations.constants - EXCLUDED_CLASSES).each do |klass|
+    (Mutations.constants - %i[BaseMutation Helpers Guild CaseStudy]).each do |klass|
       public_send(:field, klass.to_s.underscore, mutation: "Mutations::#{klass}".constantize)
     end
 
-    # Guild
-    field :create_guild_comment, mutation: Mutations::Guild::CreateComment
-    field :guild_update_last_read, mutation: Mutations::Guild::UpdateLastRead
-    field :guild_update_post_reactions, mutation: Mutations::Guild::UpdatePostReactions
-    field :create_chat_direct_message, mutation: Mutations::Guild::CreateChatDirectMessage
-    field :create_guild_post, mutation: Mutations::Guild::CreateGuildPost
-    field :update_guild_post, mutation: Mutations::Guild::UpdateGuildPost
-    field :delete_guild_post_image, mutation: Mutations::Guild::DeleteGuildPostImage
-    field :create_guild_post_image, mutation: Mutations::Guild::CreateGuildPostImage
-    field :update_guild_post_image, mutation: Mutations::Guild::UpdateGuildPostImage
-    field :delete_guild_post, mutation: Mutations::Guild::DeleteGuildPost
-    field :resolve_guild_post, mutation: Mutations::Guild::ResolveGuildPost
-    field :update_event_registration, mutation: Mutations::UpdateEventRegistration
+    Mutations::CaseStudy.constants.each do |klass|
+      klass = "Mutations::CaseStudy::#{klass}".constantize
+      public_send(:field, klass.graphql_name.underscore, mutation: klass)
+    end
+
+    Mutations::Guild.constants.each do |klass|
+      klass = "Mutations::Guild::#{klass}".constantize
+      public_send(:field, klass.graphql_name.underscore, mutation: klass)
+    end
   end
 end
