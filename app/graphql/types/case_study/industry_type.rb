5a06c38b073aec9b6694579710b93ce8c8bcfed8
Miha Rekar <miha.rekar@advisable.com>
2021-04-13 13:06:36 +0200

    Add CaseStudy Type stubs

---
+++ app/graphql/types/case_study/industry_type.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Types
+  module CaseStudy
+    class IndustryType < Types::BaseType
+      description "Type definition for CaseStudy::Industry"
+    end
+  end
+end
---
+++ app/graphql/types/case_study/skill_type.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Types
+  module CaseStudy
+    class SkillType < Types::BaseType
+      description "Type definition for CaseStudy::Skill"
+    end
+  end
+end
