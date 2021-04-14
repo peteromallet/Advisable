ec84e3e98381d47fb6e7de98984f801f7ecd958c
Thomas Cullen <thomascullen92@gmail.com>
2021-04-14 15:27:01 +0100

    Improve filters UX and UI elements

--- app/javascript/toby/views/resource/Filters/filters/CurrencyInput.js
+++ app/javascript/toby/views/resource/Filters/filters/CurrencyInput.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@advisable/donut";
 
 function CurrencyInput({ value, onChange }) {
-  const price = value[0] ? Number(value[0] / 100.0) : "";
+  const timer = useRef(null);
+  const [inputValue, setInputValue] = useState(value?.[0] || "");
+  const price = inputValue ? Number(inputValue / 100.0) : "";
 
-  const handleChange = (e) => {
-    const nextValue = e.target.value;
-    const stripped = nextValue.replace(/[^0-9.-]+/g, "");
-    const val = stripped ? Number(stripped) * 100 : undefined;
-    onChange([val.toString()]);
+  useEffect(() => {
+    return () => clearTimeout(timer.current);
+  }, []);
+
+  const saveChange = (nextValue) => {
+    onChange([nextValue]);
   };
 
-  return <Input prefix="$" value={price} onChange={handleChange} />;
+  function parseCurrencyValue(inputVal) {
+    const stripped = inputVal.replace(/[^0-9.-]+/g, "");
+    const val = stripped ? Number(stripped) * 100 : undefined;
+    return val?.toString();
+  }
+
+  const handleChange = (e) => {
+    const nextValue = parseCurrencyValue(e.target.value);
+    setInputValue(nextValue);
+    clearTimeout(timer.current);
+
+    timer.current = setTimeout(() => saveChange(nextValue), 1000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      clearTimeout(timer.current);
+      saveChange(parseCurrencyValue(e.target.value));
+    }
+  };
+
+  return (
+    <Input
+      prefix="$"
+      value={price}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  );
 }
 
 export default CurrencyInput;
