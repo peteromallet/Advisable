9eab4c4cf5efd4fa44051932e4e051d810e1248e
guandjoy <guandjoy@gmail.com>
2021-04-14 16:58:34 +0300

    Delete src/components/Heading.js

--- app/javascript/src/components/index.js
+++ app/javascript/src/components/index.js
@@ -2,7 +2,6 @@ export { default as List } from "./List";
 export { default as Layout } from "./Layout";
 export { default as Header } from "./Header";
 export { default as Divider } from "./Divider";
-export { default as Heading } from "./Heading";
 export { default as CopyURL } from "./CopyURL";
 export { default as Loading } from "./Loading";
 export { default as BottomBar } from "./BottomBar";
