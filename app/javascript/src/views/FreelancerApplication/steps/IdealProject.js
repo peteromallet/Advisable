ff3e7f27eb3f937d969e752779bcc35f7af72e5e
Thomas Cullen <thomascullen92@gmail.com>
2021-04-14 17:24:27 +0100

    Add mixpanel & tracking to specialist application flow

--- app/javascript/src/views/FreelancerApplication/steps/IdealProject.js
+++ app/javascript/src/views/FreelancerApplication/steps/IdealProject.js
@@ -12,6 +12,7 @@ import AnimatedCard from "../components/AnimatedCard";
 import { useMutation } from "@apollo/client";
 import { COMPLETE_SETUP, UPDATE_PROFILE } from "../queries";
 import { useHistory } from "react-router-dom";
+import { track } from "src/utilities/mixpanel";
 
 export const validationSchema = object().shape({
   idealProject: string()
@@ -40,9 +41,11 @@ export default function IdealProject({ specialist }) {
       return;
     }
 
-    specialist.applicationStage === "Started" &&
-      (await complete({ variables: { input: {} } }));
+    if (specialist.applicationStage === "Started") {
+      await complete({ variables: { input: {} } });
+    }
 
+    track("Ideal Project (Specialist Application)");
     history.push("/");
   };
 
