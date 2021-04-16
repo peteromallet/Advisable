e3d0188d6d77a8eb5cd7ee1b1e77297a29e4cd72
Geoff Ereth <geoffereth@gmail.com>
2021-04-16 10:19:06 -0700

    Include reactions count in feed for author

--- app/graphql/types/guild/reaction_interface.rb
+++ app/graphql/types/guild/reaction_interface.rb
@@ -1,24 +1,32 @@
 # frozen_string_literal: true
 
-module Types::Guild::ReactionInterface
-  include Types::BaseInterface
-  field_class BaseField
+module Types
+  module Guild
+    module ReactionInterface
+      include Types::BaseInterface
+      field_class BaseField
 
-  orphan_types Types::Guild::CommentType
+      orphan_types Types::Guild::CommentType
 
-  field :reactions_count, Integer, null: true do
-    description 'The number of reactions for the guild resource'
-  end
-  def reactions_count
-    object.reactionable_count
-  end
+      description "Fields for a post reaction"
 
-  field :reacted, Boolean, null: false do
-    description 'Whether the current_user has reacted to the guild resource'
-  end
-  def reacted
-    object.reactions.exists?(specialist: context[:current_user])
-  end
+      field :reactions_count, Integer, null: true do
+        description 'The number of reactions for the guild resource'
+      end
+      def reactions_count
+        return unless current_user == object.specialist
 
-  field :kind, String, null: false
+        object.reactionable_count
+      end
+
+      field :reacted, Boolean, null: false do
+        description 'Whether the current_user has reacted to the guild resource'
+      end
+      def reacted
+        object.reactions.exists?(specialist: context[:current_user])
+      end
+
+      field :kind, String, null: false
+    end
+  end
 end
--- spec/graphql/mutations/guild/update_post_reactions_spec.rb
+++ spec/graphql/mutations/guild/update_post_reactions_spec.rb
@@ -34,18 +34,18 @@ RSpec.describe Mutations::Guild::UpdatePostReactions do
     it "creates a new thanks reaction for a post" do
       expect { guild_update_post_reactions }.to change { guild_post.reload.reactionable_count }.from(0).to(1)
       expect(guild_update_post_reactions).to include({
-                                                       "reacted" => true,
-                                                       "reactionsCount" => 1
-                                                     })
+        "reacted" => true,
+        "reactionsCount" => nil
+      })
     end
 
     it "is idempotent" do
       guild_post.reactions.create(specialist: specialist)
       expect { guild_update_post_reactions }.not_to(change { guild_post.reload.reactionable_count })
       expect(guild_update_post_reactions).to include({
-                                                       "reacted" => true,
-                                                       "reactionsCount" => 1
-                                                     })
+        "reacted" => true,
+        "reactionsCount" => nil
+      })
     end
   end
 
@@ -57,17 +57,17 @@ RSpec.describe Mutations::Guild::UpdatePostReactions do
       expect(guild_post.reactionable_count).to eq(1)
       expect { guild_update_post_reactions }.to change { guild_post.reload.reactionable_count }.from(1).to(0)
       expect(guild_update_post_reactions).to include({
-                                                       "reacted" => false,
-                                                       "reactionsCount" => 0
-                                                     })
+        "reacted" => false,
+        "reactionsCount" => nil
+      })
     end
 
     it "is idempotent" do
       expect { guild_update_post_reactions }.not_to(change { guild_post.reload.reactionable_count })
       expect(guild_update_post_reactions).to include({
-                                                       "reacted" => false,
-                                                       "reactionsCount" => 0
-                                                     })
+        "reacted" => false,
+        "reactionsCount" => nil
+      })
     end
   end
 
@@ -79,9 +79,14 @@ RSpec.describe Mutations::Guild::UpdatePostReactions do
     expect { guild_update_post_reactions }.to change { guild_post.reload.reactionable_count }.from(5).to(6)
     expect(guild_post.reload.reactions.where(specialist: specialist).count).to eq(1)
     expect(guild_update_post_reactions).to include({
-                                                     "reacted" => true,
-                                                     "reactionsCount" => 6
-                                                   })
+      "reacted" => true,
+      "reactionsCount" => nil
+    })
+  end
+
+  it "only includes the reactionsCount for the author" do
+    guild_post.update(specialist: specialist)
+    expect(guild_update_post_reactions).to include({"reactionsCount" => 1})
   end
 
   describe "notifications" do
@@ -92,11 +97,11 @@ RSpec.describe Mutations::Guild::UpdatePostReactions do
 
       notification = guild_post.specialist.account.notifications.first
       expect(notification).to have_attributes({
-                                                account: guild_post.account,
-                                                actor: specialist.account,
-                                                action: "post_reaction",
-                                                notifiable: Guild::Reaction.first
-                                              })
+        account: guild_post.account,
+        actor: specialist.account,
+        action: "post_reaction",
+        notifiable: Guild::Reaction.first
+      })
     end
 
     it "does not create duplicate notifications" do
