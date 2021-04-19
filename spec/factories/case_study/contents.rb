b5f81641002d22385ab32a45062d7c877a87f3e2
Miha Rekar <miha.rekar@advisable.com>
2021-04-19 10:49:08 +0200

    Add ResultsContent

--- spec/factories/case_study/contents.rb
+++ spec/factories/case_study/contents.rb
@@ -15,4 +15,9 @@ FactoryBot.define do
     type { "heading" }
     content { {size: "h2", text: Faker::TvShows::TheITCrowd.quote} }
   end
+
+  factory :case_study_results_content, parent: :case_study_content, class: "CaseStudy::ResultsContent" do
+    type { "results" }
+    content { { results: ["1", "2", "3"] } }
+  end
 end
