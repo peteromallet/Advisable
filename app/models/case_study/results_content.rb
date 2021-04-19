21a12d4f3b9a0f43b6c9a5cdeecd01c193c08d44
Miha Rekar <miha.rekar@advisable.com>
2021-04-19 10:49:08 +0200

    Add ResultsContent

---
+++ app/models/case_study/results_content.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module CaseStudy
+  class ResultsContent < Content
+    private
+
+    # { type: "results", content: { results: ["1", "2", "3"] } }
+    def valid_content
+      return if content.keys.size == 1 && content["results"].present?
+
+      errors.add(:content, "Content does not follow the type's requirements")
+    end
+  end
+end
+
+# == Schema Information
+#
+# Table name: case_study_contents
+#
+#  id         :uuid             not null, primary key
+#  content    :jsonb
+#  position   :integer
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  section_id :uuid             not null
+#
+# Indexes
+#
+#  index_case_study_contents_on_section_id  (section_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (section_id => case_study_sections.id)
+#
---
+++ spec/models/case_study/results_content_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CaseStudy::ResultsContent, type: :model do
+  let(:results) { build(:case_study_results_content) }
+
+  it "has a valid factory" do
+    expect(results).to be_valid
+  end
+
+  context "without results" do
+    let(:results) { build(:case_study_results_content, content: {text: "Not valid 😔"}) }
+
+    it "is invalid" do
+      expect(results).not_to be_valid
+    end
+  end
+end
