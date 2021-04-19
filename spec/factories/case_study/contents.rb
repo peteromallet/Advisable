c3a30ab8e51c8088a935dc62a93b46e882b559ec
Miha Rekar <miha.rekar@advisable.com>
2021-04-19 10:30:22 +0200

    Add HeadingContent

--- spec/factories/case_study/contents.rb
+++ spec/factories/case_study/contents.rb
@@ -10,4 +10,9 @@ FactoryBot.define do
     type { "paragraph" }
     content { {text: Faker::TvShows::TheITCrowd.quote} }
   end
+
+  factory :case_study_heading_content, parent: :case_study_content, class: "CaseStudy::HeadingContent" do
+    type { "heading" }
+    content { {size: "h2", text: Faker::TvShows::TheITCrowd.quote} }
+  end
 end
