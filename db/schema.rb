89ff743c02d6e97112ab5ae8e6507ebab5c3a24d
Miha Rekar <miha.rekar@advisable.com>
2021-04-19 09:59:11 +0200

    Add position to CaseStudy::Section

--- db/schema.rb
+++ db/schema.rb
@@ -231,6 +231,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_075326) do
   create_table "case_study_sections", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "article_id", null: false
     t.string "type"
+    t.integer "position"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "log_data"
