855f2782c46fca2faf950fbf498c0a413909038e
Miha Rekar <miha.rekar@advisable.com>
2021-04-19 10:20:25 +0200

    Add ParagraphContent

--- app/models/case_study/paragraph_content.rb
+++ app/models/case_study/paragraph_content.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
 module CaseStudy
-  class Content < ApplicationRecord
-    has_logidze
-
-    belongs_to :section
-
-    has_many_attached :images
-
-    validate :valid_content
-
+  class ParagraphContent < Content
     private
 
-    def valid_content; end
+    # { type: "paragraph", content: { text: "..." } }
+    def valid_content
+      return if content.keys.size == 1 && content["text"].present?
+
+      errors.add(:content, "Content does not follow the type's requirements")
+    end
   end
 end
 
--- spec/factories/case_study/contents.rb
+++ spec/factories/case_study/contents.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :case_study_content, class: 'CaseStudy::Content' do
+  factory :case_study_content, class: "CaseStudy::Content" do
     section { association :case_study_section }
-    type { "" }
     position { 1 }
-    content { {title: "wat"} }
+  end
+
+  factory :case_study_paragraph_content, parent: :case_study_content, class: "CaseStudy::ParagraphContent" do
+    type { "paragraph" }
+    content { {text: Faker::TvShows::TheITCrowd.quote} }
   end
 end
