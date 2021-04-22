9c3c3e272f5e8f3f50edc176adc11b8a4928abd0
Miha Rekar <miha.rekar@advisable.com>
2021-04-22 15:36:40 +0200

    Spec Twilio purge from AccountDeleteJob

--- spec/jobs/account_delete_job_spec.rb
+++ spec/jobs/account_delete_job_spec.rb
@@ -4,9 +4,12 @@ require "rails_helper"
 
 RSpec.describe AccountDeleteJob do
   it "deletes accounts marked for deletion in the past 14 days" do
-    allow_any_instance_of(Specialist).to receive(:remove_from_airtable)
     fresh = create(:specialist, account: create(:account, deleted_at: 13.days.ago))
     stale = create(:specialist, account: create(:account, deleted_at: 15.days.ago))
+
+    allow_any_instance_of(Specialist).to receive(:remove_from_airtable)
+    expect_any_instance_of(TwilioChat::Client).to receive(:purge_messages!).with(stale.uid)
+    expect_any_instance_of(TwilioChat::Client).not_to receive(:purge_messages!).with(fresh.uid)
     described_class.perform_now
 
     expect(Specialist.find_by(id: fresh.id)).not_to be_nil
