27e6a479c169ebe1d3acd60a9dd9956314ced6b0
Geoff Ereth <geoffereth@gmail.com>
2021-04-26 14:07:50 -0700

    Add skills and fallback for random recommendation

--- app/graphql/types/random_recommendation.rb
+++ app/graphql/types/random_recommendation.rb
@@ -4,5 +4,7 @@ module Types
   class RandomRecommendation < Types::BaseType
     description "Represents fields for a random recommendation"
     implements Types::RecommendationInterface
+
+    field :skills, [Types::Skill], null: true
   end
 end
