455582dc75cda1cd091ab751f6f9a82b2a0eaccc
guandjoy <guandjoy@gmail.com>
2021-04-28 14:36:19 +0300

    Add application_stage to User types

--- app/graphql/types/user.rb
+++ app/graphql/types/user.rb
@@ -2,9 +2,11 @@
 
 module Types
   class User < Types::BaseType
+    description "Represents a user"
     implements Types::AccountInterface
     delegate :account, :company, to: :object
     field :airtable_id, String, null: true, deprecation_reason: "We're moving away from Airtable. Please stop using Airtable IDs."
+    field :application_stage, String, null: true, method: :application_status
 
     field :email, String, null: false do
       authorize :admin?, :user?, :candidate_for_user_project?, :owned_by_company?
