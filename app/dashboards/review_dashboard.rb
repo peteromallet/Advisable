35d007fb9911556bdb0ddf71176bf2754833d277
Miha Rekar <miha.rekar@advisable.com>
2021-04-29 09:34:37 +0200

    Add Review to Administrate

---
+++ app/dashboards/review_dashboard.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+class ReviewDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    specialist: Field::BelongsTo,
+    project: Field::Polymorphic.with_options(classes: [Project, PreviousProject]),
+    id: Field::Number,
+    airtable_id: Field::String,
+    type: Field::String,
+    comment: Field::Text,
+    ratings: Field::String.with_options(searchable: false),
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    uid: Field::String
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    specialist
+    project
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    specialist
+    project
+    id
+    airtable_id
+    type
+    comment
+    ratings
+    created_at
+    updated_at
+    uid
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    specialist
+    project
+    airtable_id
+    type
+    comment
+    ratings
+    uid
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how reviews are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(review)
+  #   "Review ##{review.id}"
+  # end
+end
