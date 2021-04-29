d77897797ed4aa0660eb3e370c2d2b1d7367053a
Miha Rekar <miha.rekar@advisable.com>
2021-04-29 09:45:13 +0200

    Reviewable isn't really used

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :review do
     specialist
     project
     sequence(:uid) { "rev_#{SecureRandom.hex[0..14]}" }
-    association :reviewable, factory: :application
     comment { Faker::Lorem.sentence(word_count: 24) }
-    ratings { { overall: 5 } }
+    ratings { {overall: 5} }
   end
 end
