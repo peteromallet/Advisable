c2764cb551e7f21c4fc7d4541129de80d1fc7c91
Miha Rekar <miha.rekar@advisable.com>
2021-04-30 09:13:18 +0200

    Support custom webdrivers

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'capybara'
-require 'capybara/rspec'
+require "capybara"
+require "capybara/rspec"
 
 Capybara.server = :puma, {Silent: true}
 Capybara.enable_aria_label = true
@@ -9,9 +9,13 @@ Capybara.default_max_wait_time = 5
 
 RSpec.configure do |config|
   config.retry_callback = ->(ex) { Capybara.reset! if ex.metadata[:js] }
-
   config.before(:each, type: :system) do
-    if ENV['HEADLESS'] == 'false'
+    if ENV["BROWSER_PATH"].present?
+      Selenium::WebDriver::Chrome.path = ENV["BROWSER_PATH"]
+      Webdrivers::Chromedriver.required_version = ENV["CHROMEDRIVER_VERSION"]
+    end
+
+    if ENV["HEADLESS"] == "false"
       driven_by :selenium, using: :chrome
     else
       driven_by :selenium, using: :headless_chrome
