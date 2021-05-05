cb2422b0a9755d7a2d0cef1d5466c9961d02a606
Miha Rekar <miha.rekar@advisable.com>
2021-05-05 14:46:11 +0200

    Extend Resizable to support many

--- app/models/previous_project.rb
+++ app/models/previous_project.rb
@@ -42,8 +42,6 @@ class PreviousProject < ApplicationRecord
 
   has_many :application_references, foreign_key: 'off_platform_project_id', inverse_of: :previous_project, dependent: :destroy
   has_many :reviews, foreign_key: :project_id, inverse_of: :project, dependent: :destroy
-  has_many :images, class_name: 'PreviousProjectImage', foreign_key: 'off_platform_project_id', inverse_of: :previous_project, dependent: :destroy
-  has_one :cover_photo, -> { where cover: true }, class_name: 'PreviousProjectImage', foreign_key: 'off_platform_project_id', inverse_of: :previous_project
 
   # Project skills
   has_many :project_skills, as: :project, dependent: :destroy
