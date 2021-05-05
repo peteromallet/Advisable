1a62cf269c9b23b9c08f2591e68b2809a43f8506
Miha Rekar <miha.rekar@advisable.com>
2021-05-05 15:03:46 +0200

    Don't need this anymore

--- app/models/previous_project_image.rb
+++ app/models/previous_project_image.rb
@@ -8,29 +8,6 @@ class PreviousProjectImage < ApplicationRecord
   belongs_to :previous_project, class_name: 'PreviousProject', foreign_key: 'off_platform_project_id', inverse_of: :images
   has_one_attached :image
   resize image: {resize_to_limit: [1600, 1600]}
-
-  after_destroy :set_first_to_cover, if: :cover
-  after_destroy :reduce_positions
-
-  private
-
-  def reduce_positions
-    always_fresh_previous_project.images.where('position > ?', position).find_each do |ppi|
-      ppi.update position: ppi.position - 1
-    end
-  end
-
-  def set_first_to_cover
-    always_fresh_previous_project.images.order(position: :asc).first.try(
-      :update,
-      cover: true
-    )
-  end
-
-  # Needed because of after_destroy hooks where the object is gone
-  def always_fresh_previous_project
-    @always_fresh_previous_project ||= PreviousProject.find(off_platform_project_id)
-  end
 end
 
 # == Schema Information
