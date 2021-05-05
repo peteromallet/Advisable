8ba75f1dcbb03c1299974cf5eb33cdb915fd1f55
Miha Rekar <miha.rekar@advisable.com>
2021-05-05 13:17:07 +0200

    Start work on new image attaching to PreviousProject

--- app/graphql/types/previous_project.rb
+++ app/graphql/types/previous_project.rb
@@ -17,28 +17,24 @@ module Types
     field :contact_name, String, null: true
     field :contact_job_title, String, null: true
     field :contact_relationship, String, null: true
+
     field :cover_photo, Types::PreviousProjectImage, null: true
-    field :industry_relevance, Integer, null: true
-    field :location_relevance, Integer, null: true
-
-    field :pending_description, String, null: true, deprecation_reason: "Descriptions dont need to be approved anymore"
-
-    field :images, [Types::PreviousProjectImage], null: false
-
-    def images
-      object.images.order(position: :asc)
+    def cover_photo
+      object.cover_photo || object.images.first
     end
 
+    field :industry_relevance, Integer, null: true
+    field :location_relevance, Integer, null: true
+    field :pending_description, String, null: true, deprecation_reason: "Descriptions dont need to be approved anymore"
+    field :images, [Types::PreviousProjectImage], null: false
     field :id, ID, null: false, method: :uid
 
     field :draft, Boolean, null: false
-
     def draft
       object.draft || false
     end
 
     field :public_use, Boolean, null: false
-
     def public_use
       object.public_use || false
     end
@@ -49,7 +45,6 @@ module Types
     # by an authenticated specialist, this should be updated to check if the project belongs
     # to the authed specialist.
     field :cost_to_hire, Integer, null: true
-
     def cost_to_hire
       return nil unless object.draft?
 
@@ -57,7 +52,6 @@ module Types
     end
 
     field :execution_cost, Integer, null: true
-
     def execution_cost
       return nil unless object.draft?
 
@@ -65,7 +59,6 @@ module Types
     end
 
     field :title, String, null: false
-
     def title
       return "Project with #{client_name}" if object.primary_skill.nil?
 
@@ -73,32 +66,27 @@ module Types
     end
 
     field :excerpt, String, null: true
-
     def excerpt
       object.description.try(:truncate, 160)
     end
 
     field :confidential, Boolean, null: false
-
     def confidential
       object.confidential || false
     end
 
     field :company_type, String, null: false
-
     def company_type
       object.company_type || "company"
     end
 
     field :client_name, String, null: false
-
     def client_name
       previous_project_company_name(object)
     end
 
     # Only show the contact email if the validation status is in progress
     field :contact_email, String, null: true
-
     def contact_email
       return object.contact_email if object.validation_status == 'In Progress'
 
@@ -106,7 +94,6 @@ module Types
     end
 
     field :similar_specialists, [Types::SpecialistType], null: false
-
     def similar_specialists
       industry = object.primary_industry
       return [] if industry.nil?
--- app/models/concerns/resizable.rb
+++ app/models/concerns/resizable.rb
@@ -7,25 +7,33 @@ module Resizable
     def resize(attachments)
       attachments.each do |name, options|
         define_method "resized_#{name}" do
-          get_resized_image(name, options)
+          image = public_send(name)
+          get_resized_image(image, options)
         end
 
         define_method "resized_#{name}_url" do
-          get_resized_image_url(name, options)
+          image = get_resized_image(public_send(name), options)
+          get_resized_image_url(image, options)
         end
+      end
+    end
+
+    def resize_many(attachments)
+      attachments.each do |name, options|
+        # TODO
       end
     end
   end
 
   private
 
-  def get_resized_image(name, options)
-    image = public_send(name)
+  def get_resized_image(image, options)
     return if image.blank?
 
     if image.variant(options).processed?
       image.variant(options)
     else
+      # TODO: This needs fixing because we don't have name
       ResizeImageJob.perform_later(self, name, **options)
       image
     end
@@ -35,8 +43,7 @@ module Resizable
     nil
   end
 
-  def get_resized_image_url(name, options)
-    image = get_resized_image(name, options)
+  def get_resized_image_url(image, _options)
     return if image.blank?
 
     if image.respond_to?(:variation)
