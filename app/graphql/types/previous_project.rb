aa47604b4e273adfab122853e169cf2a9b32fee0
Miha Rekar <miha.rekar@advisable.com>
2021-05-05 13:20:03 +0200

    RuboCop

--- app/graphql/types/previous_project.rb
+++ app/graphql/types/previous_project.rb
@@ -3,13 +3,8 @@
 module Types
   class PreviousProject < Types::BaseType
     include PreviousProjectHelper
-    field :id, ID, null: false
-    field :title, String, null: false
     field :goal, String, null: true
-    field :excerpt, String, null: true
     field :description, String, null: true
-    field :client_name, String, null: false
-    field :company_type, String, null: false
     field :specialist, Types::SpecialistType, null: false
     field :reviews, [Types::Review], null: false
     field :primary_skill, Types::Skill, null: true
@@ -17,54 +12,51 @@ module Types
     field :skills, [Types::Skill], null: false
     field :industries, [Types::IndustryType], null: false
     field :validation_status, String, null: true
-    field :on_platform, Boolean, null: false
-    field :contact_email, String, null: true
     field :contact_first_name, String, null: true
     field :contact_last_name, String, null: true
     field :contact_name, String, null: true
     field :contact_job_title, String, null: true
     field :contact_relationship, String, null: true
-    field :confidential, Boolean, null: false
-    field :draft, Boolean, null: false
-    field :public_use, Boolean, null: false
-    field :images, [Types::PreviousProjectImage], null: false
     field :cover_photo, Types::PreviousProjectImage, null: true
     field :industry_relevance, Integer, null: true
     field :location_relevance, Integer, null: true
-    field :cost_to_hire, Integer, null: true
-    field :execution_cost, Integer, null: true
-    field :similar_specialists, [Types::SpecialistType], null: false
 
     field :pending_description, String, null: true, deprecation_reason: "Descriptions dont need to be approved anymore"
+
+    field :images, [Types::PreviousProjectImage], null: false
 
     def images
       object.images.order(position: :asc)
     end
 
-    def id
-      object.uid
-    end
+    field :id, ID, null: false, method: :uid
+
+    field :draft, Boolean, null: false
 
     def draft
       object.draft || false
     end
 
+    field :public_use, Boolean, null: false
+
     def public_use
       object.public_use || false
     end
 
-    def on_platform
-      object.on_platform?
-    end
+    field :on_platform, Boolean, null: false, method: :on_platform?
 
     # Only allow cost_to_hire if object is in draft. Once previous projects can only be added
     # by an authenticated specialist, this should be updated to check if the project belongs
     # to the authed specialist.
+    field :cost_to_hire, Integer, null: true
+
     def cost_to_hire
       return nil unless object.draft?
 
       object.cost_to_hire
     end
+
+    field :execution_cost, Integer, null: true
 
     def execution_cost
       return nil unless object.draft?
@@ -72,34 +64,48 @@ module Types
       object.execution_cost
     end
 
+    field :title, String, null: false
+
     def title
       return "Project with #{client_name}" if object.primary_skill.nil?
 
       "#{object.primary_skill.try(:name)} project with #{client_name}"
     end
 
+    field :excerpt, String, null: true
+
     def excerpt
       object.description.try(:truncate, 160)
     end
+
+    field :confidential, Boolean, null: false
 
     def confidential
       object.confidential || false
     end
 
+    field :company_type, String, null: false
+
     def company_type
       object.company_type || "company"
     end
+
+    field :client_name, String, null: false
 
     def client_name
       previous_project_company_name(object)
     end
 
     # Only show the contact email if the validation status is in progress
+    field :contact_email, String, null: true
+
     def contact_email
       return object.contact_email if object.validation_status == 'In Progress'
 
       nil
     end
+
+    field :similar_specialists, [Types::SpecialistType], null: false
 
     def similar_specialists
       industry = object.primary_industry
