f9429ec2974867406807a821e2973772db5e9010
guandjoy <guandjoy@gmail.com>
2021-05-07 19:20:18 +0300

    Create schedule_client_application_interview.rb

---
+++ app/graphql/mutations/schedule_client_application_interview.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Takes calendly's eventID and fetch interview details
+module Mutations
+  class ScheduleClientApplicationInterview < BaseMutation
+    description "Schedule an interview of a client with Advisable"
+
+    field :user, Types::User, null: true
+
+    # The scheduleClientApplicationInterview mutation
+    # requires a specalist to be logged in.
+    def authorized?(*_args)
+      requires_client!
+
+      if current_user.application_status != "Invited To Interview"
+        ApiError.invalid_request(
+          "INVALID_APPLICATION_STATUS",
+          "The account status must be 'Invited To Interview' but it is #{current_user.application_status}"
+        )
+      end
+
+      true
+    end
+
+    def resolve
+      current_user.application_status = 'Interview Scheduled'
+      current_user.save_and_sync_with_responsible!(current_account_id)
+
+      {user: current_user}
+    end
+  end
+end
