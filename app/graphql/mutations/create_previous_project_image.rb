45fd496446a5ffc5b32a6160784de8bd965d44a0
Thomas Cullen <thomascullen92@gmail.com>
2021-05-07 11:05:29 +0100

    Fix image uploads

--- app/graphql/mutations/create_previous_project_image.rb
+++ app/graphql/mutations/create_previous_project_image.rb
@@ -2,6 +2,7 @@
 
 module Mutations
   class CreatePreviousProjectImage < Mutations::BaseMutation
+    description "Attaches an image to a previous project"
     argument :attachment, String, required: true
     argument :previous_project, ID, required: true
 
@@ -9,14 +10,16 @@ module Mutations
 
     def authorized?(previous_project:, **_args)
       requires_specialist!
-      project = PreviousProject.find_by!(previous_project)
+      project = PreviousProject.find_by!(uid: previous_project)
       policy = PreviousProjectPolicy.new(current_user, project)
       policy.create_image?
     end
 
     def resolve(previous_project:, attachment:)
-      project = PreviousProject.find_by!(previous_project)
-      image = project.images.attach(attachment)
+      project = PreviousProject.find_by!(uid: previous_project)
+      blob = ActiveStorage::Blob.find_signed!(attachment)
+      project.images.attach(blob)
+      image = project.reload.images.find_by_blob_id(blob.id)
 
       {image: image}
     end
