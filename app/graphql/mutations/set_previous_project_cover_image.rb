60da76ad53e442000e4a0c9dd3be9aa2d2ef732e
Thomas Cullen <thomascullen92@gmail.com>
2021-05-07 11:23:55 +0100

    Fix setPreviousProjectCoverImage mutation

--- app/graphql/mutations/set_previous_project_cover_image.rb
+++ app/graphql/mutations/set_previous_project_cover_image.rb
@@ -2,6 +2,7 @@
 
 module Mutations
   class SetPreviousProjectCoverImage < Mutations::BaseMutation
+    description "Set the cover image for a previous project"
     argument :attachment, String, required: true
     argument :previous_project, ID, required: true
 
@@ -9,14 +10,15 @@ module Mutations
 
     def authorized?(previous_project:, **_args)
       requires_specialist!
-      project = PreviousProject.find_by!(previous_project)
+      project = PreviousProject.find_by!(uid: previous_project)
       policy = PreviousProjectPolicy.new(current_user, project)
       policy.create_image?
     end
 
     def resolve(previous_project:, attachment:)
-      project = PreviousProject.find_by!(previous_project)
-      image = project.cover_photo.attach(attachment)
+      project = PreviousProject.find_by!(uid: previous_project)
+      project.cover_photo.attach(attachment)
+      image = project.reload.cover_photo.attachment
 
       {image: image}
     end
