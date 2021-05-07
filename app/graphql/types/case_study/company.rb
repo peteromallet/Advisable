80496584c2bdc51330700d4b08bb6f1cc26a6326
Miha Rekar <miha.rekar@advisable.com>
2021-05-07 13:40:36 +0200

    Rename CompanyType to Company

--- app/graphql/types/case_study/company.rb
+++ app/graphql/types/case_study/company.rb
@@ -2,7 +2,7 @@
 
 module Types
   module CaseStudy
-    class CompanyType < Types::BaseType
+    class Company < Types::BaseType
       graphql_name "CaseStudyCompany"
       description "Type definition for CaseStudy::Company"
 
