4b8a87101566c0bf088d5472c03a6411899e5e17
Miha Rekar <miha.rekar@advisable.com>
2021-05-10 15:19:18 +0200

    Add migration for transforming PreviousProjectImages to ActiveStorage::Attachments

--- app/models/previous_project_image.rb
+++ app/models/previous_project_image.rb
@@ -5,7 +5,6 @@ class PreviousProjectImage < ApplicationRecord
   include Uid
   uid_prefix 'ppi'
 
-  belongs_to :previous_project, class_name: 'PreviousProject', foreign_key: 'off_platform_project_id', inverse_of: :images
   has_one_attached :image
   resize image: {resize_to_limit: [1600, 1600]}
 end
