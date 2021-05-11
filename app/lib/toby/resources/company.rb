6c0f78686db3dd77bf08ab25fb196ba73b1fa9bd
Thomas Cullen <thomascullen92@gmail.com>
2021-05-11 13:48:25 +0100

    Add label to more resources

--- app/lib/toby/resources/company.rb
+++ app/lib/toby/resources/company.rb
@@ -14,6 +14,14 @@ module Toby
       attribute :vat_number, Attributes::String
       attribute :created_at, Attributes::DateTime, readonly: true
       attribute :updated_at, Attributes::DateTime, readonly: true
+
+      def self.label(record, context)
+        Lazy::Label.new(::Company, record.id, context, value_column: :name)
+      end
+
+      def self.search(query)
+        ::Company.where("name ilike ?", "%#{query}%")
+      end
     end
   end
 end
