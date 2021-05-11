139dfdbc075e2636300c3c5987a604d56606d7be
Miha Rekar <miha.rekar@advisable.com>
2021-05-11 12:03:01 +0200

    Rename block to value_block to be clearer

--- app/lib/toby/lazy/label.rb
+++ app/lib/toby/lazy/label.rb
@@ -3,17 +3,17 @@
 module Toby
   module Lazy
     class Label
-      attr_reader :klass, :id, :context, :id_column, :includes, :value_column, :block
+      attr_reader :klass, :id, :context, :id_column, :includes, :value_column, :value_block
 
       # rubocop: disable Metrics/ParameterLists
-      def initialize(klass, id, context, id_column: :id, includes: nil, value_column: nil, &block)
+      def initialize(klass, id, context, id_column: :id, includes: nil, value_column: nil, &value_block)
         @klass = klass
         @id = id
         @context = context
         @id_column = id_column
         @includes = includes
         @value_column = value_column
-        @block = block
+        @value_block = value_block
         state[:pending] << id
       end
       # rubocop: enable Metrics/ParameterLists
@@ -33,7 +33,7 @@ module Toby
         ar = includes ? klass.includes(*includes) : klass
         ar.where(id_column => state[:pending]).each do |record|
           key = record.public_send(id_column)
-          value = value_column ? record.public_send(value_column) : block.call(record)
+          value = value_column ? record.public_send(value_column) : value_block.call(record)
           state[:loaded][key] = value
         end
         state[:pending].clear
