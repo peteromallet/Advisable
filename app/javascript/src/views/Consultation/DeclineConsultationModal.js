61dff366a2aaa44f06d5839ea396550bc1e57344
guandjoy <guandjoy@gmail.com>
2021-05-12 11:51:48 +0300

    Resolve warnings of button types

--- app/javascript/src/views/Consultation/DeclineConsultationModal.js
+++ app/javascript/src/views/Consultation/DeclineConsultationModal.js
@@ -4,6 +4,7 @@ import { useMutation } from "@apollo/client";
 import { Modal, Box, Text, Button, Textarea } from "@advisable/donut";
 import FormField from "../../components/FormField";
 import DECLINE from "./declineConsultation";
+import SubmitButton from "src/components/SubmitButton";
 
 export default function DeclineConsultationModal({ consultation, modal }) {
   const [declineConsultation] = useMutation(DECLINE);
@@ -20,40 +21,33 @@ export default function DeclineConsultationModal({ consultation, modal }) {
     <Modal modal={modal} label="Decline Consultation Modal">
       <Box padding="l">
         <Formik onSubmit={handleSubmit} initialValues={{ reason: "" }}>
-          {(formik) => (
-            <Form>
-              <Text
-                mb="l"
-                fontSize="xl"
-                color="blue900"
-                fontWeight="medium"
-                letterSpacing="-0.02em"
-              >
-                Decline consultation with {consultation.user.companyName}
-              </Text>
-              <Box mb="l">
-                <FormField
-                  as={Textarea}
-                  minRows={3}
-                  name="reason"
-                  labelHint="Optional"
-                  placeholder="reason"
-                  label="What is your reason for declining this consultation?"
-                />
-              </Box>
-              <Button
-                type="submit"
-                loading={formik.isSubmitting}
-                variant="dark"
-                mr="xs"
-              >
-                Decline Consultation
-              </Button>
-              <Button variant="subtle" onClick={modal.hide}>
-                Cancel
-              </Button>
-            </Form>
-          )}
+          <Form>
+            <Text
+              mb="l"
+              fontSize="xl"
+              color="blue900"
+              fontWeight="medium"
+              letterSpacing="-0.02em"
+            >
+              Decline consultation with {consultation.user.companyName}
+            </Text>
+            <Box mb="l">
+              <FormField
+                as={Textarea}
+                minRows={3}
+                name="reason"
+                labelHint="Optional"
+                placeholder="reason"
+                label="What is your reason for declining this consultation?"
+              />
+            </Box>
+            <SubmitButton variant="dark" mr="xs">
+              Decline Consultation
+            </SubmitButton>
+            <Button variant="subtle" onClick={modal.hide}>
+              Cancel
+            </Button>
+          </Form>
         </Formik>
       </Box>
     </Modal>
