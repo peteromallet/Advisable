987d1dd9424a7f35617566d134b3601e8fabe764
Miha Rekar <miha.rekar@advisable.com>
2021-05-12 09:27:09 +0200

    RuboCop

--- app/graphql/mutations/create_user_from_project_verification.rb
+++ app/graphql/mutations/create_user_from_project_verification.rb
@@ -1,62 +1,64 @@
 # frozen_string_literal: true
 
-class Mutations::CreateUserFromProjectVerification < Mutations::BaseMutation
-  include Mutations::Helpers::BlacklistedEmail
+module Mutations
+  class CreateUserFromProjectVerification < Mutations::BaseMutation
+    include Mutations::Helpers::BlacklistedEmail
 
-  argument :previous_project, ID, required: true
-  argument :email, String, required: true
-  argument :fid, String, required: true
+    argument :email, String, required: true
+    argument :fid, String, required: true
+    argument :previous_project, ID, required: true
 
-  field :user, Types::User, null: true
+    field :user, Types::User, null: true
 
-  def authorized?(previous_project:, email:, fid:)
-    unless context[:oauth_viewer]
-      ApiError.invalid_request(
-              'notAuthenticated',
-              'Not logged into Linkedin'
-            )
+    def authorized?(previous_project:, email:, fid:)
+      unless context[:oauth_viewer]
+        ApiError.invalid_request(
+          'notAuthenticated',
+          'Not logged into Linkedin'
+        )
+      end
+
+      true
     end
 
-    true
-  end
+    def resolve(previous_project:, email:, fid:)
+      project = PreviousProject.find_by_uid(previous_project)
+      viewer = context[:oauth_viewer]
 
-  def resolve(previous_project:, email:, fid:)
-    project = PreviousProject.find_by_uid(previous_project)
-    viewer = context[:oauth_viewer]
+      email_blacklisted?(email)
+      account = Account.new(
+        email: email,
+        first_name: viewer.first_name,
+        last_name: viewer.last_name,
+        permissions: [:team_manager]
+      )
+      account.save!
 
-    email_blacklisted?(email)
-    account = Account.new(
-      email: email,
-      first_name: viewer.first_name,
-      last_name: viewer.last_name,
-      permissions: [:team_manager]
-    )
-    account.save!
+      company = Company.new(
+        name: Company.fresh_name_for(project.client_name),
+        kind: project.company_type,
+        industry: project.primary_industry
+      )
+      company.save!
 
-    company = Company.new(
-      name: Company.fresh_name_for(project.client_name),
-      kind: project.company_type,
-      industry: project.primary_industry
-    )
-    company.save!
-
-    user = User.new(
-      account: account,
-      company: company,
-      fid: fid,
-      contact_status: 'Application Started',
-      campaign_source: 'validation'
-    )
-    user.save_and_sync_with_responsible!(current_account_id)
-    AttachImageJob.perform_later(user, viewer.image)
-    {user: user}
-  rescue ActiveRecord::RecordInvalid
-    if account.errors.added?(:email, :taken, value: email)
-      ApiError.invalid_request("emailTaken", "The email #{email} is already used by another account.")
-    elsif account.errors.added?(:email, :blank)
-      ApiError.invalid_request("emailBlank", "Email is required.")
-    else
-      raise
+      user = User.new(
+        account: account,
+        company: company,
+        fid: fid,
+        contact_status: 'Application Started',
+        campaign_source: 'validation'
+      )
+      user.save_and_sync_with_responsible!(current_account_id)
+      AttachImageJob.perform_later(user, viewer.image)
+      {user: user}
+    rescue ActiveRecord::RecordInvalid
+      if account.errors.added?(:email, :taken, value: email)
+        ApiError.invalid_request("emailTaken", "The email #{email} is already used by another account.")
+      elsif account.errors.added?(:email, :blank)
+        ApiError.invalid_request("emailBlank", "Email is required.")
+      else
+        raise
+      end
     end
   end
 end
