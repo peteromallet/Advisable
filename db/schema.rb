707b6d3f95e575b1600fc6717c4d01901ef3626b
Miha Rekar <miha.rekar@advisable.com>
2021-05-12 10:00:29 +0200

    Drop webhook_configurations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_073154) do
+ActiveRecord::Schema.define(version: 2021_05_12_075909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -1011,15 +1011,6 @@ ActiveRecord::Schema.define(version: 2021_05_10_073154) do
     t.string "zoom_url"
     t.index ["interview_id"], name: "index_video_calls_on_interview_id"
     t.index ["uid"], name: "index_video_calls_on_uid"
-  end
-
-  create_table "webhook_configurations", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.string "type"
-    t.jsonb "criteria"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "webhook_events", force: :cascade do |t|
