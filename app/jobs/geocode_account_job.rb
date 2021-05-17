d471ce1ab663e16fde2b1d2e13c714f05ddf7cd5
Miha Rekar <miha.rekar@advisable.com>
2021-05-17 11:38:24 +0200

    Make GeocodeAccountJob

---
+++ app/jobs/geocode_account_job.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+
+class GeocodeAccountJob < ApplicationJob
+  attr_reader :account, :geocoded_ip, :country
+
+  queue_as :default
+
+  def perform(account, ip)
+    @account = account
+    @geocoded_ip = geocode(ip)
+    return if geocoded_ip.blank? || geocoded_ip&.country.blank?
+
+    @country = Country.find_by(alpha2: geocoded_ip.country)
+    geocode_user if account.user
+    geocode_specialist if account.specialist
+  end
+
+  private
+
+  def geocode_user
+    user = account.user
+    user.country = country
+    user.company.address = {city: geocoded_ip.city, country: geocoded_ip.country}
+    user.sync_to_airtable
+  end
+
+  def geocode_specialist
+    # TODO
+  end
+
+  def geocode(ip)
+    if Rails.env.development?
+      OpenStruct.new(country: 'IE', city: 'Dublin')
+    else
+      Geocoder.search(ip)&.first
+    end
+  end
+end
