48a8bd1aace6f851b0ee452658eeaabce3c9b844
Miha Rekar <miha.rekar@advisable.com>
2021-05-18 09:17:58 +0200

    Add authorization to ResendInterviewRequest

--- spec/graphql/mutations/resend_interview_request_spec.rb
+++ spec/graphql/mutations/resend_interview_request_spec.rb
@@ -46,4 +46,45 @@ RSpec.describe Mutations::ResendInterviewRequest do
       expect(message).to eq("FAILED_TO_RESEND")
     end
   end
+
+  context "when logged in user is a specialist" do
+    let(:current_user) { interview.specialist }
+
+    it "sets the status to 'More Time Options Added'" do
+      expect_any_instance_of(Interview).to receive(:sync_to_airtable)
+      response = AdvisableSchema.execute(query, context: context)
+      status = response["data"]["resendInterviewRequest"]["interview"]["status"]
+      expect(status).to eq("More Time Options Added")
+      expect(interview.reload.more_time_options_added_at).to be_within(1.second).of(Time.zone.now)
+    end
+  end
+
+  context "when logged in user is a different specialist" do
+    let(:current_user) { create(:specialist) }
+
+    it "raises an error" do
+      response = AdvisableSchema.execute(query, context: context)
+      error = response["errors"].first["extensions"]["type"]
+      expect(error).to eq("NOT_AUTHORIZED")
+    end
+  end
+
+  context "when logged in user is a different user" do
+    let(:current_user) { create(:user) }
+
+    it "raises an error" do
+      response = AdvisableSchema.execute(query, context: context)
+      error = response["errors"].first["extensions"]["type"]
+      expect(error).to eq("NOT_AUTHORIZED")
+    end
+  end
+
+  context "when not logged in" do
+    let(:context) { {current_user: nil} }
+
+    it "returns an error" do
+      response = AdvisableSchema.execute(query, context: context)
+      expect(response["errors"][0]["extensions"]["type"]).to eq("NOT_AUTHENTICATED")
+    end
+  end
 end
