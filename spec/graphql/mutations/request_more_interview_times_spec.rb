ecb02cd0b4a695c55ae30914b26f490d31a89709
Miha Rekar <miha.rekar@advisable.com>
2021-05-18 09:28:30 +0200

    Add authorization to RequestMoreInterviewTimes

--- spec/graphql/mutations/request_more_interview_times_spec.rb
+++ spec/graphql/mutations/request_more_interview_times_spec.rb
@@ -30,8 +30,7 @@ RSpec.describe Mutations::RequestMoreInterviewTimes do
   end
 
   it "sets the status to 'Need More Time Options'" do
-    status =
-      response['data']['requestMoreInterviewTimes']['interview']['status']
+    status = response['data']['requestMoreInterviewTimes']['interview']['status']
     expect(status).to eq('Need More Time Options')
   end
 
@@ -41,7 +40,7 @@ RSpec.describe Mutations::RequestMoreInterviewTimes do
   end
 
   context "when the status is not 'Call Requested'" do
-    let(:interview) { create(:interview, status: 'Call Completed') }
+    let(:interview) { create(:interview, user: user, status: 'Call Completed') }
 
     it 'returns an error' do
       error = response['errors'][0]['extensions']['code']
@@ -49,12 +48,41 @@ RSpec.describe Mutations::RequestMoreInterviewTimes do
     end
   end
 
-  context "when no user is logged in" do
-    let(:current_user) { nil }
+  context "when logged in user is a specialist" do
+    let(:current_user) { interview.specialist }
+
+    it "sets the status to 'Need More Time Options'" do
+      status = response['data']['requestMoreInterviewTimes']['interview']['status']
+      expect(status).to eq('Need More Time Options')
+    end
+  end
+
+  context "when logged in user is a different specialist" do
+    let(:current_user) { create(:specialist) }
+
+    it "raises an error" do
+      response = AdvisableSchema.execute(query, context: context)
+      error = response["errors"].first["extensions"]["type"]
+      expect(error).to eq("NOT_AUTHORIZED")
+    end
+  end
+
+  context "when logged in user is a different user" do
+    let(:current_user) { create(:user) }
+
+    it "raises an error" do
+      response = AdvisableSchema.execute(query, context: context)
+      error = response["errors"].first["extensions"]["type"]
+      expect(error).to eq("NOT_AUTHORIZED")
+    end
+  end
+
+  context "when not logged in" do
+    let(:context) { {current_user: nil} }
 
     it "returns an error" do
-      error = response["errors"][0]["extensions"]["code"]
-      expect(error).to eq("notAuthorized")
+      response = AdvisableSchema.execute(query, context: context)
+      expect(response["errors"][0]["extensions"]["type"]).to eq("NOT_AUTHENTICATED")
     end
   end
 end
