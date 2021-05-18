f94733fb051e439e3e1f8acad9b7e393b5381dd6
Miha Rekar <miha.rekar@advisable.com>
2021-05-18 10:25:52 +0200

    WIP remove airtable on interview

--- spec/factories/interviews.rb
+++ spec/factories/interviews.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     user
     status { "Call Scheduled" }
     sequence(:uid) { "int_#{SecureRandom.hex[0..14]}" }
-    sequence(:airtable_id) { |id| "recinterview#{id}" }
     starts_at { 2.days.from_now.change({ hour: 12, min: 0, sec: 0 }) }
   end
 end
--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
-  include_examples 'Airtable::Syncable'
-
   it "has a valid factory" do
     interview = build(:interview)
     expect(interview).to be_valid
