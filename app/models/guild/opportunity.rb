3db359bef01ac28fe68634fe5a005882739384cd
Miha Rekar <miha.rekar@advisable.com>
2021-05-20 13:16:18 +0200

    Add CaseStudy::Article reference to Guild::Post

--- app/models/guild/opportunity.rb
+++ app/models/guild/opportunity.rb
@@ -24,15 +24,18 @@ end
 #  type               :string           default("Post"), not null
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
+#  article_id         :bigint
 #  post_prompt_id     :uuid
 #  specialist_id      :bigint
 #
 # Indexes
 #
+#  index_guild_posts_on_article_id      (article_id)
 #  index_guild_posts_on_post_prompt_id  (post_prompt_id)
 #  index_guild_posts_on_specialist_id   (specialist_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (article_id => case_study_articles.id)
 #  fk_rails_...  (specialist_id => specialists.id)
 #
