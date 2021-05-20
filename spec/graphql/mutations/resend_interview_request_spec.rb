4f47dc7eb532eb1ef5c8be8e2177ae2ca15f0812
Miha Rekar <miha.rekar@advisable.com>
2021-05-20 08:47:14 +0200

    Fix spec

--- spec/graphql/mutations/resend_interview_request_spec.rb
+++ spec/graphql/mutations/resend_interview_request_spec.rb
@@ -46,7 +46,6 @@ RSpec.describe Mutations::ResendInterviewRequest do
     let(:current_user) { interview.specialist }
 
     it "sets the status to 'More Time Options Added'" do
-      expect_any_instance_of(Interview).to receive(:sync_to_airtable)
       response = AdvisableSchema.execute(query, context: context)
       status = response["data"]["resendInterviewRequest"]["interview"]["status"]
       expect(status).to eq("More Time Options Added")
