ec3bac8e0b0341fb93c98c56b14fa86262413fc1
Miha Rekar <miha.rekar@advisable.com>
2021-05-21 13:56:47 +0200

    userBelongsToDifferentCompany -> USER_BELONGS_TO_A_DIFFERENT_COMPANY

--- app/graphql/mutations/remove_user_from_company.rb
+++ app/graphql/mutations/remove_user_from_company.rb
@@ -11,7 +11,7 @@ module Mutations
 
       user = User.find_by!(uid: user_id)
       if current_user.company_id != user.company_id
-        ApiError.invalid_request("userBelongsToDifferentCompany", "This user belongs to a different company and can't be removed by you.")
+        ApiError.invalid_request("USER_BELONGS_TO_A_DIFFERENT_COMPANY", "This user belongs to a different company and can't be removed by you.")
       elsif user.company.users.count == 1
         ApiError.invalid_request("noUsersLeftInCompany", "This user is the last user in the company and can't be deleted")
       end
