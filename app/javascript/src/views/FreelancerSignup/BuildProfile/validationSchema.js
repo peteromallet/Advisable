57ffeb8402f3549fd479c122303d1db0a1d8ef0b
Thomas Cullen <thomascullen92@gmail.com>
2021-05-21 07:48:56 +0100

    Remove old FreelancerSignup

--- app/javascript/src/views/FreelancerSignup/BuildProfile/validationSchema.js
+++
@@ -1,9 +0,0 @@
-import { object, string } from "yup";
-
-const validationSchema = object().shape({
-  bio: string().nullable().required("Please add a short bio"),
-  city: string().nullable().required("Please set your city"),
-  country: string().nullable().required("Please set your country"),
-});
-
-export default validationSchema;
