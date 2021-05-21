be42ecf344199d0893a58728f3a702139da249f7
Miha Rekar <miha.rekar@advisable.com>
2021-05-21 10:58:27 +0200

    Exclude archived from search

--- spec/models/case_study/search_spec.rb
+++ spec/models/case_study/search_spec.rb
@@ -46,15 +46,26 @@ RSpec.describe CaseStudy::Search, type: :model do
       expect(results.pluck(:id)).to match_array([article1.id])
     end
 
+    it "does not include archived" do
+      article1.skills.create(skill: skill1)
+      article2.skills.create(skill: skill1)
+      search = create(:case_study_search, archived: [article1.id])
+      search.skills.create(skill: skill1)
+      results = search.results
+      expect(results.pluck(:id)).to match_array([article2.id])
+    end
+
     it "works with all combined" do
       article1.skills.create(skill: skill1)
       article1.skills.create(skill: skill2)
       article2.skills.create(skill: skill1)
       article3.skills.create(skill: skill3)
+      article4.skills.create(skill: skill1)
       article1.update(goals: %w[one two], company_type: "B2B")
       article2.update(goals: %w[one three], company_type: "B2B")
       article3.update(goals: %w[three], company_type: "B2C")
-      search = create(:case_study_search, business_type: "B2B", goals: %w[one two])
+      article4.update(goals: %w[two], company_type: "B2B")
+      search = create(:case_study_search, business_type: "B2B", goals: %w[one two], archived: [article4.id])
       search.skills.create(skill: skill1)
       search.skills.create(skill: skill2)
       results = search.results
