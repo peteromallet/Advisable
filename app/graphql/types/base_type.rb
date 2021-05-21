45e2355761c79a22995d3036faad4f2c4333e7a8
Miha Rekar <miha.rekar@advisable.com>
2021-05-21 13:48:44 +0200

    invalidPermissions -> INVALID_PERMISSIONS

--- app/graphql/types/base_type.rb
+++ app/graphql/types/base_type.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-class Types::BaseType < GraphQL::Schema::Object
-  field_class BaseField
+module Types
+  class BaseType < GraphQL::Schema::Object
+    field_class BaseField
 
-  def current_user
-    context[:current_user]
-  end
+    def current_user
+      context[:current_user]
+    end
 
-  private
+    private
 
-  def requires_current_user!
-    return true if current_user.present?
+    def requires_current_user!
+      return true if current_user.present?
 
-    ApiError.not_authenticated
-  end
+      ApiError.not_authenticated
+    end
 
-  def requires_guild_user!
-    requires_current_user!
-    return true if current_user.guild
+    def requires_guild_user!
+      requires_current_user!
+      return true if current_user.guild
 
-    ApiError.invalid_request("invalidPermissions", "Not a guild user")
+      ApiError.invalid_request("INVALID_PERMISSIONS", "Not a guild user")
+    end
   end
 end
