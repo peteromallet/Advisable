a6d9da4943565b81ce408eb2f114c958da168e62
Thomas Cullen <thomascullen92@gmail.com>
2021-05-21 14:10:21 +0100

    Capture feedback when archiving an article

--- app/javascript/src/views/CaseStudyRecommendations/RecommendationCard.js
+++ app/javascript/src/views/CaseStudyRecommendations/RecommendationCard.js
@@ -16,40 +16,35 @@ export default function RecommendationCard({ search, caseStudy, zIndex }) {
   }, [history, search, caseStudy]);
 
   return (
-    <StyledRecommendationCard
-      display="flex"
-      borderRadius="16px"
-      padding={6}
-      zIndex={zIndex}
-      alignItems="center"
-      onClick={handleClick}
-    >
-      <StyledRecommendationCardAvatar
-        mr={6}
-        flexShrink={0}
-        style={{
-          backgroundImage: `url(${caseStudy.specialist.avatar})`,
-        }}
-      />
-      <Box>
-        <Text
-          mb={3}
-          fontSize="2xl"
-          fontWeight={500}
-          className="title"
-          lineHeight="24px"
-          letterSpacing="-0.03rem"
-        >
-          {caseStudy.title}
-        </Text>
-        <Text
-          fontSize="16px"
-          lineHeight="20px"
-          color="neutral800"
-          className="subtitle"
-        >
-          {caseStudy.subtitle}
-        </Text>
+    <StyledRecommendationCard borderRadius="16px" padding={6} zIndex={zIndex}>
+      <Box display="flex" onClick={handleClick} alignItems="center">
+        <StyledRecommendationCardAvatar
+          mr={6}
+          flexShrink={0}
+          style={{
+            backgroundImage: `url(${caseStudy.specialist.avatar})`,
+          }}
+        />
+        <Box>
+          <Text
+            mb={3}
+            fontSize="2xl"
+            fontWeight={500}
+            className="title"
+            lineHeight="24px"
+            letterSpacing="-0.03rem"
+          >
+            {caseStudy.title}
+          </Text>
+          <Text
+            fontSize="16px"
+            lineHeight="20px"
+            color="neutral800"
+            className="subtitle"
+          >
+            {caseStudy.subtitle}
+          </Text>
+        </Box>
       </Box>
       <StyledRecommendationCardActions>
         <ArchiveButton search={search} article={caseStudy} />
