490c0b0d7951943dd1776788af59c374a7165d7d
Miha Rekar <miha.rekar@advisable.com>
2021-05-21 14:07:36 +0200

    consultations.failedToUpdate -> CONSULTATIONS_FAILED_TO_UPDATE

--- app/graphql/mutations/update_consultation.rb
+++ app/graphql/mutations/update_consultation.rb
@@ -19,7 +19,7 @@ module Mutations
     def resolve(**args)
       consultation = Consultation.find_by_uid_or_airtable_id!(args[:id])
 
-      ApiError.invalid_request("consultations.failedToUpdate", "Can't update the consultation becaue its status is #{consultation.status}") unless ALLOWED_STATUSES.include?(consultation.status)
+      ApiError.invalid_request("CONSULTATIONS_FAILED_TO_UPDATE", "Can't update the consultation becaue its status is #{consultation.status}") unless ALLOWED_STATUSES.include?(consultation.status)
 
       consultation.topic = args[:topic]
 
