2b3c107e86ec5fe819f95953158d73d242d2e7fc
Miha Rekar <miha.rekar@advisable.com>
2021-05-21 14:09:45 +0200

    request_password_reset.account_not_found -> ACCOUNT_NOT_FOUND

--- app/graphql/mutations/request_password_reset.rb
+++ app/graphql/mutations/request_password_reset.rb
@@ -10,7 +10,7 @@ module Mutations
       account = Account.find_by(email: email)
 
       if account.blank?
-        ApiError.invalid_request("request_password_reset.account_not_found")
+        ApiError.invalid_request("ACCOUNT_NOT_FOUND")
       elsif !account.has_password? && account.specialist.present?
         WebhookEvent.trigger("specialists.forgotten_password_for_non_account", WebhookEvent::Specialist.data(account.specialist))
         ApiError.invalid_request("request_password_reset.application_required")
