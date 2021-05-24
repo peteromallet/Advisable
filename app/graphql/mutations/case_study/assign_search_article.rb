1d6806b983bd4665e63a0cc62a8dd810762d0428
Miha Rekar <miha.rekar@advisable.com>
2021-05-24 14:38:06 +0200

    Unify archive and save search article mutations into one assign mutation

---
+++ app/graphql/mutations/case_study/assign_search_article.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Mutations
+  module CaseStudy
+    class AssignSearchArticle < Mutations::BaseMutation
+      description "Assign a Case Study Article on a Case Study Search to be saved or archived."
+      graphql_name "AssignCaseStudySearchArticle"
+
+      argument :action, String, required: true
+      argument :article, ID, required: true
+      argument :feedback, String, required: false
+      argument :search, ID, required: true
+
+      field :article, Types::CaseStudy::Article, null: false
+
+      def authorized?(search:, **_args)
+        requires_client!
+
+        search = ::CaseStudy::Search.find_by!(uid: search)
+        policy = ::CaseStudy::SearchPolicy.new(current_user, search)
+        return true if policy.assign_article?
+
+        ApiError.not_authorized("You do not have permission to assign article on this search")
+      end
+
+      def resolve(search:, article:, action:, **args)
+        search = ::CaseStudy::Search.find_by!(uid: search)
+        article = ::CaseStudy::Article.find_by!(uid: article)
+
+        case action
+        when "archive"
+          search.archived = search.archived + [article.id]
+
+          if args[:feedback]
+            ::CaseStudy::SearchFeedback.create!(
+              search: search,
+              article: article,
+              feedback: args[:feedback]
+            )
+          end
+        when "save"
+          search.saved = search.saved + [article.id]
+        end
+
+        current_account_responsible_for do
+          search.save
+        end
+
+        {article: article}
+      end
+    end
+  end
+end
