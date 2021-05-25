1738d40df0ef003cb6aad860df497c30f34ed202
Thomas Cullen <thomascullen92@gmail.com>
2021-05-25 18:38:00 +0100

    Add input for toby Date attribute types

--- app/lib/toby/resources/specialist.rb
+++ app/lib/toby/resources/specialist.rb
@@ -7,16 +7,13 @@ module Toby
       attribute :uid, Attributes::String, readonly: true
       attribute :email, Lookups::Accounts::Email
       attribute :application_stage, Attributes::Select, options: ::Specialist::VALID_APPLICATION_STAGES
-      attribute :previous_projects, Attributes::HasMany
       attribute :account, Attributes::BelongsTo
       attribute :bio, Attributes::LongText
       attribute :linkedin, Attributes::String
       attribute :website, Attributes::String
       attribute :hourly_rate, Attributes::Currency
       attribute :country, Attributes::BelongsTo
-      attribute :applications, Attributes::HasMany
       attribute :skills, Attributes::HasManyThrough
-      attribute :reviews, Attributes::HasMany
       attribute :unavailable_until, Attributes::Date
       attribute :created_at, Attributes::DateTime, readonly: true
       attribute :updated_at, Attributes::DateTime, readonly: true
