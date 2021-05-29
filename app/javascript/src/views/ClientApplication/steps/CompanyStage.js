9c4d825352c56515b4052e4cb37a7d61c7591f69
guandjoy <guandjoy@gmail.com>
2021-05-29 15:25:41 +0300

    Refactor CompanyStage and ChevronButtonInput

--- app/javascript/src/views/ClientApplication/steps/CompanyStage.js
+++ app/javascript/src/views/ClientApplication/steps/CompanyStage.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { object, string } from "yup";
-import { Formik, Form } from "formik";
 import { useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
-import { Box, Error } from "@advisable/donut";
+import { Box } from "@advisable/donut";
 import ChevronButtonInput from "src/components/ChevronButtonInput";
 import { useNotifications } from "src/components/Notifications";
-import FormField from "src/components/FormField";
 import AnimatedCard from "../components/AnimatedCard";
 import Header from "../components/Header";
 import Description from "../components/Description";
@@ -18,18 +16,13 @@ export const validationSchema = object().shape({
 });
 
 export default function CompanyStage({ clientApplication }) {
-  const [update] = useMutation(UPDATE_CLIENT_APPLICATION);
+  const [update, { loading }] = useMutation(UPDATE_CLIENT_APPLICATION);
   const { error } = useNotifications();
   const history = useHistory();
 
-  const initialValues = {
-    companyType: clientApplication.companyType || "",
-  };
-
-  const handleSubmit = async (values, { setStatus }) => {
-    setStatus(null);
+  const handleSelection = (option) => {
     update({
-      variables: { input: values },
+      variables: { input: { companyType: option } },
       optimisticResponse: {
         __typename: "Mutation",
         updateClientApplication: {
@@ -37,7 +30,7 @@ export default function CompanyStage({ clientApplication }) {
           clientApplication: {
             __typename: "ClientApplication",
             ...clientApplication,
-            ...values,
+            companyType: option,
           },
         },
       },
@@ -49,57 +42,42 @@ export default function CompanyStage({ clientApplication }) {
 
   return (
     <AnimatedCard>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {(formik) => (
-          <Form>
-            <StepNumber>Step 2 of 4</StepNumber>
-            <Header>Company Stage</Header>
-            <Description>
-              This will help us tailor our recommendations to you.
-            </Description>
-            <Box mb={4}>
-              <FormField
-                size="l"
-                as={ChevronButtonInput}
-                fullWidth
-                alignWidth
-                name="companyType"
-                error={null}
-                label={null}
-                options={[
-                  {
-                    label: "Individual Entrepreneur",
-                    value: "Individual Entrepreneur",
-                  },
-                  {
-                    label: "Small Business",
-                    value: "Small Business",
-                  },
-                  {
-                    label: "Medium-Sized Business",
-                    value: "Medium-Sized Business",
-                  },
-                  { label: "Startup", value: "Startup" },
-                  {
-                    label: "Growth-Stage Startup",
-                    value: "Growth-Stage Startup",
-                  },
-                  {
-                    label: "Major Corporation",
-                    value: "Major Corporation",
-                  },
-                ]}
-                value={formik.values.companyType}
-              />
-            </Box>
-            <Error>{formik.status}</Error>
-          </Form>
-        )}
-      </Formik>
+      <StepNumber>Step 2 of 4</StepNumber>
+      <Header>Company Stage</Header>
+      <Description>
+        This will help us tailor our recommendations to you.
+      </Description>
+      <Box mb={4}>
+        <ChevronButtonInput
+          size="l"
+          value={clientApplication.companyType || ""}
+          options={[
+            {
+              label: "Individual Entrepreneur",
+              value: "Individual Entrepreneur",
+            },
+            {
+              label: "Small Business",
+              value: "Small Business",
+            },
+            {
+              label: "Medium-Sized Business",
+              value: "Medium-Sized Business",
+            },
+            { label: "Startup", value: "Startup" },
+            {
+              label: "Growth-Stage Startup",
+              value: "Growth-Stage Startup",
+            },
+            {
+              label: "Major Corporation",
+              value: "Major Corporation",
+            },
+          ]}
+          onSelect={handleSelection}
+          loading={loading}
+        />
+      </Box>
     </AnimatedCard>
   );
 }
