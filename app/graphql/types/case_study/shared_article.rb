cf723e79532e3463bfd0d94eb1af266f2e8cb5ea
Miha Rekar <miha.rekar@advisable.com>
2021-06-02 14:18:05 +0200

    Update types to support SharedArticle

---
+++ app/graphql/types/case_study/shared_article.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  module CaseStudy
+    class SharedArticle < Types::BaseType
+      graphql_name "CaseStudySharedArticle"
+      description "Type definition for CaseStudy::SharedArticle"
+
+      field :id, ID, null: false, method: :uid
+      field :article, Article, null: false
+      field :shared_with, Types::User, null: false
+      field :shared_by, Types::User, null: true
+      field :message, String, null: true
+      field :archived_at, GraphQL::Types::ISO8601DateTime, null: true
+    end
+  end
+end
