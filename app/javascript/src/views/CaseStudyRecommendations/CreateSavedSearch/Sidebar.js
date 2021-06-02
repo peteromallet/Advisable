92f68e79bd5b25b5b87e80b132ba1fe946f7067d
guandjoy <guandjoy@gmail.com>
2021-06-02 13:11:14 +0300

    Init Sidebar steps

--- app/javascript/src/views/CaseStudyRecommendations/CreateSavedSearch/Sidebar.js
+++ app/javascript/src/views/CaseStudyRecommendations/CreateSavedSearch/Sidebar.js
@@ -1,17 +1,43 @@
 import React from "react";
-import { Text } from "@advisable/donut";
+import { Card, Text } from "@advisable/donut";
+import MultistepMenu from "src/components/MultistepMenu";
 
 export default function Sidebar() {
   return (
-    <Text
-      color="neutral400"
-      textTransform="uppercase"
-      fontWeight="medium"
-      fontSize="2xs"
-      mb={4}
-      mt={6}
+    <Card
+      top="0"
+      left="0"
+      padding={6}
+      width="300px"
+      height="100vh"
+      position="fixed"
+      paddingTop="60px"
+      borderRadius="0px"
     >
-      New Saved Search
-    </Text>
+      <Text
+        color="neutral400"
+        textTransform="uppercase"
+        fontWeight="medium"
+        fontSize="2xs"
+        mb={4}
+        mt={6}
+      >
+        Create a Saved Search
+      </Text>
+      <MultistepMenu>
+        <MultistepMenu.Item to="/explore/new" exact isComplete={false}>
+          Skills
+        </MultistepMenu.Item>
+        <MultistepMenu.Item to="/explore/new/id/goals" isComplete={false}>
+          Goals
+        </MultistepMenu.Item>
+        <MultistepMenu.Item to="/explore/new/id/preferences" isComplete={false}>
+          Preferences
+        </MultistepMenu.Item>
+        <MultistepMenu.Item to="/explore/new/id/review" isComplete={false}>
+          Review
+        </MultistepMenu.Item>
+      </MultistepMenu>
+    </Card>
   );
 }
