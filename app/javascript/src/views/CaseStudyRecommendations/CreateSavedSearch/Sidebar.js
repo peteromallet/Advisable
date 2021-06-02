eb57afeaa43c35574e0689e501214a8be78975c1
guandjoy <guandjoy@gmail.com>
2021-06-02 13:00:20 +0300

    Init routes of a new saved search steps

---
+++ app/javascript/src/views/CaseStudyRecommendations/CreateSavedSearch/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text } from "@advisable/donut";
+
+export default function Sidebar() {
+  return (
+    <Text
+      color="neutral400"
+      textTransform="uppercase"
+      fontWeight="medium"
+      fontSize="2xs"
+      mb={4}
+      mt={6}
+    >
+      New Saved Search
+    </Text>
+  );
+}
