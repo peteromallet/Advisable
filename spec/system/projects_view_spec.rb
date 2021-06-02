d4f4ed02605bd0e6a03622e7b6c5eb9065db29ad
guandjoy <guandjoy@gmail.com>
2021-06-02 15:47:11 +0300

    Update projects_view_spec.rb

--- spec/system/projects_view_spec.rb
+++ spec/system/projects_view_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Projects view', type: :system do
+RSpec.describe("Projects view", type: :system) do
   let(:user) { create(:user) }
 
   before do
@@ -22,25 +22,5 @@ RSpec.describe 'Projects view', type: :system do
     visit "/projects"
     find("*[aria-label='Find a new freelancer']").click
     expect(page).to have_content("What skills should this specialist have?")
-  end
-
-  context "when the user's company industry isn't set" do
-    let(:company) { create(:company, industry: nil) }
-    let(:user) { create(:user, company: company) }
-
-    it 'asks the user for their industry' do
-      create(:industry, name: "Testing")
-      authenticate_as(user)
-      visit "/projects"
-      expect(page).to have_content("We need to confirm some details")
-      modal = find("*[role=dialog]")
-      within(modal) do
-        industry = find_field("Industry")
-        industry.send_keys("test", :down, :return)
-        select("Startup", from: "companyType")
-        click_on("Update Details")
-      end
-      expect(page).not_to have_css("*[role=dialog]")
-    end
   end
 end
