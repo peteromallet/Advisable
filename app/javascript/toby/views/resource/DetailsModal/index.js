23a53c2308d738861340fbbaf1b23953612671ff
Thomas Cullen <thomascullen92@gmail.com>
2021-06-10 13:52:57 +0100

    Show resource version history

--- app/javascript/toby/views/resource/DetailsModal/index.js
+++ app/javascript/toby/views/resource/DetailsModal/index.js
@@ -1,7 +1,8 @@
+import { DateTime } from "luxon";
 import React, { useMemo, useCallback, useEffect } from "react";
 import { useDialogState } from "reakit/Dialog";
 import { Formik, Form } from "formik";
-import { Modal, Box, Text, Tag } from "@advisable/donut";
+import { Modal, Box, Text, Tag, Stack } from "@advisable/donut";
 import { useNotifications } from "src/components/Notifications";
 import SubmitButton from "src/components/SubmitButton";
 import { matchPath, useHistory, useLocation } from "react-router";
@@ -50,6 +51,56 @@ function useRoutedModal(path, returnPath) {
     show: handleShow,
     hide: handleHide,
   };
+}
+
+function History({ history }) {
+  return (
+    <Box
+      width={300}
+      maxHeight="100%"
+      bg="neutral100"
+      borderRadius="12px"
+      padding={4}
+    >
+      <Stack spacing="md">
+        {history.map((h) => (
+          <Box bg="neutral200" key={h.number} borderRadius="8px">
+            <Box padding={3}>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                mb={1}
+                alignItems="flex-end"
+              >
+                <Text fontSize="xs" fontWeight={500}>
+                  Version {h.number}
+                </Text>
+                <Text fontSize="2xs" pl={1} color="neutral600">
+                  {DateTime.fromISO(h.createdAt).toFormat("HH:mm dd LLL yyyy")}
+                </Text>
+              </Box>
+              <Text fontSize="2xs" color="neutral700">
+                {h.responsible || "Anonymous"}
+              </Text>
+            </Box>
+            <Box mx={3} height="1px" bg="neutral300" />
+            <Box padding={3}>
+              {h.changes.map((change) => (
+                <Box marginBottom={3} key={change.attribute}>
+                  <Text fontSize="xs" fontWeight={500} mb={1}>
+                    {change.attribute}
+                  </Text>
+                  <Text fontSize="xs" color="neutral800">
+                    {change.value}
+                  </Text>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
+  );
 }
 
 function Details({ id, resource }) {
@@ -103,48 +154,53 @@ function Details({ id, resource }) {
   };
 
   return (
-    <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-      <Form>
-        {resource.attributes.map((attr) => {
-          return (
-            <Box key={attr.name} mb={5}>
-              <Box
-                mb={2}
-                width="100%"
-                display="flex"
-                justifyContent="space-between"
-              >
-                <Text fontWeight={500} as="span">
-                  {attr.columnLabel}
-                </Text>
-                {attr.readonly && (
-                  <Tag ml={2} size="xs">
-                    Readonly
-                  </Tag>
-                )}
-              </Box>
-              {attr.description && (
-                <Text
-                  fontSize="sm"
-                  lineHeight="20px"
-                  color="neutral800"
-                  marginBottom={3}
-                >
-                  {attr.description}
-                </Text>
-              )}
-              <AttributeInput
-                resource={resource}
-                record={data.record}
-                attribute={attr}
-              />
-              <Box mt={5} height={1} bg="neutral100" />
-            </Box>
-          );
-        })}
-        <SubmitButton mt={4}>Save Changes</SubmitButton>
-      </Form>
-    </Formik>
+    <Box display="flex">
+      <Box paddingRight={8}>
+        <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+          <Form>
+            {resource.attributes.map((attr) => {
+              return (
+                <Box key={attr.name} mb={5}>
+                  <Box
+                    mb={2}
+                    width="100%"
+                    display="flex"
+                    justifyContent="space-between"
+                  >
+                    <Text fontWeight={500} as="span">
+                      {attr.columnLabel}
+                    </Text>
+                    {attr.readonly && (
+                      <Tag ml={2} size="xs">
+                        Readonly
+                      </Tag>
+                    )}
+                  </Box>
+                  {attr.description && (
+                    <Text
+                      fontSize="sm"
+                      lineHeight="20px"
+                      color="neutral800"
+                      marginBottom={3}
+                    >
+                      {attr.description}
+                    </Text>
+                  )}
+                  <AttributeInput
+                    resource={resource}
+                    record={data.record}
+                    attribute={attr}
+                  />
+                  <Box mt={5} height={1} bg="neutral100" />
+                </Box>
+              );
+            })}
+            <SubmitButton mt={4}>Save Changes</SubmitButton>
+          </Form>
+        </Formik>
+      </Box>
+      <History history={data.record._history} />
+    </Box>
   );
 }
 
@@ -155,7 +211,7 @@ export default function DetailsModal({ resource }) {
   );
 
   return (
-    <Modal width={800} modal={modal} label="Details">
+    <Modal width={1000} modal={modal} label="Details">
       {modal.visible && <Details id={modal.params.id} resource={resource} />}
     </Modal>
   );
