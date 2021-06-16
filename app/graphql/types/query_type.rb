55b952b9cb4f47c0dbc37bfc5756438ea3b03efe
Miha Rekar <miha.rekar@advisable.com>
2021-06-16 16:13:19 +0200

    Address review points

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -373,20 +373,23 @@ module Types
       current_user.searches
     end
 
-    field :case_study_skills, [Types::Skill], null: true
+    field :case_study_skills, Types::Skill.connection_type, null: true
 
     def case_study_skills
       ::Skill.where(id: ::CaseStudy::Skill.select(:skill_id).distinct)
     end
 
     field :popular_case_study_skills, [Types::Skill], null: true do
-      argument :first, Integer, required: false
+      argument :limit, Integer, required: false
     end
 
-    def popular_case_study_skills(first = nil)
-      skills_by_usage = ::CaseStudy::Skill.select("skill_id, COUNT(skill_id) as usage").order("usage DESC").group("skill_id")
-      skills_by_usage = skills_by_usage.limit(first) if first.present?
-      ::Skill.where(id: skills_by_usage.map(&:skill_id))
+    def popular_case_study_skills(limit = 20)
+      skills_by_usage = ::CaseStudy::Skill.select("skill_id, COUNT(skill_id) as usage").
+        order("usage DESC").
+        group("skill_id").
+        limit(limit).
+        map(&:skill_id)
+      ::Skill.where(id: skills_by_usage)
     end
   end
 end
