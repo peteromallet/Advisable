d8d904a8fa3c61b2158fa3c7571d109689825737
guandjoy <guandjoy@gmail.com>
2021-06-16 19:02:52 +0300

    Update Review step

--- app/javascript/src/views/CaseStudyRecommendations/CreateSavedSearch/steps/Review.js
+++ app/javascript/src/views/CaseStudyRecommendations/CreateSavedSearch/steps/Review.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import { useHistory } from "react-router";
+import { Pencil } from "@styled-icons/heroicons-solid/Pencil";
 import { ArrowRight } from "@styled-icons/feather/ArrowRight";
-import { Error } from "@advisable/donut";
+import { Error, Text, Tags, Link, Button, Box, Stack } from "@advisable/donut";
 import SubmitButton from "src/components/SubmitButton";
 import AnimatedCard from "../components/AnimatedCard";
 import StepNumber from "../components/StepNumber";
 import Header from "../components/Header";
-import { useHistory } from "react-router";
+import Description from "../components/Description";
 
-export default function Review({ id }) {
+export default function Review({ caseStudySearch }) {
   const history = useHistory();
 
   const initialValues = {
@@ -16,7 +18,7 @@ export default function Review({ id }) {
   };
 
   const handleSubmit = () => {
-    history.push(`/explore/${id}`);
+    history.push(`/explore/${caseStudySearch?.id}`);
   };
 
   return (
@@ -26,9 +28,71 @@ export default function Review({ id }) {
           <Form>
             <StepNumber>Step 4 of 4</StepNumber>
             <Header>Review</Header>
+            <Description>Verify the information you have provided</Description>
+
+            <Stack spacing={12} divider="neutral100">
+              <Box>
+                <Text
+                  mb={3}
+                  fontSize="2xl"
+                  color="blue900"
+                  lineHeight="xl"
+                  fontWeight="medium"
+                  letterSpacing="-0.06rem"
+                >
+                  Skills
+                </Text>
+                <Tags
+                  size="m"
+                  marginBottom={6}
+                  tags={caseStudySearch.skills.map((s) => s.skill.label)}
+                />
+                <Link
+                  to={{
+                    pathname: `/explore/new/${caseStudySearch.id}/skills`,
+                    state: { readyToPublish: true },
+                  }}
+                >
+                  <Button
+                    variant="subtle"
+                    size="s"
+                    prefix={<Pencil size={24} />}
+                  >
+                    Edit Skills
+                  </Button>
+                </Link>
+              </Box>
+              <Box>
+                <Text
+                  mb={3}
+                  fontSize="2xl"
+                  color="blue900"
+                  lineHeight="xl"
+                  fontWeight="medium"
+                  letterSpacing="-0.06rem"
+                >
+                  Goals
+                </Text>
+                <Tags size="m" marginBottom={6} tags={caseStudySearch.goals} />
+                <Link
+                  to={{
+                    pathname: `/explore/new/${caseStudySearch.id}/goals`,
+                    state: { readyToPublish: true },
+                  }}
+                >
+                  <Button
+                    variant="subtle"
+                    size="s"
+                    prefix={<Pencil size={24} />}
+                  >
+                    Edit Goals
+                  </Button>
+                </Link>
+              </Box>
+            </Stack>
             <Error>{formik.status}</Error>
             <SubmitButton
-              mt={4}
+              marginTop={10}
               suffix={<ArrowRight />}
               variant="gradient"
               size="l"
