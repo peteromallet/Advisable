78f4b2eaed58e460c531650259dda772728aa17c
Geoff Ereth <geoffereth@gmail.com>
2021-07-05 13:12:34 -0700

    Removes orbits fallback cover

--- app/javascript/guild/components/Post/components/Cover/ArticleCover.js
+++ app/javascript/guild/components/Post/components/Cover/ArticleCover.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { Box } from "@advisable/donut";
 import { CoverImage } from "@guild/components/CoverImage";
-import OrbitsBackground from "@guild/components/Event/OrbitsBackground";
 
 export default function ArticleCover({ post }) {
-  return (
+  return post.coverImage?.url ? (
     <Box margin={{ _: "-18px -18px 24px", s: "-26px -26px 24px" }}>
       <a href={`/guild/posts/${post.id}`}>
-        {post.coverImage?.url ? (
-          <CoverImage height="269px" cover={post.coverImage.url} />
-        ) : (
-          <OrbitsBackground height="269px" borderRadius="12px 12px 0 0" />
-        )}
+        <CoverImage height="269px" cover={post.coverImage.url} />
       </a>
     </Box>
-  );
+  ) : null;
 }
