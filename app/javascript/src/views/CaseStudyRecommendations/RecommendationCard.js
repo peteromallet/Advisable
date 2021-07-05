36e3ec08db3e14ad4f4f4806989d2d79b31b95e7
Thomas Cullen <thomascullen92@gmail.com>
2021-07-05 15:58:48 +0100

    Add basic new case study explore ui

--- app/javascript/src/views/CaseStudyRecommendations/RecommendationCard.js
+++ app/javascript/src/views/CaseStudyRecommendations/RecommendationCard.js
@@ -1,22 +1,20 @@
 import React, { useCallback } from "react";
-import { useHistory } from "react-router";
-import { Text, Box } from "@advisable/donut";
-import CaseStudyActions from "./CaseStudyActions";
+import { Link, useHistory } from "react-router-dom";
+import { Text, Box, Circle } from "@advisable/donut";
 import {
-  StyledRecommendationCard,
+  StyledRecommendationTitle,
   StyledRecommendationCardAvatar,
-  StyledRecommendationCardActions,
 } from "./styles";
 
-export default function RecommendationCard({ search, caseStudy, zIndex }) {
+export default function RecommendationCard({ caseStudy }) {
   const history = useHistory();
 
   const handleClick = useCallback(() => {
-    history.push(`/explore/${search.id}/${caseStudy.id}`);
-  }, [history, search, caseStudy]);
+    history.push(`/explore/${caseStudy.id}`);
+  }, [history, caseStudy]);
 
   return (
-    <StyledRecommendationCard borderRadius="16px" padding={6} zIndex={zIndex}>
+    <>
       <Box display="flex" onClick={handleClick} alignItems="center">
         <StyledRecommendationCardAvatar
           mr={6}
@@ -24,31 +22,38 @@ export default function RecommendationCard({ search, caseStudy, zIndex }) {
           style={{
             backgroundImage: `url(${caseStudy.specialist.avatar})`,
           }}
-        />
+        >
+          <Circle
+            size="40px"
+            bg="white"
+            position="absolute"
+            right="-8px"
+            top="-8px"
+          >
+            <img src={caseStudy.favicon} width="16px" />
+          </Circle>
+        </StyledRecommendationCardAvatar>
         <Box>
-          <Text
-            mb={3}
+          <StyledRecommendationTitle
+            mb={2}
+            as={Link}
             fontSize="2xl"
-            fontWeight={500}
+            fontWeight={600}
             className="title"
             lineHeight="24px"
             letterSpacing="-0.03rem"
+            to={`/explore/${caseStudy.id}`}
           >
             {caseStudy.title}
+          </StyledRecommendationTitle>
+          <Text mb={4} fontSize="md" color="neutral500">
+            {caseStudy.specialist.name}
           </Text>
-          <Text
-            fontSize="16px"
-            lineHeight="20px"
-            color="neutral800"
-            className="subtitle"
-          >
+          <Text lineHeight="20px" color="neutral900" className="subtitle">
             {caseStudy.subtitle}
           </Text>
         </Box>
       </Box>
-      <StyledRecommendationCardActions>
-        <CaseStudyActions search={search} caseStudy={caseStudy} />
-      </StyledRecommendationCardActions>
-    </StyledRecommendationCard>
+    </>
   );
 }
