d4f3c10c61c8740f3a89a33fe966c0c0ed145acc
Thomas Cullen <thomascullen92@gmail.com>
2021-07-08 10:38:49 +0100

    Set API call for shared view

--- app/javascript/src/views/CaseStudyRecommendations/Shared.js
+++ app/javascript/src/views/CaseStudyRecommendations/Shared.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Text } from "@advisable/donut";
 import CaseStudiesList from "./CaseStudiesList";
+import { useSharedArticles } from "./queries";
 
 export default function SharedArticles() {
+  const { data, loading } = useSharedArticles();
+  const articles = data?.sharedArticles?.map((s) => s.article) || [];
+
   return (
     <div>
       <Text fontSize="5xl" fontWeight={600} letterSpacing="-0.04rem" mb={2}>
@@ -11,7 +15,8 @@ export default function SharedArticles() {
       <Text size="lg" color="neutral800" mb={12}>
         Case studies that have been shared with you.
       </Text>
-      {/* <CaseStudiesList /> */}
+      {loading && <>loading...</>}
+      {articles.length ? <CaseStudiesList articles={articles} /> : null}
     </div>
   );
 }
