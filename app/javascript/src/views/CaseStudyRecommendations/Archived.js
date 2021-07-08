841473e0e7903d6347f257a6785bec11fe0640d5
Thomas Cullen <thomascullen92@gmail.com>
2021-07-08 08:59:33 +0100

    Link archived view to new APIs

--- app/javascript/src/views/CaseStudyRecommendations/Archived.js
+++ app/javascript/src/views/CaseStudyRecommendations/Archived.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Text } from "@advisable/donut";
 import CaseStudiesList from "./CaseStudiesList";
+import { useArchivedArticles } from "./queries";
 
 export default function ArchivedArticles() {
+  const { data, loading } = useArchivedArticles();
+  const articles = data?.ArchivedArticles?.nodes || [];
+
   return (
     <div>
-      <Text fontSize="5xl" fontWeight={600} letterSpacing="-0.04rem" mb={2}>
+      <Text fontSize="5xl" fontWeight={600} letterSpacing="-0.04rem" mb={12}>
         Archived
       </Text>
-      <Text size="lg" color="neutral800" mb={12}>
-        See how our freelancers have helped similar companies achieve their
-        goals.
-      </Text>
-      <CaseStudiesList />
+      {loading && <>loading...</>}
+      {articles.length ? <CaseStudiesList articles={articles} /> : null}
     </div>
   );
 }
