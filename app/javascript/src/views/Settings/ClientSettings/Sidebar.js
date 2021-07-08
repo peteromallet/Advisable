3c8ab5f82931f65b3e8434d9cd3064fc59dc73a9
guandjoy <guandjoy@gmail.com>
2021-07-08 23:54:57 +0300

    Refactor ClientSettings to use View component instead of Layout

--- app/javascript/src/views/Settings/ClientSettings/Sidebar.js
+++ app/javascript/src/views/Settings/ClientSettings/Sidebar.js
@@ -1,23 +1,13 @@
 import React from "react";
-import { padding } from "styled-system";
-import { motion } from "framer-motion";
 import { Card } from "@styled-icons/ionicons-outline/Card";
 import { Document } from "@styled-icons/ionicons-outline/Document";
 import { LockClosed } from "@styled-icons/ionicons-outline/LockClosed";
 import { PeopleCircle } from "@styled-icons/ionicons-outline/PeopleCircle";
 import NavigationMenu from "../../../components/NavigationMenu";
 import styled from "styled-components";
-import { Text, useBreakpoint } from "@advisable/donut";
+import { Text } from "@advisable/donut";
 import useViewer from "src/hooks/useViewer";
-
-const SidebarContainer = styled.div`
-  width: 280px;
-  position: relative;
-
-  @media (max-width: 900px) {
-    width: 100%;
-  }
-`;
+import View from "src/components/View";
 
 const SidebarTitle = styled.div`
   @media (max-width: 900px) {
@@ -25,72 +15,41 @@ const SidebarTitle = styled.div`
   }
 `;
 
-const Sidebar = styled.div`
-  left: 0;
-  top: 60px;
-  width: 280px;
-  position: fixed;
-  height: calc(100vh - 60px);
-  background: white;
-  box-shadow: 0px 1px 20px rgba(14, 31, 91, 0.04);
-
-  @media (max-width: 900px) {
-    position: relative;
-    height: auto;
-    background: none;
-    box-shadow: none;
-    top: 0;
-    width: 100%;
-    padding: 0 0 20px 0;
-  }
-
-  ${padding};
-`;
-
 const SettingsSidebar = () => {
   const viewer = useViewer();
-  const isDesktop = useBreakpoint("lUp");
 
   return (
-    <SidebarContainer>
-      <Sidebar
-        as={isDesktop ? motion.div : "div"}
-        padding="m"
-        initial={{ opacity: 0, left: -100 }}
-        animate={{ opacity: 1, left: 0 }}
-        transition={{ duration: 0.5 }}
+    <View.Sidebar>
+      <Text
+        as={SidebarTitle}
+        fontSize="xl"
+        color="blue900"
+        fontWeight="medium"
+        mb="s"
+        mt="xxs"
+        ml="xs"
       >
-        <Text
-          as={SidebarTitle}
-          fontSize="xl"
-          color="blue900"
-          fontWeight="medium"
-          mb="s"
-          mt="xxs"
-          ml="xs"
-        >
-          Settings
-        </Text>
-        <NavigationMenu title="Settings">
-          {viewer.isTeamManager ? (
-            <>
-              <NavigationMenu.Item to="/settings/payments" icon={<Card />}>
-                Payments
-              </NavigationMenu.Item>
-              <NavigationMenu.Item to="/settings/team" icon={<PeopleCircle />}>
-                Team Members
-              </NavigationMenu.Item>
-              <NavigationMenu.Item to="/settings/invoices" icon={<Document />}>
-                Invoices
-              </NavigationMenu.Item>
-            </>
-          ) : null}
-          <NavigationMenu.Item to="/settings/password" icon={<LockClosed />}>
-            Password
-          </NavigationMenu.Item>
-        </NavigationMenu>
-      </Sidebar>
-    </SidebarContainer>
+        Settings
+      </Text>
+      <NavigationMenu title="Settings">
+        {viewer.isTeamManager ? (
+          <>
+            <NavigationMenu.Item to="/settings/payments" icon={<Card />}>
+              Payments
+            </NavigationMenu.Item>
+            <NavigationMenu.Item to="/settings/team" icon={<PeopleCircle />}>
+              Team Members
+            </NavigationMenu.Item>
+            <NavigationMenu.Item to="/settings/invoices" icon={<Document />}>
+              Invoices
+            </NavigationMenu.Item>
+          </>
+        ) : null}
+        <NavigationMenu.Item to="/settings/password" icon={<LockClosed />}>
+          Password
+        </NavigationMenu.Item>
+      </NavigationMenu>
+    </View.Sidebar>
   );
 };
 
