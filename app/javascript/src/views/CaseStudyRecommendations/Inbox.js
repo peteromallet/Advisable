f67d64a2c0ab55f86dbb2753fc1722fd32378da5
Thomas Cullen <thomascullen92@gmail.com>
2021-07-08 08:56:22 +0100

    Hook up saved searches with newer APIs

--- app/javascript/src/views/CaseStudyRecommendations/Inbox.js
+++ app/javascript/src/views/CaseStudyRecommendations/Inbox.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { Text } from "@advisable/donut";
+import { Box, Text } from "@advisable/donut";
 import CaseStudiesList from "./CaseStudiesList";
+import { useParams } from "react-router-dom";
+import { useCaseStudySearch } from "./queries";
 
 export default function ExploreInbox() {
+  const { id } = useParams();
+  const { data, loading } = useCaseStudySearch({
+    variables: { id },
+  });
+
+  if (loading) return <>loading...</>;
+
+  const search = data.caseStudySearch;
+
   return (
-    <div>
-      <Text fontSize="5xl" fontWeight={600} letterSpacing="-0.04rem" mb={2}>
-        Recommendations
-      </Text>
-      <Text size="lg" color="neutral800" mb={12}>
-        See how our freelancers have helped similar companies achieve their
-        goals.
-      </Text>
-      <CaseStudiesList />
-    </div>
+    <>
+      <Box marginBottom={12}>
+        <Text fontSize="5xl" fontWeight={600} letterSpacing="-0.04rem" mb={2}>
+          {search.name}
+        </Text>
+        {search.companyRecomendation && (
+          <Text size="lg" color="neutral800">
+            See how freelancers in our network have helped similar companies
+            achieve their goals.
+          </Text>
+        )}
+      </Box>
+      <CaseStudiesList articles={search.results.nodes} />
+    </>
   );
 }
--- app/javascript/src/views/CaseStudyRecommendations/index.js
+++ app/javascript/src/views/CaseStudyRecommendations/index.js
@@ -2,21 +2,38 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import View from "src/components/View";
 import { Box, useBreakpoint } from "@advisable/donut";
-import Navigation from "./Navigation";
 import Inbox from "./Inbox";
+import Shared from "./Shared";
+import Archived from "./Archived";
+import Favorites from "./Favorites";
+import Navigation from "./Navigation";
+import { useCaseStudySearches } from "./queries";
 
 export default function CaseStudyExplorer() {
   const isLargeScreen = useBreakpoint("mUp");
+
+  const { loading, data } = useCaseStudySearches();
+  if (loading) return <>loading...</>;
+
+  const defaultSearch =
+    data.caseStudySearches.find((s) => s.companyRecomendation) ||
+    data.caseStudySearches[0];
+
   return (
     <View>
-      <View.Sidebar width={["100%", "100%", "300px"]}>
-        <Navigation />
-      </View.Sidebar>
+      <Route path="/explore" exact={!isLargeScreen}>
+        <View.Sidebar width={["100%", "100%", "300px"]}>
+          <Navigation data={data} />
+        </View.Sidebar>
+      </Route>
       <View.Content>
         <Box maxWidth={800} paddingY={12} paddingX={4} marginX="auto">
           <Switch>
-            <Route path="/explore/inbox" component={Inbox} />
-            {isLargeScreen && <Redirect to="/explore/inbox" />}
+            <Route path="/explore/shared" component={Shared} />
+            <Route path="/explore/favorites" component={Favorites} />
+            <Route path="/explore/archived" component={Archived} />
+            <Route path="/explore/:id" component={Inbox} />
+            {isLargeScreen && <Redirect to={`/explore/${defaultSearch.id}`} />}
           </Switch>
         </Box>
       </View.Content>
