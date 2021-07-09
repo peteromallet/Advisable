ebc41a75388f411cf85a3ca16bc603ac6daad5cc
guandjoy <guandjoy@gmail.com>
2021-07-09 20:04:20 +0300

    Create horizontal, vertical, and contentOnly variants of View component

--- app/javascript/src/components/View/styles.js
+++ app/javascript/src/components/View/styles.js
@@ -1,22 +1,60 @@
 import styled from "styled-components";
-import { compose, width, padding, display } from "styled-system";
-
-export const StyledView = styled.div`
-  display: flex;
-  position: relative;
-`;
-
-export const StyledViewContent = styled.div`
-  flex: 1;
-  height: 100%;
-  overflow-y: scroll;
-`;
+import { variant, compose, padding, display } from "styled-system";
 
 export const StyledSidebar = styled.div`
-  ${compose(width, padding, display)};
+  ${compose(padding, display)};
 
   z-index: 3;
   background: white;
   height: calc(100vh - 60px);
   box-shadow: 0px 1px 20px rgba(14, 31, 91, 0.04);
+`;
+
+export const StyledViewContent = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const viewVariants = variant({
+  prop: "type",
+  variants: {
+    horizontal: {
+      flexDirection: "row",
+      [StyledSidebar]: {
+        display: "block",
+        background: "white",
+        boxShadow: "0px 1px 20px rgba(14, 31, 91, 0.04)",
+        height: "calc(100vh - 60px)",
+        width: "300px",
+      },
+      [StyledViewContent]: {
+        overflowY: "scroll",
+      },
+    },
+    vertical: {
+      flexDirection: "column",
+      [StyledSidebar]: {
+        display: "block",
+        background: "transparent",
+        boxShadow: "none",
+        height: "auto",
+        width: "100%",
+      },
+      [StyledViewContent]: {
+        overflowY: "visible",
+      },
+    },
+    contentOnly: {
+      [StyledSidebar]: {
+        display: "none",
+      },
+    },
+  },
+});
+
+export const StyledView = styled.div`
+  ${viewVariants}
+
+  display: flex;
+  position: relative;
 `;
