85d3466f03635851b664d0dd04329f0b8d9fbae9
Miha Rekar <miha.rekar@advisable.com>
2021-07-09 09:00:39 +0200

    Add ability to get all the similar skills

--- spec/models/skill_similarity_spec.rb
+++ spec/models/skill_similarity_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe SkillSimilarity, type: :model do
   it "has a valid factory" do
     expect(build(:skill_similarity)).to be_valid
   end
+
+  describe "#similar_to" do
+    let(:instagram_ads) { create(:skill, name: "Instagram Ads") }
+    let(:facebook_ads) { create(:skill, name: "Facebook Ads") }
+    let(:social_ads) { create(:skill, name: "Social Ads") }
+    let(:instagram_optimization) { create(:skill, name: "Instagram Optimization") }
+
+    before do
+      described_class.create(skill1: instagram_ads, skill2: facebook_ads, similarity: 75)
+      described_class.create(skill1: social_ads, skill2: instagram_ads, similarity: 100)
+      described_class.create(skill1: instagram_ads, skill2: instagram_optimization, similarity: 25)
+      described_class.create(skill1: facebook_ads, skill2: instagram_optimization, similarity: 0)
+    end
+
+    it "returns skills that are similar to the given skill" do
+      expect(described_class.similar_to(instagram_ads)).to match_array([facebook_ads, social_ads])
+    end
+
+    it "takes similarity as an optional param" do
+      expect(described_class.similar_to(instagram_ads, 25)).to match_array([facebook_ads, social_ads, instagram_optimization])
+    end
+  end
 end
