dc932c7860dbda357ddb0f5546a4f14e92641351
Miha Rekar <miha.rekar@advisable.com>
2021-07-12 13:40:27 +0200

    Create on session intent if off session fails

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,6 +4,8 @@ class Payment < ApplicationRecord
   include Uid
   uid_prefix "pay"
 
+  VALID_STATUSES = %w[requires_payment_method requires_confirmation requires_action processing requires_capture canceled succeeded failed pending].freeze
+
   belongs_to :company
   belongs_to :specialist
   belongs_to :task, optional: true
@@ -11,31 +13,54 @@ class Payment < ApplicationRecord
   before_create :set_admin_fee
 
   validates :amount, presence: true
+  validates :status, inclusion: {in: VALID_STATUSES}, allow_nil: true
 
-  def create_in_stripe!
-    if payment_intent_id.blank?
-      intent = Stripe::PaymentIntent.create(
-        amount: amount_with_fee,
-        currency: "usd",
-        customer: company.stripe_customer_id,
-        confirm: true,
-        off_session: true,
-        metadata: {admin_fee: admin_fee}
-      )
-      update(payment_intent_id: intent.id, status: intent.status)
-    end
-
-    payment_intent_id
-  rescue Stripe::InvalidRequestError => e
-    Slack.message(channel: "client_engagement", text: "Something went wrong with the payment for *#{company.name}* (#{company_id}) with *#{specialist.account.name}* (#{specialist.uid})! Payment: #{uid}")
-    Sentry.capture_exception(e, extra: {stripe_error: e.json_body[:error]})
-  end
+  scope :with_status, ->(status) { where(status: status) }
 
   def amount_with_fee
     amount + admin_fee
   end
 
+  def create_in_stripe!
+    if payment_intent_id.blank?
+      intent = Stripe::PaymentIntent.create(
+        stripe_params.merge({confirm: true, off_session: true}),
+        {idempotency_key: "#{uid}_off_session"}
+      )
+      update!(payment_intent_id: intent.id, status: intent.status)
+    end
+
+    payment_intent_id
+  rescue Stripe::InvalidRequestError => e
+    update!(status: "failed")
+    Sentry.capture_exception(e, extra: {stripe_error: e.json_body[:error]})
+    Slack.message(channel: "client_engagement", text: "Something went wrong with the payment for *#{company.name}* (#{company_id}) with *#{specialist.account.name}* (#{specialist.uid})! Payment: #{uid}")
+    create_on_session_intent!
+    payment_intent_id
+  end
+
   private
+
+  def create_on_session_intent!
+    return if payment_intent_id.present?
+
+    intent = Stripe::PaymentIntent.create(
+      stripe_params,
+      {idempotency_key: "#{uid}_on_session"}
+    )
+    update!(payment_intent_id: intent.id, status: intent.status)
+  rescue Stripe::InvalidRequestError => e
+    Sentry.capture_exception(e, extra: {stripe_error: e.json_body[:error]})
+  end
+
+  def stripe_params
+    {
+      amount: amount_with_fee,
+      currency: "usd",
+      customer: company.stripe_customer_id,
+      metadata: {admin_fee: admin_fee}
+    }
+  end
 
   def set_admin_fee
     return if admin_fee.present?
