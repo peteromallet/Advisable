6458a6399cfe3ec756666eae2cd82260e3f99842
guandjoy <guandjoy@gmail.com>
2021-07-12 11:17:28 +0300

    Refactor SpecialistSettings to use View component

--- app/javascript/src/views/Settings/SpecialistSettings/Sidebar.js
+++ app/javascript/src/views/Settings/SpecialistSettings/Sidebar.js
@@ -1,89 +1,48 @@
 import React from "react";
-import { padding } from "styled-system";
-import { motion } from "framer-motion";
+import styled from "styled-components";
 import { Calendar } from "@styled-icons/ionicons-outline/Calendar";
 import { Card } from "@styled-icons/ionicons-outline/Card";
 import { LockClosed } from "@styled-icons/ionicons-outline/LockClosed";
 import { PersonCircle } from "@styled-icons/ionicons-outline/PersonCircle";
-import NavigationMenu from "../../../components/NavigationMenu";
-import styled from "styled-components";
-import { Text, useBreakpoint } from "@advisable/donut";
-
-const SidebarContainer = styled.div`
-  width: 280px;
-  position: relative;
-
-  @media (max-width: 900px) {
-    width: 100%;
-  }
-`;
+import { Text } from "@advisable/donut";
+import NavigationMenu from "src/components/NavigationMenu";
+import View from "src/components/View";
 
 const SidebarTitle = styled.div`
-  @media (max-width: 900px) {
+  @media (max-width: 1024px) {
     margin-left: 0;
   }
 `;
 
-const Sidebar = styled.div`
-  left: 0;
-  top: 60px;
-  width: 280px;
-  position: fixed;
-  height: calc(100vh - 60px);
-  background: white;
-  box-shadow: 0px 1px 20px rgba(14, 31, 91, 0.04);
-
-  @media (max-width: 900px) {
-    position: relative;
-    height: auto;
-    background: none;
-    box-shadow: none;
-    top: 0;
-    width: 100%;
-    padding: 0 0 20px 0;
-  }
-
-  ${padding};
-`;
-
 const SettingsSidebar = () => {
-  const isDesktop = useBreakpoint("lUp");
   return (
-    <SidebarContainer>
-      <Sidebar
-        as={isDesktop ? motion.div : "div"}
-        padding="m"
-        initial={{ opacity: 0, left: -100 }}
-        animate={{ opacity: 1, left: 0 }}
-        transition={{ duration: 0.5 }}
+    <View.Sidebar>
+      <Text
+        as={SidebarTitle}
+        fontSize="xl"
+        color="blue900"
+        fontWeight="medium"
+        mb="s"
+        mt="xxs"
+        ml="xs"
       >
-        <Text
-          as={SidebarTitle}
-          fontSize="xl"
-          color="blue900"
-          fontWeight="medium"
-          mb="s"
-          mt="xxs"
-          ml="xs"
-        >
-          Settings
-        </Text>
-        <NavigationMenu title="Settings">
-          <NavigationMenu.Item to="/settings/general" icon={<PersonCircle />}>
-            General
-          </NavigationMenu.Item>
-          <NavigationMenu.Item to="/settings/availability" icon={<Calendar />}>
-            Availability
-          </NavigationMenu.Item>
-          <NavigationMenu.Item to="/settings/payment-settings" icon={<Card />}>
-            Payment Settings
-          </NavigationMenu.Item>
-          <NavigationMenu.Item to="/settings/password" icon={<LockClosed />}>
-            Account
-          </NavigationMenu.Item>
-        </NavigationMenu>
-      </Sidebar>
-    </SidebarContainer>
+        Settings
+      </Text>
+      <NavigationMenu title="Settings">
+        <NavigationMenu.Item to="/settings/general" icon={<PersonCircle />}>
+          General
+        </NavigationMenu.Item>
+        <NavigationMenu.Item to="/settings/availability" icon={<Calendar />}>
+          Availability
+        </NavigationMenu.Item>
+        <NavigationMenu.Item to="/settings/payment-settings" icon={<Card />}>
+          Payment Settings
+        </NavigationMenu.Item>
+        <NavigationMenu.Item to="/settings/password" icon={<LockClosed />}>
+          Account
+        </NavigationMenu.Item>
+      </NavigationMenu>
+    </View.Sidebar>
   );
 };
 
