9360994a07693fb7904dee15186002dad523a869
Thomas Cullen <thomascullen92@gmail.com>
2021-07-13 12:07:01 +0100

    Create page for handling failed payments

---
+++ app/javascript/src/views/Payment/HandlePayment.js
@@ -0,0 +1,56 @@
+import { useApolloClient } from "@apollo/client";
+import React, { useState } from "react";
+import { Text } from "@advisable/donut";
+import PaymentMethodForm from "src/components/PaymentMethodForm";
+
+export default function HandlePayment({ payment }) {
+  const client = useApolloClient();
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState(null);
+
+  // Handler for PaymentMethodForm component card details. This is used to
+  // complete the payment when the user does not have an existing payment
+  // method.
+  const handleCardDetails = async (stripe, details) => {
+    setProcessing(true);
+    setError(null);
+
+    const response = await stripe.confirmCardPayment(
+      payment.paymentIntent.secret,
+      {
+        payment_method: {
+          card: details.card,
+          billing_details: {
+            name: details.cardholder,
+          },
+        },
+      },
+    );
+
+    setProcessing(false);
+
+    if (response.error) {
+      setError(response.error);
+    } else {
+      setError(null);
+      client.cache.modify({
+        id: client.cache.identify(payment),
+        fields: {
+          status() {
+            return "succeeded";
+          },
+        },
+      });
+    }
+  };
+  return (
+    <>
+      <PaymentMethodForm
+        loading={processing}
+        buttonLabel="Complete Payment"
+        handleCardDetails={handleCardDetails}
+      />
+      {error ? <Text color="red600">{error}</Text> : null}
+    </>
+  );
+}
