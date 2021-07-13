5cf71932b8226a1e4b7091a17b2f9ae22849e8f8
Miha Rekar <miha.rekar@advisable.com>
2021-07-13 10:45:26 +0200

    Address PR review points

--- app/graphql/types/payment.rb
+++ app/graphql/types/payment.rb
@@ -8,7 +8,12 @@ module Types
     field :status, String, null: true
     field :amount, Int, null: false
     field :admin_fee, Int, null: false
-    field :payment_intent_id, ID, null: false
+
+    field :payment_intent, PaymentIntentType, null: false
+    def payment_intent
+      Stripe::PaymentIntent.retrieve(object.payment_intent_id)
+    end
+
     field :company, CompanyType, null: false
     field :specialist, SpecialistType, null: false
     field :task, TaskType, null: true
