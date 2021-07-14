93bd0648df117746bd727df912bce580811f1996
Miha Rekar <miha.rekar@advisable.com>
2021-07-14 09:00:20 +0200

    Replace Users::AttachPaymentMethod service with UpdateCompanysPaymentMethodJob

---
+++ app/jobs/update_companys_payment_method_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class UpdateCompanysPaymentMethodJob < ApplicationJob
+  queue_as :default
+
+  def perform(company, payment_method_id)
+    stripe_customer_id = company.stripe_customer_id
+
+    existing = Stripe::PaymentMethod.list(customer: stripe_customer_id, type: "card")
+    unless existing.data.map(&:id).include?(payment_method_id)
+      # Attach the payment method to the customer
+      Stripe::PaymentMethod.attach(payment_method_id, {customer: stripe_customer_id})
+    end
+
+    # Set the payment method as the customer's default payment method
+    Stripe::Customer.update(stripe_customer_id, {invoice_settings: {default_payment_method: payment_method_id}})
+
+    company.update(stripe_payment_method: payment_method_id)
+  end
+end
--- app/lib/stripe_events/setup_intent_succeeded.rb
+++ app/lib/stripe_events/setup_intent_succeeded.rb
@@ -1,31 +1,32 @@
+# frozen_string_literal: true
+
 # Handler for the stripe setup_intent.succeeded webhook event. This fires when
 # a user has completed the setup flow for adding a card. We do two things
 # inside this class. We attach the payment method to the customer object by
-# calling the AttachPaymentMethod service. Then we update the users
+# calling UpdateCompanysPaymentMethod. Then we update the users
 # 'setup_intent_status' is set to 'succeeded' to indicate that the setup process
 # has been complete. During the flow for adding a card we poll the API until the
 # users setup_intent_status changes from pending to succeeded.
-class StripeEvents::SetupIntentSucceeded < StripeEvents::BaseEvent
-  def process
-    # If the record wasn't found then just return true
-    return true if company.nil?
+module StripeEvents
+  class SetupIntentSucceeded < StripeEvents::BaseEvent
+    def process
+      # If the record wasn't found then just return true
+      return true if company.nil?
 
-    # Attach the payment method
-    Users::AttachPaymentMethod.call(
-      user: company.first_account.user,
-      payment_method_id: setup_intent.payment_method
-    )
-    # Update the setup_intent_status so the frontend can respond
-    company.update_columns(setup_intent_status: 'succeeded') # rubocop:disable Rails/SkipsModelValidations
-  end
+      UpdateCompanysPaymentMethodJob.perform_later(company, setup_intent.payment_method)
 
-  private
+      # Update the setup_intent_status so the frontend can respond
+      company.update_columns(setup_intent_status: "succeeded") # rubocop:disable Rails/SkipsModelValidations
+    end
 
-  def setup_intent
-    event.data.object
-  end
+    private
 
-  def company
-    @company ||= Company.find_by(stripe_setup_intent_id: setup_intent.id)
+    def setup_intent
+      event.data.object
+    end
+
+    def company
+      @company ||= Company.find_by(stripe_setup_intent_id: setup_intent.id)
+    end
   end
 end
