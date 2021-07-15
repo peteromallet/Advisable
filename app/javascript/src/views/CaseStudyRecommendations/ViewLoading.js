6c36ca62d2cc538c4c0864b9c0d24f541b51617c
Thomas Cullen <thomascullen92@gmail.com>
2021-07-15 14:32:37 +0100

    Improve positioning of action bar

--- app/javascript/src/views/CaseStudyRecommendations/ViewLoading.js
+++ app/javascript/src/views/CaseStudyRecommendations/ViewLoading.js
@@ -6,7 +6,9 @@ export default function ViewLoading() {
   return (
     <>
       <Box>
-        <Skeleton height="24px" width="30%" />
+        <Skeleton height="24px" width="40%" mb={5} />
+        <Skeleton height="12px" width="100%" mb={3} />
+        <Skeleton height="12px" width="60%" />
         <Box marginY={10} height="1px" bg="neutral200" />
         <CardsSkeleton />
       </Box>
