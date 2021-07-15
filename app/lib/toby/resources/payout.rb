ef32abac89e504bd41272692cd273c6bb93582fe
Miha Rekar <miha.rekar@advisable.com>
2021-07-15 11:42:54 +0200

    Add Payment and Payout to Toby

---
+++ app/lib/toby/resources/payout.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Toby
+  module Resources
+    class Payout < BaseResource
+      model_name ::Payout
+      attribute :uid, Attributes::String, readonly: true
+      attribute :specialist, Attributes::BelongsTo
+      attribute :amount, Attributes::Currency
+      attribute :sourcing_fee, Attributes::Currency
+      attribute :status, Attributes::String
+      attribute :processed_at, Attributes::DateTime, readonly: true
+      attribute :created_at, Attributes::DateTime, readonly: true
+      attribute :updated_at, Attributes::DateTime, readonly: true
+    end
+  end
+end
