82b6b4eb560356101a860d787347fcf43ce35f69
Miha Rekar <info@mr.si>
2021-07-19 10:30:48 +0200

    Fold Tasks::Assign service into mutation

--- app/graphql/mutations/assign_task.rb
+++ app/graphql/mutations/assign_task.rb
@@ -2,6 +2,8 @@
 
 module Mutations
   class AssignTask < Mutations::BaseMutation
+    description "Assigns a Task"
+
     argument :task, ID, required: true
 
     field :task, Types::TaskType, null: true
@@ -16,13 +18,19 @@ module Mutations
 
     def resolve(**args)
       task = Task.find_by_uid!(args[:task])
-
       user = task.application.project.user
-      user.account.complete_tutorial('fixed_projects') unless user.account.completed_tutorial?('fixed_projects')
+      user.account.complete_tutorial("fixed_projects") unless user.account.completed_tutorial?("fixed_projects")
 
-      {task: Tasks::Assign.call(task: task, responsible_id: current_account_id)}
-    rescue Service::Error => e
-      ApiError.service_error(e)
+      ApiError.invalid_request("tasks.nameRequired") if task.name.blank?
+      ApiError.invalid_request("tasks.descriptionRequired") if task.description.blank?
+      ApiError.invalid_request("tasks.alreadyAssigned") unless ["Not Assigned", "Requested To Start", "Quote Requested", "Quote Provided"].include?(task.stage)
+
+      success = current_account_responsible_for { task.update(stage: "Assigned", assigned_at: Time.zone.now) }
+      ApiError.invalid_request(task.errors.full_messages.first) unless success
+
+      task.sync_to_airtable
+
+      {task: task}
     end
   end
 end
