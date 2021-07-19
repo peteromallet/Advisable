c8d6735d16cdfd9d19e91fa6e38d29a17f6c7d83
Miha Rekar <info@mr.si>
2021-07-19 14:02:05 +0200

    Skip for nil/0 final_cost

--- spec/system/managing_talent_spec.rb
+++ spec/system/managing_talent_spec.rb
@@ -1,61 +1,61 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Managing talent', type: :system do
-  let(:application) { create(:application, status: 'Working') }
+RSpec.describe "Managing talent", type: :system do
+  let(:application) { create(:application, status: "Working") }
 
   before do
     allow_any_instance_of(Task).to receive(:sync_to_airtable)
     authenticate_as application.project.user
-    application.project.user.account.complete_tutorial('fixed_projects')
+    application.project.user.account.complete_tutorial("fixed_projects")
   end
 
-  context 'when there are no tasks' do
-    it 'enables the client to add a task' do
+  context "when there are no tasks" do
+    it "enables the client to add a task" do
       visit "/manage/#{application.uid}"
-      click_on 'Add a project'
-      fill_in 'name', with: 'This is a task'
-      click_on 'Due Date'
-      click_on 'Next Month'
+      click_on "Add a project"
+      fill_in "name", with: "This is a task"
+      click_on "Due Date"
+      click_on "Next Month"
       first("div[aria-disabled='false']").click
-      fill_in 'description', with: 'This is a description'
-      click_on 'Close Drawer'
-      expect(page).to have_content('This is a task')
+      fill_in "description", with: "This is a description"
+      click_on "Close Drawer"
+      expect(page).to have_content("This is a task")
     end
   end
 
-  context 'when there is an existing task' do
+  context "when there is an existing task" do
     before do
       create(
         :task,
         {
           estimate: 8,
-          stage: 'Assigned',
+          stage: "Assigned",
           application: application,
           due_date: 2.days.from_now,
-          name: 'This is an existing task',
-          description: 'This is the task description'
+          name: "This is an existing task",
+          description: "This is the task description"
         }
       )
     end
 
-    it 'enables the client to view the task' do
+    it "enables the client to view the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      expect(page).to have_content('This is the task description')
+      find("h5", text: "This is an existing task").click
+      expect(page).to have_content("This is the task description")
     end
 
-    it 'allows the client to create a new task for the freelancer' do
+    it "allows the client to create a new task for the freelancer" do
       visit "/manage/#{application.uid}"
-      click_on 'Add a project'
-      fill_in 'name', with: 'This is a task'
-      click_on 'Due Date'
-      click_on 'Next Month'
+      click_on "Add a project"
+      fill_in "name", with: "This is a task"
+      click_on "Due Date"
+      click_on "Next Month"
       first("div[aria-disabled='false']").click
-      fill_in 'description', with: 'This is a description'
-      click_on 'Close Drawer'
-      expect(page).to have_content('This is a task')
+      fill_in "description", with: "This is a description"
+      click_on "Close Drawer"
+      expect(page).to have_content("This is a task")
     end
   end
 
@@ -65,42 +65,42 @@ RSpec.describe 'Managing talent', type: :system do
         :task,
         {
           estimate: nil,
-          stage: 'Not Assigned',
+          stage: "Not Assigned",
           application: application,
           due_date: 2.days.from_now,
-          name: 'This is an existing task',
-          description: 'This is the task description'
+          name: "This is an existing task",
+          description: "This is the task description"
         }
       )
     end
 
-    it 'enables the client to request a quote for the task' do
+    it "enables the client to request a quote for the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      click_on 'Request Quote'
+      find("h5", text: "This is an existing task").click
+      click_on "Request Quote"
       expect(page).to have_content(
-        'A quote has been requested for this project'
+        "A quote has been requested for this project"
       )
     end
 
-    it 'enables the client to assign the task' do
+    it "enables the client to assign the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      click_on 'Assign Task'
+      find("h5", text: "This is an existing task").click
+      click_on "Assign Task"
       within "div[class^='styles__ConfirmationContainer']" do
-        click_on 'Assign'
+        click_on "Assign"
       end
-      expect(page).to have_content('This project has been assigned')
+      expect(page).to have_content("This project has been assigned")
     end
 
-    it 'allows the client to delete it' do
+    it "allows the client to delete it" do
       allow_any_instance_of(Task).to receive(:remove_from_airtable)
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      click_on 'Open task actions menu'
-      click_on 'Delete task'
-      click_on 'Delete'
-      expect(page).not_to have_content('This is an existing task')
+      find("h5", text: "This is an existing task").click
+      click_on "Open task actions menu"
+      click_on "Delete task"
+      click_on "Delete"
+      expect(page).not_to have_content("This is an existing task")
     end
   end
 
@@ -110,29 +110,29 @@ RSpec.describe 'Managing talent', type: :system do
         :task,
         {
           estimate: nil,
-          stage: 'Quote Requested',
+          stage: "Quote Requested",
           application: application,
           due_date: 2.days.from_now,
-          name: 'This is an existing task',
-          description: 'This is the task description'
+          name: "This is an existing task",
+          description: "This is the task description"
         }
       )
     end
 
-    it 'enables the client to assign the task' do
+    it "enables the client to assign the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      click_on 'Assign Task'
-      click_on 'Assign'
-      expect(page).to have_content('This project has been assigned')
+      find("h5", text: "This is an existing task").click
+      click_on "Assign Task"
+      click_on "Assign"
+      expect(page).to have_content("This project has been assigned")
     end
 
-    it 'allows the client do edit the task' do
+    it "allows the client do edit the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      fill_in 'name', with: 'Updated task name'
-      click_on 'Close Drawer'
-      expect(page).to have_content('Updated task name')
+      find("h5", text: "This is an existing task").click
+      fill_in "name", with: "Updated task name"
+      click_on "Close Drawer"
+      expect(page).to have_content("Updated task name")
     end
   end
 
@@ -142,31 +142,31 @@ RSpec.describe 'Managing talent', type: :system do
         :task,
         {
           estimate: 8,
-          stage: 'Quote Provided',
+          stage: "Quote Provided",
           application: application,
           due_date: 2.days.from_now,
-          name: 'This is an existing task',
-          description: 'This is the task description'
+          name: "This is an existing task",
+          description: "This is the task description"
         }
       )
     end
 
-    it 'enables the client to assign the task' do
+    it "enables the client to assign the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      click_on 'Assign Task'
-      click_on 'Assign'
-      expect(page).to have_content('This project has been assigned')
+      find("h5", text: "This is an existing task").click
+      click_on "Assign Task"
+      click_on "Assign"
+      expect(page).to have_content("This project has been assigned")
     end
 
-    it 'allows the client to edit the task and remove the estimate' do
+    it "allows the client to edit the task and remove the estimate" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
+      find("h5", text: "This is an existing task").click
       find("textarea[name='name']").click
-      click_on 'Continue'
-      fill_in 'name', with: 'Changed'
-      click_on 'Close Drawer'
-      expect(page).to have_content('Changed')
+      click_on "Continue"
+      fill_in "name", with: "Changed"
+      click_on "Close Drawer"
+      expect(page).to have_content("Changed")
     end
   end
 
@@ -176,27 +176,27 @@ RSpec.describe 'Managing talent', type: :system do
         :task,
         {
           estimate: 8,
-          stage: 'Submitted',
+          stage: "Submitted",
           application: application,
           due_date: 2.days.from_now,
-          name: 'This is an existing task',
-          description: 'This is the task description'
+          name: "This is an existing task",
+          description: "This is the task description"
         }
       )
     end
 
-    it 'enables the client to approve the task' do
+    it "enables the client to approve the task" do
       visit "/manage/#{application.uid}"
-      find('h5', text: 'This is an existing task').click
-      click_on 'Approve'
+      find("h5", text: "This is an existing task").click
+      click_on "Approve"
       within "div[class^='styles__ConfirmationContainer']" do
-        click_on 'Approve'
+        click_on "Approve"
       end
-      expect(page).to have_content('This project has been completed')
+      expect(page).to have_content("This project has been completed")
     end
   end
 
-  it 'allows user to report an issue with the freelancer' do
+  it "allows user to report an issue with the freelancer" do
     visit "/manage/#{application.uid}"
     click_on "Report Issue"
     within "*[role=dialog]" do
