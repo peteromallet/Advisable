5c93055d96ab125dd5fc4a53199f3dfd3ba987f9
Thomas Cullen <thomascullen92@gmail.com>
2021-07-20 08:58:44 +0100

    Move payment capture from task assignment to when it is started
    This is because a task can be assigned to a freelancer without a quote,
    however, a freelancer must add a quote before starting work on a task.

--- app/graphql/mutations/start_task.rb
+++ app/graphql/mutations/start_task.rb
@@ -2,6 +2,7 @@
 
 module Mutations
   class StartTask < Mutations::BaseMutation
+    description "Starts a task"
     argument :task, ID, required: true
 
     field :task, Types::TaskType, null: true
@@ -17,9 +18,33 @@ module Mutations
     def resolve(**args)
       task = Task.find_by_uid!(args[:task])
 
-      {task: Tasks::Start.call(task: task, responsible_id: current_account_id)}
-    rescue Service::Error => e
-      ApiError.service_error(e)
+      ApiError.invalid_request("tasks.mustBeAssigned") if task.stage != "Assigned"
+      ApiError.invalid_request("tasks.estimateRequired") if task.estimate.blank?
+      ApiError.invalid_request("tasks.dueDateRequired") if task.due_date.blank?
+
+      success = current_account_responsible_for { task.update(stage: "Working", started_working_at: Time.zone.now) }
+      ApiError.invalid_request(task.errors.full_messages.first) unless success
+      task.sync_to_airtable
+
+      create_payment!(task)
+
+      {task: task}
+    end
+
+    private
+
+    def create_payment!(task)
+      if task.fixed_estimate?
+        amount = task.estimate.to_i
+      else
+        hours = [task.flexible_estimate, task.estimate].select { |e| e.to_i.positive? }.min
+        amount = task.application.invoice_rate.to_i * hours.to_i
+      end
+
+      return unless amount.positive?
+
+      payment = Payment.create!(company_id: task.application.project.user.company_id, specialist_id: task.application.specialist_id, amount: amount, task: task, status: "pending")
+      payment.create_in_stripe!
     end
   end
 end
