4501caa6b50a9f6427d5ee690e8b2575a828affa
Miha Rekar <miha.rekar@advisable.com>
2021-07-23 09:43:32 +0200

    Extract #mark_as_read_for

--- app/graphql/mutations/update_last_read.rb
+++ app/graphql/mutations/update_last_read.rb
@@ -18,8 +18,7 @@ module Mutations
 
     def resolve(conversation:)
       conversation = Conversation.find_by!(uid: conversation)
-      participant = conversation.participants.find_by(account_id: current_account_id)
-      participant&.update!(last_read_at: Time.zone.now)
+      conversation.mark_as_read_for!(current_account)
       {conversation: conversation}
     end
   end
