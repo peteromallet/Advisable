aeb4c582807865640bba00d1bda3f4a1faabbfcb
Miha Rekar <miha.rekar@advisable.com>
2021-07-26 11:38:33 +0200

    Spec up MessageNotifierJob

--- app/jobs/message_notifier_job.rb
+++ app/jobs/message_notifier_job.rb
@@ -10,18 +10,15 @@ class MessageNotifierJob < ApplicationJob
     conversation.participants.each do |participant|
       next if participant.account == message.author
 
-      new_ones = new_ones_for(participant)
-      next if new_ones.any?
+      new_ones = conversation.messages.
+        where("created_at > ?", message.created_at).
+        any?
+      next if new_ones
 
-      AccountMailer.notify_of_new_messages(participant.account, new_ones).deliver_later
+      all_new = conversation.messages
+      last_at = participant.conversation.messages.where(author_id: participant.account_id).maximum(:created_at)
+      all_new = all_new.where("created_at > ?", last_at) if last_at
+      AccountMailer.notify_of_new_messages(participant.account, all_new.pluck(:id)).deliver_later
     end
-  end
-
-  private
-
-  def new_ones_for(participant)
-    conversation.messages.
-      where.not(author_id: participant.account_id).
-      where(created_at: message.created_at..)
   end
 end
