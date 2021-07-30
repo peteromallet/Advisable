0012e1e6019e4f8f4a27aafbe61580337a05d329
Thomas Cullen <thomascullen92@gmail.com>
2021-07-30 08:34:30 +0100

    Use clearbit logo API as a fallback

--- app/graphql/types/case_study/company.rb
+++ app/graphql/types/case_study/company.rb
@@ -13,10 +13,18 @@ module Types
       field :business_type, String, null: true
       field :description, String, null: true
       field :articles, [Article], null: false
-      
+
       field :favicon, String, null: true
       def favicon
-        object.favicon.url
+        object.favicon.url || clearbit_logo
+      end
+
+      private
+
+      def clearbit_logo
+        return nil if object.website.blank?
+
+        "//logo.clearbit.com/#{object.website}"
       end
     end
   end
