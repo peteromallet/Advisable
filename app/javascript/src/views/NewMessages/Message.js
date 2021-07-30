be035bfee17686cc2ccf69ac17fae511ed6f6c96
Thomas Cullen <thomascullen92@gmail.com>
2021-07-30 14:54:50 +0100

    Add basic attachments

--- app/javascript/src/views/NewMessages/Message.js
+++ app/javascript/src/views/NewMessages/Message.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Avatar, Box, Text } from "@advisable/donut";
+import { Avatar, Box, Stack, Text } from "@advisable/donut";
 import renderLineBreaks from "src/utilities/renderLineBreaks";
+import { DocumentText } from "@styled-icons/heroicons-solid/DocumentText";
 import { DateTime } from "luxon";
 
 export default function Message({ message }) {
@@ -36,6 +37,53 @@ export default function Message({ message }) {
         <Text autoLink color="neutral800" lineHeight="24px">
           {renderLineBreaks(message.content)}
         </Text>
+        {message.attachments.length > 0 && (
+          <Stack marginTop={4} spacing={2}>
+            {message.attachments.map((attachment) => (
+              <Box
+                width="100%"
+                padding={2}
+                bg="neutral100"
+                key={attachment.id}
+                borderRadius="8px"
+                display="flex"
+                alignItems="center"
+              >
+                <Box
+                  flexShrink={0}
+                  marginTop="-1px"
+                  color="neutral600"
+                  paddingRight={1.5}
+                >
+                  <DocumentText size={16} />
+                </Box>
+                <Box width="100%" minWidth="0">
+                  <Text
+                    $truncate
+                    paddingY={1}
+                    fontSize="sm"
+                    fontWeight={500}
+                    color="neutral900"
+                  >
+                    {attachment.filename}
+                  </Text>
+                </Box>
+                <Box flexShrink={0}>
+                  <Text
+                    as="a"
+                    target="_blank"
+                    href={attachment.url}
+                    color="blue700"
+                    fontWeight={500}
+                    fontSize="sm"
+                  >
+                    Download
+                  </Text>
+                </Box>
+              </Box>
+            ))}
+          </Stack>
+        )}
       </Box>
     </Box>
   );
