f88a4750af33b79fce433393eeb8897696577e95
Thomas Cullen <thomascullen92@gmail.com>
2021-08-05 08:28:25 +0100

    Fix avatar stack in conversation header

--- app/javascript/src/views/NewMessages/components/ConversationHeader.js
+++ app/javascript/src/views/NewMessages/components/ConversationHeader.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Avatar, Box, Text } from "@advisable/donut";
 import commaSeparated from "src/utilities/commaSeparated";
 
 export default function ConversationHeader({ conversation }) {
+  const firstFour = useMemo(() => {
+    return conversation.participants.slice(0, 5);
+  }, [conversation.participants]);
+
+  const remaining = conversation.participants.length - firstFour.length;
+
   return (
     <Box
       height="72px"
@@ -20,8 +26,8 @@ export default function ConversationHeader({ conversation }) {
         maxWidth="700px"
         alignItems="center"
       >
-        <Box marginRight={2}>
-          {conversation.participants.map((participant, index) => (
+        <Box marginRight={2} flexShrink={0} display="flex">
+          {firstFour.map((participant, index) => (
             <Avatar
               size="s"
               key={participant.id}
@@ -29,9 +35,30 @@ export default function ConversationHeader({ conversation }) {
               borderColor="white"
               url={participant.avatar}
               name={participant.firstName}
-              marginLeft={index > 0 ? "-16px" : null}
+              marginLeft={index > 0 ? "-20px" : null}
             />
           ))}
+          {remaining > 0 && (
+            <Avatar
+              size="s"
+              border="2px solid"
+              borderColor="white"
+              marginLeft="-20px"
+            >
+              <Box
+                width="100%"
+                fontSize="xs"
+                height="100%"
+                display="flex"
+                fontWeight={500}
+                color="neutral600"
+                alignItems="center"
+                justifyContent="center"
+              >
+                +{remaining}
+              </Box>
+            </Avatar>
+          )}
         </Box>
         <Text fontSize="lg" fontWeight={500} $truncate>
           {commaSeparated(conversation.participants.map((p) => p.firstName))}
