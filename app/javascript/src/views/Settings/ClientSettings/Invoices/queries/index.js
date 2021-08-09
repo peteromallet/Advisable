5185c59e65810e9a04aabc5756210a2a27811514
Thomas Cullen <thomascullen92@gmail.com>
2021-08-09 10:09:25 +0100

    Fix failing JS test

--- app/javascript/src/views/Settings/ClientSettings/Invoices/queries/index.js
+++ app/javascript/src/views/Settings/ClientSettings/Invoices/queries/index.js
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import INVOICE from "./invoiceDetails.gql";
 import INVOICES from "./invoices.gql";
+export default { INVOICES, INVOICE };
 
 export function useInvoices() {
   return useQuery(INVOICES);
