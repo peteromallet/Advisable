97a02fdf3dbc59786c71753fe1f8da1e712aa265
Thomas Cullen <thomascullen92@gmail.com>
2021-08-09 11:21:07 +0100

    Don't stick composer to the page

--- app/javascript/src/views/NewMessages/components/Conversation.js
+++ app/javascript/src/views/NewMessages/components/Conversation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box } from "@advisable/donut";
 import { useParams } from "react-router-dom";
-import MessageComposer from "./MessageComposer";
 import ConversationMessages from "./ConversationMessages";
 import ConversationHeader from "./ConversationHeader";
 
@@ -13,10 +12,7 @@ export default function Conversation({ conversations, currentAccount }) {
     <Box height="100%" display="flex" flexDirection="column">
       <ConversationHeader conversation={conversation} />
       <Box height="100%" minHeight="0" width="100%" flexGrow={1} flexShrink={1}>
-        <ConversationMessages conversation={conversation} />
-      </Box>
-      <Box width="100%" paddingBottom={4} maxWidth="720px" px={4} mx="auto">
-        <MessageComposer
+        <ConversationMessages
           conversation={conversation}
           currentAccount={currentAccount}
         />
