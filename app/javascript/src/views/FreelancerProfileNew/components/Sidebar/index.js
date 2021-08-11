4c19ce2e8ac86e719e17c326448b27b933dd6db6
guandjoy <guandjoy@gmail.com>
2021-08-11 12:11:51 +0300

    Cut biography

---
+++ app/javascript/src/views/FreelancerProfileNew/components/Sidebar/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Map } from "@styled-icons/heroicons-outline/Map";
+import { Button, Box, Text } from "@advisable/donut";
+import PassportAvatar from "src/components/PassportAvatar";
+
+export default function Sidebar({ data }) {
+  const bio = data.specialist.bio.slice(0, 140);
+
+  return (
+    <Box position="sticky" width="280px">
+      <PassportAvatar
+        size="2xl"
+        name={data.specialist.name}
+        src={data.specialist.avatar}
+        marginBottom={4}
+      />
+      <Text
+        fontSize="5xl"
+        fontWeight="semibold"
+        color="neutral900"
+        lineHeight="4xl"
+        letterSpacing="-0.03rem"
+        marginBottom={1.5}
+      >
+        {data.specialist.name}
+      </Text>
+      <Box display="flex" color="neutral400" alignItems="center" mb={4}>
+        <Map height="20px" width="20px" color="neutral500" />
+        <Text
+          fontSize="17px"
+          fontWeight="medium"
+          color="neutral400"
+          lineHeight="l"
+          marginLeft={1}
+        >
+          {data.specialist.location}
+        </Text>
+      </Box>
+      <Text fontSize="l" lineHeight="l" color="neutral700" mb={7}>
+        {bio}
+      </Text>
+      <Button variant="gradient" size="l">
+        Work together
+      </Button>
+    </Box>
+  );
+}
