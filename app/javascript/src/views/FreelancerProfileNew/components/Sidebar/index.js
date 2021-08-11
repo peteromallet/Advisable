3b0ce240f8bd09bc05050e3e185c95910c6258c2
guandjoy <guandjoy@gmail.com>
2021-08-11 13:31:09 +0300

    Implement sticky sidebar

--- app/javascript/src/views/FreelancerProfileNew/components/Sidebar/index.js
+++ app/javascript/src/views/FreelancerProfileNew/components/Sidebar/index.js
@@ -7,41 +7,43 @@ export default function Sidebar({ data }) {
   const bio = data.specialist.bio.slice(0, 140);
 
   return (
-    <Box position="sticky" width="280px">
-      <PassportAvatar
-        size="2xl"
-        name={data.specialist.name}
-        src={data.specialist.avatar}
-        marginBottom={4}
-      />
-      <Text
-        fontSize="5xl"
-        fontWeight="semibold"
-        color="neutral900"
-        lineHeight="4xl"
-        letterSpacing="-0.03rem"
-        marginBottom={1.5}
-      >
-        {data.specialist.name}
-      </Text>
-      <Box display="flex" color="neutral400" alignItems="center" mb={4}>
-        <Map height="20px" width="20px" color="neutral500" />
+    <Box position="relative" top="-156px">
+      <Box position="sticky" top="108px" width="280px" mr={8} ml={7}>
+        <PassportAvatar
+          size="2xl"
+          name={data.specialist.name}
+          src={data.specialist.avatar}
+          marginBottom={4}
+        />
         <Text
-          fontSize="17px"
-          fontWeight="medium"
-          color="neutral400"
-          lineHeight="l"
-          marginLeft={1}
+          fontSize="5xl"
+          fontWeight="semibold"
+          color="neutral900"
+          lineHeight="4xl"
+          letterSpacing="-0.03rem"
+          marginBottom={1.5}
         >
-          {data.specialist.location}
+          {data.specialist.name}
         </Text>
+        <Box display="flex" color="neutral400" alignItems="center" mb={4}>
+          <Map height="20px" width="20px" color="neutral500" />
+          <Text
+            fontSize="17px"
+            fontWeight="medium"
+            color="neutral400"
+            lineHeight="l"
+            marginLeft={1}
+          >
+            {data.specialist.location}
+          </Text>
+        </Box>
+        <Text fontSize="l" lineHeight="l" color="neutral700" mb={7}>
+          {bio}
+        </Text>
+        <Button variant="gradient" size="l">
+          Work together
+        </Button>
       </Box>
-      <Text fontSize="l" lineHeight="l" color="neutral700" mb={7}>
-        {bio}
-      </Text>
-      <Button variant="gradient" size="l">
-        Work together
-      </Button>
     </Box>
   );
 }
