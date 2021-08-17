b2692a731a0188c5763423a42f9d7495c2a3225d
Miha Rekar <miha.rekar@advisable.com>
2021-08-17 11:36:31 +0200

    Add PdfInvoice model and persist to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_074400) do
+ActiveRecord::Schema.define(version: 2021_08_17_092744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -737,6 +737,16 @@ ActiveRecord::Schema.define(version: 2021_07_26_074400) do
     t.index ["uid"], name: "index_payouts_on_uid", unique: true
   end
 
+  create_table "pdf_invoices", force: :cascade do |t|
+    t.integer "month"
+    t.integer "year"
+    t.uuid "company_id", null: false
+    t.string "key"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_pdf_invoices_on_company_id"
+  end
+
   create_table "post_prompts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "prompt"
     t.string "cta"
@@ -1201,6 +1211,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_074400) do
   add_foreign_key "payments", "tasks"
   add_foreign_key "payouts", "specialists"
   add_foreign_key "payouts", "tasks"
+  add_foreign_key "pdf_invoices", "companies"
   add_foreign_key "post_prompts", "labels"
   add_foreign_key "problematic_flags", "applications"
   add_foreign_key "problematic_flags", "users"
