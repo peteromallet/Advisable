6dfce97895e0c85efa6b5c270f81f5b789673d65
Miha Rekar <miha.rekar@advisable.com>
2021-08-17 13:00:11 +0200

    Rename PdfInvoice to Invoice

--- spec/factories/pdf_invoices.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :pdf_invoice do
-    month { 8 }
-    year { 2021 }
-    company
-  end
-end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe PdfInvoice, type: :model do
+RSpec.describe Invoice, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
