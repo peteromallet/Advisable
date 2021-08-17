0e10b7194ba9ec17edf27873923d227af15112eb
Miha Rekar <miha.rekar@advisable.com>
2021-08-17 13:00:11 +0200

    Rename PdfInvoice to Invoice

---
+++ app/models/invoice.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Invoice < ApplicationRecord
+  URL_EXPIRES_AT = 1.hour.to_i
+
+  belongs_to :company
+
+  validates :year, :month, presence: true
+  validates :month, uniqueness: {scope: %i[company_id year]}
+
+  def date_range
+    @date_range ||= Date.parse("1.#{month}.#{year}").all_month
+  end
+
+  def pdf_url(regenerate: false)
+    self.key = nil if regenerate
+    GenerateInvoicePdfJob.perform_now(self) if key.blank?
+    obj = Aws::S3::Object.new(bucket_name: ENV["AWS_S3_BUCKET"], key: key)
+    obj.presigned_url(:get, expires_in: URL_EXPIRES_AT)
+  end
+end
+
+# == Schema Information
+#
+# Table name: invoices
+#
+#  id         :bigint           not null, primary key
+#  key        :string
+#  month      :integer          not null
+#  year       :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :uuid             not null
+#
+# Indexes
+#
+#  index_invoices_on_company_id                     (company_id)
+#  index_invoices_on_company_id_and_year_and_month  (company_id,year,month) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
