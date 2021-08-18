22acb0b2f13dc24d19ab1229bfa0482e2a8d3a4f
Miha Rekar <miha.rekar@advisable.com>
2021-08-18 09:40:56 +0200

    WIP

--- app/models/talkjs_api.rb
+++ app/models/talkjs_api.rb
@@ -3,8 +3,15 @@
 class TalkjsApi
   API_ROOT = "https://api.talkjs.com/v1/#{ENV["TALKJS"]}/"
 
-  def messages(conversation_id)
-    get_request("conversations/#{conversation_id}/messages")
+  def messages(conversation_id, starting_after_id = nil)
+    starting_after = starting_after_id.nil? ? "" : "?startingAfter=#{starting_after_id}"
+
+    get_request("conversations/#{conversation_id}/messages?limit=100#{starting_after}")
+  end
+
+  def conversations(starting_after_id = nil)
+    starting_after = starting_after_id.nil? ? "" : "?startingAfter=#{starting_after_id}"
+    get_request("conversations?limit=20#{starting_after}")
   end
 
   def conversations_by(user_id)
@@ -18,10 +25,22 @@ class TalkjsApi
   private
 
   def get_request(url)
+    retries = 1
     response = Faraday.get(API_ROOT + url, nil, request_headers)
-    raise ApiRequestError, response unless response.status == 200
+    case response.status
+    when 200
+      JSON[response.body]["data"]
+    when 429
+      raise ApiRequestError, response if retries > 2
 
-    JSON[response.body]["data"]
+      retries += 1
+      p response.headers
+      # maybe? sleep(response.headers["Retry-After"])
+      sleep 2**retries
+      get_request(url)
+    else
+      raise ApiRequestError, response
+    end
   end
 
   def delete_request(url)
