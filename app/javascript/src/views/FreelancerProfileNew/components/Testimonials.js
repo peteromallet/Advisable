a8384a79afb557a0ca896038165b7fe00b297f60
guandjoy <guandjoy@gmail.com>
2021-08-18 14:07:46 +0200

    Create SectionTitle.js

--- app/javascript/src/views/FreelancerProfileNew/components/Testimonials.js
+++ app/javascript/src/views/FreelancerProfileNew/components/Testimonials.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, Box, Stack } from "@advisable/donut";
+import { Box, Stack } from "@advisable/donut";
+import SectionTitle from "./SectionTitle";
 
 export default function CaseStudies() {
   return (
     <Box>
-      <Text>Testimonials</Text>
+      <SectionTitle>Case Studies</SectionTitle>
       <Stack spacing={6}>
         <Box width="720px" height="120px" bg="neutral100" borderRadius="12px" />
         <Box width="720px" height="120px" bg="neutral100" borderRadius="12px" />
