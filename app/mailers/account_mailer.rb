d7310fb9ab79ca89e1f7b81ca8f52b3be1bbb354
Miha Rekar <miha.rekar@advisable.com>
2021-08-23 09:02:06 +0200

    Pass conversation to notifier job

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -13,11 +13,11 @@ class AccountMailer < ApplicationMailer
     end
   end
 
-  # TODO: Make per conversation and replace Conversation.first
-  def notify_of_new_messages(account, message_ids)
+  def notify_of_new_messages(account, conversation, message_ids)
     @account = account
+    @conversation = conversation
     @messages = Message.where(id: message_ids).order(:created_at)
-    reply_to = "#{Conversation.first.uid}@#{ENV["MESSAGE_REPLIES_DOMAIN"]}"
+    reply_to = "#{conversation.uid}@#{ENV["MESSAGE_REPLIES_DOMAIN"]}"
 
     mail(to: @account.email, reply_to: reply_to, subject: "New messages in conversation") do |f|
       f.html { render(layout: "email_v2") }
