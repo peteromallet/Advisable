680ebde01d678477908158623bc4f0fdce353f78
Miha Rekar <miha.rekar@advisable.com>
2021-08-23 09:02:06 +0200

    Pass conversation to notifier job

--- test/mailers/previews/account_mailer_preview.rb
+++ test/mailers/previews/account_mailer_preview.rb
@@ -6,12 +6,22 @@ class AccountMailerPreview < ActionMailer::Preview
   end
 
   def notify_of_new_messages
-    AccountMailer.notify_of_new_messages(random_account, Message.last(5).pluck(:id))
+    AccountMailer.notify_of_new_messages(
+      random_account,
+      random_conversation,
+      random_conversation.messages.last(5).pluck(:id)
+    )
   end
 
   private
 
   def random_account
     @random_account ||= Account.order("RANDOM()").first
+  end
+
+  def random_conversation
+    message_conversations = Message.distinct.pluck(:conversation_id)
+    @random_conversation ||= Conversation.where(id: message_conversations).order("RANDOM()").first
+    Conversation.find_by(uid: "cnv_FC6PcH7X94zgh6g")
   end
 end
