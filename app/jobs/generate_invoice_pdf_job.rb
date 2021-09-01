91108808c41833ef9a421a4688e00ab7e41f2f13
Miha Rekar <miha.rekar@advisable.com>
2021-09-01 09:47:25 +0200

    Refactor GenerateInvoicePdfJob for clarity

--- app/jobs/generate_invoice_pdf_job.rb
+++ app/jobs/generate_invoice_pdf_job.rb
@@ -4,10 +4,45 @@ class GenerateInvoicePdfJob < ApplicationJob
   TEMPLATE_ID = "81e7c7a3-7989-4f4c-aae8-03d0f0324811"
   VAT_TEMPLATE_ID = "41FBCD83-E7E5-4CD1-B323-7BD9D16CF981"
 
+  attr_reader :invoice
+
   queue_as :default
 
   def perform(invoice)
-    data = {
+    @invoice = invoice
+
+    generate_pdf
+  end
+
+  private
+
+  def generate_pdf
+    template = invoice.company.vat_number&.starts_with?("IE") ? VAT_TEMPLATE_ID : TEMPLATE_ID
+    document = Pdfmonkey::Document.generate!(template, pdf_monkey_data)
+    if document.status == "success"
+      upload_document(document)
+    else
+      Sentry.capture_message("Something went wrong in invoice generation!", extra: {invoice: invoice})
+    end
+  end
+
+  def upload_document(document)
+    res = Faraday.get(document.download_url)
+    if res.status == 200
+      key = "invoices/#{invoice.company.name}/#{invoice.year}-#{invoice.month}.pdf"
+      tempfile = Tempfile.new(key, binmode: true)
+      tempfile.write(res.body)
+      tempfile.close
+      obj = Aws::S3::Object.new(bucket_name: ENV["AWS_S3_BUCKET"], key: key)
+      obj.upload_file(tempfile.path)
+      invoice.update(key: key)
+    else
+      Sentry.capture_message("Could not download invoice from pdfmonkey!", extra: {invoice: invoice})
+    end
+  end
+
+  def pdf_monkey_data
+    {
       billing_address: invoice.company.address.inline,
       vat_number: invoice.company.vat_number,
       client_name: invoice.company.name,
@@ -18,28 +53,7 @@ class GenerateInvoicePdfJob < ApplicationJob
       deposit: invoice.payments.sum(:deposit) / 100.0,
       lineItems: line_items(invoice.payments)
     }
-
-    template = invoice.company.vat_number&.starts_with?("IE") ? VAT_TEMPLATE_ID : TEMPLATE_ID
-    document = Pdfmonkey::Document.generate!(template, data)
-    if document.status == "success"
-      res = Faraday.get(document.download_url)
-      if res.status == 200
-        key = "invoices/#{invoice.company.name}/#{invoice.year}-#{invoice.month}.pdf"
-        tempfile = Tempfile.new(key, binmode: true)
-        tempfile.write(res.body)
-        tempfile.close
-        obj = Aws::S3::Object.new(bucket_name: ENV["AWS_S3_BUCKET"], key: key)
-        obj.upload_file(tempfile.path)
-        invoice.update(key: key)
-      else
-        Sentry.capture_message("Could not download invoice from pdfmonkey!", extra: {invoice: invoice})
-      end
-    else
-      Sentry.capture_message("Something went wrong in invoice generation!", extra: {invoice: invoice})
-    end
   end
-
-  private
 
   def line_items(payments)
     payments.flat_map do |payment|
