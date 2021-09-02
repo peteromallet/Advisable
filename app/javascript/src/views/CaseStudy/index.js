9ae02b31de9df09ff584cefb3f6fa222fcb81e03
Thomas Cullen <thomascullen92@gmail.com>
2021-09-02 09:47:51 +0100

    Add public view for case studies

---
+++ app/javascript/src/views/CaseStudy/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Article from "../CaseStudyRecommendations/views/Article";
+
+export default function CaseStudyView() {
+  return <Article showActionBar={false} />;
+}
