66448a82fc28600ad5a8896c39c2d686746b9c22
Thomas Cullen <thomascullen92@gmail.com>
2021-09-02 11:11:37 +0100

    Add support for sending attachments from working view

--- app/javascript/src/components/TalkModal/SendMessage.js
+++ app/javascript/src/components/TalkModal/SendMessage.js
@@ -1,11 +1,21 @@
 import React, { useState, useMemo } from "react";
 import commaSeparated from "src/utilities/commaSeparated";
-import { Modal, Textarea, Heading, Button } from "@advisable/donut";
+import { Box, Modal, Textarea, Heading, Stack, Button } from "@advisable/donut";
 import { useCreateConversation } from "./queries";
+import useAttachments from "src/views/NewMessages/hooks/useAttachments";
+import AddAttachmentsButton from "src/views/NewMessages/components/AddAttachmentsButton";
+import Attachment from "src/views/NewMessages/components/Attachment";
 
 export default function SendMessage({ dialog, participants }) {
   const [content, setContent] = useState("");
   const [sendMessage, { loading }] = useCreateConversation();
+  const {
+    signedIds,
+    attachments,
+    addAttachments,
+    completeUpload,
+    removeAttachment,
+  } = useAttachments();
 
   const trimmedContent = useMemo(() => {
     return content.trim().replace(/^\s+|\s+$/g, "");
@@ -18,6 +28,7 @@ export default function SendMessage({ dialog, participants }) {
       variables: {
         input: {
           content: trimmedContent,
+          attachments: signedIds,
           participants: participants.map((p) => p.account.id),
         },
       },
@@ -29,6 +40,10 @@ export default function SendMessage({ dialog, participants }) {
   const handleChange = (e) => {
     setContent(e.target.value);
   };
+
+  const hasAttachments = useMemo(() => {
+    return attachments.length > 0;
+  }, [attachments]);
 
   const hasContent = useMemo(() => {
     return trimmedContent.length > 0;
@@ -46,11 +61,33 @@ export default function SendMessage({ dialog, participants }) {
           autoFocus
           minRows={8}
           name="content"
-          marginBottom={6}
+          marginBottom={4}
           placeholder="Send a message..."
           onChange={handleChange}
         />
-        <Button loading={loading} disabled={!hasContent}>
+
+        {hasAttachments && (
+          <Stack pb={4} divider="neutral100">
+            {attachments.map((a) => (
+              <Attachment
+                key={a.id}
+                attachment={a}
+                completeUpload={completeUpload}
+                onRemove={() => removeAttachment(a.id)}
+              />
+            ))}
+          </Stack>
+        )}
+        <Box mb={8}>
+          <AddAttachmentsButton
+            onSelect={(e) => addAttachments(e.target.files)}
+          />
+        </Box>
+        <Button
+          width="100%"
+          loading={loading}
+          disabled={!(hasContent || hasAttachments)}
+        >
           Send Message
         </Button>
       </form>
