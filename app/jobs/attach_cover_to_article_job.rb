e25516074aef5a063c4ff5851f4d7c4119291412
Miha Rekar <miha.rekar@advisable.com>
2021-09-13 13:14:52 +0200

    Add trivial spec. Don't know how to test randomness

--- app/jobs/attach_cover_to_article_job.rb
+++ app/jobs/attach_cover_to_article_job.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "open-uri"
+
 class AttachCoverToArticleJob < ApplicationJob
   BUCKET_NAME = "case-study-covers"
 
@@ -13,7 +15,6 @@ class AttachCoverToArticleJob < ApplicationJob
     objects = client.list_objects(bucket: BUCKET_NAME)
     random_object = objects.contents.sample
     obj = Aws::S3::Object.new(bucket_name: BUCKET_NAME, key: random_object.key)
-
     uri = URI.parse(obj.presigned_url(:get))
     article.cover_photo.attach(io: uri.open, filename: random_object.key)
     article.save!
