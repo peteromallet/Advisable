ffd119ed33eaf486f30120e0a4992d7a0fb51853
guandjoy <guandjoy@gmail.com>
2021-09-16 15:09:21 +0300

    Update content of CaseStudy article

--- app/javascript/src/views/FreelancerProfileNew/components/Article.js
+++ app/javascript/src/views/FreelancerProfileNew/components/Article.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Box, Text } from "@advisable/donut";
 import CaseStudyContent from "src/components/CaseStudyContent";
+import AdvisableComment from "src/components/AdvisableComment";
+import CaseStudyCard from "./CaseStudyCard";
+import CaseStudyResultsRow from "src/components/CaseStudyResultsRow";
 import { useCaseStudy } from "../queries";
 import { useParams } from "react-router";
 import Loading from "src/components/Loading";
@@ -13,16 +16,22 @@ export default function Article() {
 
   return (
     <Box maxWidth="700px" position="relative">
+      <Box marginBottom={12}>
+        <CaseStudyCard caseStudy={caseStudy} />
+      </Box>
+      <Box marginBottom={12}>
+        <CaseStudyResultsRow caseStudy={caseStudy} />
+      </Box>
       <Text fontSize="2xl" lineHeight="28px" fontWeight={450}>
         {caseStudy.subtitle}
       </Text>
       {caseStudy.comment ? (
         <Box marginTop={12}>
-          {/* <AdvisableComment>{caseStudy.comment}</AdvisableComment> */}
+          <AdvisableComment>{caseStudy.comment}</AdvisableComment>
         </Box>
       ) : null}
       <Box height="1px" bg="neutral100" marginY={12} />
-      {/* <CaseStudyContent caseStudy={caseStudy} /> */}
+      <CaseStudyContent caseStudy={caseStudy} />
     </Box>
   );
 }
