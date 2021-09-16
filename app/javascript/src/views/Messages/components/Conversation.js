2e87f76642edf62ee354be5eb29db6f925a15684
Thomas Cullen <thomascullen92@gmail.com>
2021-09-16 09:32:40 +0100

    Add error boundaries to messages to prevent full page crash

--- app/javascript/src/views/Messages/components/Conversation.js
+++ app/javascript/src/views/Messages/components/Conversation.js
@@ -1,8 +1,25 @@
 import React from "react";
+import * as Sentry from "@sentry/react";
 import { Box } from "@advisable/donut";
 import { useParams } from "react-router-dom";
 import ConversationMessages from "./ConversationMessages";
 import ConversationHeader from "./ConversationHeader";
+
+function ConversationError() {
+  return (
+    <Box
+      my={8}
+      mx="auto"
+      padding={4}
+      width="400px"
+      bg="neutral100"
+      textAlign="center"
+      borderRadius="12px"
+    >
+      Failed to load conversation, please try again.
+    </Box>
+  );
+}
 
 export default function Conversation({ conversations, currentAccount }) {
   const { id } = useParams();
@@ -10,13 +27,21 @@ export default function Conversation({ conversations, currentAccount }) {
 
   return (
     <Box height="100%" display="flex" flexDirection="column">
-      <ConversationHeader conversation={conversation} />
-      <Box height="100%" minHeight="0" width="100%" flexGrow={1} flexShrink={1}>
-        <ConversationMessages
-          conversation={conversation}
-          currentAccount={currentAccount}
-        />
-      </Box>
+      <Sentry.ErrorBoundary fallback={ConversationError}>
+        <ConversationHeader conversation={conversation} />
+        <Box
+          height="100%"
+          minHeight="0"
+          width="100%"
+          flexGrow={1}
+          flexShrink={1}
+        >
+          <ConversationMessages
+            conversation={conversation}
+            currentAccount={currentAccount}
+          />
+        </Box>
+      </Sentry.ErrorBoundary>
     </Box>
   );
 }
