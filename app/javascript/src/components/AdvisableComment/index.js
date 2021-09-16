aa244ba9508d7c55a556282fab5ad74a46f7d6f5
guandjoy <guandjoy@gmail.com>
2021-09-16 15:07:44 +0300

    Move AdvisableComment to general src/components folder

--- app/javascript/src/components/AdvisableComment/index.js
+++ app/javascript/src/components/AdvisableComment/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Box, Text } from "@advisable/donut";
+import css from "@styled-system/css";
 
 function QuoteIcon() {
   return (
@@ -22,4 +24,33 @@ function QuoteIcon() {
   );
 }
 
-export default QuoteIcon;
+export default function AdvisableComment({ children }) {
+  return (
+    <Box position="relative" paddingRight={5}>
+      <Text fontWeight={450} mb={1.5} color="neutral700" fontSize="sm">
+        Comment from The Advisable Team
+      </Text>
+      <Box
+        css={`
+          transform: rotate(-0.4deg);
+        `}
+      >
+        <Text
+          fontSize="xl"
+          fontWeight={450}
+          lineHeight="32px"
+          display="inline"
+          paddingX={1}
+          css={css({
+            background: "#FFEDC9",
+          })}
+        >
+          {children}
+        </Text>
+      </Box>
+      <Box position="absolute" right="0" top={1}>
+        <QuoteIcon />
+      </Box>
+    </Box>
+  );
+}
