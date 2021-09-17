e9c8b6c74c1a5f064246ecd8395861f9e8ab6c77
Miha Rekar <miha.rekar@advisable.com>
2021-09-17 14:07:39 +0200

    Add methods for similar skills and for removing aliased skills

--- app/models/skill_category.rb
+++ app/models/skill_category.rb
@@ -5,6 +5,19 @@ class SkillCategory < ApplicationRecord
   has_many :skills, through: :skill_category_skills
 
   validates :name, presence: true
+
+  def skills_with_similar
+    SkillSimilarity.similar_to(skills)
+  end
+
+  def skills_without_aliases
+    non_aliased_skills = []
+    skills.each do |skill|
+      aliases = SkillSimilarity.similar_to(skill, 100).pluck(:id)
+      non_aliased_skills << skill.id if non_aliased_skills.intersection(aliases).none?
+    end
+    Skill.where(id: non_aliased_skills)
+  end
 end
 
 # == Schema Information
