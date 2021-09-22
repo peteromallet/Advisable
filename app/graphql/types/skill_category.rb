7db235d14c5e2ae8d39bb77298b43cbf53ca1f35
Miha Rekar <miha.rekar@advisable.com>
2021-09-22 14:47:44 +0200

    Introduce SkillCategory type

---
+++ app/graphql/types/skill_category.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class SkillCategory < Types::BaseType
+    description "A type for SkillCategory model"
+
+    field :slug, String, null: false
+    field :name, String, null: false
+
+    field :articles, Types::CaseStudy::Article.connection_type, null: false
+    def articles
+      similar_skills = object.skills_with_similar
+      cs_skills = CaseStudy::Skill.where(skills: similar_skills)
+      CaseStudy::Article.where(skills: cs_skills).by_score
+    end
+  end
+end
