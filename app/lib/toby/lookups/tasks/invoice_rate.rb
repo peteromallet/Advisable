beab234b9fcbb64c31aad8ea522558e2a42213d8
Miha Rekar <miha.rekar@advisable.com>
2021-09-23 12:53:55 +0200

    Add invoice rate to Task toby

---
+++ app/lib/toby/lookups/tasks/invoice_rate.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Toby
+  module Lookups
+    module Tasks
+      class InvoiceRate < Attributes::String
+        def self.lookup?
+          true
+        end
+
+        def readonly
+          true
+        end
+
+        def lazy_read_class
+          Toby::Lazy::Single
+        end
+
+        def via
+          :application_id
+        end
+
+        def lazy_model
+          Application
+        end
+
+        def lazy_read(application)
+          application&.invoice_rate
+        end
+      end
+    end
+  end
+end
