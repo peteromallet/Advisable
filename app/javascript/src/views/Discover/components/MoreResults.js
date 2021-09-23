9d3e10a6e255c6586ae828fd2f746d0ae5cad6e5
Thomas Cullen <thomascullen92@gmail.com>
2021-09-23 14:28:18 +0100

    Animate stack of recommendations on shortlist

--- app/javascript/src/views/Discover/components/MoreResults.js
+++ app/javascript/src/views/Discover/components/MoreResults.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { Box, Text } from "@advisable/donut";
 import illustration from "src/illustrations/zest/search.svg";
 
 export default function MoreResults() {
   return (
-    <Box maxWidth="520px" px={8} paddingBottom={8} mx="auto" textAlign="center">
+    <Box
+      as={motion.div}
+      layoutId="moreResults"
+      maxWidth="520px"
+      px={8}
+      paddingBottom={8}
+      mx="auto"
+      textAlign="center"
+    >
       <img src={illustration} width="164px" />
       <Text
         fontSize="20px"
