3efd7b9104ffa3b69a635c6d587df5fd27c74d8f
Thomas Cullen <thomascullen92@gmail.com>
2021-09-24 12:42:43 +0100

    Add new route for reading explorer articles

--- app/javascript/src/views/Discover/index.js
+++ app/javascript/src/views/Discover/index.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, useBackground } from "@advisable/donut";
-import { Switch } from "react-router";
+import { Switch, useLocation } from "react-router";
 import Route from "src/components/Route";
 import Shortlist from "./views/Shortlist";
 import Shortlists from "./views/Shortlists";
+import ShortlistArticle from "./views/ShortlistArticle";
 
 export default function Discover() {
   useBackground("white");
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <Box marginY={12} marginX={8}>
       <Box maxWidth="920px" mx="auto">
         <Switch>
+          <Route path="/explore/:id/:articleId" component={ShortlistArticle} />
           <Route path="/explore/:id" component={Shortlist} />
           <Route path="/explore" component={Shortlists} />
         </Switch>
---
+++ app/javascript/src/views/Discover/views/ShortlistArticle.js
@@ -0,0 +1,48 @@
+import React from "react";
+import truncate from "lodash/truncate";
+import { Box, Text } from "@advisable/donut";
+import Article from "src/components/Article";
+import { useArticle } from "../queries";
+import RecommendationAvatar from "../components/RecommendationAvatar";
+import BackButton from "src/components/BackButton";
+import { useParams } from "react-router";
+import ConnectButton from "src/components/ConnectButton";
+
+export default function ShortlistArticle() {
+  const { id } = useParams();
+  const { data, loading } = useArticle();
+
+  if (loading) return <>loading</>;
+
+  return (
+    <>
+      <Box display="flex">
+        <Box width="220px" flexShrink={0}>
+          <Box position="sticky" top="108px">
+            <BackButton to={`/explore/${id}`} marginBottom={4} />
+            <RecommendationAvatar
+              size="sm"
+              specialist={data.caseStudy.specialist}
+            />
+            <Text
+              marginTop={5}
+              marginBottom={2}
+              fontSize="3xl"
+              fontWeight={600}
+              letterSpacing="-0.032em"
+            >
+              {data.caseStudy.specialist.name}
+            </Text>
+            <Text lineHeight="20px" color="neutral800" marginBottom={6}>
+              {truncate(data.caseStudy.specialist.bio, { length: 110 })}
+            </Text>
+            <ConnectButton specialist={data.caseStudy.specialist} />
+          </Box>
+        </Box>
+        <Box paddingLeft={10}>
+          <Article article={data.caseStudy} />
+        </Box>
+      </Box>
+    </>
+  );
+}
