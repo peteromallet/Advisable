084f675b5b25886163e9e081959eb6a78745dbe4
guandjoy <guandjoy@gmail.com>
2021-09-28 11:42:33 +0300

    Split social profiles and about me forms

--- app/javascript/src/views/FreelancerProfileNew/components/EditInfo/EditInfoModal.js
+++ app/javascript/src/views/FreelancerProfileNew/components/EditInfo/EditInfoModal.js
@@ -1,137 +1,19 @@
 import React from "react";
-import { object, string } from "yup";
-import { Form, Formik } from "formik";
-// Components
-import {
-  Modal,
-  Box,
-  Text,
-  Textarea,
-  Select,
-  useBreakpoint,
-} from "@advisable/donut";
-import { useNotifications } from "src/components/Notifications";
-import FormField from "src/components/FormField";
-import SubmitButton from "src/components/SubmitButton";
-// Queries
-import { useUpdateProfile, useCountries } from "../../queries";
-
-const validationSchema = object().shape({
-  city: string(),
-  country: string(),
-  bio: string().max(160, "Must be not more than 160 characters"),
-  linkedin: string().url(),
-  website: string().url(),
-});
+import { Modal, Tabs } from "@advisable/donut";
+import SocialProfilesForm from "./SocialProfilesForm";
+import AboutMeForm from "./AboutMeForm";
 
 function EditInfoModal({ modal, specialist }) {
-  const notifications = useNotifications();
-  const [mutate] = useUpdateProfile();
-  const isWidescreen = useBreakpoint("sUp");
-  const initialValues = {
-    city: specialist.city || "",
-    country: specialist.country?.id || "",
-    bio: specialist.bio || "",
-    linkedin: specialist.linkedin || "",
-    website: specialist.website || "",
-  };
-  const handleSubmit = (values) => {
-    const mutateValues = { ...values, linkedin: values.linkedin || null };
-    const optimisticResponse = {
-      __typename: "Mutation",
-      updateProfile: {
-        __typename: "UpdateProfilePayload",
-        specialist: {
-          __typename: "Specialist",
-          ...specialist,
-          ...mutateValues,
-          country: {
-            __typename: "Country",
-            id: mutateValues.country,
-          },
-        },
-      },
-    };
-    mutate({
-      variables: { input: { ...mutateValues } },
-      optimisticResponse,
-    });
-
-    notifications.notify("Your profile has been updated");
-    modal.hide();
-  };
-  const countriesQuery = useCountries();
-
   return (
     <Modal modal={modal} p="xxl" label="Edit profile info" width={640}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
-        <Form>
-          <Text
-            as="h2"
-            fontSize="xxxl"
-            fontWeight="medium"
-            color="neutral900"
-            mb="l"
-          >
-            Edit profile info
-          </Text>
-          <Box display={isWidescreen ? "flex" : null} mb="l">
-            <Box
-              mr={isWidescreen && "s"}
-              mb={!isWidescreen && "l"}
-              width="100%"
-            >
-              <FormField
-                name="city"
-                label="City"
-                width="100%"
-                placeholder="e.g Berlin"
-              />
-            </Box>
-            <Box width="100%">
-              <FormField as={Select} name="country" label="Country">
-                {countriesQuery.data?.countries.map((c) => (
-                  <option key={c.value} value={c.value}>
-                    {c.label}
-                  </option>
-                ))}
-              </FormField>
-            </Box>
-          </Box>
-          <Box mb="l">
-            <FormField
-              as={Textarea}
-              name="bio"
-              minRows={5}
-              label="About me"
-              description="Add a short title to describe who you are"
-              placeholder="Add a short title to describe who you are"
-              charLimit={160}
-            />
-          </Box>
-          <Box mb="l">
-            <FormField
-              name="linkedin"
-              label="LinkedIn"
-              placeholder="https://linkedin.com/in/your-name"
-              error={null}
-            />
-          </Box>
-          <Box mb="xl">
-            <FormField
-              name="website"
-              label="Website"
-              placeholder="https://your-website.com"
-              error={null}
-            />
-          </Box>
-          <SubmitButton>Update</SubmitButton>
-        </Form>
-      </Formik>
+      <Tabs label="settings">
+        <Tabs.Tab title="About me">
+          <AboutMeForm specialist={specialist} modal={modal} />
+        </Tabs.Tab>
+        <Tabs.Tab title="Social profiles">
+          <SocialProfilesForm specialist={specialist} modal={modal} />
+        </Tabs.Tab>
+      </Tabs>
     </Modal>
   );
 }
---
+++ app/javascript/src/views/FreelancerProfileNew/components/EditInfo/SocialProfilesForm.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { object, string } from "yup";
+import { Form, Formik } from "formik";
+import { Box } from "@advisable/donut";
+import FormField from "src/components/FormField";
+import SubmitButton from "src/components/SubmitButton";
+import { useNotifications } from "src/components/Notifications";
+import { useUpdateProfile } from "../../queries";
+
+const validationSchema = object().shape({
+  linkedin: string().url(),
+  twitter: string().url(),
+  instagram: string().url(),
+  medium: string().url(),
+  website: string().url(),
+});
+
+export default function SocialProfilesForm({ specialist, modal }) {
+  const [mutate] = useUpdateProfile();
+  const notifications = useNotifications();
+  const initialValues = {
+    linkedin: specialist.linkedin || "",
+    twitter: specialist.twitter || "",
+    instagram: specialist.instagram || "",
+    medium: specialist.medium || "",
+    website: specialist.website || "",
+  };
+
+  const handleSubmit = (values) => {
+    const optimisticResponse = {
+      __typename: "Mutation",
+      updateProfile: {
+        __typename: "UpdateProfilePayload",
+        specialist: {
+          __typename: "Specialist",
+          ...specialist,
+          ...values,
+        },
+      },
+    };
+
+    mutate({
+      variables: { input: { ...values } },
+      optimisticResponse,
+    });
+
+    notifications.notify("Your profile has been updated");
+    modal.hide();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <Box mb="l" mt={5}>
+          <FormField
+            name="linkedin"
+            label="LinkedIn"
+            placeholder="https://linkedin.com/in/your-name"
+            error={null}
+          />
+        </Box>
+        <Box mb="l">
+          <FormField
+            name="twitter"
+            label="Twitter"
+            placeholder="https://twitter.com/your-profile"
+            error={null}
+          />
+        </Box>
+        <Box mb="l">
+          <FormField
+            name="instagram"
+            label="Instagram"
+            placeholder="https://instagram.com/your-profile"
+            error={null}
+          />
+        </Box>
+        <Box mb="l">
+          <FormField
+            name="medium"
+            label="Medium"
+            placeholder="https://medium.com/@your-profile"
+            error={null}
+          />
+        </Box>
+        <Box mb="xl">
+          <FormField
+            name="website"
+            label="Website"
+            placeholder="https://your-website.com"
+            error={null}
+          />
+        </Box>
+        <SubmitButton>Update</SubmitButton>
+      </Form>
+    </Formik>
+  );
+}
