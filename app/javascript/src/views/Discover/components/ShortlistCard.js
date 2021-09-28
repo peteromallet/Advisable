e9e787b7018e50205fc7e574ae402012e4b1b80d
Thomas Cullen <thomascullen92@gmail.com>
2021-09-28 12:20:29 +0100

    Improve shortlists view

--- app/javascript/src/views/Discover/components/ShortlistCard.js
+++ app/javascript/src/views/Discover/components/ShortlistCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import pluralize from "pluralize";
 import css from "@styled-system/css";
 import styled from "styled-components";
 import { Box, Text } from "@advisable/donut";
@@ -20,7 +21,7 @@ const StyledShortlistCard = styled.div(
   }),
 );
 
-export default function ShortlistCard({ shortlist }) {
+export default function ShortlistCard({ shortlist, company }) {
   const history = useHistory();
 
   const handleClick = () => {
@@ -31,30 +32,39 @@ export default function ShortlistCard({ shortlist }) {
 
   return (
     <StyledShortlistCard r1={0.005} r2={0.3} onClick={handleClick}>
-      <Text
-        fontSize="4xl"
-        fontWeight={600}
-        marginBottom={6}
-        letterSpacing="-0.025em"
-      >
-        {shortlist.name}
-      </Text>
       <Box display="flex" justifyContent="space-between">
-        {shortlist.results.nodes.map((result, index) => (
-          <Box key={result.id}>
-            <RecommendationAvatar
-              size="md"
-              number={index + 1}
-              src={result.specialist.avatar}
-              name={result.specialist.name}
-            />
-          </Box>
-        ))}
-        {placeholders.map((n) => (
-          <Box key={n}>
-            <RecommendationAvatar size="md" />
-          </Box>
-        ))}
+        <Box maxWidth="240px">
+          <Text
+            fontSize="4xl"
+            fontWeight={600}
+            marginBottom={2}
+            letterSpacing="-0.032em"
+          >
+            {shortlist.name}
+          </Text>
+          <Text lineHeight="20px" color="neutral700">
+            Recommendations of {shortlist.skills[0]?.skill?.name?.toLowerCase()}{" "}
+            specialists relevant for {company.industry?.name?.toLowerCase()}{" "}
+            {pluralize(company.kind?.toLowerCase())}.
+          </Text>
+        </Box>
+        <Box display="flex">
+          {shortlist.results.nodes.map((result, index) => (
+            <Box key={result.id} marginLeft={2}>
+              <RecommendationAvatar
+                size="xs"
+                number={index + 1}
+                src={result.specialist.avatar}
+                name={result.specialist.name}
+              />
+            </Box>
+          ))}
+          {placeholders.map((n) => (
+            <Box key={n} marginLeft={2}>
+              <RecommendationAvatar size="xs" />
+            </Box>
+          ))}
+        </Box>
       </Box>
     </StyledShortlistCard>
   );
