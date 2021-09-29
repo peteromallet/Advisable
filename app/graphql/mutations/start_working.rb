cddc34d0e087d10f2aebe32287545104140e7e59
Miha Rekar <miha.rekar@advisable.com>
2021-09-29 14:43:20 +0200

    Inline Applications::StartWorking service

--- app/graphql/mutations/start_working.rb
+++ app/graphql/mutations/start_working.rb
@@ -1,39 +1,5 @@
 # frozen_string_literal: true
 
-module Applications
-  class StartWorking < ApplicationService
-    attr_reader :application, :project_type, :monthly_limit, :current_account_id
-
-    def initialize(application:, project_type:, monthly_limit:, current_account_id: nil)
-      super()
-      @application = application
-      @project_type = project_type
-      @monthly_limit = monthly_limit
-      @current_account_id = current_account_id
-    end
-
-    def call
-      raise Service::Error, "INVALID_PROJECT_TYPE" unless %w[Fixed Flexible].include?(project_type)
-
-      application.status = "Working"
-      application.project_type = project_type
-      application.monthly_limit = monthly_limit if project_type == "Flexible"
-
-      success = Logidze.with_responsible(current_account_id) do
-        application.save
-      end
-
-      if success
-        application.create_previous_project if application.previous_project.blank?
-        application.sync_to_airtable
-        return application
-      end
-
-      raise Service::Error, application.errors.full_messages.first
-    end
-  end
-end
-
 module Mutations
   class StartWorking < Mutations::BaseMutation
     argument :application, ID, required: true
@@ -51,11 +17,16 @@ module Mutations
     end
 
     def resolve(**args)
+      ApiError.invalid_request("INVALID_PROJECT_TYPE") unless %w[Fixed Flexible].include?(args[:project_type])
+
       application = Application.find_by_uid_or_airtable_id!(args[:application])
-      application = Applications::StartWorking.call(application: application, project_type: args[:project_type], monthly_limit: args[:monthly_limit], current_account_id: current_account_id)
+      application.status = "Working"
+      application.project_type = args[:project_type]
+      application.monthly_limit = args[:monthly_limit] if args[:project_type] == "Flexible"
+      application.save_and_sync_with_responsible!(current_account_id)
+      application.create_previous_project if application.previous_project.blank?
+
       {application: application}
-    rescue Service::Error => e
-      ApiError.service_error(e)
     end
   end
 end
