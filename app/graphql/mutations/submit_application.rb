e4496b339a7013b75dd900885f5a65248d842377
Miha Rekar <miha.rekar@advisable.com>
2021-09-29 14:50:23 +0200

    Make the change easy

--- app/graphql/mutations/submit_application.rb
+++ app/graphql/mutations/submit_application.rb
@@ -1,5 +1,50 @@
 # frozen_string_literal: true
 
+# Handles the submission of an application
+module Applications
+  class Submit < ApplicationService
+    attr_reader :application, :current_account_id
+
+    def initialize(application, current_account_id: nil)
+      super()
+      @application = application
+      @current_account_id = current_account_id
+    end
+
+    def call
+      applications_open?
+      is_submittable?
+      application.status = "Applied"
+      application.applied_at = Time.zone.now if application.applied_at.blank?
+
+      success = Logidze.with_responsible(current_account_id) do
+        application.save
+      end
+
+      application.sync_to_airtable if success
+      application
+    end
+
+    private
+
+    def applications_open?
+      return if application.project.applications_open
+
+      raise Service::Error, "projects.applicationsClosed"
+    end
+
+    def is_submittable?
+      return if application.status == "Invited To Apply"
+      # Allow people to change their mind and apply after rejceting an application
+      return if application.status == "Invitation Rejected"
+      # Allow specialists to re-apply after their applcaiton has been rejected
+      return if application.status == "Application Rejected"
+
+      raise Service::Error, "applications.cannotSubmit"
+    end
+  end
+end
+
 # Used to update an application record during the application process.
 module Mutations
   class SubmitApplication < Mutations::BaseMutation
