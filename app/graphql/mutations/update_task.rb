7f18fa6e6242c37d2fcff8f1bdfeda4d297e5c76
Miha Rekar <miha.rekar@advisable.com>
2021-09-30 09:32:56 +0200

    Inline Tasks::Update service

--- app/graphql/mutations/update_task.rb
+++ app/graphql/mutations/update_task.rb
@@ -28,67 +28,47 @@ module Mutations
     end
 
     def resolve(**args)
-      @task = Task.find_by_uid!(args[:id])
+      task = Task.find_by_uid!(args[:id])
 
-      ApiError.invalid_request("APPLICATION_STATUS_NOT_WORKING", "Application status is 'Stopped Working'") if @task.application.status == "Stopped Working"
+      ApiError.invalid_request("APPLICATION_STATUS_NOT_WORKING", "Application status is 'Stopped Working'") if task.application.status == "Stopped Working"
 
-      @task.assign_attributes(args.except(:id, :trial))
-      set_trial(args[:trial]) if args.key?(:trial)
-      changes_allowed?
+      task.assign_attributes(args.except(:id, :trial))
+      set_trial(task, args[:trial]) if args.key?(:trial)
 
-      # If the stage is "Quote Requested" and the estimate has changed then set
-      # the status to "Quote Provided".
-      if @task.estimate_changed? && ["Quote Requested"].include?(@task.stage)
-        @task.stage = "Quote Provided"
-        @task.quote_provided_at = Time.zone.now
+      policy = TaskPolicy.new(current_user, task)
+      task.changes.each do |att, _changes|
+        ApiError.invalid_request("tasks.#{att}IsLocked") unless policy.public_send("update_#{att}")
       end
 
-      # If the the name, dueDate or description was changed
-
-      # clear the estimate if the client is making the edit
-
-      # Set the stage to Not Assigned if the @task was Quote Provided
-
-      if @task.name_changed? || @task.due_date_changed? || @task.description_changed?
-        @task.estimate = nil if @task.estimate? && current_user.is_a?(::User)
-        @task.flexible_estimate = nil if @task.flexible_estimate? && current_user.is_a?(::User)
-
-        @task.stage = "Not Assigned" if @task.stage == "Quote Provided"
+      if task.estimate_changed? && ["Quote Requested"].include?(task.stage)
+        task.stage = "Quote Provided"
+        task.quote_provided_at = Time.zone.now
       end
 
-      @task.sync_to_airtable if Logidze.with_responsible(current_account_id) { @task.save }
+      if task.name_changed? || task.due_date_changed? || task.description_changed?
+        task.estimate = nil if task.estimate? && current_user.is_a?(::User)
+        task.flexible_estimate = nil if task.flexible_estimate? && current_user.is_a?(::User)
+        task.stage = "Not Assigned" if task.stage == "Quote Provided"
+      end
 
-      @task.application.reload
+      task.save_and_sync_with_responsible!(current_account_id)
 
-      {task: @task}
-    rescue Service::Error => e
-      ApiError.service_error(e)
+      {task: task}
     end
 
     private
 
-    def set_trial(trial) # rubocop:disable Naming/AccessorMethodName
-      existing = @task.application.trial_task
+    def set_trial(task, trial)
+      existing = task.application.trial_task
 
-      if existing.present? &&
-         existing.stage.in?(%w[Assigned Working Submitted Approved Paid])
-        raise Service::Error, "tasks.applicationHasActiveTrialTask"
+      ApiError.invalid_request("tasks.applicationHasActiveTrialTask") if existing && %w[Assigned Working Submitted Approved Paid].include?(existing.stage)
+
+      if trial == true && existing && existing != task
+        existing.assign_attributes(trial: false)
+        existing.save_and_sync_with_responsible!(current_account_id)
       end
 
-      if trial == true && existing && existing != @task
-        existing.update(trial: false)
-        existing.sync_to_airtable
-      end
-
-      @task.trial = trial
-    end
-
-    def changes_allowed?
-      policy = TaskPolicy.new(current_user, @task)
-
-      @task.changes.each do |att, _changes|
-        raise Service::Error, "tasks.#{att}IsLocked" unless policy.public_send("update_#{att}")
-      end
+      task.trial = trial
     end
   end
 end
