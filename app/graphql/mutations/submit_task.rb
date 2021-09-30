274bf998c8588a079d349e93e625a5565aee861b
Miha Rekar <miha.rekar@advisable.com>
2021-09-30 09:14:18 +0200

    Inline Tasks::Submit service

--- app/graphql/mutations/submit_task.rb
+++ app/graphql/mutations/submit_task.rb
@@ -16,11 +16,23 @@ module Mutations
     end
 
     def resolve(**args)
-      task = Task.find_by_uid!(args[:task])
-      task = Tasks::Submit.call(task: task, final_cost: args[:final_cost], responsible_id: current_account_id)
+      task = Task.find_by!(uid: args[:task])
+
+      ApiError.invalid_request("tasks.notSubmittable", "Application status is not 'Working'") if task.application.status != "Working"
+      ApiError.invalid_request("tasks.notSubmittable") unless allowed_stages(task).include?(task.stage)
+
+      task.assign_attributes(stage: "Submitted", final_cost: args[:final_cost], submitted_at: Time.zone.now)
+      task.save_and_sync_with_responsible!(current_account_id)
+
       {task: task}
-    rescue Service::Error => e
-      ApiError.service_error(e)
+    end
+
+    private
+
+    def allowed_stages(task)
+      stages = %w[Working]
+      stages << "Not Assigned" if task.application.project_type == "Flexible"
+      stages
     end
   end
 end
