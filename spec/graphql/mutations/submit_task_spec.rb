d9621a057165e922ec53bf421c2ef2119dc93b8f
Miha Rekar <miha.rekar@advisable.com>
2021-09-30 09:14:18 +0200

    Inline Tasks::Submit service

--- spec/graphql/mutations/submit_task_spec.rb
+++ spec/graphql/mutations/submit_task_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Mutations::SubmitTask do
-  let(:application) { create(:application, status: 'Working') }
-  let(:task) { create(:task, stage: 'Working', application: application) }
+  let(:application) { create(:application, status: "Working") }
+  let(:task) { create(:task, stage: "Working", application: application) }
 
   let(:query) do
     <<-GRAPHQL
@@ -29,67 +29,67 @@ RSpec.describe Mutations::SubmitTask do
 
   it "sets the stage to 'Submitted'" do
     response = AdvisableSchema.execute(query, context: context)
-    stage = response['data']['submitTask']['task']['stage']
-    expect(stage).to eq('Submitted')
+    stage = response["data"]["submitTask"]["task"]["stage"]
+    expect(stage).to eq("Submitted")
   end
 
   context "when the specialist doesn't have access to the project" do
     let(:context) { {current_user: create(:specialist)} }
 
-    it 'returns an error' do
+    it "returns an error" do
       response = AdvisableSchema.execute(query, context: context)
-      error = response['errors'][0]
-      expect(error['extensions']['code']).to eq('notAuthorized')
+      error = response["errors"][0]
+      expect(error["extensions"]["code"]).to eq("notAuthorized")
     end
   end
 
-  context 'when there is no user' do
+  context "when there is no user" do
     let(:context) { {current_user: nil} }
 
-    it 'returns an error' do
+    it "returns an error" do
       response = AdvisableSchema.execute(query, context: context)
-      error = response['errors'][0]
-      expect(error['extensions']['code']).to eq('notAuthorized')
+      error = response["errors"][0]
+      expect(error["extensions"]["code"]).to eq("notAuthorized")
     end
   end
 
-  context 'when the client is logged in' do
+  context "when the client is logged in" do
     let(:context) { {current_user: task.application.project.user} }
 
-    it 'returns an error' do
+    it "returns an error" do
       response = AdvisableSchema.execute(query, context: context)
-      error = response['errors'][0]
-      expect(error['extensions']['code']).to eq('notAuthorized')
+      error = response["errors"][0]
+      expect(error["extensions"]["code"]).to eq("notAuthorized")
     end
   end
 
-  context 'when the task stage is Quote Provided' do
-    let(:task) { create(:task, stage: 'Quote Provided') }
+  context "when the task stage is Quote Provided" do
+    let(:task) { create(:task, stage: "Quote Provided") }
 
-    it 'returns an error' do
+    it "returns an error" do
       response = AdvisableSchema.execute(query, context: context)
-      error = response['errors'][0]
-      expect(error['extensions']['code']).to eq('tasks.notSubmittable')
+      error = response["errors"][0]
+      expect(error["extensions"]["code"]).to eq("tasks.notSubmittable")
     end
   end
 
-  context 'when the task stage is Submitted' do
-    let(:task) { create(:task, stage: 'Submitted') }
+  context "when the task stage is Submitted" do
+    let(:task) { create(:task, stage: "Submitted") }
 
-    it 'returns an error' do
+    it "returns an error" do
       response = AdvisableSchema.execute(query, context: context)
-      error = response['errors'][0]
-      expect(error['extensions']['code']).to eq('tasks.notSubmittable')
+      error = response["errors"][0]
+      expect(error["extensions"]["code"]).to eq("tasks.notSubmittable")
     end
   end
 
   context "when the application status is not 'Working'" do
-    let(:application) { create(:application, status: 'Proposed') }
+    let(:application) { create(:application, status: "Proposed") }
 
-    it 'returns an error' do
+    it "returns an error" do
       response = AdvisableSchema.execute(query, context: context)
-      error = response['errors'][0]
-      expect(error['message']).to eq("Application status is not 'Working'")
+      error = response["errors"][0]
+      expect(error["message"]).to eq("Application status is not 'Working'")
     end
   end
 end
