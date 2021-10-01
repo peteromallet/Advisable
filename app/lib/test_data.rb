255f58893ce847d6bb0565b45205e651ca2e698b
Miha Rekar <miha.rekar@advisable.com>
2021-10-01 13:07:49 +0200

    Start reworking test data with move to yml with insert_all

--- app/lib/test_data.rb
+++ app/lib/test_data.rb
@@ -1,8 +1,117 @@
 # frozen_string_literal: true
 
-# rubocop:disable Security/Open
+require "open-uri"
 
 class TestData
+  def self.create_seed_data_v2
+    country = Country.find_or_create_by(name: "Ireland")
+    company = Company.find_or_create_by(name: "Advisable")
+    sales_person = SalesPerson.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, username: Faker::Internet.username)
+    now = Time.zone.now
+    yml = YAML.load_file("db/seeds/test_data.yml")
+
+    skills_data = yml[:skills].map do |skill|
+      skill.merge(created_at: now, updated_at: now)
+    end
+    Skill.delete_all
+    Skill.insert_all(skills_data)
+
+    industries_data = yml[:industries].map do |skill|
+      skill.merge(created_at: now, updated_at: now)
+    end
+    Industry.delete_all
+    Industry.insert_all(industries_data)
+
+    advisable_data = yml[:advisable].flat_map do |advisable|
+      [
+        {
+          account: {
+            uid: Account.generate_uid,
+            email: advisable[:email].sub("@", "+specialist@"),
+            first_name: advisable[:first_name],
+            last_name: "#{advisable[:last_name]} Specialist",
+            password_digest: "$2a$12$4COpROFiSO8HSEzpDRbwjOvjklTclASMbHz5L8FdUsmo1e/nQFCWm", # testing123
+            permissions: [],
+            confirmed_at: 1.hour.ago,
+            updated_at: now,
+            created_at: now
+          },
+          specialist: {
+            uid: Specialist.generate_uid,
+            bio: advisable[:bio],
+            application_stage: "Accepted",
+            country_id: country.id,
+            city: "Scranton",
+            updated_at: now,
+            created_at: now
+          }
+        },
+        {
+          account: {
+            uid: Account.generate_uid,
+            email: advisable[:email],
+            first_name: advisable[:first_name],
+            last_name: advisable[:last_name],
+            password_digest: "$2a$12$4COpROFiSO8HSEzpDRbwjOvjklTclASMbHz5L8FdUsmo1e/nQFCWm", # testing123
+            permissions: %w[admin team_manager editor],
+            confirmed_at: 1.hour.ago,
+            updated_at: now,
+            created_at: now
+          },
+          user: {
+            uid: User.generate_uid,
+            company_id: company.id,
+            country_id: country.id,
+            sales_person_id: sales_person.id,
+            updated_at: now,
+            created_at: now
+          }
+        }
+      ]
+    end
+
+    accounts = Account.upsert_all(advisable_data.pluck(:account), unique_by: :email)
+    accounts.each_with_index do |account, i|
+      key = i.even? ? :specialist : :user
+      advisable_data[i][key][:account_id] = account["id"]
+    end
+    specialist_data = advisable_data.pluck(:specialist).compact
+    user_data = advisable_data.pluck(:user).compact
+    specialists = Specialist.upsert_all(specialist_data, unique_by: :account_id)
+    users = User.upsert_all(user_data, unique_by: :account_id)
+
+    Specialist.where(id: specialists.pluck("id")).each_with_index do |specialist, i|
+      path = "db/seeds/assets/avatars/#{yml[:advisable][i][:avatar]}"
+      specialist.avatar.attach(io: File.open(path), filename: yml[:advisable][i][:avatar])
+    end
+
+    User.where(id: users.pluck("id")).each_with_index do |user, i|
+      path = "db/seeds/assets/avatars/#{yml[:advisable][i][:avatar]}"
+      user.avatar.attach(io: File.open(path), filename: yml[:advisable][i][:avatar])
+    end
+
+    binding.pry
+  end
+
+  def self.populate_from_airtable
+    yml = YAML.load_file("db/seeds/test_data.yml")
+
+    Skill.delete_all
+    Airtable::Skill.sync(filter: nil)
+    attrs = %i[name category profile uid active airtable_id]
+    yml[:skills] = Skill.pluck(*attrs).map { |p| attrs.zip(p).to_h }
+
+    Industry.delete_all
+    Airtable::Industry.sync(filter: nil)
+    attrs = %i[name color active uid airtable_id]
+    yml[:industries] = Industry.pluck(*attrs).map { |p| attrs.zip(p).to_h }
+
+    File.write("db/seeds/test_data.yml", yml.to_yaml)
+  end
+
+  ### 👆 NEW STUFF 👆 ###
+  ######################
+  ### 👇 OLD STUFF 👇 ###
   def self.stock_image
     n = %w[01 02 03 04 05 06 07 08 09 10 11].sample
     "https://advisable-test-assets.s3.eu-central-1.amazonaws.com/stock-photos/#{n}.jpg"
@@ -333,4 +442,3 @@ class TestData
     project.update_application_counts
   end
 end
-# rubocop:enable Security/Open
