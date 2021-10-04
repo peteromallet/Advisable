f244460052bdcc29c5ca53a394eb60a9fa11a56a
Miha Rekar <miha.rekar@advisable.com>
2021-10-04 12:16:38 +0200

    Allow for some time tolerance

--- spec/models/unresponsiveness_report_spec.rb
+++ spec/models/unresponsiveness_report_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe UnresponsivenessReport do
       create(:message, conversation: conversation, created_at: 1.day.ago, author: specialist)
       create(:message, conversation: conversation, created_at: 2.days.ago, author: user)
 
-      expect(report.last_message_by_specialist_at).to eq(one_hour)
+      expect(report.last_message_by_specialist_at).to be_within(1.second).of(one_hour)
     end
 
     it "returns nil when no messages by specialist" do
@@ -40,7 +40,7 @@ RSpec.describe UnresponsivenessReport do
       create(:message, conversation: conversation, created_at: 1.day.ago, author: user)
       create(:message, conversation: conversation, created_at: 2.days.ago, author: specialist)
 
-      expect(report.last_message_by_client_at).to eq(one_hour)
+      expect(report.last_message_by_client_at).to be_within(1.second).of(one_hour)
     end
 
     it "returns nil when no messages by user" do
