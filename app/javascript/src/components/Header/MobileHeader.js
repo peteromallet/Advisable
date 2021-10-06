d933e1014c0abe5ba487769e3d853640cf1c14ba
Thomas Cullen <thomascullen92@gmail.com>
2021-10-06 14:53:27 +0100

    Fix header positioning

--- app/javascript/src/components/Header/MobileHeader.js
+++ app/javascript/src/components/Header/MobileHeader.js
@@ -1,7 +1,12 @@
 // Renders the primary header for the app
 import React from "react";
 import { X } from "@styled-icons/feather/X";
-import { StyledHeader, Hamburger, CloseNav } from "./styles";
+import {
+  StyledHeader,
+  StyledHeaderSpacer,
+  Hamburger,
+  CloseNav,
+} from "./styles";
 import { Box } from "@advisable/donut";
 import useLogoURL from "../ApplicationProvider/useLogoURL";
 import LogoMark from "src/components/LogoMark";
@@ -19,38 +24,41 @@ export default function MobileHeader() {
   };
 
   return (
-    <StyledHeader justifyContent="space-between">
-      <Box>
-        <Hamburger onClick={() => setNavOpen(true)}>
-          <div />
-          <div />
-          <div />
-        </Hamburger>
-        {navOpen && (
-          <Box
-            top="0"
-            left="0"
-            right="0"
-            bottom="0"
-            bg="white"
-            zIndex={2}
-            padding={5}
-            position="fixed"
-          >
-            <CloseNav onClick={() => setNavOpen(false)}>
-              <X />
-            </CloseNav>
+    <>
+      <StyledHeader justifyContent="space-between">
+        <Box>
+          <Hamburger onClick={() => setNavOpen(true)}>
+            <div />
+            <div />
+            <div />
+          </Hamburger>
+          {navOpen && (
+            <Box
+              top="0"
+              left="0"
+              right="0"
+              bottom="0"
+              bg="white"
+              zIndex={2}
+              padding={5}
+              position="fixed"
+            >
+              <CloseNav onClick={() => setNavOpen(false)}>
+                <X />
+              </CloseNav>
 
-            <Navigation isMobile closeNav={handleCloseNav} />
-          </Box>
-        )}
-      </Box>
-      <Box>
-        <a href={logoURL}>
-          <LogoMark size={24} />
-        </a>
-      </Box>
-      <Box>{viewer?.isSpecialist && <Notifications />}</Box>
-    </StyledHeader>
+              <Navigation isMobile closeNav={handleCloseNav} />
+            </Box>
+          )}
+        </Box>
+        <Box>
+          <a href={logoURL}>
+            <LogoMark size={24} />
+          </a>
+        </Box>
+        <Box>{viewer?.isSpecialist && <Notifications />}</Box>
+      </StyledHeader>
+      <StyledHeaderSpacer />
+    </>
   );
 }
