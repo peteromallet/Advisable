c37712fcb9eb888d3edbc1bbf21bf2ca9587b9e5
guandjoy <guandjoy@gmail.com>
2021-10-06 16:22:04 +0300

    Add useImage hook in testimonial illustration

--- app/javascript/src/views/TestimonialFlow/components/MockTestimonials.js
+++ app/javascript/src/views/TestimonialFlow/components/MockTestimonials.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { useImage } from "react-image";
 import { Box } from "@advisable/donut";
 import illustration from "src/illustrations/testimonials.png";
 
 export default function MockTestimonials() {
+  const { src } = useImage({ srcList: illustration });
+
   return (
     <Box
       as="img"
       width="100%"
-      src={illustration}
+      src={src}
       alt="Simplified example of a testimonial from profile"
       css={`
         user-select: none;
