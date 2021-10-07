6091412967cd3174731f8b846a3bc57d7f1b3a58
guandjoy <guandjoy@gmail.com>
2021-10-07 21:57:13 +0300

    Add client version of action button in CS section

--- app/javascript/src/views/FreelancerProfileNew/components/CaseStudies.js
+++ app/javascript/src/views/FreelancerProfileNew/components/CaseStudies.js
@@ -1,10 +1,14 @@
 import React from "react";
+import useViewer from "src/hooks/useViewer";
 import { Link, Box, Stack } from "@advisable/donut";
 import SectionTitle from "./SectionTitle";
 import CaseStudyCard from "./CaseStudyCard";
 import SectionActionButton from "./SectionActionButton";
 
 export default function CaseStudies({ caseStudies, specialist }) {
+  const viewer = useViewer();
+  const isOwner = viewer?.id === specialist.id;
+
   const cards = caseStudies.map((cs) => (
     <CaseStudyCard caseStudy={cs} key={cs.id} />
   ));
@@ -15,14 +19,16 @@ export default function CaseStudies({ caseStudies, specialist }) {
       <Stack spacing={6} mt={3}>
         {cards}
       </Stack>
-      <SectionActionButton
-        as={Link.External}
-        href={`https://csi.advisable.com/freelancer/onboarding?specialist_id=${specialist.id}&contact_name=${specialist.firstName}`}
-        target="_blank"
-        mt={6}
-      >
-        Add a Case Study
-      </SectionActionButton>
+      {isOwner ? (
+        <SectionActionButton
+          as={Link.External}
+          href={`https://csi.advisable.com/freelancer/onboarding?specialist_id=${specialist.id}&contact_name=${specialist.firstName}`}
+          target="_blank"
+          mt={6}
+        >
+          Add a Case Study
+        </SectionActionButton>
+      ) : null}
     </Box>
   );
 }
