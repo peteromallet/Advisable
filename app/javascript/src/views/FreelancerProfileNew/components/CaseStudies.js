fb2d58ff6157a56256d7130b983efc387288534a
guandjoy <guandjoy@gmail.com>
2021-10-11 16:35:40 +0300

    Add headers for case studies and testimonials empty states

--- app/javascript/src/views/FreelancerProfileNew/components/CaseStudies.js
+++ app/javascript/src/views/FreelancerProfileNew/components/CaseStudies.js
@@ -4,22 +4,29 @@ import { Link, Box, Stack } from "@advisable/donut";
 import SectionTitle from "./SectionTitle";
 import CaseStudyCard from "./CaseStudyCard";
 import SectionActionButton from "./SectionActionButton";
+import CaseStudiesEmptyState from "./CaseStudiesEmptyState";
 
 export default function CaseStudies({ caseStudies, specialist }) {
   const viewer = useViewer();
-  const isOwner = viewer?.id === specialist.id;
 
   const cards = caseStudies.map((cs) => (
     <CaseStudyCard caseStudy={cs} key={cs.id} />
   ));
 
+  const isOwner = viewer?.id === specialist.id;
+  const isEmpty = cards.length === 0;
+
+  if (isEmpty && !isOwner) return null;
+
   return (
-    <Box>
+    <Box mb={11}>
       <SectionTitle>Case Studies</SectionTitle>
       <Stack spacing={6} mt={3}>
         {cards}
       </Stack>
-      {isOwner ? (
+      {isEmpty ? (
+        <CaseStudiesEmptyState specialist={specialist} />
+      ) : (
         <SectionActionButton
           as={Link.External}
           href={`https://csi.advisable.com/freelancer/onboarding?specialist_id=${specialist.id}&contact_name=${specialist.firstName}`}
@@ -28,7 +35,7 @@ export default function CaseStudies({ caseStudies, specialist }) {
         >
           Add a Case Study
         </SectionActionButton>
-      ) : null}
+      )}
     </Box>
   );
 }
--- app/javascript/src/views/FreelancerProfileNew/components/Testimonials.js
+++ app/javascript/src/views/FreelancerProfileNew/components/Testimonials.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Box, Stack, useModal, DialogDisclosure } from "@advisable/donut";
+import useViewer from "src/hooks/useViewer";
 import SectionActionButton from "./SectionActionButton";
 import SectionTitle from "./SectionTitle";
 import Testimonial from "./Testimonial";
 import TestimonialLinkModal from "./TestimonialLinkModal";
+import TestimonialsEmptyState from "./TestimonialsEmptyState";
 
 export default function Testimonials({ reviews }) {
   const modal = useModal();
+  const { id } = useParams();
+  const viewer = useViewer();
 
   const testimonials = reviews.map(
     (r) => !!r.comment && <Testimonial key={r.id} review={r} />,
   );
+
+  const isEmpty = testimonials.length === 0;
+  const isOwner = viewer?.id === id;
+
+  if (isEmpty && !isOwner) return null;
 
   return (
     <Box>
@@ -18,9 +28,13 @@ export default function Testimonials({ reviews }) {
       <Stack spacing={5} mt={3}>
         {testimonials}
       </Stack>
-      <DialogDisclosure as={SectionActionButton} mt={6} {...modal}>
-        Request a Testimonial
-      </DialogDisclosure>
+      {isEmpty ? (
+        <TestimonialsEmptyState modal={modal} />
+      ) : (
+        <DialogDisclosure as={SectionActionButton} mt={6} {...modal}>
+          Request a Testimonial
+        </DialogDisclosure>
+      )}
       <TestimonialLinkModal modal={modal} />
     </Box>
   );
