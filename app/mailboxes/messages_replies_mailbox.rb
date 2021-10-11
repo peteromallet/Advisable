b2cd430e5208d80b7d7995887e71dedf411f970e
Miha Rekar <miha.rekar@advisable.com>
2021-10-11 15:29:32 +0200

    Handle conversations with emails that include +

--- app/mailboxes/messages_replies_mailbox.rb
+++ app/mailboxes/messages_replies_mailbox.rb
@@ -4,17 +4,25 @@ class MessagesRepliesMailbox < ApplicationMailbox
   def process
     uid = mail.to.first[/(.*)@/, 1] # https://rubular.com/r/8PIP36d5h6D785
     conversation = Conversation.find_by!(uid: uid)
-    author = Account.find_by(email: mail.from.first)
 
-    if conversation.participants.pluck(:account_id).include?(author.id)
+    author = find_author(conversation)
+    if author
       content = EmailReplyParser.parse_reply(mail_body(mail))
-      message = conversation.messages.create!(content: content, author: author)
+      message = conversation.messages.create!(content: content, author: author.account)
       message.after_create_actions
     else
       Sentry.capture_message(
-        "#{author.email} tried to reply to a conversation that they don't participate in.",
-        extra: {author: author, conversation: conversation, mail: mail}
+        "#{mail.from.first} tried to reply to a conversation that they don't participate in.",
+        extra: {conversation: conversation, mail: mail}
       )
     end
+  end
+
+  private
+
+  def find_author(conversation)
+    participants = conversation.participants.includes(:account)
+    participants.find { |cp| cp.account.email == mail.from.first } ||
+      participants.find { |cp| cp.account.email.sub(/\+.*@/, "@") == mail.from.first.sub(/\+.*@/, "@") }
   end
 end
