ce1edc66909654118bf15b16e9a0d65d14834d05
Miha Rekar <miha.rekar@advisable.com>
2021-10-19 08:39:35 +0200

    Hotfix

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -8,7 +8,7 @@ class ApplicationMailbox < ActionMailbox::Base
 
   def mail_body(mail)
     if mail.parts.present?
-      part_to_use = message.text_part || message.html_part || message
+      part_to_use = mail.text_part || mail.html_part || mail
       encoding = part_to_use.content_type_parameters['charset'] if part_to_use.content_type_parameters
       body = part_to_use.body.decoded
       body.force_encoding(encoding).encode('UTF-8') if encoding
