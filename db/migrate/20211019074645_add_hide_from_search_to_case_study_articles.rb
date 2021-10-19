56971ce1daeec09ee19971ea1af125ace8016ea0
Miha Rekar <miha.rekar@advisable.com>
2021-10-19 09:47:55 +0200

    Add hide_from_search to Article

---
+++ db/migrate/20211019074645_add_hide_from_search_to_case_study_articles.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddHideFromSearchToCaseStudyArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :case_study_articles, :hide_from_search, :boolean
+  end
+end
