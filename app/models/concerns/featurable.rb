facb43e857aeed79d7d1a4c1cc9e1ef3b4038022
Miha Rekar <miha.rekar@advisable.com>
2021-10-22 09:04:39 +0200

    Copy stuff over to make Featurable act like Permissionable

--- app/models/concerns/featurable.rb
+++ app/models/concerns/featurable.rb
@@ -6,38 +6,36 @@ module Featurable
   extend ActiveSupport::Concern
 
   included do
-    store_accessor :features
+    scope :with_feature, ->(feature) { where("features ? :feature", feature: feature) }
   end
 
   def features
-    self["features"] ||= {}
+    self["features"] ||= []
   end
 
   class_methods do
-    def featurize(*flags)
-      flags.map(&:to_s).each do |flag|
-        define_method("#{flag}?") do
-          !!features[flag] # rubocop:disable Style/DoubleNegation
+    def featurize(*features)
+      features.map(&:to_s).each do |feature|
+        define_method("#{feature}?") do
+          self.features.include?(feature)
         end
 
-        define_method("#{flag}=") do |param|
-          features.merge!(flag => param)
+        define_method("toggle_#{feature}") do
+          self.features = if self.features.include?(feature)
+                            self.features - [feature]
+                          else
+                            self.features + [feature]
+                          end
         end
 
-        define_method("toggle_#{flag}") do
-          public_send("#{flag}=", !features[flag])
-        end
-
-        define_method("toggle_#{flag}!") do
-          public_send("toggle_#{flag}")
+        define_method("toggle_#{feature}!") do
+          public_send("toggle_#{feature}")
           save!
         end
-
-        scope("with_#{flag}_feature", -> { where("features @> ?", {flag => true}.to_json) })
       end
 
-      define_singleton_method(:feature_flags) do
-        flags
+      define_singleton_method(:all_features) do
+        features
       end
     end
   end
