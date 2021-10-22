5de3ef9fb01b905c2ca3052a988b9f3aa08a2cbb
Miha Rekar <miha.rekar@advisable.com>
2021-10-22 09:04:39 +0200

    Copy stuff over to make Featurable act like Permissionable

--- spec/models/concerns/featurable_spec.rb
+++ spec/models/concerns/featurable_spec.rb
@@ -11,26 +11,20 @@ end
 
 RSpec.describe Featurable do
   it "generates the expected set of methods" do
-    expect(DummyFeaturable.instance_methods(false)).to include(:admin?, :admin=, :admin=, :toggle_admin, :toggle_admin!, :guest?, :guest=, :toggle_guest, :toggle_guest!)
-    expect(DummyFeaturable.methods(false)).to include(:feature_flags)
+    expect(DummyFeaturable.instance_methods(false)).to include(:admin?, :toggle_admin, :toggle_admin!, :guest?, :toggle_guest, :toggle_guest!)
+    expect(DummyFeaturable.methods(false)).to include(:all_features)
   end
 
-  it "includes all feature flags" do
-    expect(DummyFeaturable.feature_flags).to match_array(%i[admin guest])
+  it "includes all features" do
+    expect(DummyFeaturable.all_features).to match_array(%i[admin guest])
   end
 
   describe "method functionality" do
     let(:dummy) { DummyFeaturable.new }
 
-    it "toggles groups enabled" do
+    it "toggles admin enabled" do
       expect(dummy.admin?).to eq(false)
       dummy.toggle_admin
-      expect(dummy.admin?).to eq(true)
-    end
-
-    it "sets groups enabled" do
-      expect(dummy.admin?).to eq(false)
-      dummy.admin = true
       expect(dummy.admin?).to eq(true)
     end
   end
