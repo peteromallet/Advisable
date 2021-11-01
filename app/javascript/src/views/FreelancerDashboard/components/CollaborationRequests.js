b1f0e7c8191e862ae5e9debceb03a874a87404b1
guandjoy <guandjoy@gmail.com>
2021-11-01 13:23:53 +0200

    Add Post button

--- app/javascript/src/views/FreelancerDashboard/components/CollaborationRequests.js
+++ app/javascript/src/views/FreelancerDashboard/components/CollaborationRequests.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box, Text } from "@advisable/donut";
+import { Box, Button, Text } from "@advisable/donut";
 import CollaborationRequest from "./CollaborationRequest";
+import { Pencil } from "@styled-icons/heroicons-outline/Pencil";
 
 export default function CollaborationRequests({ collaborationRequests }) {
   const requests = collaborationRequests.map((cr) => (
@@ -8,9 +9,14 @@ export default function CollaborationRequests({ collaborationRequests }) {
   ));
   return (
     <Box>
-      <Text color="neutral900" fontSize="2xl" fontWeight={450} mb={8}>
-        Collaboration requests
-      </Text>
+      <Box display="flex" alignItems="center" mb={8}>
+        <Text color="neutral900" fontSize="2xl" fontWeight={450}>
+          Collaboration requests
+        </Text>
+        <Button prefix={<Pencil />} ml="auto" size="s" variant="subtle">
+          Post
+        </Button>
+      </Box>
       <Box>{requests}</Box>
     </Box>
   );
