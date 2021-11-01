ef2b851ac6d8d01f402176a70bd2580f01b711d6
guandjoy <guandjoy@gmail.com>
2021-11-01 12:05:25 +0200

    Set up layout

--- app/javascript/src/views/FreelancerDashboard/index.js
+++ app/javascript/src/views/FreelancerDashboard/index.js
@@ -6,6 +6,7 @@ import CollaborationRequests from "./components/CollaborationRequests";
 import UpcomingEvents from "./components/UpcomingEvents";
 import LatestProjects from "./components/LatestProjects";
 import { useDashboardData } from "./queries";
+import css from "@styled-system/css";
 
 export default function FreelancerDashboard() {
   const { data, loading, error } = useDashboardData();
@@ -14,12 +15,27 @@ export default function FreelancerDashboard() {
 
   return (
     <Page width="1080px">
-      <Box paddingY={{ _: 8, m: 12 }} paddingX={{ _: 4, m: 8 }}>
-        <CollaborationRequests
-          collaborationRequests={data.collaborationRequests.nodes}
-        />
-        <LatestProjects topCaseStudies={data.topCaseStudies} />
-        <UpcomingEvents upcomingEvents={data.upcomingEvents} />
+      <Box
+        paddingY={{ _: 8, m: 12 }}
+        paddingX={{ _: 4, m: 8 }}
+        display={{ _: "block", l: "grid" }}
+        gridTemplateColumns="52% auto"
+        flexDirection="row"
+        maxWidth={{ _: "720px", l: "none" }}
+        mx="auto"
+        css={css({
+          columnGap: [null, null, null, "48px", "96px"],
+        })}
+      >
+        <Box gridColumn="1">
+          <CollaborationRequests
+            collaborationRequests={data.collaborationRequests.nodes}
+          />
+        </Box>
+        <Box gridColumn="2">
+          <LatestProjects topCaseStudies={data.topCaseStudies} />
+          <UpcomingEvents upcomingEvents={data.upcomingEvents} />
+        </Box>
       </Box>
     </Page>
   );
