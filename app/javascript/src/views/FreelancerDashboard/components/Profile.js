d21503018ea1fc1944083d81d6f750a8b1fa35d6
guandjoy <guandjoy@gmail.com>
2021-11-01 13:54:23 +0200

    Add cs and reviews counter

--- app/javascript/src/views/FreelancerDashboard/components/Profile.js
+++ app/javascript/src/views/FreelancerDashboard/components/Profile.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Box, Button, Text } from "@advisable/donut";
 import PassportAvatar from "src/components/PassportAvatar";
 import useViewer from "src/hooks/useViewer";
+import css from "@styled-system/css";
 
-export default function Hero() {
+export default function Hero({ caseStudies, reviews }) {
   const viewer = useViewer();
 
   return (
@@ -14,6 +15,14 @@ export default function Hero() {
           <Text fontSize="3xl" fontWeight={550} color="neutral900">
             {viewer.name}
           </Text>
+          <Box display="flex">
+            <Text marginRight={5}>
+              <span>{caseStudies.length}</span> case studies
+            </Text>
+            <Text>
+              <span>{reviews.length}</span> testimonials
+            </Text>
+          </Box>
           <Button variant="ghost" size="s">
             Update profile
           </Button>
