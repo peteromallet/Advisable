a161339b987600fe0714caf8eb845e04f3ba20a6
Miha Rekar <miha.rekar@advisable.com>
2021-11-02 14:51:50 +0100

    Make username be case insensitive

--- db/migrate/20211102132501_add_username_to_specialist.rb
+++ db/migrate/20211102132501_add_username_to_specialist.rb
@@ -3,7 +3,7 @@
 class AddUsernameToSpecialist < ActiveRecord::Migration[6.1]
   def change
     safety_assured do
-      add_column :specialists, :username, :string
+      add_column :specialists, :username, :citext
       add_index :specialists, :username, unique: true
     end
   end
