05169024d9343298bc5d74c5e9c6abede35421ef
Miha Rekar <miha.rekar@advisable.com>
2021-11-02 14:21:16 +0100

    Refactor uid and add valid_uid? class methods

--- app/models/concerns/uid.rb
+++ app/models/concerns/uid.rb
@@ -11,6 +11,22 @@ module Uid
     validates :uid, uniqueness: true, on: :create
     validate :valid_uid, on: :create
 
+    private
+
+    def generate_uid
+      self.uid = self.class.generate_uid
+    end
+
+    def valid_uid
+      return unless uid
+
+      prefix, uniq = uid.split("_")
+      errors.add(:base, "invalid_id_prefix") if prefix != self.class.prefix_for_uid
+      errors.add(:base, "id_too_short") if uniq.length != 15
+    end
+  end
+
+  class_methods do
     # self.uid_prefix can be used to override the prefix for the uid.
     # by default it will be the first three characters of the class.
     #
@@ -19,35 +35,21 @@ module Uid
     #   include Uid
     #   uid_prefix 'de'
     # end
-    def self.uid_prefix(prefix)
+    def uid_prefix(prefix)
       @uid_prefix = prefix
     end
 
-    def self.prefix_for_uid
+    def prefix_for_uid
       @uid_prefix || name[0..2].downcase
     end
 
-    # Generates the uid for the model.
-    # == example
-    # Deposit.generate_uid
-    # => dep_8Aymaf6idazxsWa
-    def self.generate_uid
+    def generate_uid
       "#{prefix_for_uid}_#{AlphanumericId.generate}"
     end
 
-    private
-
-    def generate_uid
-      self.uid = self.class.generate_uid
-    end
-
-    # Ensure that the UID is valid
-    def valid_uid
-      return unless uid
-
+    def valid_uid?(uid)
       prefix, uniq = uid.split("_")
-      errors.add(:base, "invalid_id_prefix") if prefix != self.class.prefix_for_uid
-      errors.add(:base, "id_too_short") if uniq.length != 15
+      prefix == prefix_for_uid && uniq.length == 15
     end
   end
 end
--- spec/support/uid.rb
+++ spec/support/uid.rb
@@ -1,24 +1,30 @@
 # frozen_string_literal: true
-RSpec.shared_examples "uid" do
+
+RSpec.shared_examples("uid") do
   let(:model) { described_class }
+  let(:factory) { described_class.table_name.singularize.to_sym }
+  let(:record) { create(factory) }
 
   it "can generate a UID" do
-    expect(model.generate_uid).to_not be_nil
+    expect(model.generate_uid).not_to(be_nil)
   end
 
   it "generates a UID before validation" do
     record = model.new
     expect(record.uid).to be_nil
     record.valid?
-    expect(record.uid).to_not be_nil
+    expect(record.uid).not_to(be_nil)
   end
 
   it "does not generate a new UID if the record has already been saved" do
-    factory = model.to_s.underscore.to_sym
-    record = FactoryBot.create(factory)
-    expect(record.uid).to_not be_nil
+    expect(record.uid).not_to(be_nil)
     before = record.uid
     record.valid?
     expect(record.uid).to eq(before)
+  end
+
+  it "can tell whether a uid is valid" do
+    expect(model).not_to be_valid_uid("asd_1234")
+    expect(model).to be_valid_uid(model.generate_uid)
   end
 end
