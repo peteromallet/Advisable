283d4f78aef27b44e81f659148b7abc906841bad
Thomas Cullen <thomascullen92@gmail.com>
2021-11-03 22:00:08 +0000

    Remove any floating Route components in favor of useRouteMatch

--- app/javascript/src/views/Settings/SpecialistSettings/index.js
+++ app/javascript/src/views/Settings/SpecialistSettings/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Redirect } from "react-router-dom";
+import { Switch, Redirect, useRouteMatch } from "react-router-dom";
 import Route from "src/components/Route";
 import { Container, useBreakpoint } from "@advisable/donut";
 import View from "src/components/View";
@@ -10,18 +10,19 @@ import PaymentSettings from "./PaymentSettings";
 import Availability from "./Availability";
 
 function SpecialistSettings({ match }) {
-  let breakpointS = useBreakpoint("sUp");
+  const breakpointS = useBreakpoint("sUp");
+  const isMobileView = useRouteMatch({ path: match.path, exact: !breakpointS });
 
   return (
     <View>
       {/* On mobile we only show the navigation menu if the URL is exactly
       /settings. On desktop we want to display it as a sidebar on all settings
       pages. We use a Route with exact prop to achieve this. */}
-      <Route path={match.path} exact={!breakpointS}>
+      {isMobileView && (
         <View.Sidebar>
           <Navigation />
         </View.Sidebar>
-      </Route>
+      )}
 
       <View.Content>
         <Container
