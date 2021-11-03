5039693ebce70d61c83fad9d4cc9c91e4e877fdd
guandjoy <guandjoy@gmail.com>
2021-11-03 16:15:44 +0200

    Add empty state for upcoming events

--- app/javascript/src/views/FreelancerDashboard/components/UpcomingEvents.js
+++ app/javascript/src/views/FreelancerDashboard/components/UpcomingEvents.js
@@ -2,6 +2,26 @@ import React from "react";
 import { Stack, Box, Text } from "@advisable/donut";
 import Event from "./Event";
 
+function EmptyState() {
+  return (
+    <Box
+      width="100%"
+      bg="neutral50"
+      display="flex"
+      border="2px solid"
+      borderColor="neutral100"
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="20px"
+      paddingY={10}
+    >
+      <Text color="neutral500" fontWeight={350}>
+        We have no scheduled events yet.
+      </Text>
+    </Box>
+  );
+}
+
 export default function UpcomingEvents({ upcomingEvents }) {
   const events = upcomingEvents.map((e) => <Event key={e.id} event={e} />);
 
@@ -16,9 +36,13 @@ export default function UpcomingEvents({ upcomingEvents }) {
       >
         Upcoming events
       </Text>
-      <Stack spacing={10} divider="neutral100">
-        {events}
-      </Stack>
+      {events.length > 0 ? (
+        <Stack spacing={10} divider="neutral100">
+          {events}
+        </Stack>
+      ) : (
+        <EmptyState />
+      )}
     </>
   );
 }
