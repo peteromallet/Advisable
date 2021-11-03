cc7bae83dc3a3dca949063cbcad18c556177fee7
guandjoy <guandjoy@gmail.com>
2021-11-03 14:04:25 +0200

    Add Stack to LatestProjects and UpcomingEvents

--- app/javascript/src/views/FreelancerDashboard/components/LatestProjects.js
+++ app/javascript/src/views/FreelancerDashboard/components/LatestProjects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text } from "@advisable/donut";
+import { Stack, Box, Text } from "@advisable/donut";
 import Project from "./Project";
 
 export default function LatestProjects({ topCaseStudies }) {
@@ -14,7 +14,9 @@ export default function LatestProjects({ topCaseStudies }) {
       <Text color="neutral900" fontSize="2xl" fontWeight={450} mb={8}>
         Latest projects
       </Text>
-      <Box>{caseStudies}</Box>
+      <Stack spacing={5} divider="neutral100">
+        {caseStudies}
+      </Stack>
     </>
   );
 }
