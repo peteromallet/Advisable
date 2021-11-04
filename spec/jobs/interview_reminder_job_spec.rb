4af12a33fd28da99eedaa9576d65567e1972cf49
Miha Rekar <miha.rekar@advisable.com>
2021-11-04 10:02:26 +0100

    Only send reminder email to scheduled calls

--- spec/jobs/interview_reminder_job_spec.rb
+++ spec/jobs/interview_reminder_job_spec.rb
@@ -3,8 +3,9 @@
 require "rails_helper"
 
 RSpec.describe InterviewReminderJob do
+  let(:status) { "Call Scheduled" }
   let(:starts_at) { 50.minutes.from_now }
-  let!(:interview) { create(:interview, starts_at: starts_at) }
+  let!(:interview) { create(:interview, starts_at: starts_at, status: status) }
 
   it "sends reminder only once" do
     described_class.perform_now
@@ -28,6 +29,17 @@ RSpec.describe InterviewReminderJob do
 
   context "when too far in the future" do
     let(:starts_at) { 65.minutes.from_now }
+
+    it "does not send reminder" do
+      described_class.perform_now
+
+      expect(ActionMailer::MailDeliveryJob).not_to have_been_enqueued.with("SpecialistMailer", "interview_reminder", "deliver_now", {args: [interview]})
+      expect(ActionMailer::MailDeliveryJob).not_to have_been_enqueued.with("UserMailer", "interview_reminder", "deliver_now", {args: [interview]})
+    end
+  end
+
+  context "when the interview isn't scheduled" do
+    let(:status) { "Call Requested" }
 
     it "does not send reminder" do
       described_class.perform_now
