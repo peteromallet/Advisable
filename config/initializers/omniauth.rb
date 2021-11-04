436fb169209ab7a6cd903e3f6439e37f39754e31
Miha Rekar <miha.rekar@advisable.com>
2021-11-04 14:56:19 +0100

    So much work. Sweat. Tears. Gone. Bye, Linkedin. You will not be missed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,7 @@
 
 require "omniauth/strategies/linkedin"
 
-Rails.application.config.middleware.use OmniAuth::Builder do
+Rails.application.config.middleware.use(OmniAuth::Builder) do
   provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"]
   provider :linkedin, ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"], scope: "r_basicprofile", fields: %w[id first-name last-name picture-url]
-  provider :linkedin, ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"], scope: "r_liteprofile rw_ads", name: "linkedin_ads", fields: %w[id first-name last-name picture-url]
 end
