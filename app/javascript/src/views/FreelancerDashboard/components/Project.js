616e4707f61cd593d8c0918a9e5271a19073fa86
guandjoy <guandjoy@gmail.com>
2021-11-04 17:59:46 +0200

    Add Card and Link to Event and Project components

--- app/javascript/src/views/FreelancerDashboard/components/Project.js
+++ app/javascript/src/views/FreelancerDashboard/components/Project.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Box, Text } from "@advisable/donut";
+import { Link, Box, Text } from "@advisable/donut";
 import styled from "styled-components";
 import css from "@styled-system/css";
+import Card from "./Card";
 
 const StyledCoverImage = styled.img`
   position: absolute;
@@ -24,38 +25,43 @@ const StyledAvatar = styled.img`
 
 export default function Project({ caseStudy }) {
   return (
-    <Box display="flex" css={css({ columnGap: 4 })}>
-      <Box
-        position="relative"
-        bg="neutral100"
-        width="120px"
-        minWidth="120px"
-        height="92px"
-        borderRadius="12px"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        css={`
-          overflow: hidden;
-        `}
-      >
-        <StyledCoverImage src={caseStudy.coverPhoto} />
-        <StyledAvatar src={caseStudy.specialist?.avatar} />
-      </Box>
-      <Box>
-        <Text
-          fontSize="xs"
-          color="neutral500"
-          fontWeight={450}
-          lineHeight="xs"
-          mb={1}
+    <Card
+      as={Link}
+      to={`/freelancers/${caseStudy.specialist?.id}/case_studies/${caseStudy.id}`}
+    >
+      <Box display="flex" css={css({ columnGap: 4 })}>
+        <Box
+          position="relative"
+          bg="neutral100"
+          width="120px"
+          minWidth="120px"
+          height="92px"
+          borderRadius="12px"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          css={`
+            overflow: hidden;
+          `}
         >
-          {caseStudy.specialist?.name}
-        </Text>
-        <Text color="neutral900" lineHeight="s" fontWeight={450}>
-          {caseStudy.title}
-        </Text>
+          <StyledCoverImage src={caseStudy.coverPhoto} />
+          <StyledAvatar src={caseStudy.specialist?.avatar} />
+        </Box>
+        <Box>
+          <Text
+            fontSize="xs"
+            color="neutral500"
+            fontWeight={450}
+            lineHeight="xs"
+            mb={1}
+          >
+            {caseStudy.specialist?.name}
+          </Text>
+          <Text color="neutral900" lineHeight="s" fontWeight={450}>
+            {caseStudy.title}
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </Card>
   );
 }
