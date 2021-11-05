461b78eb76f372495a050194ef867e7e1e09f7c5
Thomas Cullen <thomascullen92@gmail.com>
2021-11-05 15:27:11 +0000

    Redirect /case_studies/:id url to profile url with slug

---
+++ app/controllers/specialists_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class SpecialistsController < ApplicationController
+  before_action :prefetch_profile, only: %i[profile case_study]
+
+  def profile
+    @specialist = Specialist.find_by_username(params[:username])
+  end
+
+  def case_study
+    prefetch_query("app/javascript/src/views/FreelancerProfile/queries/getCaseStudy.gql", variables: {
+      id: params[:slug]
+    })
+    @case_study = ::CaseStudy::Article.find_by_slug(params[:slug])
+  end
+
+  private
+
+  def prefetch_profile
+    prefetch_query("app/javascript/src/views/FreelancerProfile/queries/getProfileData.gql", variables: {
+      id: params[:username]
+    })
+  end
+end
