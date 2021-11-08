3fad7bf725cdf5056473b9ec73b84164c07d9e4c
Miha Rekar <miha.rekar@advisable.com>
2021-11-08 13:12:38 +0100

    Set AS path inside GraphqlController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include ActiveStorage::SetCurrent
   include CurrentUser
 
   before_action :set_sentry_context
