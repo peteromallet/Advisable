3bc11082edf8a567c837cf6b168aaafbc0068ac1
guandjoy <guandjoy@gmail.com>
2021-11-08 20:24:10 +0200

    Add EmptyState to LatestProjects

--- app/javascript/src/views/FreelancerDashboard/components/LatestProjects.js
+++ app/javascript/src/views/FreelancerDashboard/components/LatestProjects.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { Stack, Text } from "@advisable/donut";
+import { Box, Stack, Text } from "@advisable/donut";
 import Project from "./Project";
+
+function EmptyState() {
+  return (
+    <Box
+      width="100%"
+      bg="neutral50"
+      display="flex"
+      border="2px solid"
+      borderColor="neutral100"
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="20px"
+      paddingY={10}
+    >
+      <Text color="neutral500" fontWeight={350}>
+        No Case Studies have been created recently
+      </Text>
+    </Box>
+  );
+}
 
 export default function LatestProjects({ topCaseStudies }) {
   const caseStudies = topCaseStudies.map((cs) => (
     <Project caseStudy={cs} key={cs.id} />
   ));
-
-  if (caseStudies.length === 0) return null;
 
   return (
     <>
@@ -20,9 +38,13 @@ export default function LatestProjects({ topCaseStudies }) {
       >
         Latest projects
       </Text>
-      <Stack spacing={6} divider="neutral100">
-        {caseStudies}
-      </Stack>
+      {caseStudies.length > 0 ? (
+        <Stack spacing={6} divider="neutral100">
+          {caseStudies}
+        </Stack>
+      ) : (
+        <EmptyState />
+      )}
     </>
   );
 }
