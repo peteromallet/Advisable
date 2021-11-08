1b57435fad93fed8d1347b331ae809e458c73271
guandjoy <guandjoy@gmail.com>
2021-11-08 12:44:49 +0200

    Create DashboardLoading.js

---
+++ app/javascript/src/views/FreelancerDashboard/components/DashboardLoading.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Skeleton, Box, Stack } from "@advisable/donut";
+
+export default function DashboardLoading() {
+  return (
+    <>
+      <Stack as={Box} spacing={16} mb={16} gridColumn="2" gridRow="1">
+        <Box>
+          <Skeleton width="40%" height="28px" mt={1} mb={7} />
+          <Stack spacing={12} divider="neutral100">
+            <Skeleton width="100" height="92px" />
+            <Skeleton width="100" height="92px" />
+            <Skeleton width="100" height="92px" />
+          </Stack>
+        </Box>
+        <Box>
+          <Skeleton width="40%" height="28px" mt={1} mb={7} />
+          <Stack spacing={12} divider="neutral100">
+            <Skeleton width="100" height="92px" />
+            <Skeleton width="100" height="92px" />
+            <Skeleton width="100" height="92px" />
+          </Stack>
+        </Box>
+      </Stack>
+      <Box gridColumn="1" gridRow="1">
+        <Box display="flex" width="100%" alignItems="center" mb={6}>
+          <Skeleton width="40%" height="28px" my={1} />
+          <Box ml="auto">
+            <Skeleton width="100px" height="36px" borderRadius="18px" />
+          </Box>
+        </Box>
+        <Stack spacing={4}>
+          <Skeleton width="100%" height="200px" />
+          <Skeleton width="100%" height="220px" />
+          <Skeleton width="100%" height="200px" />
+          <Skeleton width="100%" height="200px" />
+        </Stack>
+      </Box>
+    </>
+  );
+}
