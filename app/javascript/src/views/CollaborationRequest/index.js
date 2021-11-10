4589d1464f97701ab06bfc2ae4bed55cfdfe36f2
Thomas Cullen <thomascullen92@gmail.com>
2021-11-10 15:27:37 +0000

    Ability to edit a post

--- app/javascript/src/views/CollaborationRequest/index.js
+++ app/javascript/src/views/CollaborationRequest/index.js
@@ -2,12 +2,29 @@ import React from "react";
 import { X } from "@styled-icons/heroicons-solid";
 import { Box, useBackground } from "@advisable/donut";
 import CircularButton from "src/components/CircularButton";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import View from "src/components/View";
-import PostContent from "./PostContent";
+import CollaborationRequestForm from "src/components/CollaborationRequestForm";
+import { usePostCollaborationRequest } from "./queries";
 
 export default function CollaborationRequest() {
   useBackground("white");
+  const history = useHistory();
+  const [publish] = usePostCollaborationRequest();
+
+  const handleSubmit = async (values) => {
+    await publish({
+      variables: {
+        input: {
+          title: values.title,
+          body: values.body,
+          labels: values.labels.map((l) => l.value),
+        },
+      },
+    });
+
+    history.push("/");
+  };
 
   return (
     <View>
@@ -17,7 +34,7 @@ export default function CollaborationRequest() {
         </Link>
       </Box>
       <Box maxWidth="800px" marginX="auto" padding={12}>
-        <PostContent />
+        <CollaborationRequestForm onSubmit={handleSubmit} />
       </Box>
     </View>
   );
