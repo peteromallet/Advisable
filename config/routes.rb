04abacfe11abf0eff2d9bdcf3be46fba7529e10b
Thomas Cullen <thomascullen92@gmail.com>
2021-11-11 12:21:38 +0000

    Move old guild post path redirect to backend

--- config/routes.rb
+++ config/routes.rb
@@ -114,6 +114,7 @@ Rails.application.routes.draw do
   get "/guild/events/:id", to: redirect(LogAndRedirect.new("/events/%{id}"))
   get "/clients/signup", to: redirect(LogAndRedirect.new("/clients/join"))
   get "/freelancers/signup", to: redirect(LogAndRedirect.new("/freelancers/join"))
+  get "/guild/posts/:id", to: redirect(LogAndRedirect.new("/posts/%{id}"))
 
   get "verify_project/:uid", to: "application#verify_project_redirect"
 
