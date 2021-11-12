6e781dc8687279cefc8554e3676c527cf1530c4f
Thomas Cullen <thomascullen92@gmail.com>
2021-11-12 13:56:02 +0000

    Prepend new post to collaboration feed on dashboard

--- app/javascript/src/views/NewPost/queries/index.js
+++ app/javascript/src/views/NewPost/queries/index.js
@@ -1,6 +1,25 @@
 import { useMutation } from "@apollo/client";
 import POST_COLLABORATION_REQUEST from "./postCollaborationRequest.gql";
+import COLLABORATION_REQUESTS from "../../FreelancerDashboard/queries/collaborationRequests.gql";
 
 export function usePostCollaborationRequest() {
-  return useMutation(POST_COLLABORATION_REQUEST);
+  return useMutation(POST_COLLABORATION_REQUEST, {
+    update(cache, { data: { postCollaborationRequest } }) {
+      const existing = cache.readQuery({ query: COLLABORATION_REQUESTS });
+      if (existing) {
+        cache.writeQuery({
+          query: COLLABORATION_REQUESTS,
+          data: {
+            collaborationRequests: {
+              ...existing.collaborationRequests,
+              edges: [
+                { node: postCollaborationRequest.post },
+                ...existing.collaborationRequests.edges,
+              ],
+            },
+          },
+        });
+      }
+    },
+  });
 }
