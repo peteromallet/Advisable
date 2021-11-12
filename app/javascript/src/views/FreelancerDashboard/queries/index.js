433cbbb3b567ca79d8ddce36efc2f3288862ba47
guandjoy <guandjoy@gmail.com>
2021-11-12 12:41:29 +0200

    Update CollaborationRequests loading states

--- app/javascript/src/views/FreelancerDashboard/queries/index.js
+++ app/javascript/src/views/FreelancerDashboard/queries/index.js
@@ -3,14 +3,11 @@ import GET_DASHBOARD_DATA from "./dashboardData.gql";
 import GET_COLLABORATION_REQUESTS from "./collaborationRequests.gql";
 
 export const useDashboardData = () => {
-  return useQuery(GET_DASHBOARD_DATA, {
-    notifyOnNetworkStatusChange: true,
-    errorPolicy: "none",
-    fetchPolicy: "cache-first",
-    nextFetchPolicy: "cache-first",
-  });
+  return useQuery(GET_DASHBOARD_DATA);
 };
 
 export const useCollaborationRequests = () => {
-  return useQuery(GET_COLLABORATION_REQUESTS);
+  return useQuery(GET_COLLABORATION_REQUESTS, {
+    notifyOnNetworkStatusChange: true,
+  });
 };
