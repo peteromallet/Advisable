52bf0061bb911c900c356e7b386ae2db54c4b930
Miha Rekar <miha.rekar@advisable.com>
2021-11-12 12:18:47 +0100

    Create single calendar event

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -24,53 +24,31 @@ class Interview < ApplicationRecord
     conversation.new_message!(nil, "#{specialist.account.name} & #{user.account.name},\n\nNow that you've scheduled a call, you can use this thread to communicate.\n\nIf you have any questions or issues, don't hesitate to contact the Advisable team at hello@advisable.com.")
   end
 
-  def create_google_calendar_events
+  def create_google_calendar_event
     provider = AuthProvider.robot_calendar_provider
     return unless provider.is_a?(AuthProvider)
 
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = provider.google_secret.to_authorization
-    create_user_gcal_event(service)
-    create_specialist_gcal_event(service)
-  end
-
-  private
-
-  def create_user_gcal_event(service)
-    summary = "Call with #{specialist.account.name} - #{application.project.nice_name} Project"
     description = <<~DESCRIPTION.strip
-      You can use the following link to speak to #{specialist.account.first_name}: #{ApplicationMailer.default_url_options[:host]}/calls/#{video_call.uid}\n
-      You'll need to sign into your Advisable account to join this call.\n
-      If you can't reach #{specialist.account.first_name} there, you can call them directly on: #{specialist.phone}\n
-      You can also see their application here: #{ApplicationMailer.default_url_options[:host]}/manage/#{application.uid}\n
-      If you'd like to reschedule, please email #{user.company.sales_person.name} at #{user.company.sales_person.email}
+      You can use the following link for you call: #{ApplicationMailer.default_url_options[:host]}/calls/#{video_call.uid}.\n
+      Please sign in to your Advisable account to join this call.\n
+      If you'd like to reschedule, please email #{user.company.sales_person.name} at #{user.company.sales_person.email}.
     DESCRIPTION
-    attendee = Google::Apis::CalendarV3::EventAttendee.new(email: user.account.email)
-    event = Google::Apis::CalendarV3::Event.new(**common_google_calendar_event.merge(summary: summary, description: description, attendees: [attendee]))
-    service.insert_event(ENV["GOOGLE_INTERVIEW_CALENDAR_ID"], event, send_updates: "all")
-  end
-
-  def create_specialist_gcal_event(service)
-    summary = "Call with #{user.name_with_company}"
-    description = <<~DESCRIPTION.strip
-      You can use the following link to speak to #{user.account.first_name}: #{ApplicationMailer.default_url_options[:host]}/calls/#{video_call.uid}\n
-      You'll need to sign into your Advisable account to join this call.\n
-      You can also see the project details and application here:  #{ApplicationMailer.default_url_options[:host]}/clients/#{application.uid}\n
-      If you'd like to reschedule, please email #{user.company.sales_person.name} at #{user.company.sales_person.email}
-    DESCRIPTION
-    attendee = Google::Apis::CalendarV3::EventAttendee.new(email: specialist.account.email)
-    event = Google::Apis::CalendarV3::Event.new(**common_google_calendar_event.merge(summary: summary, description: description, attendees: [attendee]))
-    service.insert_event(ENV["GOOGLE_INTERVIEW_CALENDAR_ID"], event, send_updates: "all")
-  end
-
-  def common_google_calendar_event
     ends_at = starts_at + 30.minutes
-    {
-      location: "#{ApplicationMailer.default_url_options[:host]}/calls/#{video_call.uid}",
+    event = Google::Apis::CalendarV3::Event.new(
       start: Google::Apis::CalendarV3::EventDateTime.new(date_time: starts_at.rfc3339, time_zone: starts_at.time_zone.tzinfo.name),
       end: Google::Apis::CalendarV3::EventDateTime.new(date_time: ends_at.rfc3339, time_zone: starts_at.time_zone.tzinfo.name),
-      reminders: Google::Apis::CalendarV3::Event::Reminders.new(use_default: true)
-    }
+      location: "#{ApplicationMailer.default_url_options[:host]}/calls/#{video_call.uid}",
+      reminders: Google::Apis::CalendarV3::Event::Reminders.new(use_default: true),
+      summary: "Call with #{user.name_with_company} and #{specialist.account.name} about #{application.project.nice_name} Project",
+      description: description,
+      attendees: [
+        Google::Apis::CalendarV3::EventAttendee.new(email: user.account.email),
+        Google::Apis::CalendarV3::EventAttendee.new(email: specialist.account.email)
+      ]
+    )
+    service.insert_event(ENV["GOOGLE_INTERVIEW_CALENDAR_ID"], event, send_updates: "all")
   end
 end
 
