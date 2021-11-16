00f154e5851d8a2bcbe3c9e724bf4dbe24c79ba4
Miha Rekar <miha.rekar@advisable.com>
2021-11-16 14:12:18 +0100

    Extract apply_filter

---
+++ app/lib/toby/helpers/filter.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Toby
+  module Helpers
+    module Filter
+      private
+
+      def apply_filters(records, resource, filters)
+        filters.each do |filter|
+          args = filter.arguments.argument_values.transform_values(&:value)
+          next unless args.key?(:attribute)
+
+          name = args[:attribute].underscore.to_sym
+          attribute = resource.attributes.find { |attr| attr.name == name }
+          next if attribute.nil?
+
+          filter_class = attribute.class.filters.find { |f| f.name == args[:type] }
+          next if filter_class.nil?
+
+          records = apply_filter(filter_class, records, attribute, args[:value])
+        end
+        records
+      end
+
+      def apply_filter(filter_class, records, attribute, value)
+        if filter_class.block.present?
+          filter_class.block.call(records, attribute, value)
+        else
+          filter_class.apply(records, attribute, value: value)
+        end
+      end
+    end
+  end
+end
