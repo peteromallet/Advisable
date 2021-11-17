b0810e294f2ebedf8609c5f5cf0a10d6674a5391
guandjoy <guandjoy@gmail.com>
2021-11-17 19:43:51 +0200

    Init content of modal

--- app/javascript/src/views/FreelancerProfile/components/EditCaseStudyModal.js
+++ app/javascript/src/views/FreelancerProfile/components/EditCaseStudyModal.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Text, Link, Button, Modal } from "@advisable/donut";
+import {
+  Box,
+  Text,
+  Link,
+  Button,
+  Modal,
+  Heading,
+  theme,
+} from "@advisable/donut";
+import ClockIllustration from "src/illustrations/zest/clock";
 
 export default function EditCaseStudyModal({ modal, caseStudy }) {
   return (
-    <Modal modal={modal}>
-      <Text>ololo</Text>
-      <Link.External href={caseStudy.editorUrl} target="_blank">
-        <Button>Go to the editor</Button>
-      </Link.External>
+    <Modal
+      modal={modal}
+      width={440}
+      label="Go to our case study editor"
+      padding={12}
+    >
+      <Box textAlign="center" maxWidth="340px" mx="auto">
+        <ClockIllustration width="200px" color={theme.colors.blue300} mb={5} />
+        <Heading mb={2}>
+          You&apos;re about to go to our external case study editor
+        </Heading>
+        <Text lineHeight="s" color="neutral900" mb={6}>
+          Any edits you make will take up to 10 minutes to be reflected live.
+        </Text>
+        <Link.External href={caseStudy.editorUrl} target="_blank">
+          <Button size="l">Go to the editor</Button>
+        </Link.External>
+      </Box>
     </Modal>
   );
 }
