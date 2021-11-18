c001ecfac3dd9f1ce00513dffbe9cee1a95a8dd3
guandjoy <guandjoy@gmail.com>
2021-11-18 19:29:28 +0200

    Fix incircle icons in profile empty states

--- donut/src/components/Circle/styles.js
+++ donut/src/components/Circle/styles.js
@@ -1,3 +1,4 @@
+import { display } from "styled-system";
 import styled from "styled-components";
 import StyledBox from "../Box/styles";
 
@@ -6,6 +7,7 @@ export const StyledCircle = styled(StyledBox)`
   align-items: center;
   display: inline-flex;
   justify-content: center;
+  ${display}
 `;
 
 export default StyledCircle;
