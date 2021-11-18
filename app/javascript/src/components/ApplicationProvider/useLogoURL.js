15aaf6bf9f94ef779be3ac49e9f9fcea59fc7403
guandjoy <guandjoy@gmail.com>
2021-11-18 15:55:34 +0200

    Set link of logo for guests to the website

--- app/javascript/src/components/ApplicationProvider/useLogoURL.js
+++ app/javascript/src/components/ApplicationProvider/useLogoURL.js
@@ -1,10 +1,16 @@
 import { useContext, useEffect } from "react";
+import useViewer from "src/hooks/useViewer";
 import applicationContext from "../../applicationContext";
 
 function useLogoURL(url) {
   const context = useContext(applicationContext);
+  const viewer = useViewer();
 
   useEffect(() => {
+    if (!viewer) {
+      context.setLogoURL("https://advisable.com/");
+      return () => context.setLogoURL("/");
+    }
     if (url) {
       context.setLogoURL(url);
       return () => context.setLogoURL("/");
