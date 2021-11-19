fa51a58ef29d05b5b7263051b7ed7777c4759645
Thomas Cullen <thomascullen92@gmail.com>
2021-11-19 10:22:08 +0000

    Update copy and add profile test

--- app/javascript/src/views/FreelancerProfile/components/EditCaseStudyModal.js
+++ app/javascript/src/views/FreelancerProfile/components/EditCaseStudyModal.js
@@ -9,6 +9,7 @@ import {
   theme,
 } from "@advisable/donut";
 import ClockIllustration from "src/illustrations/zest/clock";
+import { ExternalLink } from "@styled-icons/heroicons-solid";
 
 export default function EditCaseStudyModal({ modal, caseStudy }) {
   return (
@@ -20,14 +21,14 @@ export default function EditCaseStudyModal({ modal, caseStudy }) {
     >
       <Box textAlign="center" maxWidth="340px" mx="auto">
         <ClockIllustration width="200px" color={theme.colors.blue300} mb={5} />
-        <Heading mb={2}>
-          You&apos;re about to go to our external case study editor
-        </Heading>
+        <Heading mb={2}>Edit case study</Heading>
         <Text lineHeight="s" color="neutral900" mb={6}>
           Any edits you make will take up to 10 minutes to be reflected live.
         </Text>
         <Link.External href={caseStudy.editorUrl} target="_blank">
-          <Button size="l">Go to the editor</Button>
+          <Button size="l" prefix={<ExternalLink />}>
+            Open editor
+          </Button>
         </Link.External>
       </Box>
     </Modal>
