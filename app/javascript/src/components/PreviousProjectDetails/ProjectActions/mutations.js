f6090767a87d2060105024d7724fc30ce9248b85
Thomas Cullen <thomascullen92@gmail.com>
2021-11-22 10:37:19 +0000

    Remove remaining previous project frontend code

--- app/javascript/src/components/PreviousProjectDetails/ProjectActions/mutations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const DELETE_PREVIOUS_PROJECT = gql`
-  mutation DeletePreviousProject($input: DeletePreviousProjectInput!) {
-    deletePreviousProject(input: $input) {
-      success
-    }
-  }
-`;
