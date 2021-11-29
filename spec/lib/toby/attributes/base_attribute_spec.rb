005e22e6e08ef91e37fbea079129b853afa134e6
Miha Rekar <miha.rekar@advisable.com>
2021-11-29 13:59:43 +0100

    Some more specs

--- spec/lib/toby/attributes/base_attribute_spec.rb
+++ spec/lib/toby/attributes/base_attribute_spec.rb
@@ -5,12 +5,75 @@ require "rails_helper"
 RSpec.describe Toby::Attributes::BaseAttribute do
   let(:name) { :created_at }
   let(:resource) { Toby::Resources::Application }
+  let(:application) { create(:application, created_at: "1.1.2021") }
   let(:options) { {} }
   let(:base) { described_class.new(name, resource, **options) }
 
   describe "#humanized_name" do
     it "returns humanized name" do
       expect(base.humanized_name).to eq("Created at")
+    end
+  end
+
+  describe "#field" do
+    it "returns field" do
+      expect(base.field).to eq("createdAt")
+    end
+  end
+
+  describe "#readonly" do
+    it "defaults to false" do
+      expect(base.readonly).to be_falsey
+    end
+
+    context "when provided a readonly option" do
+      let(:options) { {readonly: true} }
+
+      it "returns readonly option" do
+        expect(base.readonly).to be_truthy
+      end
+    end
+  end
+
+  describe "#sortable" do
+    it "defaults to true" do
+      expect(base.sortable).to be_truthy
+    end
+
+    context "when provided a sortable option" do
+      let(:options) { {sortable: false} }
+
+      it "returns sortable option" do
+        expect(base.sortable).to be_falsey
+      end
+    end
+  end
+
+  describe "#case_insensitive_compare" do
+    it "defaults to false" do
+      expect(base.case_insensitive_compare).to be_falsey
+    end
+
+    context "when provided a case_insensitive_compare option" do
+      let(:options) { {case_insensitive_compare: true} }
+
+      it "returns case_insensitive_compare option" do
+        expect(base.case_insensitive_compare).to be_truthy
+      end
+    end
+  end
+
+  describe "#read" do
+    it "reads from the object" do
+      expect(base.read(application)).to eq("1.1.2021")
+    end
+  end
+
+  describe "#write" do
+    it "writes to the object" do
+      expect(application.created_at).to eq("1.1.2021")
+      base.write(application, "2.2.2021")
+      expect(application.created_at).to eq("2.2.2021")
     end
   end
 end
