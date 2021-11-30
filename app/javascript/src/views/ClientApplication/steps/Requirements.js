0ab64ad5f48a2665d3ed10ebd3b99bf6a4bb2df6
guandjoy <guandjoy@gmail.com>
2021-11-30 12:10:27 +0200

    Init new fields in Requirements step

--- app/javascript/src/views/ClientApplication/steps/Requirements.js
+++ app/javascript/src/views/ClientApplication/steps/Requirements.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { object, string, boolean } from "yup";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form } from "formik";
 import { useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 import { ArrowRight } from "@styled-icons/feather/ArrowRight";
-import { Label, Box, Error, RadioGroup, Radio } from "@advisable/donut";
+import TilesInput from "src/components/TilesInput";
+import { Box, Error, Textarea } from "@advisable/donut";
 import FormField from "src/components/FormField";
 import CurrencyInput from "src/components/CurrencyInput";
 import SubmitButton from "src/components/SubmitButton";
@@ -20,8 +21,8 @@ import {
 export const validationSchema = object().shape({
   title: string().required("Please enter your role at the company"),
   budget: string().required("Please enter your budget"),
-  feedback: boolean().required(
-    "Please tell us if you open to provide feedback",
+  hiring: boolean().required(
+    "Please tell us if you interested in hiring a specialist",
   ),
   marketingAttitude: string().required(
     "Please select your type of marketing attitude",
@@ -36,12 +37,13 @@ export default function Requirements({ clientApplication }) {
   const initialValues = {
     title: clientApplication.title || "",
     budget: clientApplication.budget / 100 || "",
-    feedback: clientApplication.feedback || undefined,
+    hiring: undefined,
     marketingAttitude: clientApplication.marketingAttitude || "",
   };
 
   const handleSubmit = async (values, { setStatus }) => {
     setStatus(null);
+    delete values.hiring;
     const res = await update({
       variables: {
         input: { ...values, budget: values.budget * 100 },
@@ -94,6 +96,50 @@ export default function Requirements({ clientApplication }) {
                   data-testid="budget"
                 />
               </Box>
+              <Box mb={6}>
+                <FormField
+                  as={TilesInput}
+                  fullWidth
+                  alignWidth
+                  optionsPerRow={2}
+                  name="hiring"
+                  onChange={(n) => formik.setFieldValue("hiring", n)}
+                  error={null}
+                  label="Are you interested in hiring a specialist immediately or in short term?"
+                  options={[
+                    { label: "Yes", value: true },
+                    { label: "No", value: false },
+                  ]}
+                  value={formik.values.hiring}
+                />
+              </Box>
+              {formik.values.hiring === true && (
+                <>
+                  <FormField
+                    as={Textarea}
+                    name="description"
+                    minRows={3}
+                    label="Please briefly describe the specialist you are looking for"
+                    marginBottom={6}
+                    placeholder="I'm looking for..."
+                  />
+                  <FormField
+                    as={TilesInput}
+                    fullWidth
+                    alignWidth
+                    optionsPerRow={2}
+                    name="introduction"
+                    onChange={(n) => formik.setFieldValue("introduction", n)}
+                    error={null}
+                    label="Are you up for dedicating 1 hour of your time in the next few days to be introduced to the hiring process and provide feedback to our team?"
+                    options={[
+                      { label: "Yes", value: true },
+                      { label: "No", value: false },
+                    ]}
+                    value={formik.values.introduction}
+                  />
+                </>
+              )}
             </Box>
             <Error>{formik.status}</Error>
             <SubmitButton
