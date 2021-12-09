379a6226df35da03cd6aeaee2fa8745a5bc7dd20
Thomas Cullen <thomascullen92@gmail.com>
2021-12-09 12:47:09 +0000

    Send slack notification on consultation request

--- app/models/slack.rb
+++ app/models/slack.rb
@@ -4,10 +4,14 @@ class Slack
   URL = "https://slack.com/api/"
   HEADERS = {"Authorization" => "Bearer #{ENV['SLACK_BOT_TOKEN']}", "Content-type" => "application/json; charset=utf-8"}.freeze
 
+  DEFAULT_OPTIONS = {
+    username: "Advisable"
+  }.freeze
+
   def self.message(options = {})
     return if ENV["SLACK_BOT_TOKEN"].nil?
 
     api = Faraday.new(URL, headers: HEADERS)
-    api.post("chat.postMessage", options.to_json)
+    api.post("chat.postMessage", DEFAULT_OPTIONS.merge(options).to_json)
   end
 end
