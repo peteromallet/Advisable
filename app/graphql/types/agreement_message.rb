e0dd68344c9e4dce83ef177c449e82ccb2cacc52
Miha Rekar <miha.rekar@advisable.com>
2021-12-09 09:11:21 +0100

    Add optional author to AgreementType

--- app/graphql/types/agreement_message.rb
+++ app/graphql/types/agreement_message.rb
@@ -8,5 +8,11 @@ module Types
     description "Type for Message that has an Agreement associated with it."
 
     field :agreement, Types::Agreement, null: false
+    field :author, Types::Account, null: true
+    def author
+      return unless object.author_id
+
+      dataloader.with(::ActiveRecordSource, ::Account).load(object.author_id)
+    end
   end
 end
