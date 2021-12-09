7e8bf9b14d46ff232c448efbd3efaebb4195f194
Thomas Cullen <thomascullen92@gmail.com>
2021-12-09 14:19:16 +0000

    Add slack job to background slack messages

--- app/models/slack.rb
+++ app/models/slack.rb
@@ -14,4 +14,10 @@ class Slack
     api = Faraday.new(URL, headers: HEADERS)
     api.post("chat.postMessage", DEFAULT_OPTIONS.merge(options).to_json)
   end
+
+  def self.bg_message(options = {})
+    return if ENV["SLACK_BOT_TOKEN"].nil?
+
+    SlackMessageJob.perform_later(options)
+  end
 end
