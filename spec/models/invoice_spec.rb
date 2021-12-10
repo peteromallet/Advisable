9fdfe41297c47f80ec1bd8c368342f03ffbf3180
Thomas Cullen <thomascullen92@gmail.com>
2021-12-10 15:18:40 +0000

    Fix flaky invoice tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -11,22 +11,22 @@ RSpec.describe Invoice, type: :model do
 
   describe "#first_day" do
     it "returns the first day of the invoice" do
-      expect(invoice.first_day).to eq("1.9.2021".to_date)
+      expect(invoice.first_day).to eq(Date.new(2021, 9, 1))
     end
   end
 
   describe "#date_range" do
     it "returns the date range of the invoice" do
-      expect(invoice.date_range).to eq(("1.9.2021".to_date)...("1.10.2021".to_date))
+      expect(invoice.date_range).to eq(Date.new(2021, 9, 1)...(Date.new(2021, 10, 1)))
     end
   end
 
   describe "#payments" do
-    let!(:previous_month) { create(:payment, status: "succeeded", company: invoice.company, created_at: "31.8.2021") }
-    let!(:first_day) { create(:payment, status: "succeeded", company: invoice.company, created_at: "1.9.2021") }
-    let!(:mid_day) { create(:payment, status: "succeeded", company: invoice.company, created_at: "15.9.2021") }
-    let!(:last_second) { create(:payment, status: "succeeded", company: invoice.company, created_at: "1.10.2021".to_time - 1) }
-    let!(:next_month) { create(:payment, status: "succeeded", company: invoice.company, created_at: "1.10.2021") }
+    let!(:previous_month) { create(:payment, status: "succeeded", company: invoice.company, created_at: Date.new(2021, 8, 31)) }
+    let!(:first_day) { create(:payment, status: "succeeded", company: invoice.company, created_at: Date.new(2021, 9, 1)) }
+    let!(:mid_day) { create(:payment, status: "succeeded", company: invoice.company, created_at: Date.new(2021, 9, 15)) }
+    let!(:last_second) { create(:payment, status: "succeeded", company: invoice.company, created_at: DateTime.new(2021, 10, 1).to_time - 1) }
+    let!(:next_month) { create(:payment, status: "succeeded", company: invoice.company, created_at: DateTime.new(2021, 10, 1)) }
 
     it "includes only payments of current month" do
       expect(invoice.payments).to match_array([first_day, mid_day, last_second])
