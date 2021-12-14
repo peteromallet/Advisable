3e5abfe757d74f1f64fcf3058b3ed2bd53302faa
Miha Rekar <miha.rekar@advisable.com>
2021-12-14 12:18:13 +0100

    Add messaging to CreateAgreement mutation

--- app/graphql/mutations/create_agreement.rb
+++ app/graphql/mutations/create_agreement.rb
@@ -2,9 +2,11 @@
 
 module Mutations
   class CreateAgreement < Mutations::BaseMutation
+    argument :attachments, [String], required: false
     argument :collaboration, String, required: true
     argument :hourly_rate, Int, required: false
     argument :invoicing, String, required: true
+    argument :message, String, required: true
     argument :user, ID, required: true
 
     field :agreement, Types::Agreement, null: true
@@ -14,12 +16,16 @@ module Mutations
     end
 
     def resolve(user:, **args)
-      agreement = current_user.agreements.build(args)
+      agreement = current_user.agreements.build(args.except(:attachments, :message))
       agreement.user = User.find_by!(uid: user)
       agreement.company = agreement.user.company
       agreement.status = "pending"
 
       save_with_current_account!(agreement)
+
+      conversation = Conversation.by_accounts(agreement.specialist, current_account)
+      conversation.new_message!(nil, nil, kind: "AgreementCreated", send_emails: false)
+      conversation.new_message!(current_account, args[:message], attachments: args[:attachments])
 
       {agreement: agreement}
     end
