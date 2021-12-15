0121b367057d03e4a8eb1ee16f3296ff2aac4dcc
Miha Rekar <miha.rekar@advisable.com>
2021-12-15 14:04:23 +0100

    Move to new framework defaults and adjust accordingly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_080911) do
+ActiveRecord::Schema.define(version: 2021_12_15_121913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_12_08_080911) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
