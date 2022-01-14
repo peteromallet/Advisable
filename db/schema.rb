4640615cc018094d29fa0b0d86e53aa4c95f39b8
Miha Rekar <miha.rekar@advisable.com>
2022-01-14 11:16:33 +0100

    Add timezone to account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_094747) do
+ActiveRecord::Schema.define(version: 2022_01_14_101524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_12_17_094747) do
     t.jsonb "unsubscribed_from"
     t.datetime "disabled_at"
     t.jsonb "features"
+    t.string "timezone"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["uid"], name: "index_accounts_on_uid", unique: true
   end
