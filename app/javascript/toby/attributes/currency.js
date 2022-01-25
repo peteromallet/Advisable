e6a4c93cf1862f6ce158b24421e817f817515375
Thomas Cullen <thomascullen92@gmail.com>
2022-01-25 15:45:41 +0000

    Allow decimals in currency inputs in Toby

--- app/javascript/toby/attributes/currency.js
+++ app/javascript/toby/attributes/currency.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { useFormikContext } from "formik";
+import React, { useState } from "react";
+import { useField } from "formik";
+import Dinero from "dinero.js";
+import { Input } from "@advisable/donut";
 import currency from "src/utilities/currency";
-import CurrencyInput from "src/components/CurrencyInput";
-import priceInputProps from "src/utilities/priceInputProps";
+
+const CURRENCY_REGEX = /^[0-9,]+\.?(\d{1,2})?$/;
 
 export default {
   render: function RenderCurrency({ record, attribute }) {
@@ -16,8 +18,25 @@ export default {
     return record[attribute.name] || undefined;
   },
   input: function CurrencyAttributeInput({ attribute }) {
-    const formik = useFormikContext();
+    const [field, , { setValue }] = useField(attribute.name);
+    const [inputValue, setInputValue] = useState(
+      field.value ? Dinero({ amount: field.value }).toFormat("0,0.00") : "",
+    );
 
-    return <CurrencyInput {...priceInputProps(formik, attribute.name)} />;
+    const handleChange = (e) => {
+      const nextInputValue = e.target.value;
+
+      if (nextInputValue && !CURRENCY_REGEX.test(nextInputValue)) {
+        e.preventDefault();
+        return;
+      }
+
+      const stripped = nextInputValue.replace(/[^0-9.-]+/g, "");
+      const value = stripped ? Number(stripped) * 100 : undefined;
+      setValue(value);
+      setInputValue(nextInputValue);
+    };
+
+    return <Input prefix="$" value={inputValue} onChange={handleChange} />;
   },
 };
