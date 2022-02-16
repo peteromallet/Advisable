4725082e0f391a7353175d34b3d4e7d9d68098ae
Miha Rekar <miha.rekar@advisable.com>
2022-02-16 09:57:13 +0100

    Cleanup safety_assured

--- db/migrate/20220125115408_add_references_to_payment_requests.rb
+++ db/migrate/20220125115408_add_references_to_payment_requests.rb
@@ -2,9 +2,7 @@
 
 class AddReferencesToPaymentRequests < ActiveRecord::Migration[7.0]
   def change
-    safety_assured do
-      add_reference :payments, :payment_request, foreign_key: true
-      add_reference :payouts, :payment_request, foreign_key: true
-    end
+    add_reference :payments, :payment_request, foreign_key: true
+    add_reference :payouts, :payment_request, foreign_key: true
   end
 end
