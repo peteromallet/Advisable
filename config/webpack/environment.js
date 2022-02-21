ba60c8b993def6268ab30545315ca3b01160c65e
Thomas Cullen <thomascullen92@gmail.com>
2022-02-21 17:53:11 +0000

    Upgrade from webpacker to shakapacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,38 +1,43 @@
 const path = require("path");
-const { environment } = require("@rails/webpacker");
+const { webpackConfig, merge } = require("shakapacker");
 const webpack = require("webpack");
 const dotenv = require("dotenv");
 const version = require("./buildVersion");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 dotenv.config({ silent: true });
 
 process.env.RELEASED_AT = new Date().toISOString();
 process.env.BUILD_TIME = version;
 
-if (process.env.ANALYSE_BUNDLE === "true") {
-  environment.plugins.append("BundleAnalyzer", new BundleAnalyzerPlugin());
-}
-
-environment.plugins.prepend(
-  "Environment",
-  new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env))),
-);
-
-environment.loaders.append("graphql", {
-  test: /\.(graphql|gql)$/,
-  exclude: /node_modules/,
-  loader: "graphql-tag/loader",
-});
-
-environment.loaders.append("mp3", {
-  test: /\.(mp3)$/,
-  exclude: /node_modules/,
-  loader: "file-loader",
-});
-
-environment.config.merge({
+const customConfig = {
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      RELEASED_AT: null,
+      BUILD_TIME: null,
+      INTERCOM_APP_ID: null,
+      SENTRY_FRONTEND_DSN: null,
+      SENTRY_ENVIRONMENT: null,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader",
+      },
+      {
+        test: /\.(mp3)$/,
+        exclude: /node_modules/,
+        loader: "file-loader",
+      },
+      {
+        type: "javascript/auto",
+        test: /\.mjs$/,
+        include: /node_modules/,
+      },
+    ],
+  },
   resolve: {
     alias: {
       "@advisable/donut": path.join(__dirname, "../../donut/src"),
@@ -47,21 +52,8 @@ environment.config.merge({
       "@guild": path.resolve(__dirname, "..", "..", "app/javascript/guild"),
     },
   },
-});
+};
 
-environment.splitChunks();
+// webpackConfig.splitChunks();
 
-// https://github.com/framer/motion/issues/1307
-environment.config.merge({
-  module: {
-    rules: [
-      {
-        type: "javascript/auto",
-        test: /\.mjs$/,
-        include: /node_modules/,
-      },
-    ],
-  },
-});
-
-module.exports = environment;
+module.exports = merge({}, webpackConfig, customConfig);
