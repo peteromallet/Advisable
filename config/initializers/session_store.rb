64af43db76529552b084b2bbdafa559a45227317
Miha Rekar <miha.rekar@advisable.com>
2022-02-22 07:40:36 +0100

    Fix Google CSRF

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store(:cookie_store, key: "_advisable_app_session", secure: Rails.env.production?, same_site: :strict)
+# Do *not* add `same_site: :strict`` since that causes CSRF with google oauth!
+# Or, if you do, make sure google oauth still works when loggged in to Google with multiple accounts.
+Rails.application.config.session_store(:cookie_store, key: "_advisable_app_session", secure: Rails.env.production?)
