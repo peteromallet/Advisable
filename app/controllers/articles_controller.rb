2a3a1bba9039926f97f37d7120fd7f4e322d4208
Miha Rekar <miha.rekar@advisable.com>
2022-02-25 07:46:17 +0100

    Allow to narrow down by skill category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,11 +11,20 @@ class ArticlesController < ApplicationController
 
     return if @query.blank?
 
+    articles = ::CaseStudy::Article.searchable
+
+    if params[:skill_category].present?
+      category = ::SkillCategory.find(params[:skill_category])
+      skill_ids = category.skills_with_similar.pluck(:id)
+      cs_skills = ::CaseStudy::Skill.where(skill_id: skill_ids)
+      articles = articles.where(skills: cs_skills)
+    end
+
     query = client.embeddings(engine: "text-search-#{engine}-query-001", parameters: {input: @query})
     data = query["data"].first["embedding"]
     query_vector = Vector.elements(data)
     @results = []
-    CaseStudy::Embedding.public_send(engine).each do |embedding|
+    CaseStudy::Embedding.public_send(engine).where(article: articles).each do |embedding|
       @results << {
         similarity: (embedding.cosine_similarity_to(query_vector) * 100).round(3),
         article: embedding.article
