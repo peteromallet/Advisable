50a3d9025ee95cdbefef7018492170ac3897b0f0
Miha Rekar <miha.rekar@advisable.com>
2022-03-11 11:43:27 +0000

    Add button for Sofia

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,4 +3,10 @@
 class AdminController < ApplicationController
   layout "tailwind"
   before_action :admin?
+
+  def finance
+    finance_email = "Advisable Finance <finance@advisable.com>"
+    GenerateFinanceCsvJob.perform_later(finance_email)
+    redirect_to admin_index_path, notice: "Finance report is being emailed to #{finance_email}"
+  end
 end
