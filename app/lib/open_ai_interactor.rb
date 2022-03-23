3a6c3f11461e7d6ab0b19729073f15c4df9d5f74
Miha Rekar <miha.rekar@advisable.com>
2022-03-23 10:51:23 +0100

    Make document_embedding retrieval smarter

--- app/lib/open_ai_interactor.rb
+++ app/lib/open_ai_interactor.rb
@@ -11,6 +11,12 @@ class OpenAiInteractor
 
   def document_embedding_for(text)
     embedding_for("text-search-babbage-doc-001", text)
+  rescue OpenAiInteractorError => e
+    raise e unless e.message.include?("reduce your prompt or completion length")
+
+    # Cut the last 5% of words from the text and try again
+    text_array = text.split
+    document_embedding_for(text_array.first(text_array.size * 0.95).join(" "))
   end
 
   def query_embedding_for(text)
