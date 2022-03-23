20c0d52bd6314e452eb4e6a265118c93f762489a
Miha Rekar <miha.rekar@advisable.com>
2022-03-23 10:48:55 +0100

    Specify Section types explicitly

--- spec/factories/case_study/sections.rb
+++ spec/factories/case_study/sections.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :case_study_section, class: "CaseStudy::Section" do
     sequence(:uid) { "css_#{SecureRandom.hex[0..14]}" }
     article { association :case_study_article }
-    type { "" }
+    type { "overview" }
   end
 end
