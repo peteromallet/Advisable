8164e2ff14ca80dd1e95d3420cfe3575629e9ea1
guandjoy <guandjoy@gmail.com>
2022-03-28 11:16:50 +0100

    Add Divider in CompanyBox

--- app/javascript/src/views/CaseStudyArticle/components/CompanyBox.js
+++ app/javascript/src/views/CaseStudyArticle/components/CompanyBox.js
@@ -1,24 +1,30 @@
 import React from "react";
 
+const Divider = () => <div className="w-px h-8 bg-slate-400" />;
+
 export default function CompanyBox({ caseStudy }) {
   return (
-    <div className="flex gap-4">
+    <div className="flex items-center gap-4 border border-solid border-slate-400 rounded-[20px] px-6 py-4">
       <div className="flex gap-2 items-center">
         <img src={caseStudy.company.favicon} />
         <span className="text-xl">{caseStudy.company.name}</span>
       </div>
+      <Divider />
       <div>
         <div className="uppercase">type</div>
         <div>{caseStudy.company.businessType}</div>
       </div>
+      <Divider />
       <div>
         <div className="uppercase">industry</div>
         <div>{caseStudy.company.businessType}</div>
       </div>
+      <Divider />
       <div>
         <div className="uppercase">focus</div>
         <div>{caseStudy.company.businessType}</div>
       </div>
+      <Divider />
       <div>
         <div className="uppercase">website</div>
         <div>NONE</div>
