877aa244d4f66bc37b4e2e86792397804b1e7c07
Miha Rekar <miha.rekar@advisable.com>
2022-04-06 14:53:59 +0200

    Extract join clause to prevent possible typos

--- app/lib/toby/lookups/interviews/agreement.rb
+++ app/lib/toby/lookups/interviews/agreement.rb
@@ -4,18 +4,16 @@ module Toby
   module Lookups
     module Interviews
       class Agreement < Attributes::String
+        JOIN_CLAUSE = "LEFT JOIN agreements ON interviews.user_id = agreements.user_id AND interviews.specialist_id = agreements.specialist_id"
+
         include Lookup
 
         filter "is blank", Filters::CheckNil do |records, _attribute, _value|
-          records.
-            joins("LEFT JOIN agreements ON interviews.user_id = agreements.user_id AND interviews.specialist_id = agreements.specialist_id").
-            where(agreements: {id: nil})
+          records.joins(JOIN_CLAUSE).where(agreements: {id: nil})
         end
 
         filter "is not blank", Filters::CheckNotNil do |records, _attribute, _value|
-          records.
-            joins("LEFT JOIN agreements ON interviews.user_id = agreements.user_id AND interviews.specialist_id = agreements.specialist_id").
-            where.not(agreements: {id: nil})
+          records.joins(JOIN_CLAUSE).where.not(agreements: {id: nil})
         end
 
         def lazy_read_class
@@ -27,9 +25,7 @@ module Toby
         end
 
         def load_records(pending)
-          Interview.where(id: pending).
-            joins("LEFT JOIN agreements ON interviews.user_id = agreements.user_id AND interviews.specialist_id = agreements.specialist_id").
-            pluck("interviews.id", "agreements.uid")
+          Interview.where(id: pending).joins(JOIN_CLAUSE).pluck("interviews.id", "agreements.uid")
         end
       end
     end
