6cb8a0826a5674487ebd0a5558d6297603666a64
Miha Rekar <miha.rekar@advisable.com>
2022-04-06 10:58:39 +0200

    Do not remind payment requests on agreements with custom due days

--- app/jobs/payment_request_reminder_job.rb
+++ app/jobs/payment_request_reminder_job.rb
@@ -4,7 +4,14 @@ class PaymentRequestReminderJob < ApplicationJob
   REMIND_ON_DAY = 2
 
   def perform
-    PaymentRequest.unreminded.with_status(%w[pending approved]).where(created_at: ..REMIND_ON_DAY.days.ago).find_each do |payment_request|
+    requests = PaymentRequest.
+      unreminded.
+      with_status(%w[pending approved]).
+      joins(:agreement).
+      where(agreement: {due_days: nil}).
+      where(created_at: ..REMIND_ON_DAY.days.ago)
+
+    requests.find_each do |payment_request|
       payment_request.update!(reminded_at: Time.current)
       UserMailer.payment_request_reminder(payment_request).deliver_later
     end
