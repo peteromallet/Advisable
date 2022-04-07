26b4611e5fc4112b588c06a7f4b3adeafc8e17a0
Thomas Cullen <thomascullen92@gmail.com>
2022-04-07 11:50:20 +0100

    Improve industry step

--- app/javascript/src/views/UserOnboarding/Industry.js
+++ app/javascript/src/views/UserOnboarding/Industry.js
@@ -1,8 +1,9 @@
 import React, { useMemo, useState } from "react";
 import Fuse from "fuse.js";
-import { Link, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 import Input from "src/components/Input";
-import { Search } from "@styled-icons/heroicons-solid";
+import { ChevronRight, Search } from "@styled-icons/heroicons-solid";
 import { useUpdateCompany } from "./queries";
 
 export default function Industry({ data }) {
@@ -28,12 +29,17 @@ export default function Industry({ data }) {
   };
 
   return (
-    <div className="w-full">
+    <motion.div
+      className="w-full"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="text-center mb-4 md:mb-12">
-        <h2 className="font-semibold text-2xl md:text-3xl tracking-tight leading-none mb-2 text-blue900">
+        <h2 className="font-semibold text-2xl md:text-3xl tracking-tight leading-none mb-1 text-blue900">
           What industry is your company in?
         </h2>
-        <p className="md:text-lg text-neutral700 mb-4">
+        <p className="md:text-lg text-neutral700 mb-6">
           We’ll use this to recommend projects from similar industries.
         </p>
         <div className="w-auto md:w-[400px] mx-auto">
@@ -52,13 +58,14 @@ export default function Industry({ data }) {
           <div
             key={industry.id}
             data-testid="industry"
-            className="bg-white shadow-md rounded-md p-5 cursor-pointer border-2 border-solid border-transparent hover:shadow-xl hover:border-neutral700"
+            className="bg-white flex items-center shadow-md rounded-md p-5 cursor-pointer border-2 border-solid border-transparent hover:shadow-xl hover:border-neutral700"
             onClick={handleSelection(industry)}
           >
-            {industry.name}
+            <div className="flex-1">{industry.name}</div>
+            <ChevronRight className="w-5 h-5 text-neutral600 shrink-0" />
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
