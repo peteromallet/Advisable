7bb78058ca9f6f80f1b0cb160bcdab176984ab4a
guandjoy <guandjoy@gmail.com>
2022-04-07 15:07:35 +0100

    Add isNotFound error handler

--- app/javascript/src/views/CaseStudyArticle/index.js
+++ app/javascript/src/views/CaseStudyArticle/index.js
@@ -2,15 +2,16 @@ import React from "react";
 import { useArticle } from "./queries";
 import Loading from "src/components/Loading";
 import ErrorBoundary from "src/components/ErrorBoundary";
+import NotFound, { isNotFound } from "src/views/NotFound";
 import SidebarCard from "./components/SidebarCard";
 import ArticleIntro from "./components/ArticleIntro";
 import ArticleContent from "./components/ArticleContent";
 
 export default function ShortlistArticle() {
   const { data, loading, error } = useArticle();
-  if (loading) return <Loading />;
 
-  console.log("data", data);
+  if (loading) return <Loading />;
+  if (isNotFound(error)) return <NotFound />;
 
   return (
     <ErrorBoundary>
