cffe8d00c58093f3489e0996eedeb7b3ec1f6921
guandjoy <guandjoy@gmail.com>
2022-04-08 08:30:06 +0100

    Update spacing of SidebarCard

--- app/javascript/src/views/CaseStudyArticle/components/SidebarCard.js
+++ app/javascript/src/views/CaseStudyArticle/components/SidebarCard.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { DateTime } from "luxon";
+import { ChatAlt, Clock } from "@styled-icons/heroicons-solid";
+import Button from "src/components/Button";
 
 const Availability = ({ unavailableUntil }) => (
-  <div className="flex justify-items-center items-center">
+  <div className="flex justify-items-center items-center mb-4">
     {unavailableUntil ? (
       <>
         <div className="h-2.5 w-2.5 bg-gray-600 rounded-full mr-2" />
@@ -40,20 +42,36 @@ export default function SidebarCard({ specialist }) {
   }, [specialist.account.timezone, time]);
 
   return (
-    <div className="min-w-[320px] rounded-3xl bg-white drop-shadow p-8 pt-10 flex flex-col">
-      <div className="rounded-full overflow-hidden w-48 h-48 mx-auto mb-6">
-        <img src={specialist.avatar} className="h-full w-full object-cover" />
-      </div>
+    <div className="min-w-[320px] w-[320px] rounded-[40px] bg-white drop-shadow p-8 pt-10 flex flex-col">
       <Link to={specialist.profilePath}>
-        <h4 className="font-bold text-3xl">{specialist.name}</h4>
+        <div className="rounded-full overflow-hidden w-48 h-48 mx-auto mb-6">
+          <img src={specialist.avatar} className="h-full w-full object-cover" />
+        </div>
       </Link>
-      <Availability unavailableUntil={specialist.unavailableUntil} />
-      <hr />
-      <span>{specialist.location}</span>
-      <span>{timezoneFormat}</span>
-      <span>{clock}</span>
-      <hr />
-      <p>{specialist.bio}</p>
+      <Link to={specialist.profilePath}>
+        <h4 className="font-bold text-3xl leading-8 mb-0.5">
+          {specialist.name}
+        </h4>
+      </Link>
+      <Availability
+        unavailableUntil={specialist.unavailableUntil}
+        className="mb-5"
+      />
+      <Button prefix={<ChatAlt />} variant="primary" size="lg" className="mb-4">
+        Connect
+      </Button>
+      <div className="border-y border-solid border-gray-300 pt-4 pb-5 mb-4">
+        <span className="text-lg leading-5">{specialist.location}</span>
+        <div className="flex items-center gap-3">
+          <span className="text-gray-600 leading-5 mb-1">{timezoneFormat}</span>
+          <div className="w-px h-[16px] bg-gray-400" />
+          <div className="flex gap-1">
+            <Clock className="fill-gray-400 w-[16px]" />
+            <span className="text-gray-600 leading-5">{clock}</span>
+          </div>
+        </div>
+      </div>
+      <p className="text-[1.0625rem] leading-6">{specialist.bio}</p>
     </div>
   );
 }
