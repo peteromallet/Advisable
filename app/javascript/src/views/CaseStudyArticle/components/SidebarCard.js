a83869cdffb5560f6f1cf07ae10628e7c3f1db6d
guandjoy <guandjoy@gmail.com>
2022-04-10 20:10:05 +0100

    Update colours and spacing of SidebarCard

--- app/javascript/src/views/CaseStudyArticle/components/SidebarCard.js
+++ app/javascript/src/views/CaseStudyArticle/components/SidebarCard.js
@@ -1,50 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Link } from "@advisable/donut";
 import Button from "src/components/Button";
 import { ChatAlt } from "@styled-icons/heroicons-solid";
 import Timezone from "./Timezone";
 
-const Availability = ({ unavailableUntil }) => (
-  <div className="flex justify-items-center items-center mb-4">
-    {unavailableUntil ? (
-      <>
-        <div className="h-2.5 w-2.5 bg-gray-600 rounded-full mr-2" />
-        <p>Unavailable for hire</p>
-      </>
-    ) : (
-      <>
-        <div className="h-2.5 w-2.5 bg-blue-700 rounded-full mr-2" />
-        <p> Available for hire </p>
-      </>
-    )}
-  </div>
-);
+const Availability = ({ unavailableUntil }) =>
+  unavailableUntil ? (
+    <div className="flex justify-items-center items-center mb-3">
+      <div className="h-2.5 w-2.5 bg-neutral600 rounded-full mr-2" />
+      <div className="text-neutral600 leading-5 text-lg font-[440]">
+        Unavailable for hire
+      </div>
+    </div>
+  ) : (
+    <div className="flex justify-items-center items-center mb-3">
+      <div className="h-2.5 w-2.5 bg-blue500 rounded-full mr-2" />
+      <div className="text-neutral600 leading-5 text-lg font-[440]">
+        Available for hire
+      </div>
+    </div>
+  );
 
 export default function SidebarCard({ specialist }) {
   return (
     <div className="min-w-[320px] w-[320px] rounded-[40px] bg-white drop-shadow p-8 pt-10 flex flex-col">
-      <Link to={specialist.profilePath}>
-        <div className="rounded-full overflow-hidden w-48 h-48 mx-auto mb-6">
-          <img src={specialist.avatar} className="h-full w-full object-cover" />
+      <Link
+        as="div"
+        className="mb-7 mx-auto rounded-full"
+        to={specialist.profilePath}
+      >
+        <div className="rounded-full overflow-hidden w-48 h-48 border-4 border-neutral100 border-solid hover:border-neutral300">
+          <img
+            src={specialist.avatar}
+            className="h-full w-full object-cover rounded-full border-2 border-white border-solid"
+          />
         </div>
       </Link>
       <Link to={specialist.profilePath}>
-        <h4 className="font-bold text-3xl leading-8 mb-0.5">
+        <h4 className="font-bold text-3xl text-neutral800 hover:text-neutral800 leading-8 pt-px pb-[3px] mb-1 hover:underline decoration-neutral500">
           {specialist.name}
         </h4>
       </Link>
-      <Availability
-        unavailableUntil={specialist.unavailableUntil}
-        className="mb-5"
-      />
-      <Button prefix={<ChatAlt />} variant="primary" size="lg" className="mb-4">
+      <Availability unavailableUntil={specialist.unavailableUntil} />
+      <Button prefix={<ChatAlt />} variant="primary" size="lg" className="mb-5">
         Connect
       </Button>
-      <div className="border-y border-solid border-gray-300 pt-4 pb-5 mb-4">
-        <span className="text-lg leading-5">{specialist.location}</span>
-        <Timezone timezone={specialist.account.timezone} />
+      <hr className="border-neutral200 pb-[3px] mb-2" />
+      <div className="text-lg font-[450] text-blue900 leading-5 mb-1 pt-[3px] pb-px">
+        {specialist.location}
       </div>
-      <p className="text-[1.0625rem] leading-6">{specialist.bio}</p>
+      <Timezone timezone={specialist.account.timezone} />
+      <hr className="border-neutral200 mt-[3px] mb-3" />
+      <p className="text-[1.0625rem] text-neutral900 leading-6 py-0.5">
+        {specialist.bio}
+      </p>
     </div>
   );
 }
--- app/javascript/src/views/CaseStudyArticle/components/Timezone.js
+++ app/javascript/src/views/CaseStudyArticle/components/Timezone.js
@@ -22,12 +22,12 @@ export default function Timezone({ timezone }) {
   if (!timezone) return null;
 
   return (
-    <div className="flex items-center gap-3">
-      <span className="text-gray-600 leading-5 mb-1">{timezoneFormat}</span>
-      <div className="w-px h-[16px] bg-gray-400" />
-      <div className="flex gap-1">
-        <Clock className="fill-gray-400 w-[16px]" />
-        <span className="text-gray-600 leading-5">{clock}</span>
+    <div className="flex items-center gap-3 mb-3">
+      <div className="text-neutral600 leading-5">{timezoneFormat}</div>
+      <div className="w-px h-[16px] bg-neutral600" />
+      <div className="flex gap-1 items-center">
+        <Clock className="fill-neutral500 w-[16px]" />
+        <div className="text-neutral600 leading-5">{clock}</div>
       </div>
     </div>
   );
