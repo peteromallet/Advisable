db18d455b8476ddbcf01321824b5dca57462b027
guandjoy <guandjoy@gmail.com>
2022-04-10 22:49:20 +0100

    Update colouring and spacing of AdvisableComment box

--- app/javascript/src/views/CaseStudyArticle/components/AdvisableComment.js
+++ app/javascript/src/views/CaseStudyArticle/components/AdvisableComment.js
@@ -4,12 +4,14 @@ export default function AdvisableComment({ caseStudy }) {
   if (!caseStudy.company?.name && !caseStudy.companyType?.[0]) return null;
 
   return (
-    <div className="px-6 pt-4 pb-5 mb-5 bg-gray-50 rounded-lg border-gray-300 border-solid border">
-      <p className="font-[550] text-lg text-slate-900">
+    <div className="px-6 py-4 mb-5 bg-neutral50 rounded-lg ring-neutral200 ring-1 ring-inset">
+      <h6 className="font-[550] text-lg leading-5 text-neutral900 pt-[3px] pb-px mb-1">
         We&apos;re recommending {caseStudy.specialist.firstName} base on their
         project with {caseStudy.company?.name || caseStudy.companyType?.[0]}
+      </h6>
+      <p className="text-lg leading-6 text-neutral900 pt-px pb-[3px] font-[350]">
+        {caseStudy.comment}
       </p>
-      <p className="text-lg font-[350]">{caseStudy.comment}</p>
     </div>
   );
 }
