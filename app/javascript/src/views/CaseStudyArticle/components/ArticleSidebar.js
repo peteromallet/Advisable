8cae92278982b835a27c0b92416da18a5e1d4406
guandjoy <guandjoy@gmail.com>
2022-04-11 09:30:28 +0100

    Update colouring, spacing, and interaction of Navigation

--- app/javascript/src/views/CaseStudyArticle/components/ArticleSidebar.js
+++ app/javascript/src/views/CaseStudyArticle/components/ArticleSidebar.js
@@ -1,22 +1,24 @@
 import React from "react";
 
 const SectionType = ({ children }) => (
-  <div className="p-2 text-sm font-[650] leading-4 uppercase">{children}</div>
+  <div className="pl-2 py-2 leading-5 text-sm text-neutral700 font-[650] uppercase">
+    {children}
+  </div>
 );
 
 const SectionTypeHover = ({ children }) => (
-  <div className="bg-white group-hover:drop-shadow-lg transition-shadow rounded-xs hidden absolute top-0 left-2 right-0 group-hover:block z-10 pointer-events-none overflow-visible">
+  <div className="bg-white group-hover:drop-shadow-lg transition-shadow rounded-xs hidden absolute top-0 left-2 right-0 pb-2 group-hover:block z-10 pointer-events-none overflow-visible">
     {children}
   </div>
 );
 
 const Header = ({ children }) => (
-  <div className="p-2 leading-4">{children}</div>
+  <div className="pl-2 text-[15px] text-neutral700">{children}</div>
 );
 
 const Heading = ({ item }) => (
   <a href={`#${item.id}`}>
-    <div className="group cursor-pointer relative overflow-visible pl-2">
+    <div className="group relative overflow-visible pl-2">
       <SectionType>{item.section.type}</SectionType>
       <SectionTypeHover>
         <SectionType>{item.section.type}</SectionType>
@@ -28,11 +30,20 @@ const Heading = ({ item }) => (
 
 const Subheading = ({ item }) => {
   return (
-    <a key={item.id} href={`#${item.id}`}>
-      <h6 className="leading-4 p-2 ml-2 pl-6 cursor-pointer hover:bg-white rounded-xs hover:shadow-lg hover:z-20">
-        {item.text}
-      </h6>
-    </a>
+    <div className="ml-2">
+      <a href={`#${item.id}`}>
+        <div className="group relative overflow-visible py-2 pl-6 pr-2">
+          <h6 className="line-clamp-1 leading-5 text-neutral600 text-[15px] font-[450]">
+            {item.text}
+          </h6>
+          <div className="bg-white group-hover:drop-shadow-lg transition-shadow rounded-xs hidden absolute top-0 left-0 right-0 py-2 pl-6 pr-2 group-hover:block z-10 pointer-events-none overflow-visible">
+            <h6 className="leading-5 text-neutral600 text-[15px] font-[450]">
+              {item.text}
+            </h6>
+          </div>
+        </div>
+      </a>
+    </div>
   );
 };
 
@@ -56,8 +67,8 @@ export default function ArticleSidebar({ elements, scrollState }) {
 
   return (
     <div>
-      <div className="min-w-[320px] sticky top-[164px]">
-        <div className="absolute top-0 bottom-0 w-0.5 bg-gray-200">
+      <div className="min-w-[320px] sticky top-[164px] pt-px pb-[3px]">
+        <div className="absolute top-1 bottom-2 w-0.5 bg-gray-200">
           <div
             style={{ height: `${(100 / numOfItems) * (activeMenu + 1)}%` }}
             className={`absolute top-0 left-0 right-0 bg-blue-600 transition-height`}
