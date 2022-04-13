878ece4f16f8276f31f8001f74a98e6c2dface22
Thomas Cullen <thomascullen92@gmail.com>
2022-04-13 15:04:26 +0100

    Fix failing spec

--- spec/system/client_signup_spec.rb
+++ spec/system/client_signup_spec.rb
@@ -14,39 +14,44 @@ RSpec.describe "Client signup", type: :system do
   end
 
   it "Client can create an account and gets redirected to projects" do
-    visit("/clients/join")
-    fill_in("firstName", with: "Michael")
-    fill_in("lastName", with: "Scott")
-    fill_in("email", with: "michael+scott@dundermifflin.com")
-    click_on("Create Your Free Account")
-    expect(page).to have_content("Welcome to Advisable!")
-    fill_in("password", with: "testing123")
-    fill_in("passwordConfirmation", with: "testing123")
-    click_on("Continue")
+    Sidekiq::Testing.inline! do
+      visit("/clients/join")
+      fill_in("firstName", with: "Michael")
+      fill_in("lastName", with: "Scott")
+      fill_in("email", with: "michael+scott@dundermifflin.com")
+      click_on("Create Your Free Account")
+      expect(page).to have_content("Welcome to Advisable!")
+      fill_in("password", with: "testing123")
+      fill_in("passwordConfirmation", with: "testing123")
+      click_on("Continue")
 
-    expect(page).to have_content("Welcome to Advisable")
-    click_link("Get Started")
+      expect(page).to have_content("Welcome to Advisable")
+      click_link("Get Started")
 
-    # Company step
-    fill_in("name", with: "Dunder Mifflin")
-    select("I'm looking to hire someone", from: "intent")
-    click_on("Continue")
+      # Company step
+      fill_in("name", with: "Dunder Mifflin")
+      select("I'm looking to hire someone", from: "intent")
+      click_on("Continue")
 
-    # Industry step
-    find("*[data-testid=industry]", text: "Development").click
+      # Industry step
+      find("*[data-testid=industry]", text: "Development").click
 
-    # Customer
-    fill_in("audience", with: "I'm looking for a developer")
-    click_on("Continue")
+      # Customer
+      fill_in("audience", with: "I'm looking for a developer")
+      click_on("Continue")
 
-    # Interests
-    first(:button, "Creative PR Strategy").click
-    first(:button, "Improve SEO Rankings").click
-    click_on("Continue")
+      # Interests
+      first(:button, "Creative PR Strategy").click
+      first(:button, "Improve SEO Rankings").click
+      click_on("Continue")
 
-    expect(page).to have_content("Setting up your feed")
-    # We have an intentional 5 second delay on the 'setting up your feed' step.
-    sleep(5)
-    expect(page).to have_current_path("/explore")
+      expect(page).to have_content("Setting up your feed")
+      CaseStudy::Interest.all.each do |interest|
+        create(:case_study_interest_article, interest:)
+      end
+      # We have an intentional 5 second delay on the 'setting up your feed' step.
+      sleep(6)
+      expect(page).to have_current_path("/explore")
+    end
   end
 end
