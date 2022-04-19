ff091ee88cfed5b192f8f8c685723c1d581a336e
guandjoy <guandjoy@gmail.com>
2022-04-19 12:25:24 +0100

    Implement Title responsiveness

--- app/javascript/src/views/CaseStudyArticle/components/ArticleIntro.js
+++ app/javascript/src/views/CaseStudyArticle/components/ArticleIntro.js
@@ -4,6 +4,32 @@ import Achievements from "./Achievements";
 import AdvisableComment from "./AdvisableComment";
 import SpecialistCompanyRelation from "./SpecialistCompanyRelation";
 
+const Title = ({ children }) => (
+  <h1
+    className={`
+        text-neutral800
+          font-extrabold
+          tracking-tight
+          mb-7
+          text-2xl 
+          leading-none
+          pt-[3px]
+          pb-px
+          sm:text-3xl 
+          sm:leading-8
+          sm:pt-px
+          sm:pb-[3px]
+          md:text-4xl
+          md:leading-none
+          xl:text-5xl
+          xl:pt-0.5
+          xl:pb-0.5
+      `}
+  >
+    {children}
+  </h1>
+);
+
 export default function ArticleIntro({ caseStudy }) {
   return (
     <div>
@@ -11,9 +37,7 @@ export default function ArticleIntro({ caseStudy }) {
         company={caseStudy.company}
         specialist={caseStudy.specialist}
       />
-      <h1 className="text-5xl text-neutral800 leading-[48px] pt-0.5 pb-0.5 font-extrabold mb-7 tracking-tight">
-        {caseStudy.title}
-      </h1>
+      <Title>{caseStudy.title}</Title>
       <CompanyBox caseStudy={caseStudy} />
       <AdvisableComment caseStudy={caseStudy} />
       <Achievements sections={caseStudy.sections} />
