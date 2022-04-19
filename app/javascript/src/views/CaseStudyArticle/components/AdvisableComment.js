0a0ce77e0e4fecc8d9cb0316f62b3caddd711b9c
guandjoy <guandjoy@gmail.com>
2022-04-19 19:19:08 +0100

    Implement responsiveness of AdvisableComment

--- app/javascript/src/views/CaseStudyArticle/components/AdvisableComment.js
+++ app/javascript/src/views/CaseStudyArticle/components/AdvisableComment.js
@@ -4,12 +4,12 @@ export default function AdvisableComment({ caseStudy }) {
   if (!caseStudy.company?.name && !caseStudy.companyType?.[0]) return null;
 
   return (
-    <div className="px-6 py-4 mb-6 bg-neutral50 rounded-lg ring-neutral200 ring-1 ring-inset">
-      <h6 className="font-[550] text-lg leading-5 text-neutral900 pt-[3px] pb-px mb-1">
+    <div className="px-4 xl:px-6 pt-3 pb-4 xl:py-4 mb-6 bg-neutral50 rounded-[20px] xl:rounded-lg ring-neutral200 ring-1 ring-inset">
+      <h6 className="font-[550] text-base leading-none xl:text-lg xl:leading-5 text-neutral900 py-0.5 xl:pt-[3px] xl:pb-px mb-1">
         We&apos;re recommending {caseStudy.specialist.firstName} base on their
         project with {caseStudy.company?.name || caseStudy.companyType?.[0]}
       </h6>
-      <p className="text-lg leading-6 text-neutral900 pt-px pb-[3px] font-[350]">
+      <p className="text-base leading-5 xl:text-lg xl:leading-6 text-neutral900 xl:pt-px xl:pb-[3px] font-[350]">
         {caseStudy.comment}
       </p>
     </div>
