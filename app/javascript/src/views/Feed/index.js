b507bd81be928dc80abd577f05a37b2078d2aed5
Thomas Cullen <thomascullen92@gmail.com>
2022-04-19 09:58:47 +0100

    Navigate to /setup if onboarding isnt complete

--- app/javascript/src/views/Feed/index.js
+++ app/javascript/src/views/Feed/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import FeedSidebar from "./components/FeedSidebar";
 import Interest from "./Interest";
 import MainFeed from "./MainFeed";
 import { ErrorBoundary } from "react-error-boundary";
 import { useBackground } from "src/../../../donut/src";
 import BottomBar from "./components/BottomBar";
+import useTutorial from "src/hooks/useTutorial";
 
 function FeedViewFailed() {
   return (
@@ -18,6 +19,11 @@ function FeedViewFailed() {
 
 export default function Feed() {
   useBackground("beige");
+  const onboarding = useTutorial("onboarding");
+
+  if (!onboarding.isComplete) {
+    return <Navigate to="/setup" />;
+  }
 
   return (
     <div className="flex">
