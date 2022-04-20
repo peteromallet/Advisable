31e29277d6f5cb54030c74d9ceab4b2b10af8b8c
guandjoy <guandjoy@gmail.com>
2022-04-20 11:46:43 +0100

    Set styling of Links

--- app/javascript/src/views/CaseStudyArticle/components/Paragraph.js
+++ app/javascript/src/views/CaseStudyArticle/components/Paragraph.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { motion } from "framer-motion";
 import Linkify from "linkify-react";
 import renderLineBreaks from "src/utilities/renderLineBreaks";
 
 export default function CaseStudyParagraph({ text, ...props }) {
   return (
-    <motion.p
+    <p
       className="font-[350] text-xl text-neutral900 leading-8 pt-px pb-[3px] mb-9"
       {...props}
     >
-      <Linkify>{renderLineBreaks(text)}</Linkify>
-    </motion.p>
+      <Linkify
+        options={{
+          className: "text-blue600 hover:text-blue500 hover:underline",
+        }}
+      >
+        {renderLineBreaks(text)}
+      </Linkify>
+    </p>
   );
 }
