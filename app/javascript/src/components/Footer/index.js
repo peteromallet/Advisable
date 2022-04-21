c4ac90f48faae83acbda23263f5b97a98f60eedc
Thomas Cullen <thomascullen92@gmail.com>
2022-04-22 00:09:00 +0100

    Improve spacing in footer

--- app/javascript/src/components/Footer/index.js
+++ app/javascript/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import css from "@styled-system/css";
-import { Box, Text } from "@advisable/donut";
+import { Box } from "@advisable/donut";
 import { Twitter, LinkedinIn } from "@styled-icons/fa-brands";
 
 function SocialLink(props) {
@@ -24,26 +24,16 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <Box
-      padding={8}
-      display="flex"
-      justifyContent="space-between"
-      borderTop="1px solid"
-      borderColor="neutral100"
-    >
-      <Box>
-        <Text fontSize="sm" color="neutral600">
-          &copy; {year} Advisable
-        </Text>
-      </Box>
-      <Box>
+    <div className="py-8 px-[90px] flex justify-between border-t border-solid border-neutral100">
+      <div className="text-neutral600">&copy; {year} Advisable</div>
+      <div>
         <SocialLink href="https://ie.linkedin.com/company/advisable-com">
           <LinkedinIn size={20} />
         </SocialLink>
         <SocialLink href="https://twitter.com/advisablehq">
           <Twitter size={20} />
         </SocialLink>
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 }
