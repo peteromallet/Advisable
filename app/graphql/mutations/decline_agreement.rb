2dfd2866aa4f57652e00ae88361203887801d5f8
Miha Rekar <miha.rekar@advisable.com>
2022-04-22 09:57:51 +0200

    Fix conversation finding when impersonating a user

--- app/graphql/mutations/decline_agreement.rb
+++ app/graphql/mutations/decline_agreement.rb
@@ -22,7 +22,7 @@ module Mutations
       ApiError.invalid_request("NOT_IN_AN_DECLINABLE_STATE") unless agreement.acceptable?
 
       current_account_responsible_for { agreement.update!(status: "declined", reason: args[:message]) }
-      conversation = Conversation.by_accounts(agreement.specialist, current_account)
+      conversation = Conversation.by_accounts(agreement.specialist, current_user.account)
       conversation.new_message!(kind: "AgreementDeclined", agreement:, send_emails: false)
       conversation.new_message!(author: current_account, content: args[:message], send_emails: false) if args[:message].present?
       Slack.bg_message(channel: "consultation_requests", text: "#{current_user.name_with_company} has declined #{agreement.specialist.account.name} agreement (#{agreement.uid}). They provided the following reason: \"#{args[:message]}\".")
