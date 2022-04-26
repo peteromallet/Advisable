11812cf788d08dfce656654d5968b3f5274b203b
Thomas Cullen <thomascullen92@gmail.com>
2022-04-26 17:22:59 +0100

    Improve feed empty state

--- app/javascript/src/views/Feed/components/EmptyFeed.js
+++ app/javascript/src/views/Feed/components/EmptyFeed.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function EmptyFeed() {
   return (
-    <div className="max-w-400px mx-auto text-center">
+    <div className="bg-neutral50 p-8 rounded-[32px] max-w-400px mx-auto text-center">
       <h4 className="font-semibold">No results</h4>
       <p>It looks, like you haven't added any interests yet.</p>
       <Link to="/setup" className="text-blue600 hover:text-blue700">
