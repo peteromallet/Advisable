8e0942f561cc15981e1036aea58c16dd5fc8fb85
Miha Rekar <miha.rekar@advisable.com>
2022-04-26 09:23:41 +0200

    Don't even try to charge when no payment method

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -37,11 +37,12 @@ RSpec.describe Payment, type: :model do
   end
 
   describe "#charge!" do
-    let(:payment) { create(:payment, payment_request:, amount: 1000) }
-    let(:payment_request) { create(:payment_request) }
+    let(:company) { create(:company, stripe_payment_method: "asdf1234") }
+    let(:payment_request) { create(:payment_request, company:) }
+    let(:payment) { create(:payment, company:, payment_request:, amount: 1000) }
 
     it "charges stripe with full amount and schedules invoice creation" do
-      allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1050), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
+      allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1050, payment_method: "asdf1234"), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
       expect(payment_request.reload.status).to eq("pending")
       payment.charge!
       expect(payment.payment_method).to eq("Stripe")
@@ -53,7 +54,7 @@ RSpec.describe Payment, type: :model do
       before { allow(payment.company).to receive(:apply_vat?).and_return(true) }
 
       it "charges stripe with full amount in integer and schedules invoice creation" do
-        allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1292), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
+        allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1292, payment_method: "asdf1234"), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
         expect(payment_request.reload.status).to eq("pending")
         payment.charge!
         expect(payment.payment_method).to eq("Stripe")
@@ -65,11 +66,24 @@ RSpec.describe Payment, type: :model do
     context "when pdf is already generated" do
       it "does not schedule invoice creation" do
         payment.pdf_key = "some_key"
-        allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1050), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
+        allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1050, payment_method: "asdf1234"), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
         expect(payment_request.reload.status).to eq("pending")
         payment.charge!
         expect(GeneratePaymentInvoiceJob).not_to have_been_enqueued.with(payment, notify: true)
         expect(payment_request.reload.status).to eq("paid")
+      end
+    end
+
+    context "when company does not have a payment method" do
+      let(:company) { create(:company, stripe_payment_method: nil) }
+
+      it "creates an intent without payment method and keeps request pending" do
+        allow(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1050, setup_future_usage: "off_session"), anything).and_return(OpenStruct.new(id: "pi_#{SecureRandom.uuid}", status: "succeeded"))
+        expect(payment_request.reload.status).to eq("pending")
+        payment.charge!
+        expect(payment.payment_method).to eq("Stripe")
+        expect(GeneratePaymentInvoiceJob).to have_been_enqueued.with(payment, notify: true).once
+        expect(payment_request.reload.status).to eq("pending")
       end
     end
   end
