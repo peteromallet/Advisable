11a2532c3bf92be642eae4768f6e2d9fd18e8a51
guandjoy <guandjoy@gmail.com>
2022-04-28 10:12:03 +0100

    Fix typo in EmptyFeed component

--- app/javascript/src/views/Feed/components/EmptyFeed.js
+++ app/javascript/src/views/Feed/components/EmptyFeed.js
@@ -5,7 +5,7 @@ export default function EmptyFeed() {
   return (
     <div className="bg-neutral50 p-8 rounded-[32px] max-w-400px mx-auto text-center">
       <h4 className="font-semibold">No results</h4>
-      <p>It looks, like you haven't added any interests yet.</p>
+      <p>It looks like you haven&apos;t added any interests yet.</p>
       <Link to="/setup" className="text-blue600 hover:text-blue700">
         Setup interests
       </Link>
