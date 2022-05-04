9993638b4486078017c41967a4ecb067be9c9135
guandjoy <guandjoy@gmail.com>
2022-05-04 12:17:04 +0100

    Update account_delete_job_spec

--- spec/jobs/account_delete_job_spec.rb
+++ spec/jobs/account_delete_job_spec.rb
@@ -4,13 +4,18 @@ require "rails_helper"
 
 RSpec.describe AccountDeleteJob do
   it "deletes accounts marked for deletion in the past 14 days" do
-    fresh = create(:specialist, account: create(:account, deleted_at: 13.days.ago))
-    stale = create(:specialist, account: create(:account, deleted_at: 15.days.ago))
+    fresh_specialist = create(:specialist, account: create(:account, deleted_at: 13.days.ago))
+    stale_specialist = create(:specialist, account: create(:account, deleted_at: 15.days.ago))
+    fresh_user = create(:user, account: create(:account, deleted_at: 13.days.ago))
+    stale_user = create(:user, account: create(:account, deleted_at: 15.days.ago))
 
     allow_any_instance_of(Specialist).to receive(:remove_from_airtable)
+    allow_any_instance_of(User).to receive(:remove_from_airtable)
     described_class.perform_now
 
-    expect(Specialist.find_by(id: fresh.id)).not_to be_nil
-    expect(Specialist.find_by(id: stale.id)).to be_nil
+    expect(Specialist.find_by(id: fresh_specialist.id)).not_to be_nil
+    expect(Specialist.find_by(id: stale_specialist.id)).to be_nil
+    expect(User.find_by(id: fresh_user.id)).not_to be_nil
+    expect(User.find_by(id: stale_user.id)).to be_nil
   end
 end
