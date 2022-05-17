20dfa33a2bc941599f9b09efbea492621b983b9c
guandjoy <guandjoy@gmail.com>
2022-05-17 17:35:57 +0100

    Implement ShareArticleButton

---
+++ app/javascript/src/views/CaseStudyArticle/components/ShareArticleButton.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Link } from "@styled-icons/heroicons-outline";
+import composeStyles from "src/utilities/composeStyles";
+import { Tooltip } from "@advisable/donut";
+
+const buttonClasses = composeStyles({
+  base: `
+    group
+    flex
+    justify-center
+    items-center
+    ring-1
+    ring-inset
+    ring-neutral200
+    hover:ring-2
+    hover:ring-neutral300
+    rounded-full
+  `,
+  variants: {
+    size: {
+      sm: `h-8 min-w-[32px]`,
+      md: `h-10 min-w-[40px]`,
+    },
+  },
+});
+
+const iconClasses = composeStyles({
+  base: `
+    fill-none
+    stroke-neutral600
+    group-hover:stroke-neutral900
+  `,
+  variants: {
+    size: {
+      sm: `h-[16px] w-[16px]`,
+      md: `h-[20px] w-[20px]`,
+    },
+    active: `fill-blue500 !stroke-blue500 group-hover:fill-blue500`,
+  },
+});
+
+function ShareArticleButton({ slug, className, size }) {
+  const [copied, setCopied] = useState(false);
+  const url = `${location.origin}/articles/${slug}`;
+
+  const handleClick = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(url);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <Tooltip placement="bottom" content={copied ? "Link copied!" : "Copy Link"}>
+      <button
+        className={buttonClasses({ className, size })}
+        aria-label="Copy Link"
+        onClick={handleClick}
+      >
+        <Link className={iconClasses({ size })} />
+      </button>
+    </Tooltip>
+  );
+}
+
+ShareArticleButton.defaultProps = {
+  size: "md",
+};
+
+export default ShareArticleButton;
