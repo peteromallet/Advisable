1c7b99db87f706d916c40aad4e411be7db103ba8
guandjoy <guandjoy@gmail.com>
2022-05-23 18:35:26 +0100

    Create PaymentRequestCompleted message

--- app/javascript/src/views/Messages/components/SystemMessage/PaymentRequestCompleted.js
+++ app/javascript/src/views/Messages/components/SystemMessage/PaymentRequestCompleted.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "@advisable/donut";
 
-export default function PaymentRequestCompleted() {
-  return <div>Payment Request Completed</div>;
+export default function PaymentRequestCompleted({ message }) {
+  const { id, company } = message.paymentRequest;
+
+  return (
+    <div
+      id={message.id}
+      className="p-5 pb-5 pt-5 w-full rounded-lg border-2 border-solid border-neutral100 text-center"
+    >
+      <div className="text-[17px] leading-5 mb-2 text-neutral900">
+        <strong className="font-[550]">{company.name}</strong> fulfilled payment
+        request
+      </div>
+      <Link variant="underlined" to={`/payment_requests/${id}`}>
+        See details
+      </Link>
+    </div>
+  );
 }
