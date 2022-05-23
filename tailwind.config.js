08ffae2f01b05c0971ff7ffaeb41c8e9853f8095
guandjoy <guandjoy@gmail.com>
2022-05-23 16:31:34 +0100

    Customize tailwind shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,12 @@ module.exports = {
         feed: "0 2px 4px rgba(0, 0, 0, 0.08), 0px 4px 16px 0px rgba(0, 0, 0, 0.04)",
         articleCard:
           "0px 12px 40px -12px rgba(0, 0, 0, 0.12), 0px 2px 8px rgba(0, 0, 0, 0.04)",
+        sm: "0 1px 2px 1px rgb(0 0 0 / 0.05)",
+        base: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        md: "0 2px 6px -1px rgb(0 0 0 / 0.1), 0 0px 4px -2px rgb(0 0 0 / 0.1)",
+        lg: "0 3px 15px -3px rgb(0 0 0 / 0.1), 0 1px 6px -4px rgb(0 0 0 / 0.1)",
+        xl: "0 8px 25px -5px rgb(0 0 0 / 0.1), 0 3px 10px -6px rgb(0 0 0 / 0.1)",
+        "2xl": "0 10px 50px -12px rgb(0 0 0 / 0.25)",
       },
     },
   },
