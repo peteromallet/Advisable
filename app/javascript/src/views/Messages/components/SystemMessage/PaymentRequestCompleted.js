230478d9f1fe53fd1db2cc7da5f74e2bf74fa60f
guandjoy <guandjoy@gmail.com>
2022-05-24 16:07:42 +0100

    Make improvements based on review

--- app/javascript/src/views/Messages/components/SystemMessage/PaymentRequestCompleted.js
+++ app/javascript/src/views/Messages/components/SystemMessage/PaymentRequestCompleted.js
@@ -7,11 +7,11 @@ export default function PaymentRequestCompleted({ message }) {
   return (
     <div
       id={message.id}
-      className="p-5 pb-5 pt-5 w-full rounded-lg border-2 border-solid border-neutral100 text-center"
+      className="p-5 w-full rounded-lg border-2 border-solid border-neutral100 text-center"
     >
-      <div className="text-[17px] leading-5 mb-2 text-neutral900">
-        <strong className="font-[550]">{company.name}</strong> fulfilled payment
-        request
+      <div className="leading-5 mb-2 text-neutral900">
+        <strong className="font-semibold">{company.name}</strong> fulfilled
+        payment request
       </div>
       <Link variant="underlined" to={`/payment_requests/${id}`}>
         See details
