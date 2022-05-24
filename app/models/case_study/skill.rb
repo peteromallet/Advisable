569ed7dfbd75eb6e82ca2740813a368523e7878e
Miha Rekar <miha.rekar@advisable.com>
2022-05-24 09:32:24 +0200

    Drop deprecated tables

--- app/models/case_study/skill.rb
+++ app/models/case_study/skill.rb
@@ -10,7 +10,6 @@ module CaseStudy
     scope :primary, -> { where(primary: true) }
 
     belongs_to :article, optional: true
-    belongs_to :search, optional: true
     belongs_to :skill, class_name: "::Skill", inverse_of: :case_study_skills
   end
 end
@@ -25,19 +24,16 @@ end
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  article_id :bigint
-#  search_id  :bigint
 #  skill_id   :bigint           not null
 #
 # Indexes
 #
 #  index_case_study_skills_on_article_id  (article_id)
-#  index_case_study_skills_on_search_id   (search_id)
 #  index_case_study_skills_on_skill_id    (skill_id)
 #  index_case_study_skills_on_uid         (uid) UNIQUE
 #
 # Foreign Keys
 #
 #  fk_rails_...  (article_id => case_study_articles.id)
-#  fk_rails_...  (search_id => case_study_searches.id)
 #  fk_rails_...  (skill_id => skills.id)
 #
