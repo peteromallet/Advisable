4f5737f491cf9552e64b154163497419905430ce
guandjoy <guandjoy@gmail.com>
2022-05-25 17:08:19 +0100

    Rename Testimonial component as Review

--- app/javascript/src/views/CaseStudyArticle/components/ArticleContent.js
+++ app/javascript/src/views/CaseStudyArticle/components/ArticleContent.js
@@ -4,7 +4,7 @@ import Heading from "./Heading";
 import Paragraph from "./Paragraph";
 import ArticleSidebar from "./ArticleSidebar";
 import SimilarArticles from "./SimilarArticles";
-import Testimonial from "./Testimonial";
+import Review from "./Review";
 
 const CONTENT_TYPES = {
   Images,
@@ -38,7 +38,7 @@ export default function ArticleContent({ caseStudy }) {
             data-content-block={index}
           />
         ))}
-        <Testimonial
+        <Review
           data-content-block={elements.length}
           review={caseStudy.review}
         />
