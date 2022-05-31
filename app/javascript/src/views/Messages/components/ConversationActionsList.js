2b8022446e5c89cf1a36a98d2666d1df0a5cc50a
Thomas Cullen <thomascullen92@gmail.com>
2022-05-31 09:50:01 +0100

    Fix conversation actions list styling

--- app/javascript/src/views/Messages/components/ConversationActionsList.js
+++ app/javascript/src/views/Messages/components/ConversationActionsList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ConversationActionsList({ children }) {
   return (
-    <div className="w-fulldivide-y divide-solid divide-neutral100">
+    <div className="w-full divide-y divide-solid divide-neutral100">
       {children}
     </div>
   );
