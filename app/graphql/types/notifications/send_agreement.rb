7d2d603672e35e66a585484810e71af2de5bce8c
Miha Rekar <miha.rekar@advisable.com>
2022-06-13 09:49:42 +0200

    Get User and Specialist on SendAgreement type

--- app/graphql/types/notifications/send_agreement.rb
+++ app/graphql/types/notifications/send_agreement.rb
@@ -6,6 +6,16 @@ module Types
       implements Types::NotificationInterface
 
       field :interview, Types::Interview, null: false
+
+      field :specialist, Types::SpecialistType, null: false
+      def specialist
+        object.interview&.specialist
+      end
+
+      field :user, Types::User, null: false
+      def user
+        object.interview&.user
+      end
     end
   end
 end
