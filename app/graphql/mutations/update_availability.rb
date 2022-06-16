cd001848d85e669b5a8bd92501f362d1bb497893
Miha Rekar <miha.rekar@advisable.com>
2022-06-16 12:19:04 +0200

    Remove time_zone updating

--- app/graphql/mutations/update_availability.rb
+++ app/graphql/mutations/update_availability.rb
@@ -4,9 +4,6 @@ module Mutations
   class UpdateAvailability < Mutations::BaseMutation
     argument :availability, [String], required: true, description: "An array of ISO strings"
 
-    # Do we need this anymore now that we have #timezone on Account?
-    argument :time_zone, String, required: false
-
     # This should now handle Specialist so you need to update the frontend
     field :user, Types::User, null: true
 
@@ -14,10 +11,9 @@ module Mutations
       requires_current_user!
     end
 
-    def resolve(**args)
+    def resolve(availability:)
       current_account_responsible_for do
-        current_user.update!(time_zone: args[:time_zone]) if args[:time_zone] && current_user.respond_to?(:time_zone)
-        current_user.account.update!(availability: args[:availability])
+        current_user.account.update!(availability:)
       end
       {user: current_user}
     end
