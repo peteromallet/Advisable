8b1124a0572b7f51363d1137869c86da446b7ffa
guandjoy <guandjoy@gmail.com>
2022-06-20 11:14:17 +0100

    Update styling of request call button

--- app/javascript/src/views/Messages/components/ConversationCalls.js
+++ app/javascript/src/views/Messages/components/ConversationCalls.js
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 import { PlusSm } from "@styled-icons/heroicons-outline";
 import { VideoCamera } from "@styled-icons/heroicons-solid";
 import { Modal, useModal, DialogDisclosure } from "@advisable/donut";
-import ConversationAction from "./ConversationAction";
-import ConversationActionsList from "./ConversationActionsList";
 import ConversationCallRequest from "./ConversationCallRequest";
 
 function EmptyState({ firstName }) {
@@ -26,7 +24,7 @@ function UpcomingCall({ interview }) {
       key={interview.id}
       to={`/interviews/${interview.id}`}
       state={{ back: true }}
-      className="ring-1 ring-neutral200 hover:ring-2 hover:ring-blue300 transition-shadow p-4 rounded-sm block bg-white"
+      className="ring-1 ring-neutral200 hover:ring-2 hover:ring-blue400 transition-shadow p-4 rounded-sm block bg-white"
     >
       <div className="flex gap-2 items-center pb-0.5">
         <div className="min-w-[32px] color-blue200 bg-blue50 h-[32px] p-2 flex rounded-full">
@@ -43,6 +41,21 @@ function UpcomingCall({ interview }) {
   );
 }
 
+function RequestCallAction() {
+  return (
+    <div className="border-2 border-dashed border-neutral100 rounded-sm p-4 hover:bg-white hover:border-blue400 hover:border-solid">
+      <div className="flex gap-2 items-center pb-0.5">
+        <div className="min-w-[32px] bg-neutral100 h-[32px] p-2 flex rounded-full">
+          <PlusSm className="stroke-neutral700" />
+        </div>
+        <div className="font-medium text-neutral800 leading-none">
+          Request a call
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function ConversationCalls({ conversation }) {
   const other = conversation.participants.find((p) => !p.isViewer);
   const calls = other.upcomingInterviews;
@@ -52,7 +65,7 @@ export default function ConversationCalls({ conversation }) {
   return (
     <div className="p-8 border-t border-solid border-neutral100">
       <h4 className="leading-none font-medium mb-3">Upcoming calls</h4>
-      <div className="space-y-3 mb-1">
+      <div className="space-y-3 mb-3">
         {calls.map((interview) => (
           <UpcomingCall key={interview.id} interview={interview} />
         ))}
@@ -65,13 +78,9 @@ export default function ConversationCalls({ conversation }) {
       >
         <ConversationCallRequest specialist={other} dialog={dialog} />
       </Modal>
-      <ConversationActionsList>
-        <DialogDisclosure {...dialog}>
-          <ConversationAction icon={PlusSm} variant="neutral">
-            Request a call
-          </ConversationAction>
-        </DialogDisclosure>
-      </ConversationActionsList>
+      <DialogDisclosure {...dialog} className="w-full">
+        <RequestCallAction />
+      </DialogDisclosure>
     </div>
   );
 }
