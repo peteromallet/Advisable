a667aade26e988d02c03e836b669927c43a3b08b
Miha Rekar <miha.rekar@advisable.com>
2022-07-06 14:22:18 +0200

    Do not send post interview emails if agreement exists

--- app/jobs/post_interview_job.rb
+++ app/jobs/post_interview_job.rb
@@ -4,11 +4,11 @@ class PostInterviewJob < ApplicationJob
   def perform
     Interview.scheduled.where(starts_at: (30.minutes.ago..Time.current)).each do |interview|
       interview.update(status: "Call Completed")
-      next unless interview.specialist_and_user?
+      next if !interview.specialist_and_user? || Agreement.exists?(specialist: interview.specialist, company: interview.user.company)
 
       SpecialistMailer.post_interview(interview).deliver_later
       UserMailer.post_interview(interview).deliver_later
-      Notification.create!(account: interview.specialist.account, action: "send_agreement", interview:) unless Agreement.exists?(company: interview.user.company, specialist: interview.specialist)
+      Notification.create!(account: interview.specialist.account, action: "send_agreement", interview:)
     end
   end
 end
