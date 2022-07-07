850c870e01e700be877d70f3795adc2d16d73ea9
Miha Rekar <miha.rekar@advisable.com>
2022-07-07 09:47:31 +0200

    Good practices

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
+
 class ApplicationJob < ActiveJob::Base
+  # Automatically retry jobs that encountered a deadlock
+  retry_on StandardError, wait: :exponentially_longer, attempts: 3
+
+  # Most jobs are safe to ignore if the underlying records are no longer available
+  discard_on ActiveJob::DeserializationError, ActiveStorage::IntegrityError
 end
