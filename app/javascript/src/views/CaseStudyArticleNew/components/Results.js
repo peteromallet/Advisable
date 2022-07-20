66bba7d47751048e7fa0c1b658134fc7dcef7b5c
guandjoy <guandjoy@gmail.com>
2022-07-20 21:01:08 +0100

    Create Results.js

---
+++ app/javascript/src/views/CaseStudyArticleNew/components/Results.js
@@ -0,0 +1,34 @@
+import { ChartBar, TrendingUp } from "@styled-icons/heroicons-outline";
+import React from "react";
+
+export default function Results({ results }) {
+  if (!results) return null;
+
+  return (
+    <div className="w-[280px] bg-gradient-to-b from-[#0082FD] to-[#692FFF] rounded-lg p-5 pb-8">
+      <div className="leading-none mb-4 uppercase text-xs font-semibold text-white">
+        Results
+      </div>
+      <div className="space-y-7">
+        <div>
+          <div className="flex gap-2">
+            <div className="w-[28px] h-[28px]">
+              <ChartBar className="stroke-white" />
+            </div>
+            <div className="text-white">$400,000</div>
+          </div>
+          <div className="text-white">{results[0]}</div>
+        </div>
+        <div>
+          <div className="flex gap-2">
+            <div className="w-[28px] h-[28px]">
+              <TrendingUp className="stroke-white" />
+            </div>
+            <div className="text-white">145%</div>
+          </div>
+          <div className="text-white">{results[1]}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
