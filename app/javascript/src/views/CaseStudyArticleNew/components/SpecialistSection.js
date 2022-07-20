8870e26fc177fd6f5b840d783d3541dafd3a75f3
guandjoy <guandjoy@gmail.com>
2022-07-20 21:01:04 +0100

    Create SpecialistSection.js

---
+++ app/javascript/src/views/CaseStudyArticleNew/components/SpecialistSection.js
@@ -0,0 +1,119 @@
+import { Briefcase, Chat, Tag } from "@styled-icons/heroicons-outline";
+import { Link, useModal, DialogDisclosure } from "@advisable/donut";
+import React from "react";
+import ConnectModal from "src/components/ConnectModal";
+import Button from "src/components/Button";
+import { LocationMarker } from "@styled-icons/heroicons-solid";
+
+const StyledIcon = ({ icon: Icon, color }) => {
+  const bgColors = {
+    red: "bg-red-100",
+    purple: "bg-purple-100",
+    cyan: "bg-cyan-100",
+  };
+
+  const strokeColors = {
+    red: "stroke-red-900",
+    purple: "stroke-purple-900",
+    cyan: "stroke-blue-900",
+  };
+
+  return (
+    <div
+      className={`${bgColors[color]} rounded-full p-2 min-w-[36px] w-[36px] h-[36px] flex items-center justify-center`}
+    >
+      <Icon className={strokeColors[color]} />
+    </div>
+  );
+};
+
+const StyledCategoryName = ({ children }) => (
+  <div className="text-xs font-medium text-neutral500 leading-none">
+    {children}
+  </div>
+);
+
+export default function SpecialistSection({ article }) {
+  const { specialist } = article;
+  const { name, location, bio, hourlyRate } = specialist;
+  const modal = useModal();
+
+  return (
+    <div className="p-10 min-w-[348px] w-[348px]">
+      <Link
+        className="mb-2 mx-auto rounded-full overflow-hidden w-[104px] h-[104px] border-2 border-neutral100 border-solid hover:border-neutral300"
+        to={specialist.profilePath}
+      >
+        {specialist.avatar && (
+          <img
+            src={specialist.avatar}
+            className="h-full w-full object-cover rounded-full border-2 border-white border-solid"
+          />
+        )}
+      </Link>
+
+      <Link to={specialist.profilePath} className="mb-2">
+        <div className="text-2xl font-bold text-blue900 tracking-tight hover:underline decoration-blue200">
+          {name}
+        </div>
+      </Link>
+
+      <div className="flex gap-1 items-center mb-4">
+        <div className="flex justify-center items-center">
+          <LocationMarker className="fill-neutral400" size="20px" />
+        </div>
+        <div className="text-neutral600 leading-none line-clamp-1">
+          {location}
+        </div>
+      </div>
+
+      <div className="text-neutral900 mb-6">{bio}</div>
+
+      <div className="space-y-4">
+        {hourlyRate && (
+          <div className="flex gap-2">
+            <StyledIcon icon={Tag} color="red" />
+            <div>
+              <StyledCategoryName>Hourly Rate</StyledCategoryName>
+              <div>${specialist.hourlyRate / 100}</div>
+            </div>
+          </div>
+        )}
+
+        <div className="flex gap-2">
+          <StyledIcon icon={Chat} color="purple" />
+          <div>
+            <StyledCategoryName>Available For</StyledCategoryName>
+            <div>Consultations, Mentoring and Hands-On Work</div>
+          </div>
+        </div>
+
+        <div className="flex gap-2">
+          <StyledIcon icon={Briefcase} color="cyan" />
+          <div>
+            <StyledCategoryName>Previous Roles</StyledCategoryName>
+            <div>See work history</div>
+          </div>
+        </div>
+      </div>
+
+      <ConnectModal modal={modal} specialist={specialist} article={article} />
+      {!specialist.unavailableUntil && (
+        <div className="mt-8">
+          <DialogDisclosure {...modal}>
+            {(disclosure) => (
+              <Button
+                className="w-full"
+                size="lg"
+                aria-label="Talk with specialist"
+                {...disclosure}
+              >
+                Contact
+              </Button>
+            )}
+          </DialogDisclosure>
+        </div>
+      )}
+    </div>
+  );
+}
