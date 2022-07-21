40560bbd2d3bc454fc50ce21cdeca98a0ebd7ac3
Thomas Cullen <thomascullen92@gmail.com>
2022-07-21 15:44:09 +0100

    Add scrollt to top component

---
+++ app/javascript/src/views/Explore/ScrollToTop.js
@@ -0,0 +1,45 @@
+import { ArrowUp } from "@styled-icons/heroicons-outline";
+import React, { useEffect, useState } from "react";
+import composeStyles from "src/utilities/composeStyles";
+
+const THRESHOLD = 200;
+
+const classes = composeStyles({
+  base: "fixed z-20 w-12 h-12 rounded-full shadow-xl bg-neutral-700 hover:bg-neutral-900 grid place-items-center bottom-4 left-4 opacity-0 invisible transition-opacity",
+  variants: {
+    enabled: "!visible !opacity-100",
+  },
+});
+
+export default function ScrollToTop() {
+  const [enabled, setEnabled] = useState(false);
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const checkScroll = () => {
+      const show = window.scrollY > THRESHOLD;
+      if (!enabled && show) {
+        setEnabled(true);
+      }
+
+      if (enabled && !show) {
+        setEnabled(false);
+      }
+    };
+
+    window.addEventListener("scroll", checkScroll);
+    return () => window.removeEventListener("scroll", checkScroll);
+  }, [enabled]);
+
+  return (
+    <button className={classes({ enabled })} onClick={handleClick}>
+      <ArrowUp className="w-5 h-5 text-white" />
+    </button>
+  );
+}
--- app/javascript/src/views/Explore/index.js
+++ app/javascript/src/views/Explore/index.js
@@ -6,6 +6,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Feed from "./Feed";
 import Trending from "./Trending";
 import Favorites from "./Favorites";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Explore() {
   const location = useLocation();
@@ -13,6 +14,7 @@ export default function Explore() {
   return (
     <>
       <div className="max-w-[1300px] mx-auto px-5 pb-10">
+        <ScrollToTop />
         <TopicsBar />
         <div className="h-px mb-8 bg-neutral-200" />
         <Routes location={location.state?.backgroundLocation || location}>
