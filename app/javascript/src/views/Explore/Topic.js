3f4904367e7b62b851da9039d5bd2e1eae6119b8
Thomas Cullen <thomascullen92@gmail.com>
2022-07-22 16:38:42 +0100

    Loading state for topic title and description when not available

--- app/javascript/src/views/Explore/Topic.js
+++ app/javascript/src/views/Explore/Topic.js
@@ -9,8 +9,9 @@ export default function Topic() {
   const { slug } = useParams();
   const { loading, data, fetchMore } = useTopic(slug);
 
-  const pageInfo = data?.topic?.articles?.pageInfo;
-  const edges = data?.topic?.articles?.edges || [];
+  const topic = data?.topic || {};
+  const pageInfo = topic?.articles?.pageInfo;
+  const edges = topic?.articles?.edges || [];
   const results = edges.map((n) => n.node);
 
   const handleLoadMore = useCallback(() => {
@@ -28,10 +29,20 @@ export default function Topic() {
   return (
     <>
       <div className="mb-8">
-        <h1 className="mb-1 text-4xl font-bold tracking-tight text-neutral900">
-          {data?.topic?.name}
-        </h1>
-        <p className="text-lg text-neutral-500">{data?.topic?.description}</p>
+        <div className="mb-1">
+          {!topic.name && loading ? (
+            <div className="max-w-[250px] h-[32px] bg-neutral-200 animate-pulse rounded-md mb-5" />
+          ) : (
+            <h1 className="text-4xl font-bold tracking-tight text-neutral900">
+              {topic?.name}
+            </h1>
+          )}
+        </div>
+        {!topic.description && loading ? (
+          <div className="max-w-[420px] h-[18px] bg-neutral-200 animate-pulse rounded-md" />
+        ) : (
+          <p className="text-lg text-neutral-500">{topic?.description}</p>
+        )}
       </div>
       <CaseStudyGrid loading={loading} results={results} />
       {pageInfo?.hasNextPage && <EndlessScroll onLoadMore={handleLoadMore} />}
