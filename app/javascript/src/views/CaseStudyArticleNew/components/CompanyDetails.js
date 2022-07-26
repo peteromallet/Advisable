e0fc5dba9fda2747b4349dd371acc6a1c560baff
guandjoy <guandjoy@gmail.com>
2022-07-26 10:38:58 +0100

    Update CompanyDetails.js

--- app/javascript/src/views/CaseStudyArticleNew/components/CompanyDetails.js
+++ app/javascript/src/views/CaseStudyArticleNew/components/CompanyDetails.js
@@ -1,10 +1,10 @@
-import {
-  ExternalLink,
-  OfficeBuilding,
-  Truck,
-} from "@styled-icons/heroicons-outline";
 import React from "react";
 import Favicon from "src/components/Favicon";
+import {
+  Briefcase,
+  ExternalLink,
+  Truck,
+} from "@styled-icons/heroicons-outline";
 
 export default function CompanyDetails({ caseStudy }) {
   if (!caseStudy.company) return null;
@@ -22,7 +22,7 @@ export default function CompanyDetails({ caseStudy }) {
       <div className="leading-none mb-1.5 uppercase text-xs font-semibold text-neutral500 tracking-wide">
         Company details
       </div>
-      <div className="flex gap-2 py-3 border-b border-b-neutral200 border-solid last:border-none">
+      <div className="flex gap-2 py-3 border-b border-b-neutral100 border-solid last:border-none">
         {favicon && <Favicon src={favicon} size="xs" />}
         {website ? (
           <a
@@ -42,7 +42,7 @@ export default function CompanyDetails({ caseStudy }) {
       </div>
 
       {companyType && (
-        <div className="flex gap-2 py-3 border-b border-b-neutral200 border-solid last:border-none">
+        <div className="flex gap-2 py-3 border-b border-b-neutral100 border-solid last:border-none">
           <div className="min-w-[24px] h-[24px]">
             <Truck className="stroke-neutral700" />
           </div>
@@ -53,7 +53,7 @@ export default function CompanyDetails({ caseStudy }) {
       {businessType && (
         <div className="flex gap-2 py-3">
           <div className="min-w-[24px] h-[24px]">
-            <OfficeBuilding className="stroke-neutral700" />
+            <Briefcase className="stroke-neutral700" />
           </div>
           <div className="text-neutral700 font-medium">{businessType}</div>
         </div>
