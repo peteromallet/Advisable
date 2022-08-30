38eaf23eb2bc5eec2866d1507471dbe7bf83c3aa
Miha Rekar <miha.rekar@advisable.com>
2022-08-30 09:40:07 +0200

    No more active

--- app/lib/airtable/skill.rb
+++ app/lib/airtable/skill.rb
@@ -5,16 +5,6 @@ module Airtable
     self.table_name = "Skills"
     sync_with ::Skill
 
-    def self.active
-      all(view: "Selectable Skills", sort: {"Name" => "asc"}).reject do |s|
-        s.fields["Name"].blank?
-      end
-    end
-
-    def self.find_by_name(name)
-      all(filter: "{Name} = '#{name}'").first
-    end
-
     push_data { |skill| self["Name"] = skill.name }
   end
 end
