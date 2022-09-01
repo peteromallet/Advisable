a5a4c0a2c0e6485b33f036dab48ddc5cca1a5583
Miha Rekar <miha.rekar@advisable.com>
2022-09-01 15:07:00 +0200

    Allow editing published articles

--- app/controllers/concerns/as_specialist.rb
+++ app/controllers/concerns/as_specialist.rb
@@ -4,7 +4,7 @@ module AsSpecialist
   private
 
   def admin_or_as_specialist?
-    @as_specialist = article.published_at.nil? && current_account&.specialist&.id == article.specialist_id
+    @as_specialist = current_account&.specialist&.id == article.specialist_id
     @as_specialist || admin?
   end
 end
