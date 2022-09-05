69c7c7a0d74d4fe581f683f08a4d74f1b6cc04c6
Thomas Cullen <thomascullen92@gmail.com>
2022-09-05 08:12:26 +0100

    Improve responsiveness of article signup overlay

--- app/javascript/src/views/ArticleModal/SignupOverlay.js
+++ app/javascript/src/views/ArticleModal/SignupOverlay.js
@@ -6,20 +6,20 @@ import GridLines from "src/components/GridLines";
 export default function SignupOverlay() {
   const location = useLocation();
   return (
-    <div className="grid absolute z-20 place-items-center w-full h-full bg-transparent-white-90 rounded-t-[32px]">
-      <div className="overflow-hidden bg-white rounded-xl shadow-2xl w-[500px]">
-        <div className="overflow-hidden relative p-10 bg-blue-100 bg-gradient-to-br from-indigo-900 to-violet-900">
+    <div className="grid absolute z-20 place-items-center p-4 w-full h-full bg-transparent-white-90 rounded-t-[32px]">
+      <div className="overflow-hidden bg-white rounded-xl shadow-2xl max-w-[500px]">
+        <div className="overflow-hidden relative p-6 bg-blue-100 bg-gradient-to-br from-indigo-900 to-violet-900 sm:p-10">
           <GridLines color="rgb(255 255 255 / .1)" highlight="#FFFFFF" />
           <div className="relative z-10">
-            <h1 className="mb-4 font-serif text-4xl font-bold tracking-tight text-white">
+            <h1 className="mb-2 font-serif text-2xl font-bold tracking-tight text-white sm:mb-4 sm:text-4xl">
               Explore 100s of full case studies for free
             </h1>
-            <p className="text-lg text-indigo-50 max-w-[400px]">
+            <p className="text-indigo-50 sm:text-lg max-w-[400px]">
               Access our library of SaaS growth projects from leading marketers.
             </p>
           </div>
         </div>
-        <div className="p-10">
+        <div className="p-6 sm:p-10">
           <Link to="/clients/join" className="block mb-4">
             <Button size="lg" className="w-full">
               Get full access
