93175b4ea9183a40de6b19096b735c52ab96bb12
Miha Rekar <miha.rekar@advisable.com>
2022-09-19 13:50:59 +0200

    Remove logo from CS Company

--- app/models/case_study/company.rb
+++ app/models/case_study/company.rb
@@ -11,8 +11,6 @@ module CaseStudy
     has_many :articles, dependent: :destroy
 
     has_one_attached :favicon
-    has_one_attached :logo
-    resize logo: {resize_to_limit: [400, 400]}
 
     validates :business_type, inclusion: {in: ::Company::VALID_BUSINESS_TYPES}, allow_blank: true
   end
