9702e1dd1eabcc296245e93147825ac5cd67add7
Thomas Cullen <thomascullen92@gmail.com>
2022-09-19 10:53:43 +0100

    Allow specialists to opt out of weekly digest

--- app/javascript/src/views/Settings/SpecialistSettings/Notifications/index.js
+++ app/javascript/src/views/Settings/SpecialistSettings/Notifications/index.js
@@ -4,11 +4,16 @@ import UnsubscribeFromAll from "../../components/UnsubscribeFromAll";
 
 export default function SpecialistNotificationSettings() {
   return (
-    <div className="bg-white p-8 rounded-md shadow-lg">
-      <h2 className="text-2xl font-semibold tracking-tight border-b border-solid border-neutral100 pb-4">
+    <div className="p-8 bg-white rounded-md shadow-lg">
+      <h2 className="pb-4 text-2xl font-semibold tracking-tight border-b border-solid border-neutral100">
         Notifications & Alerts
       </h2>
-      <div className="divide-y divide-neutral100 divide-solid">
+      <div className="divide-y divide-solid divide-neutral100">
+        <NotificationSubscription
+          title="Weekly digest"
+          subscription="Weekly Digest"
+          description="Get a weekly digest of new projects that match your interests"
+        />
         <NotificationSubscription
           title="Announcements & product updates"
           subscription="Announcements"
